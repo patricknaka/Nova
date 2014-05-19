baf49fa073976f84bd452c63d5913a9f8f7e1e90
marcusmsv <marcus_msv@yahoo.com.br>
2014-05-19 16:15:01 -0300

    Update VW_BAL_Balancete.sql

--- Views LN/VW_BAL_Balancete.sql
+++ Views LN/VW_BAL_Balancete.sql
@@ -1,6 +1,7 @@
--- #FAF.005, 14-mai-2014, Fabio Ferreira,	Retirado conversão de timezone do campo data de atualização
+-- #FAF.005, 14-mai-2014, Fabio Ferreira,	Retirado conversï¿½o de timezone do campo data de atualizaï¿½ï¿½o
 --							
 --**********************************************************************************************************************************************************
+--
 SELECT
 	tfgld205.t$cono || 
 	tfgld205.t$leac || 
