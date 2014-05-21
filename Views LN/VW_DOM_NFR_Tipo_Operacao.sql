d28ee6155266b82db94279ebe42de95cac34b528
fabioInfor <fabio.ferreira@infor.com>
2014-05-21 14:36:47 -0300

    Correção FAF.008
    Correção os registros do módulo NR estavam mostrando os registros de NF

--- Views LN/VW_DOM_NFR_Tipo_Operacao.sql
+++ Views LN/VW_DOM_NFR_Tipo_Operacao.sql
@@ -1,18 +1,22 @@
---	FAF.003 - 12-jan-2014, Fabio Ferreira, 	Icluido os tipos de operação de saida
+--	FAF.003 - 12-mai-2014, Fabio Ferreira, 	Icluido os tipos de operação de saida
+--	FAF.008 - 21-jun-2014, Fabio Ferreira, 	Correção os registros do módulo NR estavam mostrando os registros de NF
 --*******************************************************************************************************************************************
 SELECT d.t$cnst COD_CONTROLE,
        l.t$desc DESCR,
 	   'NR' MODULO													--#FAF.003.n
 FROM tttadv401000 d,
      tttadv140000 l
-WHERE d.t$cpac='ci'
-AND d.t$cdom='sli.tdff.l'
+--WHERE d.t$cpac='ci'												--#FAF.008.o
+WHERE d.t$cpac='td'													--#FAF.008.n
+--AND d.t$cdom='sli.tdff.l'											--#FAF.008.o
+AND d.t$cdom='rec.trfd.l'											--#FAF.008.n
 AND d.t$vers='B61U'
 AND d.t$rele='a7'
 AND d.t$cust='glo1'
 AND l.t$clab=d.t$za_clab
 AND l.t$clan='p'
-AND l.t$cpac='ci'
+--AND l.t$cpac='tf'													--#FAF.008.o
+AND l.t$cpac='td'													--#FAF.008.n
 AND l.t$vers='B61U'
 AND l.t$rele='a7'
 AND l.t$cust='glo1'
