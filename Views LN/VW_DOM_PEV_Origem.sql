f054b05fd8b4e4792d3e4c79b21644bffa8a90df
fabioInfor <fabio.ferreira@infor.com>
2014-05-22 15:33:25 -0300

    #75 Correção registro duplicado
    correção

--- Views LN/VW_DOM_PEV_Origem.sql
+++ Views LN/VW_DOM_PEV_Origem.sql
@@ -1,3 +1,5 @@
+-- #FAF.075 - 22-mai-2014, Fabio Ferreira, 	Adicionados os códigos do banco do site
+--**********************************************************************************************************************************************************
 SELECT d.t$cnst COD_CONTROLE,
        l.t$desc DESCR
 FROM tttadv401000 d,
@@ -10,6 +12,8 @@ AND d.t$cust='npt0'
 AND l.t$clab=d.t$za_clab
 AND l.t$clan='p'
 AND l.t$cpac='zn'
-AND l.t$vers='B61U'
-AND l.t$rele='a7'
-AND l.t$cust='npt0'
+AND l.t$vers=(select max(l1.t$vers) from tttadv140000 l1 where l1.t$clab=l.t$clab AND l1.t$clan=l.t$clan AND l1.t$cpac=l.t$cpac)	--#FAF.075.sn
+AND l.T$ZC_CONT=3																													--#FAF.075.en
+--AND l.t$vers='B61U'																												--#FAF.075.so
+--AND l.t$rele='a7'
+--AND l.t$cust='npt0'																												--#FAF.075.eo
