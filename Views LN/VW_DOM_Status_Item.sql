c6dd9143142a5f8ca66a9f76899f495526a4468b
marcusmsv <marcus_msv@yahoo.com.br>
2014-05-22 11:49:18 -0300

    Alteração Alias Atributo
    De DES_STATUS para DESC_STATUS

--- Views LN/VW_DOM_Status_Item.sql
+++ Views LN/VW_DOM_Status_Item.sql
@@ -1,3 +1,3 @@
-      Select 1 COD_STATUS_ITEM, 'A' NM_STATUS, 'Aberto' DES_STATUS From Dual 
-UNION Select 2 COD_STATUS_ITEM, 'L' NM_STATUS, 'Liquidado' DES_STATUS From Dual
-UNION Select 5 COD_STATUS_ITEM, 'C' NM_STATUS, 'Cancelado' DES_STATUS From Dual
+      Select 1 COD_STATUS_ITEM, 'A' NM_STATUS, 'Aberto' DESC_STATUS From Dual 
+UNION Select 2 COD_STATUS_ITEM, 'L' NM_STATUS, 'Liquidado' DESC_STATUS From Dual
+UNION Select 5 COD_STATUS_ITEM, 'C' NM_STATUS, 'Cancelado' DESC_STATUS From Dual
