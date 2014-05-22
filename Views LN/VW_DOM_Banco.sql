82ad70b3d2f0cd1e42202bdfe7f7d7c0e484582f
fabioInfor <fabio.ferreira@infor.com>
2014-05-22 15:09:24 -0300

    #74 Adicionados os códigos do banco do site
    .

--- Views LN/VW_DOM_Banco.sql
+++ Views LN/VW_DOM_Banco.sql
@@ -1,5 +1,14 @@
-select  distinct a.t$baoc$l Cod_Banco,
-        ( select b.t$bnam from ttfcmg011101 b
+-- #FAF.064 - 22-mai-2014, Fabio Ferreira, 	Adicionados os códigos do banco do site
+--**********************************************************************************************************************************************************
+Select distinct * from																--#FAF.064.n 
+(select a.t$baoc$l Cod_Banco,
+        ( select b.t$bnam from ttfcmg011201 b
           where b.t$baoc$l=a.t$baoc$l
           and rownum=1) Ds_Banco
 from ttfcmg011201 a
+UNION select TO_CHAR(c.t$idbc$c) Cod_Banco,											--#FAF.064.sn
+		(select d.t$desc$c from BAANDB.tzncmg020201 d
+		 where d.t$idbc$c=c.t$idbc$c
+		 and rownum=1) Ds_Banco
+from BAANDB.tzncmg020201 c)
+UNION Select '0' Cod_Banco,  'Não Associado' Ds_Banco from dual						--#FAF.064.en
