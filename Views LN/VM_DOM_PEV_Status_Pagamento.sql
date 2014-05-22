15c327a11a10cee823125b3a7543ae23da726ac3
fabioInfor <fabio.ferreira@infor.com>
2014-05-22 16:34:47 -0300

    #76 View criada
    View criada para status pagamento

---
+++ Views LN/VM_DOM_PEV_Status_Pagamento.sql
@@ -0,0 +1,13 @@
+SELECT d.t$cnst COD,
+       l.t$desc DESCR
+FROM tttadv401000 d,
+     tttadv140000 l
+WHERE d.t$cpac='zn'
+AND d.t$cdom='sls.stat.c'
+AND d.t$vers='B61U'
+AND d.t$rele='a7'
+AND d.t$cust='npt0'
+AND l.t$clab=d.t$za_clab
+AND l.t$clan='p'
+AND l.t$cpac='zn'
+AND l.t$vers=(select max(l1.t$vers) from tttadv140000 l1 where l1.t$clab=l.t$clab AND l1.t$clan=l.t$clan AND l1.t$cpac=l.t$cpac)
