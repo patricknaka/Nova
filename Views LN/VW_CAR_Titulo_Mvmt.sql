24058eefc6b33945431e505131c7e513a78e695b
fabioInfor <fabio.ferreira@infor.com>
2014-05-26 12:05:40 -0300

    #79 Alteração campo SITUACAO_MOVIMENTO
    SITUACAO_MOVIMENTO extraido da programação de recebimento

--- Views LN/VW_CAR_Titulo_Mvmt.sql
+++ Views LN/VW_CAR_Titulo_Mvmt.sql
@@ -2,7 +2,8 @@
 -- #FAF.001 - 08-mai-2014, Fabio Ferreira, 	Retirar tratamento de data/hora	
 -- #FAF.002 - 09-mai-2014, Fabio Ferreira, 	Correção titulo referencia	
 -- #FAF.005 - 14-mai-2014, Fabio Ferreira, 	Incluido campo módulo do título de referência (sempre 'CR')	
--- #FAF.007 - 17-mai-2014, Fabio Ferreira, 	Correção tpitulo de referência agrupado					
+-- #FAF.007 - 17-mai-2014, Fabio Ferreira, 	Correção tpitulo de referência agrupado	
+-- #FAF.079 - 26-mai-2014, Fabio Ferreira, 	Alterado o campo SITUACAO_MOVIMENTO para usar o valor da programação de pagamento						
 --****************************************************************************************************************************************************************
 SELECT
     201 COMPANHIA,
@@ -30,9 +31,25 @@ SELECT
 	CASE WHEN tfacr200.t$amnt<0 THEN '-' ELSE '+' END SINAL,
 	tfacr200.t$docd DATA_TRANSACAO,
 	tfacr200.t$amnt VALOR_TRANSACAO,
-	tfcmg409.t$stdd SITUACAO_MOVIMENTO,
-	CAST((FROM_TZ(CAST(TO_CHAR(tfcmg409.t$rcd_utc, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
-		AT time zone sessiontimezone) AS DATE) DATA_SITUACAO_MOVIMENTO,		
+--	tfcmg409.t$stdd SITUACAO_MOVIMENTO,																	--#FAF.079.o
+	(select p.t$rpst$l from ttfacr201201 p
+	 where p.t$ttyp=tfacr200.t$ttyp
+	 and p.t$ninv=tfacr200.t$ninv 
+	 and p.t$schn=tfacr200.t$schn) SITUACAO_MOVIMENTO,													--#FAF.079.n
+--	CAST((FROM_TZ(CAST(TO_CHAR(tfcmg409.t$rcd_utc, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
+--		AT time zone sessiontimezone) AS DATE) DATA_SITUACAO_MOVIMENTO,									--#FAF.079.o
+	CASE WHEN t.t$balc=t.t$bala															-- Liquidado	--#FAF.079.sn
+	THEN (select max(t$docd) from ttfacr200201 m
+	 where m.t$ttyp=tfacr200.t$ttyp
+	 and m.t$ninv=tfacr200.t$ninv 
+	 and m.t$schn=tfacr200.t$schn)
+	WHEN t.t$balc=t.t$amnt																-- Nenhum recebimento
+	THEN tfacr200.t$docd
+	ELSE (select min(t$docd) from ttfacr200201 m										-- Primeiro rec parcial 
+	 where m.t$ttyp=tfacr200.t$ttyp
+	 and m.t$ninv=tfacr200.t$ninv 
+	 and m.t$schn=tfacr200.t$schn)
+  END DATA_SITUACAO_MOVIMENTO, 																			--#FAF.079.en
 	nvl((select znacr005.t$ttyp$c || znacr005.t$ninv$c from BAANDB.tznacr005201 znacr005				--#FAF.002.sn
 		 where znacr005.t$tty1$c=tfacr200.t$ttyp and znacr005.t$nin1$c=tfacr200.t$ninv
 		 and znacr005.T$FLAG$C=1																		--#FAF.007.n
@@ -46,25 +63,28 @@ SELECT
 			AT time zone sessiontimezone) AS DATE) DATA_E_HORA_DE_ATUALIZACAO
 FROM
 	ttfacr200201 tfacr200
-	
 	LEFT JOIN (select distinct rs.t$ttyp, rs.t$ninv, rs.t$tdoc, rs.t$docn from ttfacr200201 rs) r			--#FAF.002.sn		
 	ON r.t$tdoc=tfacr200.t$tdoc 
 	and r.t$docn=tfacr200.t$docn
 	and r.t$ttyp!=tfacr200.t$ttyp
 	and r.t$ninv!=tfacr200.t$ninv																			--#FAF.002.en
 	
-	LEFT JOIN (select a.t$ttyp, a.t$ninv, a.t$brel FROM ttfacr201201 a									--#FAF.001.sn
+	LEFT JOIN (select a.t$ttyp, a.t$ninv, a.t$brel FROM ttfacr201201 a										--#FAF.001.sn
 	where a.t$brel!=' '
 	and a.t$schn=(select min(b.t$schn) from ttfacr201201 b
               where a.t$ttyp=b.t$ttyp
               and   a.t$ninv=b.t$ninv
-              and   b.t$brel!=' ')) q1 on q1.t$ttyp=tfacr200.t$ttyp and q1.t$ninv=tfacr200.t$ninv		--#FAF.001.en
-	
+              and   b.t$brel!=' ')) q1 on q1.t$ttyp=tfacr200.t$ttyp and q1.t$ninv=tfacr200.t$ninv			--#FAF.001.en
 	LEFT JOIN ttfcmg001201 tfcmg001
 	ON  tfcmg001.t$bank=q1.t$brel
 	LEFT JOIN ttfcmg011201 tfcmg011
 	ON  tfcmg011.t$bank=tfcmg001.t$brch
 	LEFT JOIN ttfcmg409201 tfcmg409
-	ON  tfcmg409.t$btno=tfacr200.t$btno
+	ON  tfcmg409.t$btno=tfacr200.t$btno,
+	ttfacr200201 t																							--#FAF.079.n
+	
 WHERE
-  tfacr200.t$docn!=0
+      tfacr200.t$docn!=0
+AND   t.t$ttyp=tfacr200.t$ttyp																				--#FAF.079.sn
+AND   t.t$ninv=tfacr200.t$ninv
+AND   t.t$docn=0																							--#FAF.079.en
