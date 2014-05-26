ab1260775fd7ca537a8cef16bc7205242e77a3f0
marcusmsv <marcus_msv@yahoo.com.br>
2014-05-26 18:50:54 -0300

    Alteração nome Atributo - VW_DOM_Transacao
    DE: DESCR_TRANSACAO
    PARA: DESC_TRANSACAO

--- Views LN/VW_DOM_Transacao.sql
+++ Views LN/VW_DOM_Transacao.sql
@@ -6,7 +6,7 @@ SELECT
   WHEN zngld001.t$tror$c=2 THEN 'CAR' ELSE null END COD_MODULO,               --#FAF.005.n
   tfgld011.t$catg COD_TIPO_TRANSACAO,        --#FAF.005.n
   tfgld011.t$ttyp COD_TRANSACAO,  
-  tfgld011.t$desc DESCR_TRANSACAO  
+  tfgld011.t$desc DESC_TRANSACAO  
 FROM ttfgld011201 tfgld011
 LEFT JOIN ( select distinct a.t$ttyp$c, a.t$tror$c from tzngld001201 a) zngld001      --#FAF.005.n
 ON zngld001.t$ttyp$c=tfgld011.t$ttyp                            --#FAF.005.n
