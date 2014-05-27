78b88cb4e715aaac73e3e7ba3993424f36ddf0f9
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-05-27 10:22:29 -0300

    Inclusão de Alias
    O campo que traz o CPNJ não tinha alias. Foi inserido o alias CNPJ_CPF

--- Views LN/VW_DOM_Companhia.sql
+++ Views LN/VW_DOM_Companhia.sql
@@ -1,11 +1,12 @@
 SELECT  tcemm170.t$comp COD_CIA,
         tcemm170.t$desc NOME_CIA,
         tccom000.t$arcc SITUACAO_CIA,
-        tccom130.t$fovn$l
+        tccom130.t$fovn$l CNPJ_CPF
 FROM    ttcemm170201 tcemm170,
         ttccom000201 tccom000,
         ttccom130201 tccom130
 WHERE   tccom000.t$ncmp = tcemm170.t$comp
 AND     tccom000.t$indt IN (SELECT Max(ttccom000201.t$indt) FROM ttccom000201)
 AND     tccom130.t$cadr = tccom000.t$cadr
+order by 1
 
