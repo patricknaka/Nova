cfa4ccd5647892f80462952c2dc7864611330088
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-05-27 15:40:56 -0300

    Substituição da view VW_DOM_SKU_Categoria
    o arquivo .sql que tínhamos não estava com as instruções corretas.
    Provavelmente era uma versão antiga. Foi substituido pela query
    existente no ETL

--- Views LN/VW_DOM_SKU_Categoria.sql
+++ Views LN/VW_DOM_SKU_Categoria.sql
@@ -1,18 +1,16 @@
-SELECT
-  CASE WHEN tdipu001.t$ixdn$c!=3 THEN 10
-  ELSE tcibd936.t$itmu$l
-  END COD_CATEGORIA,
-ltrim(rtrim(tcibd001.t$item)) COD_ITEM,
- CASE WHEN tdipu001.t$ixdn$c!=3 THEN 'Crossdocking'
-      WHEN tcibd936.t$itmu$l=1 THEN 'Industrialização'
-      WHEN tcibd936.t$itmu$l=2 THEN 'Revenda'
-      WHEN tcibd936.t$itmu$l=3 THEN 'Item Consumo'
-      WHEN tcibd936.t$itmu$l=4 THEN 'Ativo Fixo'
-      WHEN tcibd936.t$itmu$l=5 THEN 'Ind. por Terceiros'
-      WHEN tcibd936.t$itmu$l=6 THEN 'Item Retornavel'
-      WHEN tcibd936.t$itmu$l=7 THEN 'Frete'
-      ELSE 'Não Aplicável'
-  END DESCR_CATEGORIA
-FROM  ttcibd001201 tcibd001
-LEFT JOIN ttdipu001201 tdipu001 ON tdipu001.t$item=tcibd001.t$item
-LEFT JOIN ttcibd936201 tcibd936 ON tcibd936.t$ifgc$l=tcibd001.t$ifgc$l
+
+SELECT 201 COMPANHIA, 
+	   d.t$cnst COD_CATEGORIA,
+       l.t$desc DESC_CATEGORIA
+FROM tttadv401000 d,
+     tttadv140000 l
+WHERE d.t$cpac='zn'
+AND d.t$cdom='ipu.ixdn.c'
+AND d.t$vers='B61U'
+AND d.t$rele='a7'
+AND d.t$cust='npt0'
+AND l.t$clab=d.t$za_clab
+AND l.t$clan='p'
+AND l.t$cpac='zn'
+AND l.t$vers=(select max(l1.t$vers) from tttadv140000 l1 where l1.t$clab=l.t$clab AND l1.t$clan=l.t$clan AND l1.t$cpac=l.t$cpac)
+order by 1, 2
