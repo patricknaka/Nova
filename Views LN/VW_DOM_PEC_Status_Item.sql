8fc32f834247812c73ae68ea2c000e664615811c
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-05-27 14:41:33 -0300

    Alteração Alias e inclusão order by
    Status Item : de DESC_STATUS para DESC_STATUS_ITEM
    Tipo Cadastro: de DESCR para DESC_TIPO_CADASTRO e inclusão order by
    Tipo Frete : de COD para COD_TIPO_FRETE e de DESCR para DESC_TIPO_FRETE.
    Inclusão do order by

--- Views LN/VW_DOM_PEC_Status_Item.sql
+++ Views LN/VW_DOM_PEC_Status_Item.sql
@@ -1,3 +1,3 @@
-      Select 1 COD_STATUS_ITEM, 'A' NM_STATUS, 'Aberto' DESC_STATUS From Dual 
-UNION Select 2 COD_STATUS_ITEM, 'L' NM_STATUS, 'Liquidado' DESC_STATUS From Dual
-UNION Select 5 COD_STATUS_ITEM, 'C' NM_STATUS, 'Cancelado' DESC_STATUS From Dual
+      Select 1 COD_STATUS_ITEM, 'A' NM_STATUS, 'Aberto' DESC_STATUS_ITEM From Dual 
+UNION Select 2 COD_STATUS_ITEM, 'L' NM_STATUS, 'Liquidado' DESC_STATUS_ITEM From Dual
+UNION Select 5 COD_STATUS_ITEM, 'C' NM_STATUS, 'Cancelado' DESC_STATUS_ITEM From Dual
--- Views LN/VW_DOM_PEC_Tipo_Frete.sql
+++ Views LN/VW_DOM_PEC_Tipo_Frete.sql
@@ -1,1 +1,4 @@
-select  a.t$cdec COD, a.t$dsca DESCR from ttcmcs041201 a
+select  a.t$cdec COD_TIPO_FRETE, 
+	a.t$dsca DESC_TIPO_FRETE 
+from ttcmcs041201 a
+order by 1
