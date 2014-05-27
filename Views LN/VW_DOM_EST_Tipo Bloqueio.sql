87a4f1a9e8f1ce6c2249988f1d30a49c6850c040
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-05-27 11:03:10 -0300

    Inclusão Order By e alteração alias
    Modalidade : Inclusão Order by
    Tipo Bloqueio : Alteração alias e inclusão do order by

--- Views LN/VW_DOM_EST_Tipo Bloqueio.sql
+++ Views LN/VW_DOM_EST_Tipo Bloqueio.sql
@@ -1,8 +1,9 @@
-select  a.t$cdis CODE,
-        a.t$dsca DESCR
+select  a.t$cdis COD_TIPO_BLOQUEIO,
+        a.t$dsca DESC_TIPO_BLOQUEIO
 from    ttcmcs005201 a
 WHERE   a.t$rstp=65
 UNION
 SELECT  'WN' CDOE,
         'Normal' DESCR
 FROM Dual
+order by 1
