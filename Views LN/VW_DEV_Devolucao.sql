ffc74fe4521f96567be20d82cf77fe2c72209885
marcusmsv <marcus_msv@yahoo.com.br>
2014-05-28 17:04:16 -0300

    Padronizacao Atributo - VW_DEV_Devolucao

--- Views LN/VW_DEV_Devolucao.sql
+++ Views LN/VW_DEV_Devolucao.sql
@@ -1,24 +1,24 @@
 SELECT
 	GREATEST(tdrec940.t$date$l,tdrec940.t$idat$l,tdrec940.t$odat$l) ULTIMA_ATUALIZ_NF,
-    201 COD_CIA,
+    201 CD_CIA,
 	(SELECT tcemm030.t$euca FROM ttcemm124201 tcemm124, ttcemm030201 tcemm030
 	WHERE tcemm124.t$cwoc=tdrec940.t$cofc$l
 	AND tcemm030.t$eunt=tcemm124.t$grid
 	AND tcemm124.t$loco=201
-	AND rownum=1) COD_FILIAL, 
+	AND rownum=1) CD_FILIAL, 
 	(SELECT tcemm124.t$grid FROM ttcemm124201 tcemm124
 	WHERE tcemm124.t$cwoc=tdrec940.t$cofc$l
 	AND tcemm124.t$loco=201
-	AND rownum=1)	UNID_EMPRESARIAL,
-	tdrec940.t$docn$l NUM_NOTA_FISCAL,
-	tdrec940.t$seri$l SER_NOTA_FISCAL,
-	tdrec940.t$opfc$l COD_NATUREZA_OPERACAO,
-	tdrec940.t$opor$l SEQU_NATUREZA_OPERACAO,
-	tdrec940.t$date$l DATA_FATURA,
+	AND rownum=1)	CD_UNIDADE_EMPRESARIAL,
+	tdrec940.t$docn$l NR_NF,
+	tdrec940.t$seri$l NR_SERIE_NF,
+	tdrec940.t$opfc$l CD_NATUREZA_OPERACAO,
+	tdrec940.t$opor$l SQ_NATUREZA_OPERACAO,
+	tdrec940.t$date$l DT_FATURA,
 	cisli940.t$itbp$l COD_CLIENTE_FATURA,
 	cisli940.t$stbp$l COD_CLIENTE_ENTREGA,
-	znsls401.t$sequ$c SEQ_ENTREGA,
-	znsls401.t$pecl$c NUM_PEDIDO,
+	znsls401.t$sequ$c SQ_ENTREGA,
+	znsls401.t$pecl$c NR_PEDIDO,
 	(select znsls410.t$poco$c
 	FROM tznsls410201 znsls410
 	WHERE znsls410.t$ncia$c=znsls401.t$ncia$c
@@ -33,53 +33,52 @@ SELECT
                           AND c.t$pecl$c=znsls410.t$pecl$c
                           AND c.t$sqpd$c=znsls410.t$sqpd$c
                           AND c.t$entr$c=znsls410.t$entr$c)                          
-	AND rownum=1) STATUS_DEVOLUCAO,
+	AND rownum=1) CD_STATUS,
 	(SELECT MAX(znsls410.t$dtoc$c)
 	FROM tznsls410201 znsls410
 	WHERE znsls410.t$ncia$c=znsls401.t$ncia$c
 	AND znsls410.t$uneg$c=znsls401.t$uneg$c
 	AND znsls410.t$pecl$c=znsls401.t$pecl$c
 	AND znsls410.t$sqpd$c=znsls401.t$sqpd$c
-	AND znsls410.t$entr$c=znsls401.t$entr$c) DT_HR_STATUS,
-	tdrec940.t$rfdt$l COD_TIPO_NOTA,
-	tdrec947.t$rcno$l NUM_NOTA_RECEB_DEV,
-	ltrim(rtrim(tdrec941.t$item$l)) COD_ITEM,
-	tdrec941.t$qnty$l QUANTIDADE,
+	AND znsls410.t$entr$c=znsls401.t$entr$c) DT_STATUS,
+	tdrec940.t$rfdt$l CD_TIPO_NF,
+	tdrec947.t$rcno$l NR_NFR_DEVOLUCAO,
+	ltrim(rtrim(tdrec941.t$item$l)) CD_ITEM,
+	tdrec941.t$qnty$l QT_DEVOLUCAO,
 	(SELECT tdrec942.t$amnt$l FROM ttdrec942201 tdrec942
 	WHERE tdrec942.t$fire$l=tdrec941.t$fire$l
 	AND tdrec942.t$line$l=tdrec941.t$line$l
-	AND tdrec942.t$brty$l=1) VALOR_ICMS,
-	tdrec941.t$gamt$l VALOR_PRODUTO,
-	tdrec941.t$fght$l VALOR_FRETE,
-	tdrec941.t$gexp$l VALOR_DESPESA,
-	tdrec941.t$addc$l VALOR_DESC_INCONDICIONAL,
-	-- VALOR_DE DESCONTO CONDICIONAL								-- *** DESCONSIDERAR ***
-	tdrec941.t$tamt$l VALOR_TOTAL_ITEM,
-	sls401orig.t$orno$c NUM_PEDIDO_ORIGINAL,
-	znsls400.t$dtin$c DATA_PEDIDO,
-	znsls400.t$idca$c COD_CANAL_VENDAS,
-	tccom130.t$ftyp$l COD_TIPO_CLIENTE,
-	tccom130.t$ccit COD_CIDADE,
-	tccom130.t$ccty COD_PAIS,
-	tccom130.t$cste COD_ESTADO,
-	q1.mauc VALOR_CMV,												
-	cisli940.t$docn$l NUM_NOTA_FISCAL_FATURA,
-	cisli940.t$seri$l SERIA_NOTA_FISCAL_FATURA,
+	AND tdrec942.t$brty$l=1) VL_ICMS,
+	tdrec941.t$gamt$l VL_PRODUTO,
+	tdrec941.t$fght$l VL_FRETE,
+	tdrec941.t$gexp$l VL_DESPESA,
+	tdrec941.t$addc$l VL_DESCONTO_INCONDICIONAL,
+	tdrec941.t$tamt$l VL_TOTAL_ITEM,
+	sls401orig.t$orno$c NR_PEDIDO_ORIGINAL,
+	znsls400.t$dtin$c DT_PEDIDO,
+	znsls400.t$idca$c CD_CANAL_VENDAS,
+	tccom130.t$ftyp$l CD_TIPO_CLIENTE,
+	tccom130.t$ccit CD_CIDADE,
+	tccom130.t$ccty CD_PAIS,
+	tccom130.t$cste CD_ESTADO,
+	q1.mauc VL_CMV,												
+	cisli940.t$docn$l NR_NF_FATURA,
+	cisli940.t$seri$l NR_SERIE_NF_FATURA,
 	  (select distinct a.t$docn$l from tcisli940201 a, tcisli941201 b
 	  where b.t$fire$l=tdrec940.t$fire$l
-	  and a.t$fire$l=b.t$refr$l) NUM_NOTA_FISCAL_REMESSA,									
+	  and a.t$fire$l=b.t$refr$l) NR_NF_REMESSA,									
 	  (select distinct a.t$seri$l from tcisli940201 a, tcisli941201 b
 	  where b.t$fire$l=tdrec940.t$fire$l
-	  and a.t$fire$l=b.t$refr$l) SER_NOTA_FISCAL_REMESSA,		
+	  and a.t$fire$l=b.t$refr$l) NR_SERIE_NF_REMESSA,		
 	(SELECT tdrec942.t$amnt$l FROM ttdrec942201 tdrec942
 	WHERE tdrec942.t$fire$l=tdrec941.t$fire$l
 	AND tdrec942.t$line$l=tdrec941.t$line$l
-	AND tdrec942.t$brty$l=5) VALOR_PIS,
+	AND tdrec942.t$brty$l=5) VL_PIS,
 	(SELECT tdrec942.t$amnt$l FROM ttdrec942201 tdrec942
 	WHERE tdrec942.t$fire$l=tdrec941.t$fire$l
 	AND tdrec942.t$line$l=tdrec941.t$line$l
-	AND tdrec942.t$brty$l=6) VALOR_COFINS,
-	znsls401.t$uneg$c COD_UNIDADE_NEGOCIO,
+	AND tdrec942.t$brty$l=6) VL_COFINS,
+	znsls401.t$uneg$c CD_UNIDADE_NEGOCIO,
 	CASE WHEN 
 	nvl((select	znsls401nr.t$pecl$c
 	FROM	tznsls401201 znsls401nr
@@ -89,7 +88,7 @@ SELECT
 	AND		znsls401nr.t$sqpd$c=znsls401.t$sqpd$c
 	AND		znsls401nr.t$entr$c>znsls401.t$entr$c),1)=1 then 1
 	ELSE 2
-	END FLAG_REPOSICAO
+	END IN_REPOSICAO
 FROM
 	ttdrec940201 tdrec940,
 	ttdrec941201 tdrec941
