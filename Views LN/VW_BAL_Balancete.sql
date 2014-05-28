aa089e041f0f48d561a0cff7e78ab5c9c9ae0683
marcusmsv <marcus_msv@yahoo.com.br>
2014-05-28 15:28:00 -0300

    Padronização Atributos - VW_BAL_Balancete

--- Views LN/VW_BAL_Balancete.sql
+++ Views LN/VW_BAL_Balancete.sql
@@ -16,15 +16,16 @@ SELECT
 	tfgld205.t$dim3 ||
 	tfgld205.t$dim4 ||
 	tfgld205.t$dim5  CHAVE,
-	tfgld205.t$ptyp COD_BALANCETE_GERENCIAL,
-	tfgld205.t$dim2 COD_FILIAL,
+	tfgld205.t$ptyp CD_BALANCETE_GERENCIAL,
+	tfgld205.t$dim2 CD_FILIAL,
 	(Select u.t$eunt From ttcemm030201 u where u.t$euca!=' '
 		AND TO_NUMBER(u.t$euca)=CASE WHEN tfgld205.t$dim2=' ' then 999
-		WHEN tfgld205.t$dim2<=to_char(0) then 999 else TO_NUMBER(tfgld205.t$dim2) END and rownum = 1) UNID_EMPRESARIAL,	
-	tfgld205.t$cono COD_CIA,
-	tfgld205.t$year, tfgld205.t$prno DATA_BALANCETE,
-	tfgld205.t$leac COD_CONTA,
-	tfgld205.t$dim1 COD_CENTRO_CUSTO,
+		WHEN tfgld205.t$dim2<=to_char(0) then 999 else TO_NUMBER(tfgld205.t$dim2) END and rownum = 1) CD_UNIDADE_EMPRESARIA,	
+	tfgld205.t$cono CD_CIA,
+	tfgld205.t$year NR_ANO, 
+	tfgld205.t$prno DT_BALANCETE,
+	tfgld205.t$leac CD_CONTA_CONTABIL,
+	tfgld205.t$dim1 CD_CENTRO_CUSTO,
 	nvl(tfgld212.t$ftob,0)+nvl((SELECT (SUM(s.t$fcam)-SUM(s.t$fdam))
 					FROM ttfgld205201 s
 					WHERE s.t$cono=tfgld205.t$cono
@@ -40,9 +41,9 @@ SELECT
 					AND   s.t$leac=tfgld205.t$leac
 					AND   s.t$ccur=tfgld205.t$ccur
 					AND   s.t$duac=tfgld205.t$duac
-					AND   s.t$bpid=tfgld205.t$bpid),0)	VALOR_SALDO_ANTERIOR,
-	tfgld205.t$fdam VALOR_DEBITOS,
-	tfgld205.t$fcam VALOR_CREDITOS,
+					AND   s.t$bpid=tfgld205.t$bpid),0)	VL_SALDO_ANTERIOR,
+	tfgld205.t$fdam VL_DEBITO,
+	tfgld205.t$fcam VL_CREDITO,
 	(SELECT 
 --	CAST((FROM_TZ(CAST(TO_CHAR(MAX(tfgld100.t$trdt), 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 		--#FAF.005.o
 --			AT time zone sessiontimezone) AS DATE)															--#FAF.005.o
@@ -50,7 +51,7 @@ SELECT
 	FROM ttfgld100201 tfgld100
 	WHERE tfgld100.t$year=tfgld205.t$year
 	AND   tfgld100.t$fprd=tfgld205.t$prno
-	AND   tfgld100.t$stat=6) DATA_E_HORA_ATUALIZACAO
+	AND   tfgld100.t$stat=6) DT_ATUALIZACAO
 FROM 
 	ttfgld205201 tfgld205
 	LEFT JOIN ttfgld212201 tfgld212
