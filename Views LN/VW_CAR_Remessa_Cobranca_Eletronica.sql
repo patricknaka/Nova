8a49df0e75d405e598a89ea8caea0e2278853711
marcusmsv <marcus_msv@yahoo.com.br>
2014-05-28 16:36:21 -0300

    Padronização Atributo
    VW_CAR_Remessa_Cobranca_Eletronica

--- Views LN/VW_CAR_Remessa_Cobranca_Eletronica.sql
+++ Views LN/VW_CAR_Remessa_Cobranca_Eletronica.sql
@@ -1,11 +1,11 @@
 SELECT
-	tfcmg948.t$bank$l COD_BANCO,
-	tfcmg948.t$btno$l NUM_REMESSA,
+	tfcmg948.t$bank$l CD_BANCO,
+	tfcmg948.t$btno$l NR_REMESSA,
 	tfcmg948.t$occ2$l COD_COMANDO,
-	CONCAT(tfcmg948.t$ttyp$l, TO_CHAR(tfcmg948.t$ninv$l)) NUM_TITULO,
-	'ACR' COD_MODALIDADE,
+	CONCAT(tfcmg948.t$ttyp$l, TO_CHAR(tfcmg948.t$ninv$l)) NR_TITULO,
+	'ACR' CD_MODALIDADE,
 	tfcmg948.t$stat$l SITUACAO,
 	CAST((FROM_TZ(CAST(TO_CHAR(tfcmg948.t$lach$l, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
-			AT time zone sessiontimezone) AS DATE)  DATA_E_HORA_ATUALIZACAO
+			AT time zone sessiontimezone) AS DATE)  DT_ATUALIZACAO
 FROM
 	ttfcmg948201 tfcmg948
