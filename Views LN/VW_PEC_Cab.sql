723b50c80d9624bb145b90ba4ab308531d7c3b5d
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-05-29 11:16:51 -0300

    Padronização Atributo - VW_PEC_CAB
    Padronização dos atributos entre a view e a tabela

--- Views LN/VW_PEC_Cab.sql
+++ Views LN/VW_PEC_Cab.sql
@@ -3,66 +3,66 @@
 -- #FAF.005, 14-mai-2014, Fabio Ferreira, 	Alteração alias
 --*********************************************************************************************************************************************
 SELECT DISTINCT
-    201 COMPANHIA,
-    tcemm030.t$euca FILIAL,      
-	tcemm124.t$grid UNID_EMPRESARIAL,
-    tdpur400.t$orno NUM_PEDIDO_COMPRA,
-    tdpur400.t$otbp COD_FORNECEDOR,
-    qopfc.t$opfc$l COD_NATUREZA_OPERACAO,
-    ' ' SEQUENCIA_NATUREZA_OPERAÇÃO,                  -- *** NÃO TEMOS ESTA INFORMAÇÃO NA ORDEM DE COMPRA ***
-    tdpur400.t$cpay COD_CONDICAO_PAGAMENTO,
+    201 CD_CIA,
+    tcemm030.t$euca CD_FILIAL,      
+    tdpur400.t$orno NR_PEDIDO_COMPRA,
+    tdpur400.t$otbp CD_FORNECEDOR,
+    qopfc.t$opfc$l CD_NATUREZA_OPERACAO,
+    ' ' SQ_NATUREZA_OPERACAO,                  -- *** NÃO TEMOS ESTA INFORMAÇÃO NA ORDEM DE COMPRA ***
+    tdpur400.t$cpay CD_CONDICAO_PAGAMENTO,
     (select sum((pl1.t$qoor-pl1.t$qidl)*pl1.t$pric) 
     from ttdpur401201 pl1 
     where pl1.t$orno=tdpur400.t$orno
-    and pl1.t$oltp!=2) VALOR_SALDO_PEDIDO_COMPRA,
-    tdpur400.t$cdec TIPO_FRETE,
+    and pl1.t$oltp!=2) VL_SALDO_PEC,
+    tdpur400.t$cdec CD_TIPO_FRETE,
     CASE WHEN  tdpur400.t$hdst=20 THEN 'PARCIAL'
 	WHEN tdpur400.t$hdst=25 THEN 'FECHADO'
-	ELSE 'NÃO APLICAVEL' END COD_TIPO_ATENDIMENTO,
+	ELSE 'NÃO APLICAVEL' END CD_TIPO_ATENDIMENTO,
     CASE WHEN tdpur400.t$cotp='003' THEN 1
     ELSE 2
-    END FLAGCONSUMO,
+    END IN_CONSUMO,
     tdpur400.t$odat DT_EMISSAO_PEDIDO,
-    apr.t$logn USUARIO_APROVOU_PEDIDO,
+    apr.t$logn DS_USUARIO_APROVACAO_PEDIDO,
     CASE WHEN tdpur400.t$hdst>=10 THEN 1
     ELSE 2
-    END FLAG_PEDIDO_APROVADO,
+    END IN_APROVADO,
     apr.dapr DT_APROVACAO_PEDIDO,
-    tdpur400.t$hdst SITUACAO_PEDIDO,
+    tdpur400.t$hdst CD_SITUACAO_PEDIDO,
     (select min(tdpur450.t$trdt) from ttdpur450201 tdpur450
     where tdpur450.t$orno=tdpur400.t$orno
-    and tdpur450.t$hdst=tdpur400.t$hdst) DATA_SITUACAO_PEDIDO,
+    and tdpur450.t$hdst=tdpur400.t$hdst) DT_SITUACAO_PEDIDO,
     (select tdpur450.t$logn from ttdpur450201 tdpur450
     where tdpur450.t$orno=tdpur400.t$orno
-    and rownum=1) USUARIO_CRIOU_PEDIDO,
+    and rownum=1) DS_USUARIO_GERACAO_PEDIDO,
     (select max(tdpur401.t$rcd_utc) from ttdpur401201 tdpur401
-	where tdpur401.t$orno=tdpur400.t$orno) DT_HR_ATUALIZACAO, 
+	where tdpur401.t$orno=tdpur400.t$orno) DT_ATUALIZACAO, 
     nvl((select t.t$text from ttttxt010201 t 
 	where t$clan='p'
 	AND t.t$ctxt=tdpur400.t$txta
-	and rownum=1),' ') OBS_PEDIDO,
+	and rownum=1),' ') DS_OBSERVACAO_PEDIDO,
     (select znpur003.t$citg$c from tznpur003201 znpur003
     where znpur003.t$cotp$c=tdpur400.t$cotp
-    and rownum=1) COD_DEPARTAMENTO,
+    and rownum=1) CD_DEPARTAMENTO,
 --    tdpur400.t$corg COD_TIPO_GERACAO_PEDIDO,											--#FAF.005.o
-	tdpur400.t$corg COD_TIPO_CADASTRO,													--#FAF.005.n
+	tdpur400.t$corg CD_TIPO_CADASTRO,													--#FAF.005.n
     (select z.t$vlft$c from tznfmd630201 z
-	where z.t$orno$c=tdpur400.t$orno) VALOR_FRETE,
+	where z.t$orno$c=tdpur400.t$orno) VL_FRETE,
     (select sum(brmcs941.t$tamt$l) 
     from tbrmcs941201 brmcs941,
     ttdpur401201 tdpur401
     where brmcs941.t$txre$l=tdpur401.t$txre$l
     and brmcs941.t$line$l=tdpur401.t$txli$l
     and tdpur401.t$orno=tdpur400.t$orno
-    and tdpur401.t$oltp!=2) VALOR_FINANCEIRO,
+    and tdpur401.t$oltp!=2) VL_FINANCEIRO,
     (select z.t$vlsg$c from tznfmd630201 z
-	where z.t$orno$c=tdpur400.t$orno) VALOR_SEGURO, 
-    tdpur400.t$sbim FLAG_LIQUIDACAO_AUTOMATICA,
-    tdpur400.T$COTP TIPO_ORD_COMPRA,
-	tdpur400.t$cvpc$c VPC,
-	tdpur400.t$rfdt$l COD_TIPO_NOTA,
-	tdpur400.t$fdtc$l COD_TIPO_DOC_FISCAL,
-	tdpur400.t$oamt VALOR_TOTAL_MERCADOR
+	where z.t$orno$c=tdpur400.t$orno) VL_SEGURO, 
+    tdpur400.t$sbim IN_LIQUIDACAO_AUTOMATICA,
+    tcemm124.t$grid CD_UNIDADE_EMPRESARIAL,
+    tdpur400.T$COTP CD_TIPO_ORDEM_COMPRA,
+	tdpur400.t$cvpc$c CD_CONTRATO_VPC,
+	tdpur400.t$rfdt$l CD_TIPO_NF,
+	tdpur400.t$fdtc$l CD_TIPO_DOCUMENTO_FISCAL,
+	tdpur400.t$oamt VL_TOTAL_MERCADORIA
 FROM
     ttdpur400201 tdpur400
     LEFT JOIN (select b.t$orno, min(b.t$trdt) dapr, b.t$logn from ttdpur450201 b
