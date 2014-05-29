49704d687a365b0d5d535d8c11a8f2a03a9d6fda
marcusmsv <marcus_msv@yahoo.com.br>
2014-05-29 09:11:19 -0300

    Padronização Atributo - VW_FAT_Cotacao

--- Views LN/VW_FAT_Cotacao.sql
+++ Views LN/VW_FAT_Cotacao.sql
@@ -2,15 +2,15 @@
 --								Criação do campo Efetiva
 --****************************************************************************************************************************************************************
 SELECT  tcmcs008.t$rtyp COD_COTACAO,
-        tcmcs008.t$ccur COD_MOEDA,
-        tcmcs008.t$rate VALOR_COTACAO,
+        tcmcs008.t$ccur CD_MOEDA,
+        tcmcs008.t$rate VL_COTACAO,
         CAST((FROM_TZ(CAST(TO_CHAR(tcmcs008.t$stdt, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
-          AT time zone sessiontimezone) AS DATE) DATA_COTACAO,
+          AT time zone sessiontimezone) AS DATE) DT_COTACAO,
         CAST((FROM_TZ(CAST(TO_CHAR(tcmcs008.t$apdt, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
-          AT time zone sessiontimezone) AS DATE) DATA_ATUALIZACAO,
+          AT time zone sessiontimezone) AS DATE) DT_ATUALIZACAO,
         CASE WHEN (select MAX(b.t$stdt) From ttcmcs008201 b
                   where b.t$rtyp=tcmcs008.t$rtyp
-                  and   b.t$ccur=tcmcs008.t$ccur)=tcmcs008.t$stdt THEN 1 ELSE 2 END EFETIVA
+                  and   b.t$ccur=tcmcs008.t$ccur)=tcmcs008.t$stdt THEN 1 ELSE 2 END IN_EFETIVA
 FROM    ttcmcs008201 tcmcs008
 WHERE	tcmcs008.t$bcur='BRL'
 AND		tcmcs008.t$rapr=1
