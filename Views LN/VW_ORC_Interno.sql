e591c949994dfae9c80ef5c98dfd5eb4878ae0fe
marcusmsv <marcus_msv@yahoo.com.br>
2014-05-29 13:39:39 -0300

    Padronização Atributo - VW_ORC_Interno

--- Views LN/VW_ORC_Interno.sql
+++ Views LN/VW_ORC_Interno.sql
@@ -1,11 +1,11 @@
-SELECT  201 COMPANHIA,
-        tdsta200.t$csor COD_UNID_NEGOCIO,
-        SubStr(tdsta200.t$ATVS, 7, 3) COD_CANAL_VENDA,
-        SubStr(tdsta200.t$ATVS, 1, 6) COD_DEPARTAMENTO,
-        tcccp070.t$stdt DATA_ORÇAMENTO,
-        tdsta200.t$sbam$1 VALOR_ORCAMENTO,
-        1 TIPO_ORCAMENTO,
-        SYSDATE DATA_ATUALIZACAO --*** ATIVAR DATA DE ATUALIZAÇÃO ****
+SELECT  201 CD_CIA,
+        tdsta200.t$csor CD_UNIDADE_NEGOCIO,
+        SubStr(tdsta200.t$ATVS, 7, 3) CD_CANAL_VENDAS,
+        SubStr(tdsta200.t$ATVS, 1, 6) CD_DEPARTAMENTO,
+        tcccp070.t$stdt DT_ORCAMENTO,
+        tdsta200.t$sbam$1 VL_ORCAMENTO,
+        1 CD_TIPO_ORCAMENTO,
+        SYSDATE DT_ATUALIZACAO --*** ATIVAR DATA DE ATUALIZAÇÃO ****
 FROM  ttdsta200201 tdsta200,
       ttcccp070201 tcccp070
 WHERE tcccp070.t$yrno = tdsta200.t$yrno
