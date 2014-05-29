53df555678548542a55d675a481c2e4afc5c0da5
marcusmsv <marcus_msv@yahoo.com.br>
2014-05-29 13:59:52 -0300

    Padronização Atributo - VW_TRK_Status_Pedido

--- Views LN/VW_TRK_Status_Pedido.sql
+++ Views LN/VW_TRK_Status_Pedido.sql
@@ -1,19 +1,18 @@
 -- #FAF.050, 23-mai-2014, Fabio Ferreira,	NUM_ENTREGA_PEDIDO convertido para string
 --************************************************************************************************************************************************
 SELECT
-    201 COMPANHIA,
-    tcemm030.t$euca COD_FILIAL,
-    tcemm112.t$grid UNID_EMPRESARIAL,
---  znsls410.t$entr$c NUM_ENTREGA_PEDIDO,							--#FAF.050.o
-	TO_CHAR(znsls410.t$entr$c) NUM_ENTREGA_PEDIDO,					--#FAF.050.n
-    znsls410.t$pecl$c NUM_PEDIDO,
-    znsls410.t$poco$c COD_STATUS,
-    znsls410.t$stor$c COD_SISTEMA_FONTE,	
+    201 CD_CIA,
+    tcemm030.t$euca CD_FILIAL,
+    tcemm112.t$grid CD_UNIDADE_EMPRESARIAL,
+	TO_CHAR(znsls410.t$entr$c) NR_ENTREGA_PEDIDO,					--#FAF.050.n
+    znsls410.t$pecl$c NR_PEDIDO,
+    znsls410.t$poco$c CD_STATUS,
+    znsls410.t$stor$c CD_SISTEMA_FONTE,	
 	CAST((FROM_TZ(CAST(TO_CHAR(znsls410.t$dtoc$c, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
-			AT time zone sessiontimezone) AS DATE) DT_ATUALIZACAOO_STATUS,
-    znsls410.t$nmre$c USUARIO_ALTERACAO_STATUS,
-    znsls410.T$UNEG$C UNID_NEGOCIO,
-    znsls410.t$orno$c ORD_VENDA
+			AT time zone sessiontimezone) AS DATE) DT_ATUALIZACAO_STATUS,
+    znsls410.t$nmre$c NM_USUARIO_MODIFICACAO_STATUS,
+    znsls410.T$UNEG$C CD_UNIDADE_NEGOCIO,
+    znsls410.t$orno$c NR_ORDEM_VENDA
 FROM
     tznsls410201 znsls410,
 	ttdsls400201 tdsls400,
