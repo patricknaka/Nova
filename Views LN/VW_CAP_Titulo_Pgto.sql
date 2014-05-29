d28b4e0d44544ef829fada2f631eb49fa633d34c
marcusmsv <marcus_msv@yahoo.com.br>
2014-05-29 14:26:32 -0300

    Padronização Atributo - VW_CAP_Titulo_Pgto

--- Views LN/VW_CAP_Titulo_Pgto.sql
+++ Views LN/VW_CAP_Titulo_Pgto.sql
@@ -1,7 +1,3 @@
--- FAF.002 - 09-mai-2014, Fabio Ferreira, Correção do relacinamento para o banco destino
--- FAF.003 - 12-mai-2014, Fabio Ferreira, Inclusão do campo COD_PARCEIRO
--- FAF.005 - 14-mai-2014, Fabio Ferreira, Retirada conversão de timezone do campo DT_HR_DE_ATUALIZACAO
---****************************************************************************************************************************************************************
 SELECT
 	tfacp200.t$ttyp || tfacp200.t$ninv CD_CHAVE_PRIMARIA,
 	tfacp200.t$ninv NR_TITULO,
@@ -17,45 +13,55 @@ SELECT
 	CASE WHEN tflcb230.t$revs$d=1 then TO_CHAR(tflcb230.t$lach$d)
 	ELSE ' '
 	END DT_ESTORNO,
-	tflcb230.t$send$d CD_SITUACAO_PAGAMENTO_ELETRONICO,
+	tflcb230.t$send$d CD_SITUACAO_PAGTO_ELETRONICO,
 	nvl(tfcmg109.t$stpp,0) CD_SITUACAO_PAGAMENTO,
 	tfacp600.t$ddat DT_ATUALIZACAO,
 	CASE WHEN tfacp200.t$balc=0 then
 	(select max(d.t$docd) from ttfacp200201 d
 	where d.t$ttyp=tfacp200.t$ttyp
 	and d.t$ninv=tfacp200.t$ninv) 
-	ELSE to_date('1970-01-01', 'YYYY-MM-DD')
-	END DT_LIQUIDACAO_TITULO,
-	tfacp600.t$payt CD_TIPO_TRANSACAO,
-	tfcmg011f.t$baoc$l CD_BANCO_DESTINO,
-	tfcmg011f.t$agcd$l NR_AGENCIA_DESTINO,
-	tccom125.t$bano NR_CONTA_CORRENTE_DESTINO,
-	tfacp200.t$paym CD_METODO_PAGAMENTO,
-	tfacp600.t$ptbp CD_PARCEIRO																	--#FAF.003.n	
+  ELSE to_date('1970-01-01', 'YYYY-MM-DD')
+  END DT_LIQUIDACAO_TITULO,
+  tfacp600.t$payt CD_TIPO_TRANSACAO,
+  tfcmg011f.t$baoc$l CD_BANCO_DESTINO,
+  tfcmg011f.t$agcd$l NR_AGENCIA_DESTINO,
+  tccom125.t$bano NR_CONTA_CORRENTE_DESTINO,
+  tfacp200.t$paym CD_METODO_PAGAMENTO,
+  tfacp600.t$ptbp CD_PARCEIRO                        
+
+          --#FAF.003.n  
 FROM
-	ttfacp600201 tfacp600
+  ttfacp600201 tfacp600
   LEFT JOIN ttfcmg109201 tfcmg109
   ON tfcmg109.t$btno=tfacp600.t$pbtn
   LEFT JOIN ttfcmg103201 tfcmg103
   ON tfcmg103.T$BTNO=tfcmg109.T$BTNO
-	LEFT JOIN ttfcmg001201 tfcmg001
-	ON tfcmg001.t$bank=tfacp600.t$bank
---	LEFT JOIN ttfcmg001201 tfcmg001f										--#FAF.002.o
---	ON tfcmg001f.t$bank=tfacp600.t$basu										--#FAF.002.o
-	LEFT JOIN ttccom125201 tccom125											--#FAF.002.sn										
-	ON tccom125.t$cban=tfacp600.t$basu										
-	AND tccom125.t$ptbp=tfacp600.t$ptbp										--#FAF.002.en
-	LEFT JOIN ttfcmg011201 tfcmg011
-	ON tfcmg011.t$bank=tfcmg001.t$brch
-	LEFT JOIN ttfcmg011201 tfcmg011f
---	ON tfcmg011f.t$bank=tfcmg001f.t$brch,									--#FAF.002.o
-	ON tfcmg011f.t$bank=tccom125.t$brch,									--#FAF.002.n
-	ttfacp200201 tfacp200
-	LEFT JOIN ttflcb230201 tflcb230 
-	ON tflcb230.t$docn$d=tfacp200.t$docn
-	AND tflcb230.t$ttyp$d=tfacp200.t$tdoc
-	AND tflcb230.t$ninv$d=tfacp200.t$ninv
+  LEFT JOIN ttfcmg001201 tfcmg001
+  ON tfcmg001.t$bank=tfacp600.t$bank
+--  LEFT JOIN ttfcmg001201 tfcmg001f                    
+
+--#FAF.002.o
+--  ON tfcmg001f.t$bank=tfacp600.t$basu                    
+
+--#FAF.002.o
+  LEFT JOIN ttccom125201 tccom125                      
+
+--#FAF.002.sn                    
+  ON tccom125.t$cban=tfacp600.t$basu                    
+  AND tccom125.t$ptbp=tfacp600.t$ptbp                    
+
+--#FAF.002.en
+  LEFT JOIN ttfcmg011201 tfcmg011
+  ON tfcmg011.t$bank=tfcmg001.t$brch
+  LEFT JOIN ttfcmg011201 tfcmg011f
+  ON tfcmg011f.t$bank=tccom125.t$brch,                  
+
+  ttfacp200201 tfacp200
+  LEFT JOIN ttflcb230201 tflcb230 
+  ON tflcb230.t$docn$d=tfacp200.t$docn
+  AND tflcb230.t$ttyp$d=tfacp200.t$tdoc
+  AND tflcb230.t$ninv$d=tfacp200.t$ninv
 WHERE
       tfacp200.t$tdoc=tfacp600.t$payt
-  AND	tfacp200.t$docn=tfacp600.t$payd
+  AND  tfacp200.t$docn=tfacp600.t$payd
   AND tfacp200.t$lino=tfacp600.t$payl
