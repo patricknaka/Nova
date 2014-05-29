72d546ec3aabb3710e14b9db24b848850ac69c21
marcusmsv <marcus_msv@yahoo.com.br>
2014-05-29 13:54:05 -0300

    Padronização Atributo - VW_PEV_Pagamento

--- Views LN/VW_PEV_Pagamento.sql
+++ Views LN/VW_PEV_Pagamento.sql
@@ -8,52 +8,48 @@
 -- #FAF.088 - 28-mai-2014,	Fabio Ferreira, conversão de timezone no campo DT_APROVACAO_PAGAMENTO_ERP 
 --***************************************************************************************************************************************************************
 select
-    tdsls400.t$rcd_utc  DT_ULTIMA_ATUALIZ_PEDIDO,
-    201 COMPANHIA,
-    tdsls400.t$orno ORDEM,
---    CONCAT(TRIM(sls401q.t$pecl$c), TRIM(to_char(sls401q.t$entr$c))) PEDIDO_ENTREGA, 					--#FAF.007.o
-    TO_CHAR(sls401q.t$entr$c) NUM_ENTREGA, 																--#FAF.047.1.n
---    sls401q.t$entr$c NUM_ENTREGA, 																	--#FAF.007.o	#FAF.047.1.o
-	TRIM(sls401q.t$pecl$c) PEDIDO,
---	sls401q.t$entr$c ENTREGA,																			--#FAF.047.o
-    znsls402.t$sequ$c  SEQ_PAGAMENTO,
-    znsls402.t$idmp$c  COD_MEIO_PAGAMENTO,
-    znsls402.t$cccd$c  COD_BANDEIRA,
-    znsls402.t$idbc$c  COD_BANCO,
-    znsls402.t$nupa$c  NUM_PARCELAS,
-    znsls402.t$vlmr$c  VALOR_PAGAMENTO,
-    znsls402.t$stat$c  STATUS_PAGAMENTO,
---    znsls400.t$idli$c  FLAG_VALE_LISTA_CASAMENTO,														--#FAF.049.o
-	CASE WHEN znsls400.t$idli$c!=0 THEN 1 ELSE 2 END FLAG_VALE_LISTA_CASAMENTO,							--#FAF.049.n
+    tdsls400.t$rcd_utc  DT_ULTIMA_ATUALIZACAO_PEDIDO,
+    201 CD_CIA,
+    tdsls400.t$orno NR_ORDEM,
+    TO_CHAR(sls401q.t$entr$c) NR_ENTREGA, 																--#FAF.047.1.n
+	TRIM(sls401q.t$pecl$c) NR_PEDIDO,
+    znsls402.t$sequ$c  SQ_PAGAMENTO,
+    znsls402.t$idmp$c  CD_MEIO_PAGAMENTO,
+    znsls402.t$cccd$c  CD_BANDEIRA,
+    znsls402.t$idbc$c  CD_BANCO,
+    znsls402.t$nupa$c  NR_PARCELAS,
+    znsls402.t$vlmr$c  VL_PAGAMENTO,
+    znsls402.t$stat$c  CD_STATUS_PAGAMENTO,
+	CASE WHEN znsls400.t$idli$c!=0 THEN 1 ELSE 2 END IN_VALE_LISTA_CASAMENTO,							--#FAF.049.n
 	CAST((FROM_TZ(CAST(TO_CHAR(znsls400.t$dtem$c, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
 			AT time zone sessiontimezone) AS DATE) DT_EMISSAO_PEDIDO,
-    znsls402.t$uneg$c  COD_UNIDADE_NEGOCIO,	
+    znsls402.t$uneg$c  CD_UNIDADE_NEGOCIO,	
 	CAST((FROM_TZ(CAST(TO_CHAR(znsls402.t$dtra$c, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
 			AT time zone sessiontimezone) AS DATE) DT_APROVACAO,
-    znsls402.t$valo$c  VALOR_ORIGINAL_PAGAMENTO,
-    znsls402.t$vlja$c  VALOR_JUROS_ADMINISTRADORA,
+    znsls402.t$valo$c  VL_ORIGINAL,
+    znsls402.t$vlja$c  VL_JUROS_ADMINISTRADORA,
     CASE WHEN znsls402.t$vlja$c!=0 THEN 1
 	ELSE 2
-	END FLAG_JUROS_ADMINISTRADORA,
+	END IN_JUROS_ADMINISTRADORA,
     (select 
 	CAST((FROM_TZ(CAST(TO_CHAR(min(a.t$trdt), 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 				--#FAF.088.n
 			AT time zone sessiontimezone) AS DATE)	
 	from ttdsls451201 a
     where a.t$orno=tdsls400.t$orno) DT_APROVACAO_PAGAMENTO_ERP,
-    znsls402.t$vlju$c  VALOR_JUROS,
-    ' ' COD_CICLO_PAGAMENTO,            -- *** NÃO EXISTE ESTA INFORMAÇÃO NO LN / PENDENTE DE DUVIDA ***
-    znsls402.t$cone$c  NUM_TABELA_NEGOCIACAO,
-    znsls402.t$ncam$c  BIN_CARTAO_CREDITO,
-    znsls402.t$nctf$c  NSU_TRANSACAO_CARTAO_CREDITO,
-    znsls402.t$nsua$c  NSU_AUTOR_CARTAO_CREDITO,
-    znsls402.t$auto$c  COD_AUTOR_CARTAO_CREDITO,
-    znsls402.t$maqu$c  NUM_MAQUINETA,
-    TO_CHAR(znsls402.t$nute$c)  NUM_TERMINAL,															--#FAF.089.n
-    znsls402.t$mrep$c  COD_MOTIVO_REPROVACAO,
-	znsls402.t$txrp$c  DESC_MOTIVO_REPROVACAO,															--#FAF.076.n
-    znsls402.t$idag$c  NUM_AGENCIA_DEBITO,
-    znsls402.t$idct$c  NUM_CONTA_DEBITO,
-	znsls402.t$idad$c COD_ADQUIRENTE																	--#FAF.085.n
+    znsls402.t$vlju$c  VL_JUROS,
+    ' ' CD_CICLO_PAGAMENTO,            -- *** NÃO EXISTE ESTA INFORMAÇÃO NO LN / PENDENTE DE DUVIDA ***
+    znsls402.t$cone$c  NR_TABELA_NEGOCIACAO,
+    znsls402.t$ncam$c  NR_BIN_CARTAO_CREDITO,
+    znsls402.t$nctf$c  NR_NSU_TRANSACAO_CARTAO,
+    znsls402.t$nsua$c  NR_NSU_AUTOR_CARTAO,
+    znsls402.t$auto$c  CD_AUTOR_CARTAO_CREDITO,
+    znsls402.t$maqu$c  NR_MAQUINETA,
+    TO_CHAR(znsls402.t$nute$c)  NR_TERMINAL,															--#FAF.089.n
+    znsls402.t$mrep$c  CD_MOTIVO_REPROVACAO,
+	znsls402.t$txrp$c  DS_MOTIVO_REPROVACAO,															--#FAF.076.n
+    znsls402.t$idag$c  NR_AGENCIA,
+    znsls402.t$idct$c  NR_CONTA_CORRENTE,
+	znsls402.t$idad$c CD_ADQUIRENTE																	--#FAF.085.n
 FROM  tznsls400201 znsls400,
     (select distinct 
       znsls401.t$ncia$c      t$ncia$c,
