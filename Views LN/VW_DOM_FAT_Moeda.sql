f27acf0e4bb45323a888fbd90065aa8a25dc1ca7
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-05-29 15:06:39 -0300

    Padronização de Atributos - VW_FAT_Moeda

--- Views LN/VW_DOM_FAT_Moeda.sql
+++ Views LN/VW_DOM_FAT_Moeda.sql
@@ -1,4 +1,4 @@
-SELECT  tcmcs002.t$ccur COD_MOEDA,
-        tcmcs002.t$dsca DESC_MOEDA
+SELECT  tcmcs002.t$ccur CD_MOEDA,
+        tcmcs002.t$dsca DS_MOEDA
 FROM    ttcmcs002201 tcmcs002
 order by 1
