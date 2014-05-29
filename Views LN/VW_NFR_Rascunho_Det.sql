1696c57f74ceacd09d36ecb1896b39f9d270d5d9
marcusmsv <marcus_msv@yahoo.com.br>
2014-05-29 13:35:57 -0300

    Padronização Atributo - VW_NFR_Rascunho_Det

--- Views LN/VW_NFR_Rascunho_Det.sql
+++ Views LN/VW_NFR_Rascunho_Det.sql
@@ -1,52 +1,52 @@
 -- #FAF.022 - 27-mai-2014, Fabio Ferreira, 	Correções e alteração da origem da informação para os dados da pré-nota			
 --************************************************************************************************************************************************************
 SELECT
-	brnfe941.t$fire$l NUM_RASCUNHO,
-	tdpur401.t$pono SEQU_ITEM,
-	ltrim(rtrim(tdpur401.t$item)) COD_ITEM,
-	tdpur401.t$cuqp COD_UNIDADE_DE_MEDIDA,
-	tdpur401.t$orno NUM_PEDIDO_COMPRA,
-	tdpur401.t$qoor QUANTIDADE,
-	tdpur401.t$pric VALOR_UNITARIO,
-	brnfe941.t$pric$l VALOR_PRODUTO,
+	brnfe941.t$fire$l NR_NF_RASCUNHO,
+	tdpur401.t$pono SQ_ITEM,
+	ltrim(rtrim(tdpur401.t$item)) CD_ITEM,
+	tdpur401.t$cuqp CD_UNIDADE_MEDIDA,
+	tdpur401.t$orno NR_PEDIDO_COMPRA,
+	tdpur401.t$qoor QT_RECEBIMENTO,
+	tdpur401.t$pric VL_UNITARIO,
+	brnfe941.t$pric$l VL_PRODUTO,
 
 	(SELECT SUM(i.t$amnt$l)
 	FROM tbrnfe942201 i
 	WHERE i.t$fire$l=brnfe941.t$fire$l
 	AND i.t$line$l=brnfe941.t$line$l
-	AND i.t$brty$l=3) VALOR_IPI,
+	AND i.t$brty$l=3) VL_IPI,
 	
 	(SELECT SUM(i.t$amnt$l)
 	FROM tbrnfe942201 i
 	WHERE i.t$fire$l=brnfe941.t$fire$l
 	AND i.t$line$l=brnfe941.t$line$l
-	AND i.t$brty$l=1) VALOR_ICMS,
+	AND i.t$brty$l=1) VL_ICMS,
 
 	(SELECT SUM(i.t$amnt$l)
 	FROM tbrnfe942201 i
 	WHERE i.t$fire$l=brnfe941.t$fire$l
 	AND i.t$line$l=brnfe941.t$line$l
-	AND i.t$brty$l=2) VALOR_ICMS_ST,
+	AND i.t$brty$l=2) VL_ICMS_ST,
 	
 	
-	brnfe941.t$gexp$l VALOR_DESPESA,
-	brnfe941.t$addc$l VALOR_DESCONTO,
-	brnfe941.t$fght$l VALOR_FRETE,
-	brnfe941.t$insr$l VALOR_SEGURO,
-	brnfe941.t$addc$l VALOR_DESCONTO_INCONDICIONAL,									-- *** Não existe no LN na pré nota a separação da despes e despesa incondicional
-	brnfe941.t$tamt$l VALOR_TOTAL_ITEM,
+	brnfe941.t$gexp$l VL_DESPESA,
+	brnfe941.t$addc$l VL_DESCONTO,
+	brnfe941.t$fght$l VL_FRETE,
+	brnfe941.t$insr$l VL_SEGURO,
+	brnfe941.t$addc$l VL_DESCONTO_INCONDICIONAL,									-- *** Não existe no LN na pré nota a separação da despes e despesa incondicional
+	brnfe941.t$tamt$l VL_TOTAL_ITEM,
 
 	(SELECT SUM(i.t$amnt$l)
 	FROM tbrnfe942201 i
 	WHERE i.t$fire$l=brnfe941.t$fire$l
 	AND i.t$line$l=brnfe941.t$line$l
-	AND i.t$brty$l=2) VALOR_IMPOSTO_IMPORTACAO,
+	AND i.t$brty$l=2) VL_IMPOSTO_IMPORTACAO,
 	
 	nvl((select sum(r.T$CCHR$L) FROM ttdrec941201 r
 	where r.t$fire$l=brnfe941.t$fire$c
-	and r.t$line$l=brnfe941.t$line$c),0) VALOR_DESPESA_ADUANEIRO,
+	and r.t$line$l=brnfe941.t$line$c),0) VL_DESPESA_ADUANEIRA,
 	
-	CASE WHEN tdpur400.t$rfdt$l=37 THEN brnfe941.t$addc$l ELSE 0 END VALOR_ADICIONAL_IMPORTACAO,
+	CASE WHEN tdpur400.t$rfdt$l=37 THEN brnfe941.t$addc$l ELSE 0 END VL_ADICIONAL_IMPORTACAO,
 
 	CASE WHEN tdpur400.t$rfdt$l=37 THEN
 	(SELECT SUM(i.t$amnt$l)
@@ -54,7 +54,7 @@ SELECT
 	WHERE i.t$fire$l=brnfe941.t$fire$c
 	AND i.t$line$l=brnfe941.t$line$c
 	AND i.t$brty$l=5)
-	ELSE 0 END VALOR_PIS_IMPORTACAO,	
+	ELSE 0 END VL_PIS_IMPORTACAO,	
 
 	CASE WHEN tdpur400.t$rfdt$l=37 THEN
 	(SELECT SUM(i.t$amnt$l)
@@ -62,7 +62,7 @@ SELECT
 	WHERE i.t$fire$l=brnfe941.t$fire$c
 	AND i.t$line$l=brnfe941.t$line$c
 	AND i.t$brty$l=5)
-	ELSE 0 END VALOR_COFINS_IMPORTACAO,
+	ELSE 0 END VL_COFINS_IMPORTACAO,
 	
   CASE WHEN
 	nvl((select a.t$cdec from ttdpur400201 a
@@ -70,7 +70,7 @@ SELECT
 		and a.t$cdec='001'
 		and rownum=1),0)=0 THEN 0
 	ELSE brnfe941.t$fght$l
-	END VALOR_CIF_IMPORTACAO
+	END VL_CIF_IMPORTACAO
 
 FROM
 	tbrnfe941201 brnfe941,
