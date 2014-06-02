a26d30e90d66aa359dc534c9517041596e065a08
marcusmsv <marcus_msv@yahoo.com.br>
2014-06-02 14:33:59 -0300

    Alteração Script

--- MIS/SCRIPT_ALTER_MIS_DW.sql
+++ MIS/SCRIPT_ALTER_MIS_DW.sql
@@ -1,173 +1,210 @@
 
----ALTERAÇÕES NA BASE DE DADOS MIS_DW/MIS_DW_MIGRACAO
+---ALTERAÇÕES NA BASE DE DADOS MIS_DW/MIS_DW
 
 ---------------------------------------------------------------------------------------------------------------------------------------------
 --ALTERAÇÕES DE DATA TYPE
 
 --DE NUMERIC(2) PARA INT
-ALTER TABLE MIS_DW_MIGRACAO.DBO.STG_DESPESA
+ALTER TABLE MIS_DW.DBO.STG_DESPESA
 ALTER COLUMN ID_CIA INT
 
 --DE NUMERIC(2) PARA INT
-ALTER TABLE MIS_DW_MIGRACAO.DBO.STG_DESPESA_LANCAMENTO
+ALTER TABLE MIS_DW.DBO.STG_DESPESA_LANCAMENTO
 ALTER COLUMN LANC_ID_CIA INT
 
 --DE NUMERIC(7) PARA NUMERIC(24)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.STG_DESPESA
+ALTER TABLE MIS_DW.DBO.STG_DESPESA
 ALTER COLUMN ID_CONTA NUMERIC(24)
 
 --DE NUMERIC(7) PARA NUMERIC(24)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.STG_DESPESA_LANCAMENTO
+ALTER TABLE MIS_DW.DBO.STG_DESPESA_LANCAMENTO
 ALTER COLUMN LANC_ID_CONTA NUMERIC(24)
 
 --DE NUMERIC(2) PARA INT
-ALTER TABLE MIS_DW_MIGRACAO.DBO.STG_SIGE_PURCHASE_FULL
+ALTER TABLE MIS_DW.DBO.STG_SIGE_PURCHASE_FULL
 ALTER COLUMN NR_CIA INT
 
 --DE NUMERIC(7) PARA NUMERIC(24)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.STG_DESPESA_CONTAS
+ALTER TABLE MIS_DW.DBO.STG_DESPESA_CONTAS
 ALTER COLUMN CONT_ID_CONTA NUMERIC(24)
 
 --DE NUMERIC(7) PARA NUMERIC(24)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.STG_DESPESA_CONTAS
+ALTER TABLE MIS_DW.DBO.STG_DESPESA_CONTAS
 ALTER COLUMN CONT_ID_CONTAPAI NUMERIC(24)
 
 
 --DE NUMERIC(2) para NUMERIC(4)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.stg_estoque_sige
+ALTER TABLE MIS_DW.DBO.stg_estoque_sige
 ALTER COLUMN ID_CIA NUMERIC(4)
 
 --DE VARCHAR(30) PARA VARCHAR(35)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.stg_estoque_sige
+ALTER TABLE MIS_DW.DBO.stg_estoque_sige
 ALTER COLUMN DS_MODALIDADE VARCHAR(35)
 
 --DE VARCHAR(30) PARA VARCHAR(40)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.STG_ESTOQUE_SIGE_MODALIDADE
+ALTER TABLE MIS_DW.DBO.STG_ESTOQUE_SIGE_MODALIDADE
 ALTER COLUMN DS_MODALIDADE VARCHAR(40)
 
 --DE NUMERIC(13) para NUMERIC(15)
-ALTER TABLE MIS_DW_MIGRACAO.dbo.ods_product
+ALTER TABLE MIS_DW.dbo.ods_product
 alter column ds_ean numeric(15)
 
 --DE NUMERIC(13) para NUMERIC(15)
-ALTER TABLE MIS_DW_MIGRACAO.dbo.dim_product
+ALTER TABLE MIS_DW.dbo.dim_product
 alter column ds_ean numeric(15)
 
 
 --DE VARCHAR(30) para VARCHAR(40)
-ALTER TABLE MIS_DW_MIGRACAO.dbo.ods_estoque_modalidade
+ALTER TABLE MIS_DW.dbo.ods_estoque_modalidade
 ALTER COLUMN ds_modalidade VARCHAR(40)
 
 --DE NUMERIC(13) para NUMERIC(15)
-ALTER TABLE MIS_DW_MIGRACAO.dbo.aux_produto_dw
+ALTER TABLE MIS_DW.dbo.aux_produto_dw
 alter column ds_ean numeric(15)
+----------------------------------------------------------------------------------------------------------------
 
+--ALTERAÇÃO ATRIBUTO ID_CONTA TABELA ODS_DESPESA_CONTAS
+
+--DROP OBJETO DEPENDENCIA
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_ods_despesa_ods_despesa_contas]') AND parent_object_id = OBJECT_ID(N'[dbo].[ods_despesa]'))
+ALTER TABLE [dbo].[ods_despesa] DROP CONSTRAINT [FK_ods_despesa_ods_despesa_contas]
+GO
+
+--DROP OBJETO
+IF  EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[ods_despesa_contas]') AND name = N'PK_ods_despesa_contas')
+ALTER TABLE [dbo].[ods_despesa_contas] DROP CONSTRAINT [PK_ods_despesa_contas]
+GO
+
+--EFETIVA ALTERAÇÃO
 --DE NUMERIC(7) para NUMERIC(9) --ESTA ALTERAÇÃO DEVERÁ FALHAR DEVIDO A USO DE CONSTRAINT
-ALTER TABLE MIS_DW_MIGRACAO.DBO.ODS_DESPESA_CONTAS
-ALTER COLUMN ID_CONTA NUMERIC(9)
+ALTER TABLE MIS_DW.DBO.ODS_DESPESA_CONTAS
+ALTER COLUMN ID_CONTA NUMERIC(9) NOT NULL
 
-ALTER TABLE MIS_DW_MIGRACAO.DBO.ODS_DESPESA
+
+--RECRIA OBJETO
+ALTER TABLE [dbo].[ods_despesa_contas] ADD  CONSTRAINT [PK_ods_despesa_contas] PRIMARY KEY CLUSTERED 
+(
+	[id_conta] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+GO
+
+--ALTERA OBJETO REFERENCIA
+ALTER TABLE [dbo].[ods_despesa]
+ALTER COLUMN ID_CONTA numeric(9)
+
+--RECRIA OBJETO DEPENDENCIA
+ALTER TABLE [dbo].[ods_despesa]  WITH CHECK ADD  CONSTRAINT [FK_ods_despesa_ods_despesa_contas] FOREIGN KEY([id_conta])
+REFERENCES [dbo].[ods_despesa_contas] ([id_conta])
+GO
+
+ALTER TABLE [dbo].[ods_despesa] CHECK CONSTRAINT [FK_ods_despesa_ods_despesa_contas]
+GO
+
+----------------------------------------------------------------------------------------------------------------
+
+
+ALTER TABLE MIS_DW.DBO.ODS_DESPESA
 ALTER COLUMN ID_CONTA NUMERIC(9)
 
 
 --DE NUMERIC (2) para NUMERIC(3)
-ALTER TABLE MIS_DW_MIGRACAO.dbo.ODS_DESPESA
+ALTER TABLE MIS_DW.dbo.ODS_DESPESA
 ALTER COLUMN id_cia NUMERIC(3)
 
 
 --DE NUMERIC (2) para NUMERIC(3)
-ALTER TABLE MIS_DW_MIGRACAO.dbo.AUX_ODS_DESPESA_LANCAMENTO
+ALTER TABLE MIS_DW.dbo.AUX_ODS_DESPESA_LANCAMENTO
 ALTER COLUMN id_cia NUMERIC(3)
 
 --DE NUMERIC (2) para NUMERIC(3) --PROBLEMAS COM INDICE NA TABELA
-ALTER TABLE MIS_DW_MIGRACAO.dbo.ODS_DESPESA_LANCAMENTO
+ALTER TABLE MIS_DW.dbo.ODS_DESPESA_LANCAMENTO
 ALTER COLUMN id_cia NUMERIC(3)
 
 --DE NUMERIC(7) para NUMERIC(9)
-ALTER TABLE MIS_DW_MIGRACAO.dbo.aux_ods_despesa_contas
+ALTER TABLE MIS_DW.dbo.aux_ods_despesa_contas
 ALTER COLUMN id_conta NUMERIC(9)
 
 --DE NUMERIC(24) para NUMERIC(9) --DEVIDO A FALHA NO LOOKUP
-ALTER TABLE MIS_DW_MIGRACAO.STG_DESPESA_CONTAS
+ALTER TABLE MIS_DW.STG_DESPESA_CONTAS
 ALTER COLUMN CONT_ID_CONTA NUMERIC(9)
 
 --DE NUMERIC(7) PARA NUMERIC(9) --PROBLEMA NO ALTER DEVIDO A DEPENDENCIA DE INDICE
-ALTER TABLE MIS_DW_MIGRACAO.dbo.ods_despesa_lancamento
+ALTER TABLE MIS_DW.dbo.ods_despesa_lancamento
 ALTER COLUMN ID_CONTA NUMERIC(9)
 
 --------------------------------------------------------------------
 --DE VARCHAR(2) para VARCHAR(3)
-ALTER TABLE MIS_DW_MIGRACAO.dbo.stg_sige_titulo_movimento
+ALTER TABLE MIS_DW.dbo.stg_sige_titulo_movimento
 ALTER COLUMN ID_DOCUMENTO varchar(3)
 
 --------------------------------------------------------------------
 --DE VARCHAR(2) para VARCHAR(3)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.stg_sige_titulo
+ALTER TABLE MIS_DW.DBO.stg_sige_titulo
 ALTER COLUMN ID_DOCUMENTO varchar(3)
 
 --------------------------------------------------------------------
 --DE NUMERIC(2) para NUMERIC(3)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.stg_sige_titulo
+ALTER TABLE MIS_DW.DBO.stg_sige_titulo
 ALTER COLUMN ID_CIA NUMERIC(3)
 
 --------------------------------------------------------------------
 --DE VARCHAR(2) para VARCHAR(3)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.stg_sige_titulo_documento
+ALTER TABLE MIS_DW.DBO.stg_sige_titulo_documento
 ALTER COLUMN ID_DOCUMENTO varchar(3)
 
 --------------------------------------------------------------------
 --DE NUMERIC(2) para NUMERIC(3)
 
-ALTER TABLE MIS_DW_MIGRACAO.DBO.stg_sige_titulo_transacao
+ALTER TABLE MIS_DW.DBO.stg_sige_titulo_transacao
 ALTER COLUMN ID_MODULO VARCHAR(3)
 
 --------------------------------------------------------------------
 --DE VARCHAR(2) para VARCHAR(3)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.ods_sige_titulo_documento
+ALTER TABLE MIS_DW.DBO.ods_sige_titulo_documento
 ALTER COLUMN ID_DOCUMENTO varchar(3)
 
 --------------------------------------------------------------------
 --DE NUMERIC(2) para NUMERIC(3)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.ods_sige_titulo_transacao
+ALTER TABLE MIS_DW.DBO.ods_sige_titulo_transacao
 ALTER COLUMN ID_MODULO VARCHAR(3)
 
 --------------------------------------------------------------------
 --DE VARCHAR(2) para VARCHAR(3)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.ods_sige_titulo_movimento
+ALTER TABLE MIS_DW.DBO.ods_sige_titulo_movimento
 ALTER COLUMN ID_DOCUMENTO varchar(3)
 
 --------------------------------------------------------------------
 --DE NUMERIC(2) para NUMERIC(3)  --PROBLEMAS DE CONVERSÃO DEVIDO FALHA DE PK
-ALTER TABLE MIS_DW_MIGRACAO.DBO.DIM_CONDICAO_PAGAMENTO
+ALTER TABLE MIS_DW.DBO.DIM_CONDICAO_PAGAMENTO
 ALTER COLUMN NR_CIA NUMERIC (3)
 
 
 --------------------------------------------------------------------
 --DE VARCHAR(30) para VARCHAR(40)
 
-ALTER TABLE MIS_DW_MIGRACAO.DBO.DIM_ESTOQUE_MODALIDADE
+ALTER TABLE MIS_DW.DBO.DIM_ESTOQUE_MODALIDADE
 ALTER COLUMN DS_MODALIDADE VARCHAR(40)
 
 --------------------------------------------------------------------
 --DE NUMERIC(2) para NUMERIC(3) --PROBLEMAS COM PK (DROPAR INDICE E PK)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.ods_purchase_full
+ALTER TABLE MIS_DW.DBO.ods_purchase_full
 ALTER COLUMN NR_CIA NUMERIC(3) not null
 
 --------------------------------------------------------------------
 --DE NUMERIC(12) PARA NUMERIC(40)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.ODS_SIGE_CMV_HIST
+ALTER TABLE MIS_DW.DBO.ODS_SIGE_CMV_HIST
 ALTER COLUMN ID_ITEM NUMERIC(32)
 
 
 --------------------------------------------------------------------
 --DE NUMERIC(12) PARA NUMERIC(40)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.aux_ods_sige_cmv_hist
+ALTER TABLE MIS_DW.DBO.aux_ods_sige_cmv_hist
 ALTER COLUMN ID_ITEM NUMERIC(32)
 
 --------------------------------------------------------------------
 
 --DE nvarchar (100) para varchar(100)
-ALTER TABLE MIS_DW_MIGRACAO.dbo.ods_vendedor
+ALTER TABLE MIS_DW.dbo.ods_vendedor
 ALTER COLUMN ds_vendedor_afiliado varchar(100)
 
 
@@ -184,36 +221,36 @@ ALTER COLUMN ds_vendedor_afiliado varchar(100)
 --ALTERAÇÕES DE DE-PARA DO SIGE PARA LN NA ESTRUTURA DE COMPANHIA/FILIAL
 --STAGE
 --INCLUSAO DE COLUNA DE COMPANHIA DO LN
-ALTER TABLE MIS_DW_MIGRACAO.DBO.STG_SIGE_ESTABELECIMENTO
+ALTER TABLE MIS_DW.DBO.STG_SIGE_ESTABELECIMENTO
 ADD FILI_ID_CIA_LN NUMERIC(3)
 
 --INCLUSAO DE COLUNA DE FILIAL DO LN
-ALTER TABLE MIS_DW_MIGRACAO.DBO.STG_SIGE_ESTABELECIMENTO
+ALTER TABLE MIS_DW.DBO.STG_SIGE_ESTABELECIMENTO
 ADD FILI_ID_FILIAL_LN NUMERIC(3)
 
 --ODS
-ALTER TABLE MIS_DW_MIGRACAO.DBO.ODS_ESTABELECIMENTO
+ALTER TABLE MIS_DW.DBO.ODS_ESTABELECIMENTO
 ADD nr_id_cia_ln NUMERIC(3)
 
-ALTER TABLE MIS_DW_MIGRACAO.DBO.ODS_ESTABELECIMENTO
+ALTER TABLE MIS_DW.DBO.ODS_ESTABELECIMENTO
 ADD nr_id_filial_ln NUMERIC(3)
 
 
 --------------------------------------------------------------------
 --INCLUSÃO DE ATRIBUTO PARA ADAPTAÇÃO AS REGRAS DO LN
-ALTER TABLE MIS_DW_MIGRACAO.DBO.STG_SIGE_METAS_ORCAMENTO
+ALTER TABLE MIS_DW.DBO.STG_SIGE_METAS_ORCAMENTO
 ADD nr_id_unidade_negocio int
 
 
 --------------------------------------------------------------------
 --INCLUSÃO DE ATRIBUTO DE REFERENCIA FISCAL
 
-ALTER TABLE MIS_DW_MIGRACAO.DBO.STG_SIGE_TITULO
+ALTER TABLE MIS_DW.DBO.STG_SIGE_TITULO
 ADD ID_REF_FISCAL varchar(40)
 
 --------------------------------------------------------------------
 --DE NUMERIC(12) PARA NUMERIC(40)
-ALTER TABLE MIS_DW_MIGRACAO.DBO.stg_sige_cmv_hist
+ALTER TABLE MIS_DW.DBO.stg_sige_cmv_hist
 ALTER COLUMN ID_ITEM NUMERIC(32)
 
 
@@ -235,11 +272,11 @@ ALTER COLUMN ID_CIA NUMERIC(3)
 --------------------------------------------------------------------
 
 --DE VARCHAR(2) para VARCHAR(5)
-ALTER TABLE MIS_DW_MIGRACAO.dbo.stg_estoque_sige
+ALTER TABLE MIS_DW.dbo.stg_estoque_sige
 ALTER COLUMN ID_TIPODEPOSITO VARCHAR(5)
 
 --------------------------------------------------------------------
 
 --Inclusão Atributo Tipo Bloqueio
-ALTER TABLE MIS_DW_MIGRACAO.DBO.ods_estoque_sige
+ALTER TABLE MIS_DW.DBO.ods_estoque_sige
 ADD id_tipo_bloqueio varchar(5)
