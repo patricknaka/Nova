b9ea12004c6fd856a61bc923c6647a1565651f6b
marcusmsv <marcus_msv@yahoo.com.br>
2014-06-03 16:19:55 -0300

    Alteração Atributo - MIS_DW
    Alteração Tabela stg_sige_titulo

--- MIS/SCRIPT_ALTER_MIS_DW.sql
+++ MIS/SCRIPT_ALTER_MIS_DW.sql
@@ -509,13 +509,21 @@ ALTER COLUMN ID_DOCUMENTO varchar(3)
 ---------------------------------------------------------------------------------------------------
 
 --DE INT PARA VARCHAR(3)
-
 ALTER TABLE MIS_DW..stg_sige_titulo_movimento
 ALTER COLUMN ID_TRANSACAO VARCHAR(3)
+
+--DE VARCHAR(2) para VARCHAR(3)
+ALTER TABLE MIS_DW..stg_sige_titulo
+ALTER COLUMN ID_MODULO VARCHAR(3)
+
+--DE VARCHAR(2) para VARCHAR(3)
+ALTER TABLE MIS_DW..stg_sige_titulo
+ALTER COLUMN ID_MODULO VARCHAR(3)
 
 ---------------------------------------------------------------------------------------------------
 --VERIFICAR
 --DE NUMERIC(24) para NUMERIC(9) --DEVIDO A FALHA NO LOOKUP
 --ALTER TABLE MIS_DW.STG_DESPESA_CONTAS
 --ALTER COLUMN CONT_ID_CONTA NUMERIC(9)
+
 
