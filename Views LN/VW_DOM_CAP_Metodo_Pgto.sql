23b7fe201cd37f6eacad562cc19397645cb85c10
fabioInfor <fabio.ferreira@infor.com>
2014-06-05 15:09:54 -0300

    #107 VW_DOM_CAP_Metodo_Pgto.sql
    Iclusão do campo CD_MODULO

--- Views LN/VW_DOM_CAP_Metodo_Pgto.sql
+++ Views LN/VW_DOM_CAP_Metodo_Pgto.sql
@@ -1,6 +1,7 @@
 SELECT
     a.t$paym CD_METODO_PAGAMENTO,
-    a.t$desc DS_METODO_PAGAMENTO
+    a.t$desc DS_METODO_PAGAMENTO,
+	CASE WHEN a.t$repa=1 THEN 'CAR' ELSE 'CAP' END CD_MODULO
 FROM
     ttfcmg003201 a
 order by 1
