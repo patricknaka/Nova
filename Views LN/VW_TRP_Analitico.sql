9cf4a769c40fa381044df9e6f53fd991e911c80a
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-06-06 16:11:40 -0300

    Adequação dos nomes dos atributos

--- Views LN/VW_TRP_Analitico.sql
+++ Views LN/VW_TRP_Analitico.sql
@@ -1,23 +1,20 @@
+
 select distinct
-      znsls401.T$NCIA$C dofi_filial,
-      znint002.T$DESC$C unin_nome,
-      (select min(o.T$DATE$C) 
-      from BAANDB.TZNFMD640201 o
-      where o.T$COCI$C='ROT'
-      and o.T$ETIQ$C=znfmd630.T$ETIQ$C) dofi_dt_saida_entrega,
-      znsls401.T$pecl$C dofi_pedido_cliente,
-      znfmd630.T$pecl$C dofi_pedido_interno,
-      znfmd630.t$vlft$c dofi_vl_frete_pagar_gte,
-      znsls002.T$DSCA$C tien_nome,
-      (select min(o.T$DATE$C) 
-      from BAANDB.TZNFMD640201 o
-      where o.T$COCI$C='ENT'
-      and o.T$ETIQ$C=znfmd630.T$ETIQ$C) dofi_dt_entrega_realizada,
-      CASE WHEN znfmd630.t$stat$c='F' THEN 'FINALIZADO' ELSE 'EM PROCESSO' END dofi_tp_estagio,
-      znfmd630.t$ncar$c dofi_expedicao,
+      znsls401.T$NCIA$C CD_DOFI_FILIAL,
+      znint002.T$DESC$C NM_UNIN_NOME,
+      (select min(o.T$DATE$C) from BAANDB.TZNFMD640201 o
+		where o.T$COCI$C='ROT' and o.T$ETIQ$C=znfmd630.T$ETIQ$C) DT_DOFI_SAIDA_ENTREGA,
+      znsls401.T$pecl$C NR_DOFI_PEDIDO_CLIENTE,
+      znfmd630.T$pecl$C NR_DOFI_PEDIDO_INTERNO,
+      znfmd630.t$vlft$c VL_DOFI_FRETE_PAGAR_GTE,
+      znsls002.T$DSCA$C NM_TIEN_NOME,
+      (select min(o.T$DATE$C) from BAANDB.TZNFMD640201 o
+		where o.T$COCI$C='ENT' and o.T$ETIQ$C=znfmd630.T$ETIQ$C) DT_DOFI_ENTREGA_REALIZADA,
+      CASE WHEN znfmd630.t$stat$c='F' THEN 'FINALIZADO' ELSE 'EM PROCESSO' END NM_DOFI_TP_ESTAGIO,
+      znfmd630.t$ncar$c NR_DOFI_EXPEDICAO,
 --      znfmd061.t$creg$c dofi_id_regiao_transportadora,
-      ' ' dofi_id_regiao_transportadora,
-      znfmd630.T$FILI$C dofi_id_estabelecimento,
+      ' ' CD_DOFI_REGIAO_TRANSPORTADORA,
+      znfmd630.T$FILI$C CD_DOFI_ESTABELECIMENTO,
 --      (select min(o.T$DATE$C) 
 --      from BAANDB.TZNFMD640201 o,
 --           BAANDB.TZNFMD030201 c
@@ -30,57 +27,39 @@ select distinct
 --      where o.T$ETIQ$C=znfmd630.T$ETIQ$C
 --      and   c.t$ocin$c=o.T$COCI$C
 --      and   c.t$tent$c=1) peen_horario_final,
-
       CAST((FROM_TZ(CAST(TO_CHAR(znsls401.t$dtep$c, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
-      AT time zone sessiontimezone) AS DATE) dofi_dt_prometida,
-
+      AT time zone sessiontimezone) AS DATE) DT_DOFI_PROMETIDA,
       CAST((FROM_TZ(CAST(TO_CHAR(tdsls400.t$ddat, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
-      AT time zone sessiontimezone) AS DATE) dofi_dt_entrega_prevista,
-       
-       
+      AT time zone sessiontimezone) AS DATE) DT_DOFI_ENTREGA_PREVISTA,
 --        znfmd060.t$cono$c dofi_id_contrato,
-      znfmd630.t$fire$c dofi_id_documento_interno,
-      201 dofi_cia,
-      
+      znfmd630.t$fire$c NR_DOFI_DOCUMENTO_INTERNO,
+      201 CD_DOFI_CIA,
       CAST((FROM_TZ(CAST(TO_CHAR(znsls400.t$dtem$c, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
-      AT time zone sessiontimezone) AS DATE) pedc_dt_emissao,    
-      
-      znfmd060.t$ttra$c pedc_id_tipo_transporte,
-      
+      AT time zone sessiontimezone) AS DATE) DT_PEDC_EMISSAO,    
+      znfmd060.t$ttra$c CD_PEDC_TIPO_TRANSPORTE,
       CAST((FROM_TZ(CAST(TO_CHAR(tdsls401.t$rdta, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
-      AT time zone sessiontimezone) AS DATE) pedc_dt_limite_exp,    
-    
-       
+      AT time zone sessiontimezone) AS DATE) DT_PEDC_LIMITE_EXP,    
 --      znfmd630.t$docn$c nfca_ped_cliente,
-      (select o.T$COCI$C 
-      from BAANDB.TZNFMD640201 o
-      where o.T$date$c=(select max(o1.T$date$c) from BAANDB.TZNFMD640201 o1 where o1.T$ETIQ$C=o.T$ETIQ$C)
-      and o.T$ETIQ$C=znfmd630.T$ETIQ$C
-      and rownum=1) octr_id_ocorrencia_interna,
-      (select c.t$dsci$c
-      from BAANDB.TZNFMD640201 o,
-      BAANDB.TZNFMD030201 c
-      where o.T$date$c=(select max(o1.T$date$c) from BAANDB.TZNFMD640201 o1 where o1.T$ETIQ$C=o.T$ETIQ$C)
-      and o.T$ETIQ$C=znfmd630.T$ETIQ$C
-      and   c.t$ocin$c=o.T$COCI$C
-      and rownum=1) oces_nome,
-      (select max(o.T$DATE$C) 
-      from BAANDB.TZNFMD640201 o,
-           BAANDB.TZNFMD030201 c
-      where o.T$ETIQ$C=znfmd630.T$ETIQ$C
-      and   c.t$ocin$c=o.T$COCI$C) octr_dt_ocorrencia,
-      znsls401.t$ufen$c atdo_uf_destinatario,
-      znsls401.t$cepe$c atdo_cep_dest,
-      tcmcs080.t$seak tran_apelido,
-      cisli940.t$amnt$l atdo_vl_total_nf,
-      znfmd630.t$vlfr$c atdo_vl_frete_cli,
+      (select o.T$COCI$C from BAANDB.TZNFMD640201 o
+		where o.T$date$c=(select max(o1.T$date$c) from BAANDB.TZNFMD640201 o1 where o1.T$ETIQ$C=o.T$ETIQ$C)
+		and o.T$ETIQ$C=znfmd630.T$ETIQ$C and rownum=1) CD_OCTR_OCORRENCIA_INTERNA,
+      (select c.t$dsci$c from BAANDB.TZNFMD640201 o, BAANDB.TZNFMD030201 c
+		where o.T$date$c=(select max(o1.T$date$c) from BAANDB.TZNFMD640201 o1 where o1.T$ETIQ$C=o.T$ETIQ$C)
+		and o.T$ETIQ$C=znfmd630.T$ETIQ$C and c.t$ocin$c=o.T$COCI$C and rownum=1) NM_OCES_NOME,
+      (select max(o.T$DATE$C) from BAANDB.TZNFMD640201 o, BAANDB.TZNFMD030201 c
+		where o.T$ETIQ$C=znfmd630.T$ETIQ$C and c.t$ocin$c=o.T$COCI$C) DT_OCTR_OCORRENCIA,
+      znsls401.t$ufen$c NM_ATDO_UF_DESTINATARIO,
+      to_char(znsls401.t$cepe$c) CD_ATDO_CEP_DEST,
+      tcmcs080.t$seak NM_TRAN_APELIDO,
+      cisli940.t$amnt$l VL_ATDO_TOTAL_NF,
+      znfmd630.t$vlfr$c VL_ATDO_FRETE_CLI,
 --      (select sum(znfmd171.t$volu$c)
 --      from BAANDB.TZNFMD171201 znfmd171
 --      where znfmd171.t$nent$c=znfmd630.t$nent$c) atdo_volume_m3,
       (select sum(nl.t$dqua$l) from tcisli941201 nl, ttcibd001201 i
 		where nl.t$fire$l=znfmd630.t$fire$c
 		and i.t$item=nl.t$item$l
-		and i.t$kitm<3) nfca_qt_volumes
+		and i.t$kitm<3) QT_NFCA_VOLUMES
 from  BAANDB.TZNFMD630201 znfmd630,
       BAANDB.TZNSLS401201 znsls401,
       BAANDB.TZNSLS400201 znsls400,
@@ -109,4 +88,4 @@ AND     tcmcs080.t$cfrw=znfmd630.T$CFRW$C
 AND     znint002.T$NCIA$C=znsls400.T$NCIA$C
 AND     znint002.T$UNEG$C=znsls400.t$uneg$c
 AND     znsls002.T$TPEN$C=znsls401.t$itpe$c
-AND		cisli940.t$fire$l=znfmd630.t$fire$c
+AND     cisli940.t$fire$l=znfmd630.t$fire$c
--- Views LN/VW_TRP_Transporte.sql
+++ Views LN/VW_TRP_Transporte.sql
@@ -4,37 +4,37 @@ select distinct
     AT time zone sessiontimezone) AS DATE) 
   from BAANDB.TZNFMD640201 o
   where o.T$COCI$C='ROT'
-  and o.T$ETIQ$C=znfmd630.T$ETIQ$C) dofi_dt_saida_entrega, -- Fazer relacionamentoa
+  and o.T$ETIQ$C=znfmd630.T$ETIQ$C) DT_DOFI_SAIDA_ENTREGA, -- Fazer relacionamentoa
   CAST((FROM_TZ(CAST(TO_CHAR(znsls401.t$dtep$c, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
-    AT time zone sessiontimezone) AS DATE) dofi_dt_prometida,
-  znfmd630.t$vlft$c dofi_vl_frete_pagar_gte,
-  znfmd630.t$vlfr$c atdo_vl_frete_cli,
-  znfmd630.t$vlfa$c atdo_vl_frete_cli_ori,
-  znsls401.t$cepe$c atdo_cep_dest,
-  znsls401.t$cide$c atdo_cidade_destinatario,
-  znsls401.t$ufen$c atdo_uf_destinatario,
-  znfmd630.t$docn$c dofi_nu_nf,
-  znfmd630.t$seri$c dofi_sr_nf,
-  znfmd630.T$FILI$C dofi_filial,
+    AT time zone sessiontimezone) AS DATE) DT_DOFI_PROMETIDA,
+  znfmd630.t$vlft$c VL_DOFI_FRETE_PAGAR_GTE,
+  znfmd630.t$vlfr$c VL_ATDO_FRETE_CLI,
+  znfmd630.t$vlfa$c VL_ATDO_FRETE_CLI_ORI,
+  TO_CHAR(znsls401.t$cepe$c) CD_ATDO_CEP_DEST,
+  znsls401.t$cide$c NM_ATDO_CIDADE_DESTINATARIO,
+  znsls401.t$ufen$c NM_ATDO_UF_DESTINATARIO,
+  znfmd630.t$docn$c NR_DOFI_NF,
+  znfmd630.t$seri$c NR_DOFI_SERIE_NF,
+  znfmd630.T$FILI$C CD_DOFI_FILIAL,
   CASE WHEN cisli940.t$fdty$l=16 then
     (select distinct a.t$docn$l from tcisli940201 a, tcisli941201 b
     where b.t$fire$l=cisli940.t$fire$l
     and a.t$fire$l=b.t$refr$l)
     else 0
-    end nfca_id_nota_fatura,
+    end NR_NFCA_NOTA_FATURA,
   CASE WHEN cisli940.t$fdty$l=16 then
     (select distinct a.t$seri$l from tcisli940201 a, tcisli941201 b
     where b.t$fire$l=cisli940.t$fire$l
     and a.t$fire$l=b.t$refr$l)
     else ' '
-    end nfca_serie_fatura,
-  201 dofi_cia,
-  pesovol.vol atdo_volume_m3,
-  pesovol.peso atdo_peso, 
-  pesovol.vol*300 atdo_peso_cubado,
-  znfmd630.T$CFRW$C dofi_id_transportadora,
-  znfmd060.T$REFE$C pedc_obs_romaneio,
-  znfmd630.T$PECL$C
+    end NR_NFCA_SERIE_FATURA,
+  201 CD_DOFI_CIA,
+  pesovol.vol VL_ATDO_VOLUME_M3,
+  pesovol.peso VL_ATDO_PESO, 
+  pesovol.vol*300 VL_ATDO_PESO_CUBADO,
+  znfmd630.T$CFRW$C CD_DOFI_TRANSPORTADORA,
+  znfmd060.T$REFE$C DS_PEDC_OBS_ROMANEIO,
+  znfmd630.T$PECL$C NR_ENTREGA
 from  BAANDB.TZNFMD630201 znfmd630,
       BAANDB.TZNSLS401201 znsls401,
       (select
