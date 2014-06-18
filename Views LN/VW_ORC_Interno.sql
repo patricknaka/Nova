15f26e7d7661ff616d08cdbb053321223673119c
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-06-18 10:24:45 -0300

    Alteração de Alias

--- Views LN/VW_ORC_Interno.sql
+++ Views LN/VW_ORC_Interno.sql
@@ -3,7 +3,7 @@ SELECT  201 CD_CIA,
         SubStr(tdsta200.t$ATVS, 1, 6) CD_DEPARTAMENTO,
         tcccp070.t$stdt DT_ORCAMENTO,
         tdsta200.t$sbam$1 VL_ORCAMENTO,
-        tdsta200.t$csor CD_TIPO_ORCAMENTO,
+        tdsta200.t$csor CD_ORCAMENTO,
         SYSDATE DT_ATUALIZACAO --*** ATIVAR DATA DE ATUALIZAÇÃO ****
 FROM  ttdsta200201 tdsta200,
       ttcccp070201 tcccp070
