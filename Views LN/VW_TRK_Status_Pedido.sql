2f4ab8567ea492357159c39ee4a264a00b70fda4
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-06-23 10:56:20 -0300

    Alteração de Alias

--- Views LN/VW_TRK_Status_Pedido.sql
+++ Views LN/VW_TRK_Status_Pedido.sql
@@ -1,9 +1,9 @@
+-- VIEW: VW_TRK_Status_Pedido
 -- #FAF.050, 23-mai-2014, Fabio Ferreira,	NUM_ENTREGA_PEDIDO convertido para string
 --************************************************************************************************************************************************
 SELECT
     201 CD_CIA,
     tcemm030.t$euca CD_FILIAL,
-    tcemm112.t$grid CD_UNIDADE_EMPRESARIAL,
 	TO_CHAR(znsls410.t$entr$c) NR_ENTREGA_PEDIDO,					--#FAF.050.n
     znsls410.t$pecl$c NR_PEDIDO,
     znsls410.t$poco$c CD_STATUS,
@@ -11,6 +11,7 @@ SELECT
 	CAST((FROM_TZ(CAST(TO_CHAR(znsls410.t$dtoc$c, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
 			AT time zone sessiontimezone) AS DATE) DT_ATUALIZACAO_STATUS,
     znsls410.t$nmre$c NM_USUARIO_MODIFICACAO_STATUS,
+    tcemm112.t$grid CD_UNIDADE_EMPRESARIAL,
     znsls410.T$UNEG$C CD_UNIDADE_NEGOCIO,
     znsls410.t$orno$c NR_ORDEM_VENDA
 FROM
