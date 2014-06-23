18d3b6a1e03ce668546aa5d37f95b16a6c9d801d
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-06-23 09:51:08 -0300

    Alteração de Alias

--- Views LN/VW_NFR_Rascunho_Cab.sql
+++ Views LN/VW_NFR_Rascunho_Cab.sql
@@ -4,7 +4,6 @@
 --************************************************************************************************************************************************************
 SELECT 
 	brnfe940.t$fire$l NR_NF_RASCUNHO,
-	
 	brnfe941.t$opfc$l CD_NATUREZA_OPERACAO_COMPRA,
 	tcmcs940.t$opor$l SQ_NATUREZA_OPERACAO_COMPRA,
     201 CD_CIA,
@@ -13,20 +12,13 @@ SELECT
 	AND tcemm030.t$eunt=tcemm124.t$grid
 	AND tcemm124.t$loco=201
 	AND rownum=1) CD_FILIAL,
-	(SELECT tcemm124.t$grid FROM ttcemm124201 tcemm124, ttcemm030201 tcemm030
-	WHERE tcemm124.t$cwoc=tdpur400.t$cofc
-	AND tcemm124.t$loco=201
-	AND rownum=1) CD_UNIDADE_EMPRESARIAL,	
 	tdpur400.t$otbp CD_FORNECEDOR,	
-
-	
 	TO_CHAR(brnfe940.t$docn$l) NR_NF_REFERENCIA,															--#FAF.022.1.n																
 	brnfe940.t$seri$l NR_SERIE_NFR_REFERENCIA,											
 	brnfe940.t$idat$l DT_EMISSAO_NF,													
 --	brnfe940.t$iodt$l DT_SAIDA_NF,																			--#FAF.095.o
 	CASE WHEN brnfe940.t$iodt$l<TO_DATE('01-01-1990', 'DD-MM-YYYY') THEN
 	brnfe940.t$idat$l ELSE brnfe940.t$iodt$l END DT_SAIDA_NF,												--#FAF.095.n
-													
 	tdpur400.t$orno NR_PEDIDO_COMPRA,
 	brnfe940.t$gtam$l VL_PRODUTO,
 	brnfe940.t$gexp$l VL_DESPESA,
@@ -47,97 +39,62 @@ SELECT
 	WHERE i.t$fire$l=brnfe940.t$fire$l
 	AND i.t$brty$l=3) VL_IPI,
 	brnfe940.t$tfda$l VL_TOTAL,
-	
 	CAST((FROM_TZ(CAST(TO_CHAR(tdpur400.t$odat, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
 		AT time zone sessiontimezone) AS DATE) DT_GERACAO_REGISTRO,
-	
-	 
 	(select a.t$mess$c from tznnfe004201 a
 	 where a.t$fire$c=brnfe940.t$fire$l
 	 and a.t$mess$c!=' '
 	 and rownum=1) DS_SITUACAO_ANALISE,
-	
 	CAST((FROM_TZ(CAST(TO_CHAR(brnfe940.t$fcdt$c, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
 		AT time zone sessiontimezone) AS DATE) DT_ANALISE,	
-	
-
 	brnfe940.t$stpr$c CD_SITUACAO_RASCUNHO,																
 	nvl((select t.t$text from ttttxt010201 t 
-	where t$clan='p'
-	AND t.t$ctxt=brnfe940.t$obse$l
-	and rownum=1),' ') DS_OBSERVACAO,																	
+	where t$clan='p' AND t.t$ctxt=brnfe940.t$obse$l and rownum=1),' ') DS_OBSERVACAO,																	
 	(select ttdpur451201.t$logn
-	from ttdpur451201 
-	where ttdpur451201.t$orno=tdpur400.t$orno
+	from ttdpur451201 where ttdpur451201.t$orno=tdpur400.t$orno
 	and ttdpur451201.t$trdt=(select min(d.t$trdt)
 							 from ttdpur451201 d 
-							 where d.t$orno=ttdpur451201.t$orno)
-	and rownum=1) CD_USUARIO_INCLUSAO,
+							 where d.t$orno=ttdpur451201.t$orno) and rownum=1) CD_USUARIO_INCLUSAO,
 	(select min(ttdpur451201.t$trdt)
-	from ttdpur451201 
-	where ttdpur451201.t$orno=tdpur400.t$orno) DT_INCLUSAO,
+	from ttdpur451201 where ttdpur451201.t$orno=tdpur400.t$orno) DT_INCLUSAO,
 	(select ttdpur451201.t$logn
-	from ttdpur451201 
-	where ttdpur451201.t$orno=tdpur400.t$orno
+	from ttdpur451201 where ttdpur451201.t$orno=tdpur400.t$orno
 	and ttdpur451201.t$trdt=(select max(d.t$trdt)
 							 from ttdpur451201 d 
-							 where d.t$orno=ttdpur451201.t$orno)
-	and rownum=1) CD_USUARIO_ALTERACAO,
+							 where d.t$orno=ttdpur451201.t$orno) and rownum=1) CD_USUARIO_ALTERACAO,
 	(select max(ttdpur451201.t$trdt)
-	from ttdpur451201 
-	where ttdpur451201.t$orno=tdpur400.t$orno) DT_ALTERACAO,
+	from ttdpur451201 where ttdpur451201.t$orno=tdpur400.t$orno) DT_ALTERACAO,
 	(select ttdpur451201.t$logn
-	from ttdpur451201 
-	where ttdpur451201.t$orno=tdpur400.t$orno
+	from ttdpur451201 where ttdpur451201.t$orno=tdpur400.t$orno
 	and ttdpur451201.t$trdt=(select min(d.t$trdt)
 							 from ttdpur451201 d 
 							 where d.t$orno=ttdpur451201.t$orno
-							 and d.t$clyn=1)
-	and rownum=1
-	and ttdpur451201.t$clyn=1) CD_USUARIO_CANCELAMENTO,
+							 and d.t$clyn=1) and rownum=1 and ttdpur451201.t$clyn=1) CD_USUARIO_CANCELAMENTO,
 	(select min(ttdpur451201.t$trdt)
-	from ttdpur451201 
-	where ttdpur451201.t$orno=tdpur400.t$orno
-	and ttdpur451201.t$clyn=1) DT_CANCELAMENTO,
+	from ttdpur451201 where ttdpur451201.t$orno=tdpur400.t$orno and ttdpur451201.t$clyn=1) DT_CANCELAMENTO,
 	brnfe940.t$opor$l ESPECIE_NOTA_FISCAL_RECEBIDA,														
 	brnfe940.t$frec$l NR_NOTA_RECEBIMENTO,																
 	tdpur400.t$cwar CD_DEPOSITO,
 	tdpur400.t$cpay CD_CONDICAO_PAGAMENTO,
 	nvl((SELECT SUM(i.t$amnt$l)
-	FROM tbrnfe942201 i
-	WHERE i.t$fire$l=brnfe940.t$fire$l
-	AND i.t$brty$l=16),0) VL_IMPOSTO_IMPORTACAO,
+	FROM tbrnfe942201 i WHERE i.t$fire$l=brnfe940.t$fire$l AND i.t$brty$l=16),0) VL_IMPOSTO_IMPORTACAO,
 	nvl((select sum(r.T$CCHR$L) FROM ttdrec941201 r
-  where r.t$fire$l=brnfe940.t$frec$l),0)   VL_DESPESA_ADUANEIRA,
-  
-
+		where r.t$fire$l=brnfe940.t$frec$l),0) VL_DESPESA_ADUANEIRA,
 	CASE WHEN tdpur400.t$rfdt$l=37 THEN brnfe940.t$addc$l ELSE 0 END VL_ADICIONAL_IMPORTACAO,
-
+	CASE WHEN tdpur400.t$rfdt$l=37 THEN 
+		(SELECT SUM(i.t$amnt$l) FROM tbrnfe942201 i
+			WHERE i.t$fire$l=brnfe940.t$fire$l AND i.t$brty$l=5) ELSE 0 END VL_PIS_IMPORTACAO,
 	CASE WHEN tdpur400.t$rfdt$l=37 THEN
-	(SELECT SUM(i.t$amnt$l)
-	FROM tbrnfe942201 i
-	WHERE i.t$fire$l=brnfe940.t$fire$l
-	AND i.t$brty$l=5)
-	ELSE 0 END VL_PIS_IMPORTACAO,
-	
-	CASE WHEN tdpur400.t$rfdt$l=37 THEN
-	(SELECT SUM(i.t$amnt$l)
-	FROM tbrnfe942201 i
-	WHERE i.t$fire$l=brnfe940.t$fire$l
-	AND i.t$brty$l=6)
-	ELSE 0 END VL_COFINS_IMPORTACAO,	
-
-
+		(SELECT SUM(i.t$amnt$l) FROM tbrnfe942201 i
+			WHERE i.t$fire$l=brnfe940.t$fire$l AND i.t$brty$l=6) ELSE 0 END VL_COFINS_IMPORTACAO,	
 	CASE WHEN
-	nvl((select a.t$cdec from ttdpur400201 a
-		where a.t$orno=tdpur400.t$orno
-		and a.t$cdec='001'
-		and rownum=1),0)=0 THEN 0
-	ELSE brnfe940.t$fght$l
-	END VL_CIF_IMPORTACAO	
+		nvl((select a.t$cdec from ttdpur400201 a
+			where a.t$orno=tdpur400.t$orno and a.t$cdec='001' and rownum=1),0)=0 THEN 0
+				ELSE brnfe940.t$fght$l END VL_CIF_IMPORTACAO,
+	(SELECT tcemm124.t$grid FROM ttcemm124201 tcemm124, ttcemm030201 tcemm030
+		WHERE tcemm124.t$cwoc=tdpur400.t$cofc AND tcemm124.t$loco=201 AND rownum=1) CD_UNIDADE_EMPRESARIAL	
 FROM
 	tbrnfe940201 brnfe940,
-	
 	(select a.t$fire$l, a.t$opfc$l
 	from tbrnfe941201 a
 	where a.t$line$l=(	select min(b.t$line$l)
@@ -146,18 +103,13 @@ FROM
 											where c.t$fire$l=b.t$fire$l
 											and c.t$opfc$l!=' ')
 						and b.t$opfc$l!=' ')) brnfe941,
-	
 	ttcmcs940201 tcmcs940,
 	(select distinct tdpur400.t$orno, znnfe007.t$fire$c, tdpur400.t$cofc, tdpur400.t$otbp, znnfe007.t$refr$c, tdpur400.t$rfdt$l, tdpur400.t$cpay, tdpur400.t$cwar, tdpur400.t$odat
 	 from 	tznnfe007201 znnfe007,
 			ttdpur400201 tdpur400
 	 where tdpur400.t$orno=znnfe007.t$orno$c
 	 and znnfe007.t$oorg$c=80) tdpur400
-
-						 
 WHERE
     brnfe941.t$fire$l=brnfe940.t$fire$l
 AND	tcmcs940.t$ofso$l=brnfe941.t$opfc$l
 AND	tdpur400.t$fire$c=brnfe940.t$fire$l
-	
-
