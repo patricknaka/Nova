188d57e29a8d89cd5979cb108632c92c87acdcb6
fabioInfor <fabio.ferreira@infor.com>
2014-06-24 15:07:22 -0300

    #167 VW_DOM_SKU_EAN.sql
    Filtrar somete números

--- Views LN/VW_DOM_SKU_EAN.sql
+++ Views LN/VW_DOM_SKU_EAN.sql
@@ -1,4 +1,7 @@
+--	#FAF.167 - 20-jun-2014,	Fabio Ferreira,	Filtrar somente números
+--*********************************************************************************************************************************************************
 SELECT znibd001.t$eanc$c CD_EAN,
        ltrim(rtrim(znibd001.t$item$c)) CD_ITEM
 FROM   tznibd001201 znibd001
+WHERE REGEXP_INSTR (znibd001.t$eanc$c, '[^[:digit:]]')=0
 order by 1
