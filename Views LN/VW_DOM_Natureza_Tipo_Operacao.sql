0dd47dcabc6a9b3cea29a78566c3054af79b6d65
fabioInfor <fabio.ferreira@infor.com>
2014-06-27 20:00:41 -0300

    #182 VW_DOM_Natureza_Tipo_Operacao
    Correcao

--- Views LN/VW_DOM_Natureza_Tipo_Operacao.sql
+++ Views LN/VW_DOM_Natureza_Tipo_Operacao.sql
@@ -1,3 +1,5 @@
+--	FAF.182 - 27-jun-2014, Fabio Ferreira, 	Correção descrição dominio
+--*******************************************************************************************************************************************
 SELECT 	DISTINCT			
 	  tcmcs940.t$ofso$l CD_NATUREZA_OPERACAO,
 	  tcmcs940.t$opor$l SQ_NATUREZA_OPERACAO,
@@ -5,18 +7,22 @@ SELECT 	DISTINCT
 	  dominio.DESCR DS_TIPO_OPERACAO
 FROM  ttcmcs940201 tcmcs940,
       ttcmcs947201 tcmcs947,      
-      (SELECT d.t$cnst COD, l.t$desc DESCR,'NR' MODULO FROM tttadv401000 d, tttadv140000 l
-      WHERE d.t$cpac='ci'
-      AND d.t$cdom='sli.tdff.l'
-      AND d.t$vers='B61U'
-      AND d.t$rele='a7'
-      AND d.t$cust='glo1'
-      AND l.t$clab=d.t$za_clab
-      AND l.t$clan='p'
-      AND l.t$cpac='ci'
-      AND l.t$vers='B61U'
-      AND l.t$rele='a7'
-      AND l.t$cust='glo1') dominio
+      (SELECT d.t$cnst COD,
+       l.t$desc DESCR,
+	   'NF' CD_MODULO
+		FROM tttadv401000 d,
+			 tttadv140000 l
+		WHERE d.t$cpac='ci'
+		AND d.t$cdom='sli.tdff.l'
+		AND d.t$vers='B61U'
+		AND d.t$rele='a7'
+		AND d.t$cust='glo1'
+		AND l.t$clab=d.t$za_clab
+		AND l.t$clan='p'
+		AND l.t$cpac='ci'
+		AND l.t$vers='B61U'
+		AND l.t$rele='a7'
+		AND l.t$cust='glo1') dominio
 WHERE	tcmcs947.t$cfoc$l=tcmcs940.t$ofso$l
 AND    	dominio.COD=tcmcs947.t$rfdt$l
 order by 1,2,3
