e8d3e01ab819e8ff3cfcc11298528328b52152b6
fabioInfor <fabio.ferreira@infor.com>
2014-07-02 15:35:51 -0300

    #186 VW_CAP_Titulo_Mvmt
    Correcao

--- Views LN/VW_CAP_Titulo_Mvmt.sql
+++ Views LN/VW_CAP_Titulo_Mvmt.sql
@@ -7,12 +7,14 @@
 -- #FAF.168 - 	24-jun-2014, Fabio Ferreira, 	Correção registros duplicados
 -- #FAF.188 - 	30-jun-2014, Fabio Ferreira, 	Correção alias e inclusão do número da programação
 -- #FAF.192 - 	02-jul-2014, Fabio Ferreira, 	Correção duplicidade devido a referência com pagamento previsto
+-- #FAF.186.2 - 02-jul-2014, Fabio Ferreira, 	Correção duplicidade
 --****************************************************************************************************************************************************************
 
 SELECT DISTINCT
 	201 CD_CIA,																									--#FAF.113.n
 --	nvl(r.t$lino, tfacP200.t$lino) NR_MOVIMENTO,																--#FAF.188.o
-	nvl(r.t$lino, tfacP200.t$lino) SQ_MOVIMENTO,																--#FAF.188.n
+--	nvl(r.t$lino, tfacP200.t$lino) SQ_MOVIMENTO,																--#FAF.188.n	--#FAF.186.2.o
+	tfacP200.t$lino SQ_MOVIMENTO,																				--#FAF.186.2.n
 	tfacp200.t$ninv NR_TITULO,
     tfacp200.t$ttyp CD_TRANSACAO_TITULO,
 	'CAP' CD_MODULO,																		
@@ -24,15 +26,26 @@ SELECT DISTINCT
 	tfacp200.t$docd DT_TRANSACAO,
 	CASE WHEN tfacp200.t$amth$1<0 THEN '-' ELSE '+' END IN_ENTRADA_SAIDA,
 	'CAP' CD_MODULO_REFERENCIA, 
-	nvl((select znacp004.t$ttyp$c || znacp004.t$ninv$c from BAANDB.tznacp004201 znacp004				--#FAF.002.sn
+	nvl((select znacp004.t$ttyp$c || znacp004.t$ninv$c from BAANDB.tznacp004201 znacp004						--#FAF.002.sn
 		 where znacp004.t$tty1$c=tfacp200.t$ttyp and znacp004.t$nin1$c=tfacp200.t$ninv
-		 and rownum=1), r.t$ttyp || r.t$ninv) NR_TITULO_REFERENCIA,												--#FAF.002.en
+		 and rownum=1), 
+		(select  rs.t$ttyp || rs.t$ninv from ttfacp200201 rs
+		 where rs.t$tdoc=tfacp200.t$tdoc
+		 and rs.t$docn=tfacp200.t$docn
+		 and rs.t$ttyp!=tfacp200.t$ttyp
+		 and rs.t$ninv!=tfacp200.t$ninv
+		 and rs.t$tpay!=8
+		 and rs.t$amnt=tfacp200.t$amnt*-1
+		 and rownum=1)
+--		 r.t$ttyp || r.t$ninv																					--#FAF.186.2.o
+											) NR_TITULO_REFERENCIA,												--#FAF.002.en
 	--(select t.t$stap from ttfacp200201 t 
 	--	where t.t$ttyp=tfacp200.t$ttyp and t.t$ninv=tfacp200.t$ninv and t.t$docn=0) CD_SITUACAO_MOVIMENTO,
 	(select CAST((FROM_TZ(CAST(TO_CHAR(max(s.t$sdat), 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
 			AT time zone sessiontimezone) AS DATE) from ttfacp600201 s 
 		where s.t$payt=tfacp200.t$tdoc and s.t$payd=tfacp200.t$docn) DT_SITUACAO,
-	nvl(r.t$amth$1, tfacp200.t$amth$1) VL_TRANSACAO,
+--	nvl(r.t$amth$1, tfacp200.t$amth$1) VL_TRANSACAO,															--#FAF.186.2.o
+	tfacp200.t$amth$1 VL_TRANSACAO,																				--#FAF.186.2.n
 	CASE WHEN tfacp200.t$rcd_utc<TO_DATE('1990-01-01', 'YYYY-MM-DD') THEN tfacp200.t$rcd_utc					--#FAF.003.en
 	ELSE	CAST((FROM_TZ(CAST(TO_CHAR(tfacp200.t$rcd_utc, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 	
 				AT time zone sessiontimezone) AS DATE) END DT_ATUALIZACAO,
@@ -48,11 +61,12 @@ SELECT DISTINCT
 --        AT time zone sessiontimezone) AS DATE) DT_HR_ATUALIZACAO										--#FAF.003.eo
 FROM
 	ttfacp200201 tfacp200
-  LEFT JOIN (select distinct rs.t$ttyp, rs.t$ninv, rs.t$tdoc, rs.t$docn, rs.t$lino, rs.t$amth$1, rs.t$tpay from ttfacp200201 rs) r
-  ON r.t$tdoc=tfacp200.t$tdoc 
-  and r.t$docn=tfacp200.t$docn
-  and r.t$ttyp!=tfacp200.t$ttyp
-  and r.t$ninv!=tfacp200.t$ninv
-  and r.t$tpay!=8																								--#FAF.192.n
+																																--#FAF.186.2.so
+--  LEFT JOIN (select distinct rs.t$ttyp, rs.t$ninv, rs.t$tdoc, rs.t$docn, rs.t$lino, rs.t$amth$1, rs.t$tpay from ttfacp200201 rs) r
+--  ON r.t$tdoc=tfacp200.t$tdoc 
+--  and r.t$docn=tfacp200.t$docn
+--  and r.t$ttyp!=tfacp200.t$ttyp
+--  and r.t$ninv!=tfacp200.t$ninv
+--  and r.t$tpay!=8																								--#FAF.192.n	--#FAF.186.2.eo
 WHERE tfacp200.t$docn>0
 --AND tfacp200.t$ttyp || tfacp200.t$ninv='PNG6'
