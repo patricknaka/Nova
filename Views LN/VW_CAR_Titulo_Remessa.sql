22da794027361790f207183026c67b39cb856a2f
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-07-04 08:43:39 -0300

    Alteração de Alias

--- Views LN/VW_CAR_Titulo_Remessa.sql
+++ Views LN/VW_CAR_Titulo_Remessa.sql
@@ -2,9 +2,9 @@ SELECT    tfcmg401.t$ttyp || tfcmg401.t$ninv CD_CHAVE_PRIMARIA,
           tfcmg401.t$btno NR_REMESSA,
           tfcmg409.t$date DT_REMESSA,
           tfcmg401.t$schn NR_PROGRAMACAO,
-          tfacr201.t$liqd DT_PREV_REC,
-          tfcmg401.t$paym CD_METODO_REC,
-          tfacr201.t$rpst$l CD_STAT_REC
+          tfacr201.t$liqd DT_PREVISTA_RECEBIMENTO,
+          tfcmg401.t$paym CD_METODO_RECEBIMENTO,
+          tfacr201.t$rpst$l CD_STATUS_RECEBIMENTO
 
 FROM        ttfcmg401201 tfcmg401
 INNER JOIN  ttfcmg409201 tfcmg409 ON  tfcmg409.t$btno=tfcmg401.t$btno
