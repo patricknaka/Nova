e93df5c2ef7e1655af0ae89c70c3efa96ed90746
fabioInfor <fabio.ferreira@infor.com>
2014-07-04 11:25:38 -0300

    #204 VW_CAR_Titulo_Remessa
    Adicionado campo CD_MODULO

--- Views LN/VW_CAR_Titulo_Remessa.sql
+++ Views LN/VW_CAR_Titulo_Remessa.sql
@@ -1,10 +1,14 @@
+-- #FAF.204 - 04-jul-2014, Fabio Ferreira, 	Adicionado campo CD_MODULO
+--***************************************************************************************************************************************************
+
 SELECT    tfcmg401.t$ttyp || tfcmg401.t$ninv CD_CHAVE_PRIMARIA,
           tfcmg401.t$btno NR_REMESSA,
           tfcmg409.t$date DT_REMESSA,
           tfcmg401.t$schn NR_PROGRAMACAO,
           tfacr201.t$liqd DT_PREVISTA_RECEBIMENTO,
           tfcmg401.t$paym CD_METODO_RECEBIMENTO,
-          tfacr201.t$rpst$l CD_STATUS_RECEBIMENTO
+          tfacr201.t$rpst$l CD_STATUS_RECEBIMENTO,
+		  'CAR' CD_MODULO
 
 FROM        ttfcmg401201 tfcmg401
 INNER JOIN  ttfcmg409201 tfcmg409 ON  tfcmg409.t$btno=tfcmg401.t$btno
