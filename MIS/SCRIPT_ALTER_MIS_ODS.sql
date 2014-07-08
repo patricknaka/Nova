1adf8e4b28b6ac9aa8d8d9832ca3c7303b219103
rodrigocarvm <rodrigo.marcelino@novapontocom.com.br>
2014-07-08 10:38:51 -0300

    Criação ODS do Arquivo Samsung Atacado

--- MIS/SCRIPT_ALTER_MIS_ODS.sql
+++ MIS/SCRIPT_ALTER_MIS_ODS.sql
@@ -185,3 +185,168 @@ VALUES(1,1,1),(2,1,19),(4,1,8),(5,1,5),(6,1,15),(7,1,12),(8,1,9),(9,1,17),(11,1,
 (1,2,2),(2,2,20),(4,2,7),(5,2,6),(6,2,16),(7,2,13),(8,2,10),(9,2,18),(11,2,22),(12,2,23),(13,2,25),(14,1,28),(14,2,27)
 
 -------------------------------------------------------------------------------------------------------------------------------------------------
+
+USE [mis_ods]
+GO
+
+/****** Object:  Table [com].[ods_cliente_asm_samsung]    Script Date: 07/08/2014 10:34:41 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[com].[ods_cliente_asm_samsung]') AND type in (N'U'))
+DROP TABLE [com].[ods_cliente_asm_samsung]
+GO
+
+USE [mis_ods]
+GO
+
+/****** Object:  Table [com].[ods_cliente_asm_samsung]    Script Date: 07/08/2014 10:34:41 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [com].[ods_cliente_asm_samsung](
+	[ds_cnpj_cliente] [varchar](14) NOT NULL,
+	[ds_cpf] [varchar](11) NOT NULL,
+	[nr_ddd] [char](2) NOT NULL,
+	[nr_telefone] [varchar](8) NOT NULL,
+	[nr_cep] [int] NOT NULL,
+	[ds_uf] [char](2) NOT NULL,
+	[ds_cidade] [varchar](50) NOT NULL,
+	[ds_endereco] [varchar](100) NOT NULL,
+	[ds_cliente] [varchar](50) NOT NULL,
+	[ds_cnpj_distribuidor] [varchar](14) NOT NULL,
+	[ds_email] [varchar](50) NULL,
+	[ds_classificacao_venda] [varchar](100) NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
+USE [mis_ods]
+GO
+
+/****** Object:  Table [com].[ods_estoque_asm_samsung]    Script Date: 07/08/2014 10:34:52 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[com].[ods_estoque_asm_samsung]') AND type in (N'U'))
+DROP TABLE [com].[ods_estoque_asm_samsung]
+GO
+
+USE [mis_ods]
+GO
+
+/****** Object:  Table [com].[ods_estoque_asm_samsung]    Script Date: 07/08/2014 10:34:52 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [com].[ods_estoque_asm_samsung](
+	[nr_ean13] [bigint] NOT NULL,
+	[nr_data] [int] NOT NULL,
+	[ds_cnpj_distribuidor] [varchar](14) NOT NULL,
+	[nr_qtde_estoque] [int] NOT NULL,
+	[ds_tipo_estoque] [char](2) NOT NULL,
+	[ds_part_number_prod] [varchar](20) NOT NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
+USE [mis_ods]
+GO
+
+/****** Object:  Table [com].[ods_vendas_asm_samsung]    Script Date: 07/08/2014 10:35:02 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[com].[ods_vendas_asm_samsung]') AND type in (N'U'))
+DROP TABLE [com].[ods_vendas_asm_samsung]
+GO
+
+USE [mis_ods]
+GO
+
+/****** Object:  Table [com].[ods_vendas_asm_samsung]    Script Date: 07/08/2014 10:35:02 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [com].[ods_vendas_asm_samsung](
+	[nr_nota_fiscal] [bigint] NOT NULL,
+	[nr_ean13] [bigint] NOT NULL,
+	[nr_data_emissao] [int] NOT NULL,
+	[ds_cnpj_distribuidor] [varchar](14) NOT NULL,
+	[nr_cod_vendedor] [int] NOT NULL,
+	[ds_cpf_cliente] [varchar](11) NOT NULL,
+	[ds_cnpj_cliente] [varchar](14) NOT NULL,
+	[ds_tipo_faturamento] [char](2) NOT NULL,
+	[nr_qtde_venda] [int] NOT NULL,
+	[nr_vlr_unid] [numeric](15, 2) NOT NULL,
+	[nr_vlr_bruto] [numeric](15, 2) NOT NULL,
+	[nr_vlr_liquido] [numeric](15, 2) NOT NULL,
+	[nr_percentual_icms] [numeric](4, 2) NOT NULL,
+	[ds_tipo_movimento] [char](2) NOT NULL,
+	[ds_part_number_prod] [varchar](20) NOT NULL,
+ CONSTRAINT [PK_ods_vendas_asm_samsung] PRIMARY KEY CLUSTERED 
+(
+	[nr_nota_fiscal] ASC,
+	[nr_ean13] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
+USE [mis_ods]
+GO
+
+/****** Object:  Table [com].[ods_vendedor_asm_samsung]    Script Date: 07/08/2014 10:35:13 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[com].[ods_vendedor_asm_samsung]') AND type in (N'U'))
+DROP TABLE [com].[ods_vendedor_asm_samsung]
+GO
+
+USE [mis_ods]
+GO
+
+/****** Object:  Table [com].[ods_vendedor_asm_samsung]    Script Date: 07/08/2014 10:35:13 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [com].[ods_vendedor_asm_samsung](
+	[nr_cod_vendedor] [bigint] NOT NULL,
+	[ds_nome_vendedor] [varchar](50) NOT NULL,
+	[ds_cnpj_distribuidor] [varchar](14) NOT NULL,
+ CONSTRAINT [PK_ods_vendedor_asm_samsung] PRIMARY KEY CLUSTERED 
+(
+	[nr_cod_vendedor] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
