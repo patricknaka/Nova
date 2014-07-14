cb9c6b6558b97b9e1153d9d68680c3958666dc44
marcusmsv <marcus_msv@yahoo.com.br>
2014-07-14 17:48:24 -0300

    Alteração MIS_DW

--- MIS/SCRIPT_ALTER_MIS_DW.sql
+++ MIS/SCRIPT_ALTER_MIS_DW.sql
@@ -357,6 +357,8 @@ CREATE NONCLUSTERED INDEX [IDX_0] ON [dbo].[ods_sige_cmv_hist]
 	[ID_ITEM] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 
+alter table aux_sige_cmv_hist
+alter column nr_cia numeric(3)
 
 
 --------------------------------------------------------------------
