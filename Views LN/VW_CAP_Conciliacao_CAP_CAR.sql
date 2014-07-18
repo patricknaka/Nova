bc18417e86a5b4d016a115dc79ed5592a6b33b8a
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-07-18 08:15:33 -0300

    Alteração nome View e atributos

--- Views LN/VW_CAP_Conciliacao_CAP_CAR.sql
+++ Views LN/VW_CAP_Conciliacao_CAP_CAR.sql
@@ -4,10 +4,10 @@ SELECT
         znacr017.T$TINV$C CD_TRANSACAO,
         znacr017.T$NINV$C NR_DOCUMENTO,
         SUM(znacr017.T$AMNT$C * CASE WHEN znacr017.T$TRAN$C=2 THEN -1
-                            ELSE 1 END)  VALOR,
+                            ELSE 1 END)  VL_VALOR,
         znacr017.T$TINR$C CD_TP_TRANSACAO_REVERSAO,
         znacr017.T$NINR$C NR_TRANSACAO_REVERSAO,
-        znacr017.T$TINV$C || znacr017.T$NINV$C CHAVE
+        znacr017.T$TINV$C || znacr017.T$NINV$C CD_CHAVE_PRIMARIA
 FROM    BAANDB.tznacr017201 znacr017
 GROUP BY 		znacr017.t$ttyp$c,
             znacr017.t$docn$c,
