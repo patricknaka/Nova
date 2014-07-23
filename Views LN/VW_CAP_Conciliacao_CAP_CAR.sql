efb68020fd84af5d9dd36bd45604b65f903b00da
fabioInfor <fabio.ferreira@infor.com>
2014-07-23 13:50:17 -0300

    #229 Inclusão de campos

--- Views LN/VW_CAP_Conciliacao_CAP_CAR.sql
+++ Views LN/VW_CAP_Conciliacao_CAP_CAR.sql
@@ -1,3 +1,5 @@
+-- 	FAF.229 - 23-jul-2014, Fabio Ferreira,	Inclusão dos campos data de criação do doc de baixa e reversão
+--****************************************************************************************************************************************************************
 SELECT  
         znacr017.t$ttyp$c CD_TP_TRANSACAO_BAIXA,
         znacr017.t$docn$c NR_TRANSACAO_BAIXA,
@@ -7,11 +9,21 @@ SELECT
                             ELSE 1 END)  VL_VALOR,
         znacr017.T$TINR$C CD_TP_TRANSACAO_REVERSAO,
         znacr017.T$NINR$C NR_TRANSACAO_REVERSAO,
-        znacr017.T$TINV$C || znacr017.T$NINV$C CD_CHAVE_PRIMARIA
-FROM    BAANDB.tznacr017201 znacr017
+        znacr017.T$TINV$C || znacr017.T$NINV$C CD_CHAVE_PRIMARIA,
+        tfgld018b.T$DCDT DT_BAIXA,												--#FAF.229.n
+        tfgld018b.T$DCDT DT_REVERSAO											--#FAF.229.n
+FROM    BAANDB.tznacr017201 znacr017,
+        BAANDB.ttfgld018201 tfgld018b,											--#FAF.229.n
+        BAANDB.ttfgld018201 tfgld018r											--#FAF.229.n
+WHERE   tfgld018b.T$TTYP=znacr017.T$TTYP$C										--#FAF.229.sn
+AND     tfgld018b.T$DOCN=znacr017.T$DOCN$C
+AND     tfgld018r.T$TTYP=znacr017.T$TINR$C
+AND     tfgld018r.T$DOCN=znacr017.T$NINR$C										--#FAF.229.en
 GROUP BY 		znacr017.t$ttyp$c,
             znacr017.t$docn$c,
             znacr017.T$TINV$C,
             znacr017.T$NINV$C,
             znacr017.T$TINR$C,
-            znacr017.T$NINR$C
+            znacr017.T$NINR$C,
+            tfgld018b.T$DCDT,													--#FAF.229.n	
+            tfgld018b.T$DCDT													--#FAF.229.n
