43c1f3e40fa99793f21dce26fd767dab7151f670
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-07-25 14:38:57 -0300

    Correção de Alias

--- Views LN/VW_CAP_Conciliacao_CAP_CAR.sql
+++ Views LN/VW_CAP_Conciliacao_CAP_CAR.sql
@@ -2,16 +2,6 @@
 -- 	FAF.229 - 25-jul-2014, Fabio Ferreira,	Inclusão código do parceiro
 --****************************************************************************************************************************************************************
 SELECT  
-        nvl((select a.t$itbp 
-            from BAANDB.ttfacr200201 a
-            where a.t$tdoc = znacr017.t$ttyp$c
-            and a.t$docn = znacr017.t$docn$c
-            and rownum=1),
-            (select a.t$ifbp 
-            from BAANDB.ttfacp200201 a
-            where a.t$tdoc = znacr017.t$ttyp$c
-            and a.t$docn = znacr017.t$docn$c
-            and rownum=1))  PARCEIRO,          
         znacr017.t$ttyp$c CD_TP_TRANSACAO_BAIXA,
         znacr017.t$docn$c NR_TRANSACAO_BAIXA,
         znacr017.T$TINV$C CD_TRANSACAO,
@@ -22,7 +12,17 @@ SELECT
         znacr017.T$NINR$C NR_TRANSACAO_REVERSAO,
         znacr017.T$TINV$C || znacr017.T$NINV$C CD_CHAVE_PRIMARIA,
         tfgld018b.T$DCDT DT_BAIXA,												--#FAF.229.n
-        tfgld018r.T$DCDT DT_REVERSAO											--#FAF.229.n
+        tfgld018r.T$DCDT DT_REVERSAO,											--#FAF.229.n
+		nvl((select a.t$itbp 
+			from BAANDB.ttfacr200201 a
+			where a.t$tdoc = znacr017.t$ttyp$c
+			and a.t$docn = znacr017.t$docn$c
+			and rownum=1),
+			(select a.t$ifbp 
+			from BAANDB.ttfacp200201 a
+			where a.t$tdoc = znacr017.t$ttyp$c
+			and a.t$docn = znacr017.t$docn$c
+			and rownum=1)) CD_PARCEIRO  
 FROM      BAANDB.tznacr017201 znacr017
 LEFT JOIN BAANDB.ttfgld018201 tfgld018b											--#FAF.229.sn
           ON    tfgld018b.T$TTYP=znacr017.T$TTYP$C										
