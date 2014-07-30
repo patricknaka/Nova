fe10cd7669e58a59518a23f8d3fa67cc233a08a7
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-07-30 10:05:14 -0300

    Atualização das views efetivamente utilizadas

--- Views LN/VW_DOM_WMS_MEGA_ROTA.sql
+++
@@ -1,2 +0,0 @@
-select a.t$crte CD_MEGA_ROTA, a.t$dsca DS_MEGA_ROTA 
-from baandb.ttcmcs004201 a
--- Views LN/VW_DOM_WMS_Ponto_Controle.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    znmcs002.t$poco$c CD_PONTO,
-    znmcs002.t$desc$c DS_PONTO,
-    znmcs002.t$stor$c CD_AREA
-FROM baandb.tznmcs002201 znmcs002
-order by 1
