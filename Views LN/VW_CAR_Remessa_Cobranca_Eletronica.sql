24a612d194ae0971792fa76af4fd546edd912904
marcusmsv <marcus_msv@yahoo.com.br>
2014-08-06 13:11:02 -0300

    Alteração VIEW CAR

--- Views LN/VW_CAR_Remessa_Cobranca_Eletronica.sql
+++ Views LN/VW_CAR_Remessa_Cobranca_Eletronica.sql
@@ -6,7 +6,7 @@ SELECT DISTINCT																							--#FAF.224.o
 	tfcmg948.t$btno$l NR_REMESSA,
 	tfcmg948.t$occ2$l COD_COMANDO,
 	CONCAT(tfcmg948.t$ttyp$l, TO_CHAR(tfcmg948.t$ninv$l)) NR_TITULO,
-	'ACR' CD_MODALIDADE,
+	'CAR' CD_MODALIDADE,
 	tfcmg948.t$stat$l SITUACAO,
 	CAST((FROM_TZ(CAST(TO_CHAR(tfcmg948.t$lach$l, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
 			AT time zone sessiontimezone) AS DATE)  DT_ATUALIZACAO
