095e38b8aa7b2a238f2f5de9bed5c8efdeb9868d
marcusmsv <marcus_msv@yahoo.com.br>
2014-08-11 13:47:08 -0300

    Alteração Label - VW_DOM_CAR_Comando

--- Views LN/VW_DOM_CAR_Comando.sql
+++ Views LN/VW_DOM_CAR_Comando.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT
-	tfcmg938.t$ocod$l CODIGO_OCORRENCIA,
-	tfcmg938.t$odes$l DESCRICAO
+	tfcmg938.t$ocod$l CD_COMANDO,
+	tfcmg938.t$odes$l DS_COMANDO
 FROM
 	baandb.ttfcmg938201 tfcmg938
