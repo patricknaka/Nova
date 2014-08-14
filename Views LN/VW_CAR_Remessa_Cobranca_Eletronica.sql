5188cb32be73b07c8f9f349a383a26befb5ce4c9
marcusmsv <marcus_msv@yahoo.com.br>
2014-08-14 13:11:20 -0300

    Update VW_CAR_Remessa_Cobranca_Eletronica.sql

--- Views LN/VW_CAR_Remessa_Cobranca_Eletronica.sql
+++ Views LN/VW_CAR_Remessa_Cobranca_Eletronica.sql
@@ -1,11 +1,11 @@
---	#FAF.224 - 14-jul-2014,	Fabio Ferreira,	Corrção de duplicidade
+--	#FAF.224 - 14-jul-2014,	Fabio Ferreira,	CorrÃ§Ã£o de duplicidade
 --****************************************************************************************************************************************************************
 --SELECT																								--#FAF.224.o
 SELECT DISTINCT																							--#FAF.224.o
 	tfcmg948.t$bank$l CD_BANCO,
 	tfcmg948.t$btno$l NR_REMESSA,
 	tfcmg948.t$occ2$l CD_COMANDO,
-	CONCAT(tfcmg948.t$ttyp$l, TO_CHAR(tfcmg948.t$ninv$l)) NR_TITULO,
+	CONCAT(tfcmg948.t$ttyp$l, TO_CHAR(tfcmg948.t$ninv$l)) CD_CHAVE_PRIMARIA,
 	'CAR' CD_MODALIDADE,
 	tfcmg948.t$stat$l CD_SITUACAO_PAGAMENTO,
 	CAST((FROM_TZ(CAST(TO_CHAR(tfcmg948.t$lach$l, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
