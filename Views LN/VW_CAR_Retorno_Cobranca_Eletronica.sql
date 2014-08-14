e2a1e7cef37d96260d8b5089039ae7dde929875a
marcusmsv <marcus_msv@yahoo.com.br>
2014-08-14 13:25:50 -0300

    Update VW_CAR_Retorno_Cobranca_Eletronica.sql

--- Views LN/VW_CAR_Retorno_Cobranca_Eletronica.sql
+++ Views LN/VW_CAR_Retorno_Cobranca_Eletronica.sql
@@ -3,9 +3,8 @@
 SELECT
 	tfcmg949.t$bank$l CD_BANCO,
 	tfcmg949.t$sern$l NR_RETORNO,
-	tfcmg949.t$seqf$l NM_NOME_ARQUIVO_RETORNO,
---	tfcmg949.t$ttyp$l + TO_CHAR(tfcmg949.t$ninv$l) NR_TITULO,									--#FAF.233.o
-	tfcmg949.t$ttyp$l || TO_CHAR(tfcmg949.t$ninv$l) NR_TITULO,									--#FAF.233.o
+	tfcmg949.t$seqf$l NM_ARQUIVO_RETORNO,
+	tfcmg949.t$ttyp$l || TO_CHAR(tfcmg949.t$ninv$l) CD_CHAVE_PRIMARIA,									--#FAF.233.o
 	tfcmg949.t$occu$l CD_OCORRENCIA,
 	tfcmg949.t$dare$l DT_OCORRENCIA,
 	tfcmg011.t$baoc$l NR_CONTA_CORRENTE,
@@ -17,7 +16,7 @@ SELECT
 	tfcmg949.t$disa$l VL_DESCONTO,
 	tfcmg949.t$paid$l VL_PAGO,
 	tfcmg949.t$inam$l VL_JUROS,
-	tfcmg949.t$acce$l SITUACAO
+	tfcmg949.t$acce$l CD_SITUACAO
 FROM
 	baandb.ttfcmg949201 tfcmg949
 	LEFT JOIN baandb.ttfcmg001201 tfcmg001
