8e01a4d4065f75f1de35d30444f8b2bb6ae2ff9b
fabioInfor <fabio.ferreira@infor.com>
2014-08-18 15:35:54 -0300

    #289 Inclusão do campo CD_BANDEIRA_SITE

--- Views LN/VW_DOM_Bandeira_Cartao.sql
+++ Views LN/VW_DOM_Bandeira_Cartao.sql
@@ -1,4 +1,7 @@
+--	FAF.289 - 18-aug-2014, Fabio Ferreira, 	Inclusão do código da bandeira no site
+--****************************************************************************************************************************************************************
 select a.T$BAND$C CD_BANDEIRA,
-        a.t$desc$c DS_BANDEIRA
+        a.t$desc$c DS_BANDEIRA,
+		a.t$bnds$c CD_BANDEIRA_SITE							--#FAF.289.n
 from baandb.tzncmg009201 a
 order by 1
