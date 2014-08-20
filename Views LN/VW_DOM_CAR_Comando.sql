0b7715edc4b9b37573b382300a737c30c4e93f77
fabioInfor <fabio.ferreira@infor.com>
2014-08-20 14:57:28 -0300

    #295 - Inclusão do TP_COMANDO

--- Views LN/VW_DOM_CAR_Comando.sql
+++ Views LN/VW_DOM_CAR_Comando.sql
@@ -1,5 +1,8 @@
+--	FAF.295 - 20-ago-2014, Fabio Ferreira, 	Inclusão do TP_COMANDO
+--****************************************************************************************************************************************************************
 SELECT DISTINCT
 	tfcmg938.t$ocod$l CD_COMANDO,
-	tfcmg938.t$odes$l DS_COMANDO
+	tfcmg938.t$odes$l DS_COMANDO,
+	case when tfcmg938.t$octp$l=1 then 'ENVIAR' else 'RETORNAR' end TP_COMANDO
 FROM
 	baandb.ttfcmg938201 tfcmg938
