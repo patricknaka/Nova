0a8cd6a2438d6b3f1c58fe0727cf53f2354261a9
marcusmsv <marcus_msv@yahoo.com.br>
2014-08-21 11:42:21 -0300

    Update VW_EST_Referencia.sql

--- Views LN/VW_EST_Referencia.sql
+++ Views LN/VW_EST_Referencia.sql
@@ -1,12 +1,11 @@
--- #FAF.283 - 18-ago-2014, Fabio Ferreira, Correção QT_FISICA
+-- #FAF.283 - 18-ago-2014, Fabio Ferreira, CorreÃ§Ã£o QT_FISICA
 --************************************************************************************************************************************************************
 SELECT  201 CD_CIA,
         tcemm030.t$euca CD_FILIAL,
         whina112.t$cwar CD_DEPOSITO,
         ltrim(rtrim(whina112.t$item)) CD_ITEM,
         tcmcs003.t$tpar$l CD_MODALIDADE,
---        sum(whina112.t$qstk) QT_FISICA,												--#FAF.283.o
-        sum(whina112.t$qskt) QT_FISICA,													--#FAF.283.n
+        sum(whina112.t$qstk) QT_FISICA,
 		tdrec947.t$rcno$l NR_NFR,
 		(SELECT 
 		 case when (max(whwmd215.t$qhnd) - max(whwmd215.t$qchd) - max(whwmd215.t$qnhd))=0 then 0
@@ -25,7 +24,6 @@ FROM    baandb.twhina112201 whina112,
         baandb.ttcemm030201 tcemm030,
         baandb.ttcmcs003201 tcmcs003,
 --        baandb.twhwmd200201 whwmd200,
-        baandb.twhinr110201 whinr110,
         baandb.ttdrec947201 tdrec947,
         baandb.ttdrec940201 tdrec940
 WHERE   tcemm112.t$loco = 201
@@ -33,12 +31,9 @@ AND     tcemm112.t$waid = whina112.t$cwar
 AND 	tcemm030.t$eunt = tcemm112.t$grid
 --AND		whwmd200.t$cwar = tcmcs003.t$cwar
 AND     tcmcs003.t$cwar = whina112.t$cwar
---AND		tdrec947.t$orno$l=whina112.t$orno
---AND		tdrec947.t$pono$l=whina112.t$pono
---AND		tdrec947.t$seqn$l=whina112.t$srnb
-AND   whina112.t$itid = whinr110.t$itid
-AND   whinr110.t$rcno = tdrec947.t$rcno$l
-AND   whinr110.t$rcln = tdrec947.t$rcln$l
+AND		tdrec947.t$orno$l=whina112.t$orno
+AND		tdrec947.t$pono$l=whina112.t$pono
+AND		tdrec947.t$seqn$l=whina112.t$srnb
 AND		tdrec940.t$fire$l=tdrec947.t$fire$l
 AND		(tdrec940.t$stat$l=4 OR tdrec940.t$stat$l=5)
 AND		tdrec940.t$rfdt$l not in (3,5,8,13,16,22,33)
