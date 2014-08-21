cba02b8d5cf802131f4d51856374de82cb94906e
marcusmsv <marcus_msv@yahoo.com.br>
2014-08-21 11:50:00 -0300

    Update VW_NFR_Rascunho_Cab.sql

--- Views LN/VW_NFR_Rascunho_Cab.sql
+++ Views LN/VW_NFR_Rascunho_Cab.sql
@@ -1,7 +1,7 @@
--- #FAF.022 - 27-mai-2014, Fabio Ferreira, 	Correções e alteração da origem da informação para os dados da pré-nota	
--- #FAF.095 - 29-mai-2014, Fabio Ferreira, 	Correções para 	DT_SAIDA_NF em branco
--- #FAF.022.1 - 03-jun-2014, Fabio Ferreira, 	Correções	
--- #FAF.235 - 	03-jun-2014, Fabio Ferreira, 	Correções Timezone	e retirado campo VL_DESCONTO_INCONDICIONAL		
+-- #FAF.022 - 27-mai-2014, Fabio Ferreira, 	CorreÃ§Ãµes e alteraÃ§Ã£o da origem da informaÃ§Ã£o para os dados da prÃ©-nota	
+-- #FAF.095 - 29-mai-2014, Fabio Ferreira, 	CorreÃ§Ãµes para 	DT_SAIDA_NF em branco
+-- #FAF.022.1 - 03-jun-2014, Fabio Ferreira, 	CorreÃ§Ãµes	
+-- #FAF.235 - 	03-jun-2014, Fabio Ferreira, 	CorreÃ§Ãµes Timezone	e retirado campo VL_DESCONTO_INCONDICIONAL		
 --************************************************************************************************************************************************************
 SELECT 
 	brnfe940.t$fire$l NR_NF_RASCUNHO,
@@ -17,7 +17,6 @@ SELECT
 	TO_CHAR(brnfe940.t$docn$l) NR_NF_REFERENCIA,															--#FAF.022.1.n																
 	brnfe940.t$seri$l NR_SERIE_NFR_REFERENCIA,											
 	brnfe940.t$idat$l DT_EMISSAO_NF,													
---	brnfe940.t$iodt$l DT_SAIDA_NF,																			--#FAF.095.o
 	CASE WHEN brnfe940.t$iodt$l<TO_DATE('01-01-1990', 'DD-MM-YYYY') THEN
 	brnfe940.t$idat$l ELSE brnfe940.t$iodt$l END DT_SAIDA_NF,												--#FAF.095.n
 	tdpur400.t$orno NR_PEDIDO_COMPRA,
@@ -26,7 +25,6 @@ SELECT
 	brnfe940.t$addc$l VL_DESCONTO,
 	brnfe940.t$fght$l VL_FRETE,
 	brnfe940.t$insr$l VL_SEGURO,
---	brnfe940.t$addc$l VL_DESCONTO_INCONDICIONAL,															--#FAF.235.o
 	(SELECT SUM(i.t$amnt$l)
 	FROM baandb.tbrnfe942201 i
 	WHERE i.t$fire$l=brnfe940.t$fire$l
@@ -40,14 +38,14 @@ SELECT
 	WHERE i.t$fire$l=brnfe940.t$fire$l
 	AND i.t$brty$l=3) VL_IPI,
 	brnfe940.t$tfda$l VL_TOTAL,
-	CAST((FROM_TZ(CAST(TO_CHAR(tdpur400.t$odat, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
-		AT time zone sessiontimezone) AS DATE) DT_GERACAO_REGISTRO,
+	CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdpur400.t$odat, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+    AT time zone sessiontimezone) AS DATE) DT_GERACAO_REGISTRO,
 	(select a.t$mess$c from baandb.tznnfe004201 a
 	 where a.t$fire$c=brnfe940.t$fire$l
 	 and a.t$mess$c!=' '
 	 and rownum=1) DS_SITUACAO_ANALISE,
-	CAST((FROM_TZ(CAST(TO_CHAR(brnfe940.t$fcdt$c, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
-		AT time zone sessiontimezone) AS DATE) DT_ANALISE,	
+    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(brnfe940.t$fcdt$c, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+    AT time zone sessiontimezone) AS DATE) DT_ANALISE,	
 	brnfe940.t$stpr$c CD_SITUACAO_RASCUNHO,																
 	nvl((select t.t$text from baandb.ttttxt010201 t 
 	where t$clan='p' AND t.t$ctxt=brnfe940.t$obse$l and rownum=1),' ') DS_OBSERVACAO,																	
@@ -57,8 +55,8 @@ SELECT
 							 from baandb.ttdpur451201 d 
 							 where d.t$orno=ttdpur451201.t$orno) and rownum=1) CD_USUARIO_INCLUSAO,
 	(select 
-	CAST((FROM_TZ(CAST(TO_CHAR(min(ttdpur451201.t$trdt), 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
-		AT time zone sessiontimezone) AS DATE)																	--#FAF.235.n
+	CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(min(ttdpur451201.t$trdt), 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+    AT time zone sessiontimezone) AS DATE)																	--#FAF.235.n
 	from baandb.ttdpur451201 where ttdpur451201.t$orno=tdpur400.t$orno) DT_INCLUSAO,
 	(select ttdpur451201.t$logn
 	from baandb.ttdpur451201 where ttdpur451201.t$orno=tdpur400.t$orno
@@ -66,8 +64,8 @@ SELECT
 							 from baandb.ttdpur451201 d 
 							 where d.t$orno=ttdpur451201.t$orno) and rownum=1) CD_USUARIO_ALTERACAO,
 	(select 
-	CAST((FROM_TZ(CAST(TO_CHAR(max(ttdpur451201.t$trdt), 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
-		AT time zone sessiontimezone) AS DATE)																	--#FAF.235.n	
+	CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(max(ttdpur451201.t$trdt), 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+    AT time zone sessiontimezone) AS DATE)																	--#FAF.235.n	
 	from baandb.ttdpur451201 where ttdpur451201.t$orno=tdpur400.t$orno) DT_ALTERACAO,
 	(select ttdpur451201.t$logn
 	from baandb.ttdpur451201 where ttdpur451201.t$orno=tdpur400.t$orno
@@ -76,8 +74,8 @@ SELECT
 							 where d.t$orno=ttdpur451201.t$orno
 							 and d.t$clyn=1) and rownum=1 and ttdpur451201.t$clyn=1) CD_USUARIO_CANCELAMENTO,
 	(select 
-	CAST((FROM_TZ(CAST(TO_CHAR(min(ttdpur451201.t$trdt), 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 
-		AT time zone sessiontimezone) AS DATE)																	--#FAF.235.n	
+	CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(min(ttdpur451201.t$trdt), 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+    AT time zone sessiontimezone) AS DATE)																	--#FAF.235.n	
 	from baandb.ttdpur451201 where ttdpur451201.t$orno=tdpur400.t$orno and ttdpur451201.t$clyn=1) DT_CANCELAMENTO,
 	brnfe940.t$opor$l ESPECIE_NOTA_FISCAL_RECEBIDA,														
 	brnfe940.t$frec$l NR_NOTA_RECEBIMENTO,																
@@ -110,7 +108,7 @@ FROM
 											where c.t$fire$l=b.t$fire$l
 											and c.t$opfc$l!=' ')
 						and b.t$opfc$l!=' ')) brnfe941,
-	ttcmcs940201 tcmcs940,
+	baandb.ttcmcs940201 tcmcs940,
 	(select distinct tdpur400.t$orno, znnfe007.t$fire$c, tdpur400.t$cofc, tdpur400.t$otbp, znnfe007.t$refr$c, tdpur400.t$rfdt$l, tdpur400.t$cpay, tdpur400.t$cwar, tdpur400.t$odat
 	 from 	baandb.tznnfe007201 znnfe007,
           baandb.ttdpur400201 tdpur400
