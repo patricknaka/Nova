291342f130c0a38713b5395e37f782d1a3a6ce95
Marcia-Amador <marcia@globalti.com.br>
2014-08-22 17:38:23 -0300

    Update VW_DOM_Parceiro_Tipo_Cadastro.sql

--- Views LN/VW_DOM_Parceiro_Tipo_Cadastro.sql
+++ Views LN/VW_DOM_Parceiro_Tipo_Cadastro.sql
@@ -4,15 +4,20 @@ FROM baandb.tttadv401000 d,
      baandb.tttadv140000 l
 WHERE d.t$cpac='tc'
 AND d.t$cdom='bprl'
-AND d.t$vers='B61U'
-AND d.t$rele='a'
-AND d.t$cust='stnd'
+AND rpad(d.t$vers,4) || rpad(d.t$rele,2) || rpad(d.t$cust,4)=
+                                     (select max(rpad(l1.t$vers,4) || rpad(l1.t$rele, 2) || rpad(l1.t$cust,4) ) 
+                                      from baandb.tttadv401000 l1 
+                                      where l1.t$cpac=d.t$cpac 
+                                      AND l1.t$cdom=d.t$cdom)
 AND l.t$clab=d.t$za_clab
 AND l.t$clan='p'
 AND l.t$cpac='tc'
-AND l.t$vers='B61U'
-AND l.t$rele='a'
-AND l.t$cust='stnd'
+AND rpad(l.t$vers,4) || rpad(l.t$rele,2) || rpad(l.t$cust,4)=
+                                    (select max(rpad(l1.t$vers,4) || rpad(l1.t$rele,2) || rpad(l1.t$cust,4) ) 
+                                      from baandb.tttadv140000 l1 
+                                      where l1.t$clab=l.t$clab 
+                                      AND l1.t$clan=l.t$clan 
+                                      AND l1.t$cpac=l.t$cpac)
 UNION SELECT 10, 'Transportador' FROM Dual
 UNION SELECT 11, 'Fabricante' FROM Dual
 order by 1
