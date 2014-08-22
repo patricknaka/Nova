b9df356710b45af95aca06050235f60ee1e7cfce
Marcia-Amador <marcia@globalti.com.br>
2014-08-22 16:21:01 -0300

    Update VW_DOM_Tipo_Operacao.sql

--- Views LN/VW_DOM_Tipo_Operacao.sql
+++ Views LN/VW_DOM_Tipo_Operacao.sql
@@ -10,16 +10,21 @@ FROM baandb.tttadv401000 d,
 WHERE d.t$cpac='td'													--#FAF.008.n
 --AND d.t$cdom='sli.tdff.l'											--#FAF.008.o
 AND d.t$cdom='rec.trfd.l'											--#FAF.008.n
-AND d.t$vers='B61U'
-AND d.t$rele='a7'
-AND d.t$cust='glo1'
+AND rpad(d.t$vers,4) || rpad(d.t$rele,2) || rpad(d.t$cust,4)=
+                                     (select max(rpad(l1.t$vers,4) || rpad(l1.t$rele, 2) || rpad(l1.t$cust,4) ) 
+                                      from baandb.tttadv401000 l1 
+                                      where l1.t$cpac=d.t$cpac 
+                                      AND l1.t$cdom=d.t$cdom)
 AND l.t$clab=d.t$za_clab
 AND l.t$clan='p'
 --AND l.t$cpac='tf'													--#FAF.008.o
 AND l.t$cpac='td'													--#FAF.008.n
-AND l.t$vers='B61U'
-AND l.t$rele='a7'
-AND l.t$cust='glo1'
+AND rpad(l.t$vers,4) || rpad(l.t$rele,2) || rpad(l.t$cust,4)=
+                                    (select max(rpad(l1.t$vers,4) || rpad(l1.t$rele,2) || rpad(l1.t$cust,4) ) 
+                                      from baandb.tttadv140000 l1 
+                                      where l1.t$clab=l.t$clab 
+                                      AND l1.t$clan=l.t$clan 
+                                      AND l1.t$cpac=l.t$cpac)
 UNION																--#FAF.003.sn
 SELECT d.t$cnst CD_TIPO_OPERACAO,
        l.t$desc DS_TIPO_OPERACAO,
@@ -28,14 +33,16 @@ FROM baandb.tttadv401000 d,
      baandb.tttadv140000 l
 WHERE d.t$cpac='ci'
 AND d.t$cdom='sli.tdff.l'
-AND d.t$vers || d.t$rele || d.t$cust=(select max(l1.t$vers || l1.t$rele || l1.t$cust ) 
+AND rpad(d.t$vers,4) || rpad(d.t$rele,2) || rpad(d.t$cust,4)=
+                                     (select max(rpad(l1.t$vers,4) || rpad(l1.t$rele, 2) || rpad(l1.t$cust,4) ) 
                                       from baandb.tttadv401000 l1 
                                       where l1.t$cpac=d.t$cpac 
                                       AND l1.t$cdom=d.t$cdom)
 AND l.t$clab=d.t$za_clab
 AND l.t$clan='p'
 AND l.t$cpac='ci'
-AND l.t$vers || l.t$rele || l.t$cust=(select max(l1.t$vers || l1.t$rele || l1.t$cust ) 
+AND rpad(l.t$vers,4) || rpad(l.t$rele,2) || rpad(l.t$cust,4)=
+                                    (select max(rpad(l1.t$vers,4) || rpad(l1.t$rele,2) || rpad(l1.t$cust,4) ) 
                                       from baandb.tttadv140000 l1 
                                       where l1.t$clab=l.t$clab 
                                       AND l1.t$clan=l.t$clan 
