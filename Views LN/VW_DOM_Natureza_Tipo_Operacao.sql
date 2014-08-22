e02e6867043bbec525b510f8e1f37f81e2c906ca
Marcia-Amador <marcia@globalti.com.br>
2014-08-22 15:04:07 -0300

    Update VW_DOM_Natureza_Tipo_Operacao.sql
    22/08/2014, Marcia A. R. Torres, Atualização no domínio para retorno do enumerate.

--- Views LN/VW_DOM_Natureza_Tipo_Operacao.sql
+++ Views LN/VW_DOM_Natureza_Tipo_Operacao.sql
@@ -1,4 +1,4 @@
---	FAF.182 - 27-jun-2014, Fabio Ferreira, 	Correção descrição dominio
+--	FAF.182 - 27-jun-2014, Fabio Ferreira, 	CorreÃ§Ã£o descriÃ§Ã£o dominio
 --*******************************************************************************************************************************************
 SELECT 	DISTINCT			
 	  tcmcs940.t$ofso$l CD_NATUREZA_OPERACAO,
@@ -14,15 +14,18 @@ FROM  baandb.ttcmcs940201 tcmcs940,
 			 baandb.tttadv140000 l
 		WHERE d.t$cpac='td'										
 		AND d.t$cdom='rec.trfd.l'					
-		AND d.t$vers='B61U'
-		AND d.t$rele='a7'
-		AND d.t$cust='glo1'
+    AND d.t$vers || d.t$rele || d.t$cust=(select max(l1.t$vers || l1.t$rele || l1.t$cust ) 
+                                          from baandb.tttadv401000 l1 
+                                          where l1.t$cpac=d.t$cpac 
+                                          AND l1.t$cdom=d.t$cdom)
 		AND l.t$clab=d.t$za_clab
 		AND l.t$clan='p'
 		AND l.t$cpac='td'												
-		AND l.t$vers='B61U'
-		AND l.t$rele='a7'
-		AND l.t$cust='glo1') dominio
+    AND l.t$vers || l.t$rele || l.t$cust=(select max(l1.t$vers || l1.t$rele || l1.t$cust ) 
+                                          from baandb.tttadv140000 l1 
+                                          where l1.t$clab=l.t$clab 
+                                          AND l1.t$clan=l.t$clan 
+                                          AND l1.t$cpac=l.t$cpac)) dominio
 WHERE	tcmcs947.t$cfoc$l=tcmcs940.t$ofso$l
 AND    	dominio.COD=tcmcs947.t$rfdt$l
 order by 1,2,3
