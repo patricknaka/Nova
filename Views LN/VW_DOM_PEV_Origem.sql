ca083fc445067af835255b7a2d5734b3027fe2f6
Marcia-Amador <marcia@globalti.com.br>
2014-08-22 17:43:59 -0300

    Update VW_DOM_PEV_Origem.sql

--- Views LN/VW_DOM_PEV_Origem.sql
+++ Views LN/VW_DOM_PEV_Origem.sql
@@ -1,4 +1,4 @@
--- #FAF.075 - 22-mai-2014, Fabio Ferreira, 	Adicionados os códigos do banco do site
+-- #FAF.075 - 22-mai-2014, Fabio Ferreira, 	Adicionados os cÃ³digos do banco do site
 --**********************************************************************************************************************************************************
 SELECT d.t$cnst CD_ORIGEM_PEDIDO,
        l.t$desc DS_ORIGEM_PEDIDO
@@ -6,13 +6,20 @@ FROM baandb.tttadv401000 d,
      baandb.tttadv140000 l
 WHERE d.t$cpac='zn'
 AND d.t$cdom='sls.orig.c'
-AND d.t$vers='B61U'
-AND d.t$rele='a7'
-AND d.t$cust='npt0'
+AND rpad(d.t$vers,4) || rpad(d.t$rele,2) || rpad(d.t$cust,4)=
+                                     (select max(rpad(l1.t$vers,4) || rpad(l1.t$rele, 2) || rpad(l1.t$cust,4) ) 
+                                      from baandb.tttadv401000 l1 
+                                      where l1.t$cpac=d.t$cpac 
+                                      AND l1.t$cdom=d.t$cdom)
 AND l.t$clab=d.t$za_clab
 AND l.t$clan='p'
 AND l.t$cpac='zn'
-AND l.t$vers=(select max(l1.t$vers) from baandb.tttadv140000 l1 where l1.t$clab=l.t$clab AND l1.t$clan=l.t$clan AND l1.t$cpac=l.t$cpac)	--#FAF.075.sn
+AND rpad(l.t$vers,4) || rpad(l.t$rele,2) || rpad(l.t$cust,4)=
+                                    (select max(rpad(l1.t$vers,4) || rpad(l1.t$rele,2) || rpad(l1.t$cust,4) ) 
+                                      from baandb.tttadv140000 l1 
+                                      where l1.t$clab=l.t$clab 
+                                      AND l1.t$clan=l.t$clan 
+                                      AND l1.t$cpac=l.t$cpac)
 AND l.T$ZC_CONT=3																													--#FAF.075.en
 --AND l.t$vers='B61U'																												--#FAF.075.so
 --AND l.t$rele='a7'
