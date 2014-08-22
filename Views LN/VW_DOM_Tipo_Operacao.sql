2f4bb27f5e2fbd4a68275379120734b2ffec4e94
Marcia-Amador <marcia@globalti.com.br>
2014-08-22 14:22:02 -0300

    Update VW_DOM_Tipo_Operacao.sql
    22/08/2014, Marcia A. R. Torres, Atualização no domínio para retorno do enumerate.

--- Views LN/VW_DOM_Tipo_Operacao.sql
+++ Views LN/VW_DOM_Tipo_Operacao.sql
@@ -1,5 +1,5 @@
---	FAF.003 - 12-mai-2014, Fabio Ferreira, 	Icluido os tipos de operação de saida
---	FAF.008 - 21-mai-2014, Fabio Ferreira, 	Correção os registros do módulo NR estavam mostrando os registros de NF
+--	FAF.003 - 12-mai-2014, Fabio Ferreira, 	Icluido os tipos de operaÃ§Ã£o de saida
+--	FAF.008 - 21-mai-2014, Fabio Ferreira, 	CorreÃ§Ã£o os registros do mÃ³dulo NR estavam mostrando os registros de NF
 --*******************************************************************************************************************************************
 SELECT d.t$cnst CD_TIPO_OPERACAO,
        l.t$desc DS_TIPO_OPERACAO,
@@ -28,13 +28,16 @@ FROM baandb.tttadv401000 d,
      baandb.tttadv140000 l
 WHERE d.t$cpac='ci'
 AND d.t$cdom='sli.tdff.l'
-AND d.t$vers='B61U'
-AND d.t$rele='a7'
-AND d.t$cust='glo1'
+AND d.t$vers || d.t$rele || d.t$cust=(select max(l1.t$vers || l1.t$rele || l1.t$cust ) 
+                                      from baandb.tttadv401000 l1 
+                                      where l1.t$cpac=d.t$cpac 
+                                      AND l1.t$cdom=d.t$cdom)
 AND l.t$clab=d.t$za_clab
 AND l.t$clan='p'
 AND l.t$cpac='ci'
-AND l.t$vers='B61U'
-AND l.t$rele='a7'
-AND l.t$cust='glo1'
+AND l.t$vers || l.t$rele || l.t$cust=(select max(l1.t$vers || l1.t$rele || l1.t$cust ) 
+                                      from baandb.tttadv140000 l1 
+                                      where l1.t$clab=l.t$clab 
+                                      AND l1.t$clan=l.t$clan 
+                                      AND l1.t$cpac=l.t$cpac)
 order by 1													--#FAF.003.en
