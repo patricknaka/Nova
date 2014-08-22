6cc7a716a2344fdbfb701e5cb1d843fcef4e9200
Marcia-Amador <marcia@globalti.com.br>
2014-08-22 16:56:07 -0300

    Update VW_DOM_NFV_Status_Sefaz.sql

--- Views LN/VW_DOM_NFV_Status_Sefaz.sql
+++ Views LN/VW_DOM_NFV_Status_Sefaz.sql
@@ -4,14 +4,16 @@
        baandb.tttadv140000 l
   WHERE d.t$cpac='ci'
   AND d.t$cdom='sli.nfes.l'
-  AND d.t$vers || d.t$rele || d.t$cust=(select max(l1.t$vers || l1.t$rele || l1.t$cust ) 
+  AND rpad(d.t$vers,4) || rpad(d.t$rele,2) || rpad(d.t$cust,4)=
+                                       (select max(rpad(l1.t$vers,4) || rpad(l1.t$rele, 2) || rpad(l1.t$cust,4) ) 
                                         from baandb.tttadv401000 l1 
                                         where l1.t$cpac=d.t$cpac 
                                         AND l1.t$cdom=d.t$cdom)
   AND l.t$clab=d.t$za_clab
   AND l.t$clan='p'
   AND l.t$cpac='ci'
-  AND l.t$vers || l.t$rele || l.t$cust=(select max(l1.t$vers || l1.t$rele || l1.t$cust ) 
+  AND rpad(l.t$vers,4) || rpad(l.t$rele,2) || rpad(l.t$cust,4)=
+                                      (select max(rpad(l1.t$vers,4) || rpad(l1.t$rele,2) || rpad(l1.t$cust,4) ) 
                                         from baandb.tttadv140000 l1 
                                         where l1.t$clab=l.t$clab 
                                         AND l1.t$clan=l.t$clan 
