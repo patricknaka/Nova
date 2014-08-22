cab92cead56445cf9e8ba35916926148c8452491
fabioInfor <fabio.ferreira@infor.com>
2014-08-22 14:22:21 -0300

    VW_DOM_Situacao_Pagamento - Correção

--- Views LN/VW_DOM_Situacao_Pagamento.sql
+++ Views LN/VW_DOM_Situacao_Pagamento.sql
@@ -5,15 +5,19 @@ FROM baandb.tttadv401000 d,
      baandb.tttadv140000 l
 WHERE d.t$cpac='tf'
 AND d.t$cdom='cmg.stpp'
-AND d.t$vers='B61'
-AND d.t$rele='a'
-AND d.t$cust=' '
+AND d.t$vers || d.t$rele || d.t$cust=(select max(l1.t$vers || l1.t$rele || l1.t$cust ) 
+                                      from baandb.tttadv401000 l1 
+                                      where l1.t$cpac=d.t$cpac 
+                                      AND l1.t$cdom=d.t$cdom)
+
 AND l.t$clab=d.t$za_clab
 AND l.t$clan='p'
 AND l.t$cpac='tf'
-AND l.t$vers='B61'
-AND l.t$rele='a'
-AND l.t$cust=' '
+AND l.t$vers || l.t$rele || l.t$cust=(select max(l1.t$vers || l1.t$rele || l1.t$cust ) 
+                                      from baandb.tttadv140000 l1 
+                                      where l1.t$clab=l.t$clab 
+                                      AND l1.t$clan=l.t$clan 
+                                      AND l1.t$cpac=l.t$cpac)
 UNION
 SELECT d.t$cnst CD_SITUACAO_PAGAMENTO,
        l.t$desc DS_SITUACAO_PAGAMENTO,
@@ -22,13 +26,16 @@ FROM baandb.tttadv401000 d,
      baandb.tttadv140000 l
 WHERE d.t$cpac='tf'
 AND d.t$cdom='cmg.stat.l'
-AND d.t$vers='B61U'
-AND d.t$rele='a7'
-AND d.t$cust='glo1'
+AND d.t$vers || d.t$rele || d.t$cust=(select max(l1.t$vers || l1.t$rele || l1.t$cust ) 
+                                      from baandb.tttadv401000 l1 
+                                      where l1.t$cpac=d.t$cpac 
+                                      AND l1.t$cdom=d.t$cdom)
 AND l.t$clab=d.t$za_clab
 AND l.t$clan='p'
 AND l.t$cpac='tf'
-AND l.t$vers='B61U'
-AND l.t$rele='a7'
-AND l.t$cust='glo1'
+AND l.t$vers || l.t$rele || l.t$cust=(select max(l1.t$vers || l1.t$rele || l1.t$cust ) 
+                                      from baandb.tttadv140000 l1 
+                                      where l1.t$clab=l.t$clab 
+                                      AND l1.t$clan=l.t$clan 
+                                      AND l1.t$cpac=l.t$cpac)
 order by 1
