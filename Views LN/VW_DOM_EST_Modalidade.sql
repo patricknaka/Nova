a40d6e69cda817905b9205fcdf0df4af57551847
Marcia-Amador <marcia@globalti.com.br>
2014-08-22 14:57:21 -0300

    Update VW_DOM_EST_Modalidade.sql
    22/08/2014, Marcia A. R. Torres, Atualização no domínio para retorno do enumerate.

--- Views LN/VW_DOM_EST_Modalidade.sql
+++ Views LN/VW_DOM_EST_Modalidade.sql
@@ -4,15 +4,19 @@ FROM baandb.tttadv401000 d,
      baandb.tttadv140000 l
 WHERE d.t$cpac='tc'
 AND d.t$cdom='mcs.tpar.l'
-AND d.t$vers='B61U'
-AND d.t$rele='a7'
-AND d.t$cust='glo1'
+AND d.t$vers || d.t$rele || d.t$cust=(select max(l1.t$vers || l1.t$rele || l1.t$cust ) 
+                                      from baandb.tttadv401000 l1 
+                                      where l1.t$cpac=d.t$cpac 
+                                      AND l1.t$cdom=d.t$cdom)
 AND l.t$clab=d.t$za_clab
 AND l.t$clan='p'
 AND l.t$cpac='tc'
-AND l.t$vers='B61U'
-AND l.t$rele='a7'
-AND l.t$cust='glo1'
+
+AND l.t$vers || l.t$rele || l.t$cust=(select max(l1.t$vers || l1.t$rele || l1.t$cust ) 
+                                      from baandb.tttadv140000 l1 
+                                      where l1.t$clab=l.t$clab 
+                                      AND l1.t$clan=l.t$clan 
+                                      AND l1.t$cpac=l.t$cpac)
 UNION
 SELECT 0 CD_ESTOQUE_MODALIDADE,
        'NORMAL' DS_MODALIDADE
