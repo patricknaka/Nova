d13d93dec5a7804466855a02a196adfd89b2d708
Marcia-Amador <marcia@globalti.com.br>
2014-08-22 18:02:34 -0300

    Update VW_DOM_Natureza_Lancamento.sql

--- Views LN/VW_DOM_Natureza_Lancamento.sql
+++ Views LN/VW_DOM_Natureza_Lancamento.sql
@@ -1,4 +1,4 @@
--- #FAF.005, 14-mai-2014, Fabio Ferreira,	Aterado o campo Natureza do lançamento para relacionar com a cat. Transação
+-- #FAF.005, 14-mai-2014, Fabio Ferreira,	Aterado o campo Natureza do lanÃ§amento para relacionar com a cat. TransaÃ§Ã£o
 --							
 --**********************************************************************************************************************************************************
 SELECT d.t$cnst CD_NATUREZA_LANCAMENTO,
@@ -8,11 +8,18 @@ FROM baandb.tttadv401000 d,
 WHERE d.t$cpac='tf'
 --AND d.t$cdom='cmg.tcsh'																			--#FAF.005.o
 AND d.t$cdom='gld.catg'																				--#FAF.005.n
-AND d.t$vers='B61U'
-AND d.t$rele='a'
-AND d.t$cust='stnd'
+AND rpad(d.t$vers,4) || rpad(d.t$rele,2) || rpad(d.t$cust,4)=
+                                     (select max(rpad(l1.t$vers,4) || rpad(l1.t$rele, 2) || rpad(l1.t$cust,4) ) 
+                                      from baandb.tttadv401000 l1 
+                                      where l1.t$cpac=d.t$cpac 
+                                      AND l1.t$cdom=d.t$cdom)
 AND l.t$clab=d.t$za_clab
 AND l.t$clan='p'
 AND l.t$cpac='tf'
-AND l.t$vers=(select max(l1.t$vers) from baandb.tttadv140000 l1 where l1.t$clab=l.t$clab AND l1.t$clan=l.t$clan AND l1.t$cpac=l.t$cpac)
+AND rpad(l.t$vers,4) || rpad(l.t$rele,2) || rpad(l.t$cust,4)=
+                                    (select max(rpad(l1.t$vers,4) || rpad(l1.t$rele,2) || rpad(l1.t$cust,4) ) 
+                                      from baandb.tttadv140000 l1 
+                                      where l1.t$clab=l.t$clab 
+                                      AND l1.t$clan=l.t$clan 
+                                      AND l1.t$cpac=l.t$cpac)
 order by 1
