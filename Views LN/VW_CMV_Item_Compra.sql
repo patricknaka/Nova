62264c2640a3f1a5eaeefe64c63cd2e699df7f60
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-09-04 14:47:34 -0300

    VW_CMV_Item_Compra - cópia da 301

--- Views LN/VW_CMV_Item_Compra.sql
+++ Views LN/VW_CMV_Item_Compra.sql
@@ -1,7 +1,8 @@
 select 
 ltrim(rtrim(tdipu001.t$item)) as CD_ITEM,
-tdipu001.t$prip as VL_COMPRA
+tdipu001.t$prip as VL_PRECO_COMPRA
+--tcibd001.t$lmdt
 from baandb.ttdipu001201 tdipu001
      inner join baandb.ttcibd001201 tcibd001
      ON tdipu001.t$item=tcibd001.t$item
-WHERE tcibd001.t$lmdt>=TRUNC(sysdate-1, 'DAY')
+--WHERE tcibd001.t$lmdt>=TRUNC(sysdate-1, 'DAY')
