c0600cb3249acd0b628dc5a7797891648b92e0cb
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-09-04 14:50:40 -0300

    VW_CMV_NProduto_Compra - cópia da 301

--- Views LN/VW_CMV_NProduto_Compra.sql
+++ Views LN/VW_CMV_NProduto_Compra.sql
@@ -1,8 +1,8 @@
 select  RTRIM(LTRIM(ipu001.t$item)) CD_ITEM, 
         ipu001.t$prip VL_PRECO_COMPRA
-    from    ttdipu001201 ipu001, --DADOS DE COMPRA DO ITEM
-            ttcibd001201 ibd001, --DADOS GERAIS DO ITEM
-            tznisa002201 isa002 --DADOS DE VENDA DO ITEM
+    from    BAANDB.ttdipu001201 ipu001, --DADOS DE COMPRA DOS ITENS
+            BAANDB.ttcibd001201 ibd001, --DADOS GERAIS DOS ITENS
+            BAANDB.tznisa002201 isa002  --DADOS DE VENDA DOS ITENS
         where   ipu001.t$item   = ibd001.t$item
             and ibd001.t$npcl$c = isa002.t$npcl$c 
             and ibd001.t$kitm   in (4,5)
