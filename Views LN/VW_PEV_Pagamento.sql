874c93f32fb5e6ab423f378315e1599a8e7cf03f
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-09-04 14:20:21 -0300

    Revert "VW_PEV_Pagamento - cópia da 301"
    This reverts commit 5c826c8be15f4366337e961db44a1c9ced4a7338.

--- Views LN/VW_PEV_Pagamento.sql
+++ Views LN/VW_PEV_Pagamento.sql
@@ -9,48 +9,94 @@
 -- #FAF.088 - 28-mai-2014,	Fabio Ferreira, conversão de timezone no campo DT_APROVACAO_PAGAMENTO_ERP 
 -- #FAF.317 - 03-sep-2014,	Fabio Ferreira, VL_PAGAMENTO calculado com base no valor da entrega
 --***************************************************************************************************************************************************************
-SELECT DISTINCT
-		znsls400.t$ncia$c CD_CIA,																								--#FAF.113.n
-	   znsls401.t$pecl$c NR_PEDIDO,
-       TO_CHAR(znsls401.t$entr$c) NR_ENTREGA,																	-- #FAF.048.1.n
-	   tdsls400.t$orno NR_ORDEM,
-       tdsls400.t$ofbp CD_CLIENTE_FATURA,
-       tccom130.t$ftyp$l CD_TIPO_CLIENTE,
-       tccom130.t$ccit CD_CIDADE_FATURA,
-       tccom130.t$ccty CD_PAIS_FATURA,
-       tccom130.t$cste CD_ESTADO_FATURA,
-       tccom130.t$pstc CD_CEP_FATURA,
-       tccom130.t$namc DS_ENDERECO_FATURA,
-       tccom130.t$dist$l DS_BAIRRO_FATURA,
-       tccom130.t$hono NR_FATURA,
-       znsls400.t$comf$c DS_COMPLEMENTO_FATURA,
-       znsls400.t$reff$c DS_REFERENCIA_ENDERECO_FATURA,
-       tdsls400.t$stbp CD_CLIENTE_ENTREGA,
-       tccom130c.t$ccit CD_CIDADE_ENTREGA,
-       tccom130c.t$ccty CD_PAIS_ENTREGA,
-       tccom130c.t$cste CD_ESTADO_ENTREGA,
-       tccom130c.t$pstc CD_CEP_ENTREGA,
-       tccom130c.t$namc DS_ENDERECO_ENTREGA,
-       tccom130c.t$dist$l DS_BAIRRO_ENTREGA,
-       tccom130c.t$hono NR_ENTREGA_ENDERECO,																		--#FAF.002.n
-       znsls401.t$come$c DS_COMPLEMENTO_ENTREGA,
-       znsls401.t$refe$c DS_REFERENCIA_ENDERECO_ENTREGA,
-	   CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znsls400.t$dtem$c, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-    AT time zone sessiontimezone) AS DATE) DT_EMISSAO,
-	   CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znsls400.t$dtin$c, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-    AT time zone sessiontimezone) AS DATE) DT_CHEGADA_PEDIDO,
-	   CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls400.t$rcd_utc, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+select
+    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls400.t$rcd_utc, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
     AT time zone sessiontimezone) AS DATE) DT_ULT_ATUALIZACAO,
-	   znsls400.t$idli$c NR_LISTA_CASAMENTO																		--#FAF.005.n
-FROM baandb.tznsls401201 znsls401,
-     baandb.tznsls400201 znsls400,
-     baandb.ttdsls400201 tdsls400,
-     baandb.ttccom130201 tccom130,
-     baandb.ttccom130201 tccom130c
-WHERE znsls400.t$ncia$c = znsls401.t$ncia$c
-AND znsls400.t$uneg$c = znsls401.t$uneg$c
-AND znsls400.t$pecl$c = znsls401.t$pecl$c
-AND znsls400.t$sqpd$c = znsls401.t$sqpd$c
-AND tdsls400.t$orno = znsls401.t$orno$c
-AND tccom130.t$cadr = tdsls400.t$itad
-AND tccom130c.t$cadr = tdsls400.t$stad
+    201 CD_CIA,
+    tdsls400.t$orno NR_ORDEM,
+	TRIM(sls401q.t$pecl$c) NR_PEDIDO,
+    TO_CHAR(sls401q.t$entr$c) NR_ENTREGA, 																--#FAF.047.1.n
+    znsls402.t$sequ$c  SQ_PAGAMENTO,
+    znsls402.t$idmp$c  CD_MEIO_PAGAMENTO,
+    CASE WHEN (znsls402.t$idmp$c = 4) THEN 0 ELSE znsls402.t$cccd$c END CD_BANDEIRA,
+    CASE WHEN (znsls402.t$idmp$c = 4) THEN 0 ELSE znsls402.t$idbc$c END CD_BANCO,
+    znsls402.t$nupa$c  NR_PARCELAS,
+    -- abs(znsls402.t$vlmr$c)  VL_PAGAMENTO,																						--#FAF.317.o
+	(sls401q.VL_PGTO_ENTR/sls401p.VL_PGTO_PED)*znsls402.t$vlmr$c VL_PAGAMENTO,														--#FAF.317.n																							--#FAF.317.n
+    znsls402.t$stat$c  CD_STATUS_PAGAMENTO,
+	CASE WHEN (znsls402.t$idmp$c = 4 and znsls400.t$idli$c!=0) THEN 1 ELSE 2 END IN_VALE_LISTA_CASAMENTO,							--#FAF.049.n
+	CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znsls400.t$dtem$c, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+    AT time zone sessiontimezone) AS DATE) DT_EMISSAO_PEDIDO,
+    znsls402.t$uneg$c  CD_UNIDADE_NEGOCIO,	
+	CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znsls402.t$dtra$c, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+    AT time zone sessiontimezone) AS DATE) DT_APROVACAO,
+    znsls402.t$valo$c  VL_ORIGINAL,
+    (sls401q.VL_PGTO_ENTR/sls401p.VL_PGTO_PED)*znsls402.t$vlja$c  VL_JUROS_ADMINISTRADORA,
+    CASE WHEN znsls402.t$vlja$c!=0 THEN 1
+	ELSE 2
+	END IN_JUROS_ADMINISTRADORA,
+    (select 
+	CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(min(a.t$trdt), 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+    AT time zone sessiontimezone) AS DATE)
+	from baandb.ttdsls451201 a
+    where a.t$orno=tdsls400.t$orno) DT_APROVACAO_PAGAMENTO_ERP,
+    (sls401q.VL_PGTO_ENTR/sls401p.VL_PGTO_PED)*znsls402.t$vlju$c  VL_JUROS,
+    ' ' CD_CICLO_PAGAMENTO,            -- *** NÃO EXISTE ESTA INFORMAÇÃO NO LN / PENDENTE DE DUVIDA ***
+    znsls402.t$cone$c  NR_TABELA_NEGOCIACAO,
+    znsls402.t$ncam$c  NR_BIN_CARTAO_CREDITO,
+    znsls402.t$nctf$c  NR_NSU_TRANSACAO_CARTAO,
+    znsls402.t$nsua$c  NR_NSU_AUTOR_CARTAO,
+    znsls402.t$auto$c  CD_AUTOR_CARTAO_CREDITO,
+    znsls402.t$maqu$c  NR_MAQUINETA,
+    TO_CHAR(znsls402.t$nute$c)  NR_TERMINAL,															--#FAF.089.n
+    znsls402.t$mrep$c  CD_MOTIVO_REPROVACAO,
+	znsls402.t$txrp$c  DS_MOTIVO_REPROVACAO,															--#FAF.076.n
+    znsls402.t$idag$c  NR_AGENCIA,
+    znsls402.t$idct$c  NR_CONTA_CORRENTE,
+	znsls402.t$idad$c CD_ADQUIRENTE																		--#FAF.085.n
+FROM  baandb.tznsls400201 znsls400,
+    (select distinct 
+		znsls401.t$ncia$c      	t$ncia$c,
+		znsls401.t$uneg$c       t$uneg$c,
+		znsls401.t$pecl$c       t$pecl$c,
+		znsls401.t$sqpd$c       t$sqpd$c,
+		znsls401.t$entr$c       t$entr$c,
+		znsls401.t$orno$c      	t$orno$c,
+		sum((znsls401.t$vlun$c*znsls401.t$qtve$c)+znsls401.t$vlfr$c-znsls401.t$vldi$c+znsls401.t$vlde$c) VL_PGTO_ENTR	--#FAF.317.n	
+    from baandb.tznsls401201 znsls401
+	group by
+		znsls401.t$ncia$c,																								--#FAF.317.sn
+		znsls401.t$uneg$c,
+		znsls401.t$pecl$c,
+		znsls401.t$sqpd$c,
+		znsls401.t$entr$c,
+		znsls401.t$orno$c) sls401q,																						--#FAF.317.en
+		
+    (select distinct 																									--#FAF.317.sn
+		znsls401.t$ncia$c      	t$ncia$c,
+		znsls401.t$uneg$c       t$uneg$c,
+		znsls401.t$pecl$c       t$pecl$c,
+		znsls401.t$sqpd$c       t$sqpd$c,
+		sum((znsls401.t$vlun$c*znsls401.t$qtve$c)+znsls401.t$vlfr$c-znsls401.t$vldi$c+znsls401.t$vlde$c) VL_PGTO_PED		
+    from baandb.tznsls401201 znsls401
+	group by
+		znsls401.t$ncia$c,																				
+		znsls401.t$uneg$c,
+		znsls401.t$pecl$c,
+		znsls401.t$sqpd$c) sls401p,																						--#FAF.317.en
+    baandb.ttdsls400201 tdsls400,
+    baandb.tznsls402201 znsls402
+where
+	   sls401q.t$ncia$c=znsls400.t$ncia$c
+and    sls401q.t$uneg$c=znsls400.t$uneg$c
+and    sls401q.t$pecl$c=znsls400.t$pecl$c
+and    sls401q.t$sqpd$c=znsls400.t$sqpd$c
+and	   sls401p.t$ncia$c=znsls400.t$ncia$c																				--#FAF.317.sn
+and    sls401p.t$uneg$c=znsls400.t$uneg$c
+and    sls401p.t$pecl$c=znsls400.t$pecl$c
+and    sls401p.t$sqpd$c=znsls400.t$sqpd$c																				--#FAF.317.en
+and    sls401q.t$orno$c=tdsls400.t$orno
+and    znsls402.t$ncia$c=znsls400.t$ncia$c
+and    znsls402.t$uneg$c=znsls400.t$uneg$c
+and    znsls402.t$pecl$c=znsls400.t$pecl$c
+and    znsls402.t$sqpd$c=znsls400.t$sqpd$c;
