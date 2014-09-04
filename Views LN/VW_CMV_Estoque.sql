a15aab09ba8a24fb4e30e35d877a646690c67aba
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-09-04 14:33:37 -0300

    VW_CMV_Estoque - cópia da 301

--- Views LN/VW_CMV_Estoque.sql
+++ Views LN/VW_CMV_Estoque.sql
@@ -4,10 +4,10 @@ SELECT  ltrim(rtrim(a.t$item)) CD_ITEM,
         cast(sum(a.t$mauc$1) as numeric(15,5)) VL_CMV, 
         cast(sum(d.t$qhnd-d.t$qblk) as numeric(15,0)) QT_ESTOQUE_LIVRE,
         cast(sum(d.t$qhnd)as numeric(15,0)) QT_ESTOQUE_TOTAL
-FROM    twhina113201 a, 
-        twhina112201 b, 
-        ttcemm112201 c, 
-        twhwmd215201 d
+FROM    BAANDB.twhina113201 a, 
+        BAANDB.twhina112201 b, 
+        BAANDB.ttcemm112201 c, 
+        BAANDB.twhwmd215201 d
 WHERE   a.t$item=b.t$item
       AND   a.t$cwar=b.t$cwar
       AND   a.t$trdt=b.t$trdt
@@ -16,11 +16,11 @@ WHERE   a.t$item=b.t$item
       AND   c.t$waid=a.t$cwar
       AND   d.t$item=b.t$item
       AND   d.t$cwar=b.t$cwar
-      AND   a.t$trdt = (select max(a1.t$trdt) from twhina113201 a1
+      AND   a.t$trdt = (select max(a1.t$trdt) from baandb.twhina113201 a1
                         where            a1.t$cwar=a.t$cwar
                   AND   a1.t$item=a.t$item
                   AND   a1.t$inwp=a.t$inwp)
-    AND  a.t$seqn=(select max(a2.t$seqn) from twhina113201 a2
+    AND  a.t$seqn=(select max(a2.t$seqn) from baandb.twhina113201 a2
             where   a2.t$cwar=a.t$cwar
             AND     a2.t$item=a.t$item
             AND     a2.t$inwp=a.t$inwp
