c74f6f56a367e0fb407cb80584e8007d82e85bb0
fabioInfor <fabio.ferreira@infor.com>
2014-09-19 18:39:32 -0300

    #330 VW_CAP_Titulo_lancamento

--- Views LN/VW_CAP_TITULO_Lancamento.sql
+++ Views LN/VW_CAP_TITULO_Lancamento.sql
@@ -9,7 +9,32 @@ SELECT
     IDBCR.DBCRDESC                  IN_DEBITO_CREDITO,
     TDREC952.T$AMTH$L$1             VL_LANCAMENTO,
     TDREC952.T$LEAC$L               CD_CONTA_CONTABIL,
-    TFGLD008.T$DESC                 DS_CONTA_CONTABIL
+    TFGLD008.T$DESC                 DS_CONTA_CONTABIL,
+    TDREC952.T$LINE$L				NR_LINHA,
+	
+	(SELECT  
+		   l.t$desc DS_TIPO_NF
+	FROM baandb.tttadv401000 d,
+		 baandb.tttadv140000 l
+	WHERE d.t$cpac='tc'
+	AND d.t$cdom='mcs.brty.l'
+	AND rpad(d.t$vers,4) || rpad(d.t$rele,2) || rpad(d.t$cust,4)=
+										 (select max(rpad(l1.t$vers,4) || rpad(l1.t$rele, 2) || rpad(l1.t$cust,4) ) 
+										  from baandb.tttadv401000 l1 
+										  where l1.t$cpac=d.t$cpac 
+										  AND l1.t$cdom=d.t$cdom)
+	AND l.t$clab=d.t$za_clab
+	AND l.t$clan='p'
+	AND l.t$cpac='tc'
+	AND d.t$cnst=TDREC952.T$brty$l
+	AND rpad(l.t$vers,4) || rpad(l.t$rele,2) || rpad(l.t$cust,4)=
+										(select max(rpad(l1.t$vers,4) || rpad(l1.t$rele,2) || rpad(l1.t$cust,4) ) 
+										  from baandb.tttadv140000 l1 
+										  where l1.t$clab=l.t$clab 
+										  AND l1.t$clan=l.t$clan 
+										  AND l1.t$cpac=l.t$cpac)) IMPOSTO	
+	
+	
   
 FROM
 	BAANDB.TTDREC940201 TDREC940,
