935383d40724c66eb0d0e98b56c3f08f34497a7e
ronaldocs <ronaldo.cristiano@gmail.com>
2014-09-26 18:22:01 -0300

    Ajuste
    Ajuste de queries e otimização.

--- Relatórios LN/LN_FISCAL_Apuracao_imposto_federal_alt.sql
+++ Relatórios LN/LN_FISCAL_Apuracao_imposto_federal_alt.sql
@@ -1,13 +1,12 @@
-SELECT
+SELECT DISTINCT 
   tdrec940.t$idat$l                       DATA_EMISSAO,
-  
-  CASE WHEN tfacp200.t$balc = 0 
-         THEN ( select max(p.t$docd) 
-                  from baandb.ttfacp200301 p
-                 where p.t$ttyp = tfacp200.t$ttyp 
-                   and p.t$ninv = tfacp200.t$ninv )
-       ELSE NULL 
-   END                                    DATA_LIQUIDACAO,
+
+ ( select max(p.t$docd) 
+     from baandb.ttfacp200301 p
+    where tdrec940.t$ttyp$l = p.t$ttyp
+      and tdrec940.t$invn$l = p.t$ninv
+      and p.t$balc = 0 )                  DATA_LIQUIDACAO,
+	  
   tccom130.t$fovn$l                       CNPJ_FORN,
   tccom100.t$nama                         NOME_FORN,
   tdrec940.t$fire$l                       NR,
@@ -73,13 +72,12 @@ SELECT
        AND tdrec949.t$brty$l = 17 )       VALOR_INSS_RETIDO_PF
 
 FROM      baandb.ttdrec940301 tdrec940
+
 LEFT JOIN baandb.ttccom100301 tccom100 
        ON tccom100.t$bpid   = tdrec940.t$bpid$l
+       
 LEFT JOIN BAANDB.ttccom130301 tccom130 
        ON tccom130.t$cadr=tccom100.t$cadr
-LEFT JOIN baandb.ttfacp200301 tfacp200 
-       ON tdrec940.t$ttyp$l = tfacp200.t$ttyp
-      AND tdrec940.t$invn$l = tfacp200.t$ninv
-    
+           
 WHERE Trim(tdrec940.t$opfc$l) in ('1933', '2933', '1300')  
   AND Trunc(tdrec940.t$idat$l) Between :DataEmissaoDe AND :DataEmissaoAte
--- Relatórios LN/LN_Supply - Ordem_de_Venda_VES_EA.sql
+++ Relatórios LN/LN_Supply - Ordem_de_Venda_VES_EA.sql
@@ -6,18 +6,19 @@ SELECT
    znsls401.t$entr$c     NUME_OV,
    znsls401.t$orno$c     NUME_OV_LN,
    znsls401.t$sequ$c     NUME_ITEM,
+   znsls400.t$pecl$c     NUME_PEDIDO,
    znsls401.t$ufen$c     UF,
    znsls401.t$idor$c     ORIGEM,
    znsls401.t$dtap$c     DATA_APR, 
    tdsls400.t$ddat       DATA_PLANENT,   
    tdsls400.t$prdt       DATA_PLANREC,   
    
-   CASE WHEN znsls401.t$tpes$c='X' THEN 'Crossdocking'
-        WHEN znsls401.t$tpes$c='F' THEN 'Fingido'
-        WHEN znsls401.t$tpes$c='P' THEN 'Pré-Venda'
+   CASE WHEN znsls401.t$tpes$c = 'X' THEN 'Crossdocking'
+        WHEN znsls401.t$tpes$c = 'F' THEN 'Fingido'
+        WHEN znsls401.t$tpes$c = 'P' THEN 'Pré-Venda'
         ELSE 'NORMAL' 
       END                TIPO_ESTOQ,
-	  
+    
    znsls400.t$dtem$c     DATA_ORDEM,
    Trim(tdsls401.t$item) CODE_ITEM,
    tcibd001.t$dsca       DECR_ITEM,
@@ -25,36 +26,36 @@ SELECT
    znsls401.t$qtve$c     QUAN_ORD,
    whinp200.t$qoqu       QUAN_ALOC,
    
-   CASE WHEN whinp200.t$qoqu -(tcibd100.t$blck+tcibd100.t$allo)-tcibd100.t$stoc<=0 THEN 0
+   CASE WHEN whinp200.t$qoqu -(tcibd100.t$blck+tcibd100.t$allo)-tcibd100.t$stoc< = 0 THEN 0
         ELSE whinp200.t$qoqu -(tcibd100.t$blck+tcibd100.t$allo)-tcibd100.t$stoc 
       END                QUAN_FALT,
-	  
+    
    tcibd100.t$ordr       QUAN_EMPED,
    tttxt010.t$text       TEXT_ORD,
    tccom130.t$fovn$l     CNPJ_FORN,
    tccom130.t$nama       NOME_FORNEC,
    
    ( select sum(a.t$tamt$l) 
-       from baandb.tbrmcs941201 a, 
-            baandb.tbrmcs940201 b 
-      where a.t$txre$l=b.t$txre$l 
-        and a.t$txre$l=tdsls401.t$txre$l 
-        and a.t$line$l=tdsls401.t$txli$l 
-        and b.t$txor$l=2 ) 
+       from baandb.tbrmcs941301 a, 
+            baandb.tbrmcs940301 b 
+      where a.t$txre$l = b.t$txre$l 
+        and a.t$txre$l = tdsls401.t$txre$l 
+        and a.t$line$l = tdsls401.t$txli$l 
+        and b.t$txor$l = 2 ) 
                          VALO_PREVIMP,
 
-   ( select case when (max(whwmd215.t$qhnd) - max(whwmd215.t$qchd) - max(whwmd215.t$qnhd))=0 then 0
+   ( select case when (max(whwmd215.t$qhnd) - max(whwmd215.t$qchd) - max(whwmd215.t$qnhd)) = 0 then 0
                  else round(sum(a.t$mauc$1) /(max(whwmd215.t$qhnd) - max(whwmd215.t$qchd) - max(whwmd215.t$qnhd)),4) 
              end mauc
-       from baandb.twhwmd217201 a,
-            baandb.ttcemm112201 tcemm112,
-            baandb.twhwmd215201 whwmd215
-      where tcemm112.t$waid=a.t$cwar
-        AND tcemm112.t$loco=201
-        AND whwmd215.t$cwar=a.t$cwar
-        AND whwmd215.t$item=a.t$item
-        AND a.t$item=tdsls401.t$item
-        AND tcemm112.t$grid=tcemm124.t$grid
+       from baandb.twhwmd217301 a,
+            baandb.ttcemm112301 tcemm112,
+            baandb.twhwmd215301 whwmd215
+      where tcemm112.t$waid = a.t$cwar
+        AND tcemm112.t$loco = 201
+        AND whwmd215.t$cwar = a.t$cwar
+        AND whwmd215.t$item = a.t$item
+        AND a.t$item = tdsls401.t$item
+        AND tcemm112.t$grid = tcemm124.t$grid
    group by a.t$item,
             tcemm112.t$grid) 
                          VALOR_CMV_UNITARIO,
@@ -69,98 +70,120 @@ SELECT
    znsls002.t$dsca$c     DECR_TIPO_ENTREGA,
    iTIPOXD.DESCR         DESCR_XD
   
-FROM      baandb.tznsls400201 znsls400,
-          baandb.tznsls401201 znsls401,
-          baandb.ttdsls400201 tdsls400 
-          
-LEFT JOIN baandb.ttttxt010101 tttxt010 
-       ON tttxt010.t$ctxt = tdsls400.t$txta, 
+FROM      baandb.tznsls400301 znsls400
 
-          baandb.ttdsls401201 tdsls401
-
-LEFT JOIN baandb.tznwmd200201 znwmd200
-       ON znwmd200.t$item$c = tdsls401.t$item
-      AND znwmd200.t$cwar$c = tdsls401.t$cwar          
-      AND znwmd200.t$supl$c = tdsls401.T$ofbp
-     
-LEFT JOIN baandb.twhinp200201 whinp200
-       ON whinp200.t$oorg = 3
-      AND whinp200.t$orno = tdsls401.t$orno
-      AND whinp200.t$pono = tdsls401.t$pono
-      AND whinp200.t$serb = tdsls401.t$sqnb
-      AND whinp200.t$item = tdsls401.t$item,
-
-          baandb.ttcibd001201 tcibd001 
-
-LEFT JOIN baandb.ttdipu001201 tdipu001
-       ON tdipu001.t$item = tcibd001.t$item
-
-LEFT JOIN baandb.ttccom130201 tccom130
-       ON tccom130.t$cadr = tdipu001.t$otbp, 
-       
-          baandb.ttcibd100201 tcibd100,
-          baandb.ttcmcs023201 tcmcs023,
-          baandb.twhinp100201 whinp100,      
-          baandb.tznsls004201 znsls004,
-          baandb.ttcemm124201 tcemm124, 
-          baandb.ttcemm122201 tcemm122,
-          baandb.ttccom100201 tccom100b,
-          baandb.ttccom130201 tccom130b,
-          baandb.ttcemm030201 tcemm030,
-          baandb.ttcmcs031201 tcmcs031,
-          baandb.tznint002201 znint002,
-          baandb.tznsls002201 znsls002,
-  
-( SELECT d.t$cnst CODE,
-         l.t$desc DESCR
-    FROM baandb.tttadv401000 d,
-         baandb.tttadv140000 l
-   WHERE d.t$cpac='zn'
-     AND d.t$cdom='ipu.ixdn.c'
-     AND d.t$vers='B61U'
-     AND d.t$rele='a7'
-     AND d.t$cust='npt0'
-     AND l.t$clab=d.t$za_clab
-     AND l.t$clan='p'
-     AND l.t$cpac='zn'
-     AND l.t$vers=( select max(l1.t$vers)
-                      from baandb.tttadv140000 l1 
-                     where l1.t$clab=l.t$clab 
-                  AND l1.t$clan=l.t$clan 
-                  AND l1.t$cpac=l.t$cpac)) iTIPOXD
+INNER JOIN baandb.tznsls401301 znsls401
+        ON znsls400.t$ncia$c = znsls401.t$ncia$c 
+       AND znsls400.t$uneg$c = znsls401.T$UNEG$c 
+       AND znsls400.t$pecl$c = znsls401.T$pecl$c 
+       AND znsls400.t$sqpd$c = znsls401.T$sqpd$c
     
-WHERE znsls400.t$ncia$c = znsls401.t$ncia$c 
-  AND znsls400.t$uneg$c = znsls401.T$UNEG$c 
-  AND znsls400.t$pecl$c = znsls401.T$pecl$c 
-  AND znsls400.t$sqpd$c = znsls401.T$sqpd$c
-  AND znsls401.t$orno$c = tdsls401.t$orno 
-  AND znsls401.t$pono$c = tdsls401.t$pono
-  AND znsls401.t$orno$c = tdsls400.t$orno
-  AND znsls004.t$ncia$c = znsls401.t$ncia$c 
-  AND znsls004.t$uneg$c = znsls401.T$UNEG$c 
-  AND znsls004.t$pecl$c = znsls401.T$pecl$c 
-  AND znsls004.t$sqpd$c = znsls401.T$sqpd$c 
-  AND znsls004.t$orno$c = znsls401.T$orno$c
-  AND znsls004.t$pono$c = znsls401.T$pono$c
-  AND tcibd001.t$item = tdsls401.t$item
-  AND tcemm124.t$cwoc = tdsls400.t$cofc
-  AND tcemm122.t$grid = tcemm124.t$grid
-  AND tccom100b.t$bpid=tcemm122.t$bupa
-  AND tccom130b.t$cadr=tccom100b.t$cadr
-  AND tcemm124.t$dtyp = 1 
-  AND tcemm030.t$eunt = tcemm124.t$grid
-  AND tcibd100.t$item = tcibd001.t$item
+INNER JOIN baandb.ttdsls400301 tdsls400 
+        ON znsls401.t$orno$c = tdsls400.t$orno
+
+ LEFT JOIN baandb.ttttxt010301 tttxt010 
+        ON tttxt010.t$ctxt = tdsls400.t$txta
+       AND tttxt010.t$seqe = 1  
+
+INNER JOIN baandb.ttdsls401301 tdsls401
+        ON znsls401.t$orno$c = tdsls401.t$orno 
+       AND znsls401.t$pono$c = tdsls401.t$pono
+
+ LEFT JOIN baandb.tznwmd200301 znwmd200
+        ON znwmd200.t$item$c = tdsls401.t$item
+       AND znwmd200.t$cwar$c = tdsls401.t$cwar          
+       AND znwmd200.t$supl$c = tdsls401.T$ofbp
+      
+ LEFT JOIN baandb.twhinp200301 whinp200
+        ON whinp200.t$oorg = 3
+       AND whinp200.t$orno = tdsls401.t$orno
+       AND whinp200.t$pono = tdsls401.t$pono
+       AND whinp200.t$serb = tdsls401.t$sqnb
+       AND whinp200.t$item = tdsls401.t$item
+
+INNER JOIN baandb.ttcibd001301 tcibd001
+        ON tcibd001.t$item = tdsls401.t$item
+
+ LEFT JOIN baandb.ttdipu001301 tdipu001
+        ON tdipu001.t$item = tcibd001.t$item
+ 
+ LEFT JOIN baandb.ttccom130301 tccom130
+        ON tccom130.t$cadr = tdipu001.t$otbp
+       
+INNER JOIN baandb.ttcibd100301 tcibd100
+        ON tcibd100.t$item = tcibd001.t$item
+
+INNER JOIN baandb.ttcmcs023301 tcmcs023
+        ON tcmcs023.t$citg = tcibd001.t$citg
+
+INNER JOIN baandb.twhinp100301 whinp100
+        ON whinp100.t$item = tdsls401.t$item 
+       AND whinp100.t$orno = tdsls401.t$orno
+
+INNER JOIN baandb.tznsls004301 znsls004
+        ON znsls004.t$ncia$c = znsls401.t$ncia$c 
+       AND znsls004.t$uneg$c = znsls401.T$UNEG$c 
+       AND znsls004.t$pecl$c = znsls401.T$pecl$c 
+       AND znsls004.t$sqpd$c = znsls401.T$sqpd$c 
+       AND znsls004.t$orno$c = znsls401.T$orno$c
+       AND znsls004.t$pono$c = znsls401.T$pono$c
+      
+INNER JOIN baandb.ttcemm124301 tcemm124
+        ON tcemm124.t$cwoc = tdsls400.t$cofc
+
+INNER JOIN baandb.ttcemm122301 tcemm122
+        ON tcemm122.t$grid = tcemm124.t$grid
+
+INNER JOIN baandb.ttccom100301 tccom100b
+        ON tccom100b.t$bpid = tcemm122.t$bupa
+      
+INNER JOIN baandb.ttccom130301 tccom130b
+        ON tccom130b.t$cadr = tccom100b.t$cadr
+      
+INNER JOIN baandb.ttcemm030301 tcemm030
+        ON tcemm030.t$eunt = tcemm124.t$grid
+      
+INNER JOIN baandb.ttcmcs031301 tcmcs031
+        ON tcmcs031.t$cbrn = tdsls400.T$CBRN
+      
+INNER JOIN baandb.tznint002301 znint002
+        ON znint002.t$uneg$c = znsls400.t$uneg$c
+       AND znint002.t$ncia$c = znsls400.t$ncia$c
+      
+INNER JOIN baandb.tznsls002301 znsls002
+        ON znsls002.t$tpen$c = znsls401.t$itpe$c
+  
+ LEFT JOIN( SELECT d.t$cnst CODE,
+                   l.t$desc DESCR
+              FROM baandb.tttadv401000 d,
+                   baandb.tttadv140000 l
+             WHERE d.t$cpac = 'zn'
+               AND d.t$cdom = 'ipu.ixdn.c'
+               AND l.t$clan = 'p'
+               AND l.t$cpac = 'zn' 
+               AND l.t$clab = d.t$za_clab
+               AND rpad(d.t$vers,4) || 
+                   rpad(d.t$rele,2) || 
+                   rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) || 
+                                                   rpad(l1.t$rele,2) || 
+                                                   rpad(l1.t$cust,4) ) 
+                                          from baandb.tttadv401000 l1 
+                                         where l1.t$cpac = d.t$cpac 
+                                           and l1.t$cdom = d.t$cdom )
+               AND rpad(l.t$vers,4) || 
+                   rpad(l.t$rele,2) || 
+                   rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) || 
+                                                   rpad(l1.t$rele,2) || 
+                                                   rpad(l1.t$cust,4) ) 
+                                          from baandb.tttadv140000 l1 
+                                         where l1.t$clab = l.t$clab 
+                                           and l1.t$clan = l.t$clan 
+                                           and l1.t$cpac = l.t$cpac ) ) iTIPOXD
+        ON iTIPOXD.CODE = tdipu001.t$ixdn$c
+    
+WHERE tcemm124.t$dtyp = 1 
   AND whinp100.t$koor = 3 
   AND whinp100.t$kotr = 2 
-  AND whinp100.t$item = tdsls401.t$item 
-  AND whinp100.t$orno = tdsls401.t$orno
-  AND tcmcs023.t$citg = tcibd001.t$citg
-  AND tcmcs031.t$cbrn = tdsls400.T$CBRN
-  AND znsls002.t$tpen$c = znsls401.t$itpe$c
-  AND iTIPOXD.CODE = tdipu001.t$ixdn$c
-  AND znint002.t$uneg$c = znsls400.t$uneg$c
-  AND znint002.t$ncia$c = znsls400.t$ncia$c
-  
   
   AND Trunc(tdsls400.t$ddat) Between NVL(:DataEntregaDe, tdsls400.t$ddat) and  NVL(:DataEntregaAte, tdsls400.t$ddat)
   AND Trunc(tdsls400.t$prdt) Between NVL(:DataRecebeDe, tdsls400.t$prdt) and  NVL(:DataRecebeAte, tdsls400.t$prdt)
