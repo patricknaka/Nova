53ea6d7fb2088ded96b90d65df2ef1c5c4d700c7
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-09-30 12:48:23 -0300

    LN_Transport_Operações - Transporte DOCA_DIT_DRT
    Atualização Timezone

--- Relatórios LN/LN_Transport_Operações - Transporte DOCA_DIT_DRT.sql
+++ Relatórios LN/LN_Transport_Operações - Transporte DOCA_DIT_DRT.sql
@@ -3,13 +3,13 @@
             cisli940.t$docn$l   NF,
             cisli940.t$seri$l   SERIE,
             ( select a.t$fovn$l 
-                from ttccom130201 a
+                from baandb.ttccom130201 a
                where a.t$cadr = cisli940.t$sfra$l ) 
                                 CNPJ_FILIAL,
             znfmd630.t$fili$c   NR_FILIAL
-       FROM tznsls005201 znsls005
- INNER JOIN tcisli940201 cisli940 
+       FROM baandb.tznsls005201 znsls005
+ INNER JOIN baandb.tcisli940201 cisli940 
          ON cisli940.t$fire$l = znsls005.t$firs$c
- INNER JOIN tznfmd630201 znfmd630 
+ INNER JOIN baandb.tznfmd630201 znfmd630 
          ON znfmd630.t$orno$c = znsls005.t$orno$c
       WHERE znsls005.t$orde$c != ' '
