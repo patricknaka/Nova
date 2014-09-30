0c98a091ba1456405fb833d683b4756140a74e91
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-09-30 13:51:58 -0300

    LN_Transporte Financeiro - NR por Emissao
    Atualização Timezone

--- Relatórios LN/LN_Transporte Financeiro - NR por Emissao.sql
+++ Relatórios LN/LN_Transporte Financeiro - NR por Emissao.sql
@@ -6,26 +6,29 @@ select
         AND tcemm030.t$eunt=tcemm124.t$grid
         AND tcemm124.t$loco=201
         AND rownum = 1 ) 
-                      NOCA_ID_FILIAL,
+                          NOCA_ID_FILIAL,
 
     tdrec940.t$docn$l || '/' || 
-    tdrec940.t$seri$l NOTA_FORN,
-    tdrec947.t$rcno$l RECEBTO,
-    tdrec940.t$cpay$l CONPAG,
-    tcmcs013a.t$dsca  DESCRICAO,
-    tdrec940.t$fire$l NOCA_ID_NR,
-    tdrec940.t$fovn$l CNPJ,
-    tdrec940.t$fids$l NOME,
-    tdrec940.t$idat$l DT_EMISSAO,
-    tdrec940.t$opfc$l ID_NATOPE,
-    tdrec940.t$opor$l SEQ_NATOPE,
-    tcmcs940.t$dsca$l CFO_NOME,
+    tdrec940.t$seri$l     NOTA_FORN,
+    tdrec947.t$rcno$l     RECEBTO,
+    tdrec940.t$cpay$l     CONPAG,
+    tcmcs013a.t$dsca      DESCRICAO,
+    tdrec940.t$fire$l     NOCA_ID_NR,
+    tdrec940.t$fovn$l     CNPJ,
+    tdrec940.t$fids$l     NOME,
+    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdrec940.t$idat$l, 
+      'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+        AT time zone sessiontimezone) AS DATE)
+                          DT_EMISSAO,
+    tdrec940.t$opfc$l     ID_NATOPE,
+    tdrec940.t$opor$l     SEQ_NATOPE,
+    tcmcs940.t$dsca$l     CFO_NOME,
     Trim(tdrec941.t$item$l) 
-                      ID_ITEM,
-    tdrec941.t$dsca$l DESC_ITEM,
-    tcmcs023.t$dsca   DEPA_NOME,
-    tdrec947.t$orno$l PECD_NUM_PED,
-    tdpur400.t$corg   TP_GERACAO,
+                          ID_ITEM,
+    tdrec941.t$dsca$l     DESC_ITEM,
+    tcmcs023.t$dsca       DEPA_NOME,
+    tdrec947.t$orno$l     PECD_NUM_PED,
+    tdpur400.t$corg       TP_GERACAO,
     
     ( SELECT l.t$desc DS_TIPO_CADASTRO
         FROM baandb.tttadv401000 d,
@@ -42,12 +45,18 @@ select
          AND l.t$rele='a'
          AND l.t$cust=' '
          AND d.t$cnst=tdpur400.t$corg ) 
-                      DESCR_TP_GERACAO,    
-    tdpur400.t$cpay   CONPAG_PED,
-    tcmcs013b.t$dsca  CONP_NOME_PED,
-    tdpur400.t$odat   DT_EMISSAO_PED,
-    tdpur400.t$ddat   DT_ENTREGA_PED,
-    tdpur400.t$hdst   NOCA_SITUACAO,
+                          DESCR_TP_GERACAO,    
+    tdpur400.t$cpay       CONPAG_PED,
+    tcmcs013b.t$dsca      CONP_NOME_PED,
+    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdpur400.t$odat, 
+      'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+        AT time zone sessiontimezone) AS DATE)
+                          DT_EMISSAO_PED,
+    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdpur400.t$ddat, 
+      'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+        AT time zone sessiontimezone) AS DATE)
+                          DT_ENTREGA_PED,
+    tdpur400.t$hdst       NOCA_SITUACAO,
     
     ( SELECT l.t$desc DS_SITUACAO_PEDIDO
         FROM baandb.tttadv401000 d,
@@ -68,38 +77,38 @@ select
          AND d.t$cnst=tdpur400.t$hdst) 
                       DESCR_NOCA_SITUACAO,
       
-    tdpur401.t$qidl   QT_RECEBIDA,
-    tdpur401.t$qoor   QT_PEDIDA,  
-    tdrec941.t$gamt$l VL_MERC_ITEM,
-    tdrec941.t$tamt$l VL_TOTAL,
+    tdpur401.t$qidl       QT_RECEBIDA,
+    tdpur401.t$qoor       QT_PEDIDA,  
+    tdrec941.t$gamt$l     VL_MERC_ITEM,
+    tdrec941.t$tamt$l     VL_TOTAL,
  
     ( SELECT tdrec942.t$amnt$l 
         FROM baandb.ttdrec942201 tdrec942
        WHERE tdrec942.t$fire$l=tdrec941.t$fire$l
          AND tdrec942.t$line$l=tdrec941.t$line$l
          AND tdrec942.t$brty$l=1 ) 
-                      VL_ICMS,
+                          VL_ICMS,
 
     ( SELECT tdrec942.t$amnt$l 
         FROM baandb.ttdrec942201 tdrec942
        WHERE tdrec942.t$fire$l=tdrec941.t$fire$l
          AND tdrec942.t$line$l=tdrec941.t$line$l
          AND tdrec942.t$brty$l=5) 
-                      VL_PIS,
+                          VL_PIS,
 
     ( SELECT tdrec942.t$amnt$l 
         FROM baandb.ttdrec942201 tdrec942
        WHERE tdrec942.t$fire$l=tdrec941.t$fire$l
          AND tdrec942.t$line$l=tdrec941.t$line$l
          AND tdrec942.t$brty$l=6) 
-                      VL_COFINS,
+                          VL_COFINS,
  
     ( SELECT tdrec942.t$amnt$l 
         FROM baandb.ttdrec942201 tdrec942
        WHERE tdrec942.t$fire$l=tdrec941.t$fire$l
          AND tdrec942.t$line$l=tdrec941.t$line$l
          AND tdrec942.t$brty$l=3) 
-                      VL_IPI
+                          VL_IPI
   
 from baandb.ttdrec940201 tdrec940,
      baandb.ttdrec941201 tdrec941,
@@ -123,4 +132,6 @@ where tdrec947.t$fire$l = tdrec941.t$fire$l
   and tcmcs013a.t$cpay  = tdrec940.t$cpay$l 
   and tcmcs013b.t$cpay  = tdpur400.t$cpay
   
-  and Trunc(tdrec940.t$idat$l) Between &DataEmissaoNFDe and &DataEmissaoNFAte
+  and Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdrec940.t$idat$l, 
+      'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+        AT time zone sessiontimezone) AS DATE)) Between &DataEmissaoNFDe and &DataEmissaoNFAte
