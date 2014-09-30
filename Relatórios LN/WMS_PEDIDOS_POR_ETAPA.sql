3e7cff6fcd933c826c24d0fc2d81a74b57db7b9b
rosanaprignolato <rosana.prignolato@iteris.com.br>
2014-09-30 16:52:09 -0300

    WMS_PEDIDOS_POR_ETAPA
    Atualização Timezone

--- Relatórios LN/WMS_PEDIDOS_POR_ETAPA.sql
+++ Relatórios LN/WMS_PEDIDOS_POR_ETAPA.sql
@@ -1,16 +1,22 @@
-SELECT  
+﻿SELECT  
   DISTINCT 
     -- orderdetail.orderkey || 
     -- orderdetail.ORDERLINENUMBER           CHAVE,
     WMSADMIN.PL_DB.DB_ALIAS               DSC_PLANTA,
-	CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ORDERS.SCHEDULEDSHIPDATE, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-	AT time zone sessiontimezone) AS DATE) DATA_LIMITE,
+    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ORDERS.SCHEDULEDSHIPDATE, 
+      'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+        AT time zone sessiontimezone) AS DATE)
+                                          DATA_LIMITE,
     ORDERS.ORDERKEY                       PEDIDO,
-	CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ORDERS.ADDDATE , 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-	AT time zone sessiontimezone) AS DATE) DATA_REGISTRO,
+    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ORDERS.ADDDATE, 
+      'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+        AT time zone sessiontimezone) AS DATE) 
+                                          DATA_REGISTRO,
     ORDERSTATUSSETUP2.DESCRIPTION         EVENTO,
-	CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ORDERSTATUSHISTORY.ADDDATE , 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-	AT time zone sessiontimezone) AS DATE) DATA_ULT_EVENTO,                
+    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ORDERSTATUSHISTORY.ADDDATE, 
+      'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+        AT time zone sessiontimezone) AS DATE) 
+                                          DATA_ULT_EVENTO,                
     ORDERSTATUSHISTORY.ADDWHO             OPERADOR,
     ORDERDETAIL.SKU                       ITEM,
     SKU.DESCR                             NOME_ITEM,
@@ -38,64 +44,74 @@ SELECT
     ORDERS.C_CITY                         MUNICIPIO,
     ORDERS.C_STATE                        ESTADO,
     ORDERDETAIL.SHIPPEDQTY                QUANTIDADE,
-	sls400.VALOR,
-	(select clkp.description from WMWHSE5.codelkup clkp
-	 where clkp.code=orders.INCOTERM
-	 and clkp.listname='INCOTERMS') REDESPACHO,
-	CASE WHEN sls400.t$tpes$c='X' THEN 'Crossdocking'
-        WHEN sls400.t$tpes$c='F' THEN 'Fingido'
-        WHEN sls400.t$tpes$c='P' THEN 'Pré-Venda'
-        ELSE 'NORMAL' 
-    END                TIPO_ESTOQ,
-	(select clkp.description from WMWHSE5.codelkup clkp
-	 where clkp.code=ORDERDETAIL.SHORTSHIPREASON
-	 and clkp.listname='SHORTRSNS') RESTRICAO,
-	maucLN.mauc,
-	ORDERS.C_ADDRESS1	DESTINATARIO
-	
+    sls400.                               VALOR,
+    (select clkp.description from WMWHSE5.codelkup clkp
+     where clkp.code=orders.INCOTERM
+     and clkp.listname='INCOTERMS')       REDESPACHO,
+    CASE WHEN sls400.t$tpes$c='X' THEN 'Crossdocking'
+          WHEN sls400.t$tpes$c='F' THEN 'Fingido'
+          WHEN sls400.t$tpes$c='P' THEN 'Pré-Venda'
+          ELSE 'NORMAL' 
+      END                                 TIPO_ESTOQ,
+    (select clkp.description from WMWHSE5.codelkup clkp
+     where clkp.code=ORDERDETAIL.SHORTSHIPREASON
+     and clkp.listname='SHORTRSNS')       RESTRICAO,
+    maucLN.mauc,
+    ORDERS.C_ADDRESS1	                    DESTINATARIO
 	
 FROM WMWHSE5.ORDERS
+
 INNER JOIN WMWHSE5.ORDERDETAIL 
         ON ORDERDETAIL.ORDERKEY = ORDERS.ORDERKEY
+        
 INNER JOIN WMWHSE5.SKU
         ON SKU.SKU = ORDERDETAIL.SKU 
-LEFT JOIN ENTERPRISE.CODELKUP cl 	ON 	UPPER(cl.UDF1)=UPPER(ORDERS.WHSEID)
-									AND	cl.listname='SCHEMA'
+        
+LEFT JOIN  ENTERPRISE.CODELKUP cl 	
+       ON  UPPER(cl.UDF1)=UPPER(ORDERS.WHSEID)
+      AND	 cl.listname='SCHEMA'
+      
 LEFT JOIN                                  
-  (	select    trim(whina113.t$item) item,
-			whina113.t$cwar cwar,
-			sum(whina113.t$mauc$1) mauc
-	from      BAANDB.Twhina113301@pln01 whina113
-	where (whina113.t$trdt, whina113.t$seqn)=(	select max(b.t$trdt), max(b.t$seqn) 
-												from BAANDB.Twhina113301@pln01 b
+  (	select trim(whina113.t$item) item,
+           whina113.t$cwar cwar,
+           sum(whina113.t$mauc$1) mauc
+    from  BAANDB.Twhina113301@pln01 whina113
+    where (whina113.t$trdt, whina113.t$seqn) = (select max(b.t$trdt), max(b.t$seqn) 
+												from  BAANDB.Twhina113301@pln01 b
 												where b.t$item=whina113.t$item
 												and   b.t$cwar=whina113.t$cwar
-												and   b.t$trdt=(select max(c.t$trdt) from BAANDB.Twhina113301@pln01 c
+												and   b.t$trdt = (select max(c.t$trdt) from BAANDB.Twhina113301@pln01 c
 																where c.t$item=b.t$item
 																and   c.t$cwar=b.t$cwar))                                    
-	group by  whina113.t$item,
-		  whina113.t$cwar) maucLN   ON  maucLN.cwar=subStr(cl.DESCRIPTION,3,6)
-									AND maucLN.item=sku.sku
+    group by  whina113.t$item, whina113.t$cwar) maucLN   
+        ON maucLN.cwar=subStr(cl.DESCRIPTION,3,6)
+       AND maucLN.item=sku.sku
 		
  LEFT JOIN WMWHSE5.WAVEDETAIL
         ON WAVEDETAIL.ORDERKEY = ORDERDETAIL.ORDERKEY  
+        
  LEFT JOIN WMWHSE5.CAGEIDDETAIL
         ON CAGEIDDETAIL.ORDERID = ORDERDETAIL.ORDERKEY 
+        
  LEFT JOIN WMWHSE5.CAGEID
         ON CAGEID.CAGEID = CAGEIDDETAIL.CAGEID
+        
 INNER JOIN WMSADMIN.PL_DB
         ON UPPER(WMSADMIN.PL_DB.DB_LOGID) = ORDERS.WHSEID 
+        
  LEFT JOIN BAANDB.TZNSLS002301@pln01 SLS002
         ON UPPER(TRIM(SLS002.t$dsca$c)) = UPPER(TRIM(ORDERS.SUSR1)) 
+        
  LEFT JOIN BAANDB.TWHWMD400301@pln01 whwmd400 
         ON TRIM(whwmd400.t$item) = TRIM(SKU.SKU)
+        
  LEFT JOIN ( SELECT a.ORDERKEY, 
                     a.ORDERLINENUMBER , 
                     max(a.STATUS) STATUS, 
                     max(a.ADDDATE) ADDDATE, 
                     max(a.ADDWHO) ADDWHO 
-               FROM WMWHSE5.ORDERSTATUSHISTORY a
-              WHERE a.ADDDATE = ( select max(b.adddate) 
+                 FROM WMWHSE5.ORDERSTATUSHISTORY a
+                WHERE a.ADDDATE = ( select max(b.adddate) 
                                     from WMWHSE5.ORDERSTATUSHISTORY b
                                    where b.ORDERKEY = a.ORDERKEY
                                      and b.ORDERLINENUMBER = a.ORDERLINENUMBER )
@@ -103,14 +119,18 @@ INNER JOIN WMSADMIN.PL_DB
                     a.ORDERLINENUMBER ) ORDERSTATUSHISTORY
         ON ORDERSTATUSHISTORY.ORDERKEY = ORDERDETAIL.ORDERKEY 
        AND ORDERSTATUSHISTORY.ORDERLINENUMBER = ORDERDETAIL.ORDERLINENUMBER 
-       AND ORDERSTATUSHISTORY.STATUS = ORDERDETAIL.STATUS            
+       AND ORDERSTATUSHISTORY.STATUS = ORDERDETAIL.STATUS  
+       
  LEFT JOIN WMWHSE5.ORDERSTATUSSETUP
         ON ORDERSTATUSSETUP.CODE = ORDERS.STATUS
+        
  LEFT JOIN WMWHSE5.ORDERSTATUSSETUP ORDERSTATUSSETUP2
-        ON ORDERSTATUSSETUP2.CODE = ORDERSTATUSHISTORY.STATUS   
+        ON ORDERSTATUSSETUP2.CODE = ORDERSTATUSHISTORY.STATUS
+        
  LEFT JOIN WMWHSE5.TASKDETAIL
         ON TASKDETAIL.ORDERKEY = ORDERDETAIL.ORDERKEY 
-       AND TASKDETAIL.ORDERLINENUMBER = ORDERDETAIL.ORDERLINENUMBER     
+       AND TASKDETAIL.ORDERLINENUMBER = ORDERDETAIL.ORDERLINENUMBER  
+       
  LEFT JOIN ( select 
 				q.T$IDCA$C, 
 				ZNSLS004.T$ORNO$C,
@@ -136,9 +156,11 @@ INNER JOIN WMSADMIN.PL_DB
 								 znsls401.t$tpes$c) SLS400
         ON  SLS400.T$ORNO$C = ORDERS.REFERENCEDOCUMENT
         AND SLS400.t$item$c = ORDERDETAIL.SKU
+        
 WHERE SLS002.T$TPEN$C IN (:TipoEntrega)
-  AND Trunc(ORDERSTATUSHISTORY.ADDDATE) Between :DataUltEventoDe 
-  AND :DataUltEventoAte
+  AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ORDERSTATUSHISTORY.ADDDATE, 
+      'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+        AT time zone sessiontimezone) AS DATE)) Between :DataUltEventoDe AND :DataUltEventoAte
   AND ORDERSTATUSSETUP.CODE IN (:ClasseEventos)
   AND ORDERS.C_VAT IN (:MegaRota)
 		
