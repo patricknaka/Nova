418631ed5b892f6634b88a184cd46b22be2dcb70
Humberto Kasai <humberto@globalti.com.br>
2014-10-10 10:52:57 -0300

    Alterar posição VL_CMV Estoque Reversa
    A posição onde foi colocada o campo VL_CMV estava em local incorreto, alterando uma operação de outro campo.

--- Relatórios LN/LN_query_estoque_reversa.sql
+++ Relatórios LN/LN_query_estoque_reversa.sql
@@ -14,16 +14,15 @@ select *
                       int1.T$NCIA$C AS     ID_CIA,
                       fmd.T$FILI$C AS      ESTABELECIMENTO,
                       est.T$QHND,
-                      q1.mauc               VL_CMV,
                       - 
                       ( select nvl(sum(whwmd630.t$qbls), 0)
-                          from baandb.twhwmd630201    whwmd630
+                          from baandb.twhwmd630301    whwmd630
                          where whwmd630.t$item = est.T$ITEM
                            and whwmd630.t$cwar = est.T$CWAR )
                       - 
                       ( select NVL(SUM(whinp100.t$qana),0)
-                          from baandb.twhinp100201 whinp100
-                    inner join baandb.ttdsls401201 tdsls401
+                          from baandb.twhinp100301 whinp100
+                    inner join baandb.ttdsls401301 tdsls401
                             on tdsls401.t$orno = whinp100.t$orno
                            and tdsls401.t$pono = whinp100.t$pono
                            and tdsls401.t$sqnb = whinp100.t$ponb
@@ -36,10 +35,10 @@ select *
                            and whinp100.t$cdis$c = ' ' )  -- Sem restriç?o
                       - 
                       ( select NVL(SUM(sls2.T$QOOR * sls2.T$CVQS),0)
-                          from baandb.TTDSLS401201 sls2
-                    inner join baandb.TTDSLS420201 sls3
+                          from baandb.TTDSLS401301 sls2
+                    inner join baandb.TTDSLS420301 sls3
                             on sls2.T$ORNO = sls3.T$ORNO
-                    inner join baandb.TTDSLS090201 sls4
+                    inner join baandb.TTDSLS090301 sls4
                             on sls3.T$HREA = sls4.T$HREA
                          where sls2.T$ITEM = est.T$ITEM
                            and sls2.T$CWAR = est.T$CWAR
@@ -49,9 +48,9 @@ select *
                      
                      CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(est.t$rcd_utc, 
                        'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-                         AT time zone sessiontimezone) AS DATE) AS ULTIMAATUAL
-
-                 FROM baandb.TWHWMD215201 est
+                         AT time zone sessiontimezone) AS DATE) AS ULTIMAATUAL,
+                      q1.mauc               VL_CMV 
+                 FROM baandb.TWHWMD215301 est
                  
          LEFT JOIN (  SELECT 
                       whwmd217.t$item,
@@ -59,25 +58,25 @@ select *
                       case when (max(whwmd215.t$qhnd))=0 then 0
                       else round(sum(whwmd217.t$mauc$1)/(max(whwmd215.t$qhnd)),4) 
                       end mauc
-                      FROM baandb.twhwmd217201 whwmd217, baandb.twhwmd215201 whwmd215
+                      FROM baandb.twhwmd217301 whwmd217, baandb.twhwmd215301 whwmd215
                       WHERE whwmd215.t$cwar=whwmd217.t$cwar
                       AND whwmd215.t$item=whwmd217.t$item
                       group by  whwmd217.t$item, whwmd217.t$cwar) q1 
               ON q1.t$item = est.t$item AND q1.t$cwar = est.t$cwar
               
-           INNER JOIN baandb.TTCIBD001201 item  
+           INNER JOIN baandb.TTCIBD001301 item  
                    ON est.T$ITEM = item.T$item
-            LEFT JOIN baandb.TZNISA002201 clas1 
+            LEFT JOIN baandb.TZNISA002301 clas1 
                    ON item.T$NPCL$C = clas1.T$NPCL$C
-            LEFT JOIN baandb.TZNISA001201 clas2 
+            LEFT JOIN baandb.TZNISA001301 clas2 
                    ON clas1.T$NPTP$C = clas2.T$NPTP$C
-           INNER JOIN baandb.TTCMCS003201 mcs   
+           INNER JOIN baandb.TTCMCS003301 mcs   
                    ON est.T$CWAR = mcs.T$CWAR
-           INNER JOIN baandb.TZNINT001201 int1  
+           INNER JOIN baandb.TZNINT001301 int1  
                    ON mcs.T$COMP = int1.T$COMP$C
-           INNER JOIN baandb.TTCCOM130201 com   
+           INNER JOIN baandb.TTCCOM130301 com   
                    ON mcs.T$CADR = com.T$CADR
-           INNER JOIN baandb.TZNFMD001201 fmd   
+           INNER JOIN baandb.TZNFMD001301 fmd   
                    ON com.T$FOVN$L = fmd.T$FOVN$C
  
 
@@ -115,8 +114,8 @@ select *
                      ( Sum(est.T$QBLS)
                        - 
                        ( SELECT NVL(SUM(sls.T$QOOR * sls.T$CVQS),0) AS qtdeReservado
-                           FROM baandb.TWHINH220201 inh
-                     INNER JOIN baandb.TTDSLS401201 sls
+                           FROM baandb.TWHINH220301 inh
+                     INNER JOIN baandb.TTDSLS401301 sls
                              ON sls.T$ORNO = inh.T$ORNO
                             AND sls.T$PONO = inh.T$PONO
                             AND sls.T$SQNB = inh.T$SEQN
@@ -124,15 +123,14 @@ select *
                             AND inh.T$LSTA <> 30
                             AND inh.T$ITEM = est.T$ITEM
                             AND sls.t$cdis$c = est.t$bloc ) ),
-                      MAX(q1.mauc)        VL_CMV,
                  -
                  ( SELECT NVL(SUM(sls2.T$QOOR * sls2.T$CVQS),0)
-                     FROM baandb.TTDSLS401201 sls2
-               INNER JOIN baandb.TTDSLS420201 sls3
+                     FROM baandb.TTDSLS401301 sls2
+               INNER JOIN baandb.TTDSLS420301 sls3
                        ON sls2.T$ORNO = sls3.T$ORNO
                       AND sls2.T$PONO = sls3.T$PONO
                       AND sls2.T$SQNB = sls3.T$SQNB
-               INNER JOIN baandb.TTDSLS090201 sls4
+               INNER JOIN baandb.TTDSLS090301 sls4
                        ON sls3.T$HREA = sls4.T$HREA
                     WHERE sls2.T$ITEM = est.T$ITEM
                       AND sls4.t$REST$C = 1
@@ -141,10 +139,11 @@ select *
                      --MAX(TO_CHAR(est.T$RCD_UTC, 'YYYY-MM-DD HH24:MI:SS')) AS ULTIMAATUAL
                      CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(max(est.t$rcd_utc), 
                        'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-                         AT time zone sessiontimezone) AS DATE) AS ULTIMAATUAL
+                         AT time zone sessiontimezone) AS DATE) AS ULTIMAATUAL,
+                  MAX(q1.mauc)        VL_CMV
 
          			 
-                 FROM baandb.TWHWMD630201 est
+                 FROM baandb.TWHWMD630301 est
                  
               LEFT JOIN (  SELECT 
                       whwmd217.t$item,
@@ -152,25 +151,25 @@ select *
                       case when (max(whwmd215.t$qhnd))=0 then 0
                       else round(sum(whwmd217.t$mauc$1)/(max(whwmd215.t$qhnd)),4) 
                       end mauc
-                      FROM baandb.twhwmd217201 whwmd217, baandb.twhwmd215201 whwmd215
+                      FROM baandb.twhwmd217301 whwmd217, baandb.twhwmd215301 whwmd215
                       WHERE whwmd215.t$cwar=whwmd217.t$cwar
                       AND whwmd215.t$item=whwmd217.t$item
                       group by  whwmd217.t$item, whwmd217.t$cwar) q1 
               ON q1.t$item = est.t$item AND q1.t$cwar = est.t$cwar                 
                  
-           INNER JOIN baandb.TTCIBD001201 item 
+           INNER JOIN baandb.TTCIBD001301 item 
                    ON est.T$ITEM = item.T$item
-            LEFT JOIN baandb.TZNISA002201 clas1 
+            LEFT JOIN baandb.TZNISA002301 clas1 
                    ON item.T$NPCL$C = clas1.T$NPCL$C
-            LEFT JOIN baandb.TZNISA001201 clas2 
+            LEFT JOIN baandb.TZNISA001301 clas2 
                    ON clas1.T$NPTP$C = clas2.T$NPTP$C
-           INNER JOIN baandb.TTCMCS003201 mcs 
+           INNER JOIN baandb.TTCMCS003301 mcs 
                    ON est.T$CWAR = mcs.T$CWAR
-           INNER JOIN baandb.TZNINT001201 int1 
+           INNER JOIN baandb.TZNINT001301 int1 
                    ON mcs.T$COMP = int1.T$COMP$C
-           INNER JOIN baandb.TTCCOM130201 com 
+           INNER JOIN baandb.TTCCOM130301 com 
                    ON mcs.T$CADR = com.T$CADR
-           INNER JOIN baandb.TZNFMD001201 fmd 
+           INNER JOIN baandb.TZNFMD001301 fmd 
                    ON com.T$FOVN$L = fmd.T$FOVN$C
          		  
                 WHERE 1 = 1
@@ -195,7 +194,7 @@ AND RESTRICAO IN (:Restricao)
 		 
   select a.t$cdis  CD_TIPO_BLOQUEIO,
          a.t$dsca  DS_TIPO_BLOQUEIO
-    from baandb.ttcmcs005201 a
+    from baandb.ttcmcs005301 a
    where a.t$rstp = 65
 
 UNION
