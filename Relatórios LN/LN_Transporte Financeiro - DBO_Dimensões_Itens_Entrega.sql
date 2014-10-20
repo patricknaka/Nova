5f33ebaea1fb6e55bf0e2389fba2274b6a0c2925
leonardovianna <leonardo.vianna@novapontocom.com.br>
2014-10-20 15:23:18 -0200

    LN_Transporte Financeiro - DBO_Dimensões_Itens_Entrega
    Ajuste Query Domínio, Otimização da Query e ajuste dos DateTimes.

--- Relatórios LN/LN_Transporte Financeiro - DBO_Dimensões_Itens_Entrega.sql
+++ Relatórios LN/LN_Transporte Financeiro - DBO_Dimensões_Itens_Entrega.sql
@@ -1,27 +1,28 @@
 SELECT 
   DISTINCT
-    znfmd630.t$pecl$c     NUME_ENTREGA,
-    cisli940.t$docn$l     NUME_NOTA,
-    cisli940.t$seri$l     NUME_SERIE,
-    znfmd630.t$fili$c     NUME_FILIAL,
-    cisli941.t$item$l     NUME_ITEM,
-    cisli941.t$desc$l     DESC_ITEM,
-    cisli941.T$IPRT$L     VALOR_ITEM,
-    cisli941.T$DQUA$L     QTDE_ITEM,
-    whwmd400.t$hght       ALTURA,
-    whwmd400.t$wdth       LARGURA,
-    whwmd400.t$dpth       COMPRIMENTO,
-    znfmd630.t$wght$c     PESO,
-    whwmd400.t$hght*
-    whwmd400.t$wdth       M3
+    znfmd630.t$pecl$c                  NUME_ENTREGA,
+    cisli940.t$docn$l                  NUME_NOTA,
+    cisli940.t$seri$l                  NUME_SERIE,
+    znfmd630.t$fili$c                  NUME_FILIAL,
+    Trim(cisli941.t$item$l)            NUME_ITEM,
+    cisli941.t$desc$l                  DESC_ITEM,
+    cisli941.T$IPRT$L                  VALOR_ITEM,
+    cisli941.T$DQUA$L                  QTDE_ITEM,
+    whwmd400.t$hght                    ALTURA,
+    whwmd400.t$wdth                    LARGURA,
+    whwmd400.t$dpth                    COMPRIMENTO,
+    znfmd630.t$wght$c                  PESO,
+    whwmd400.t$hght * whwmd400.t$wdth  M3
                       
-FROM baandb.tznfmd630201 znfmd630,
-     baandb.tcisli941201 cisli941,
-     baandb.tcisli940201 cisli940,
-     baandb.twhwmd400201 whwmd400
+FROM       baandb.tznfmd630201 znfmd630
 
-WHERE cisli941.T$FIRE$L=znfmd630.t$fire$c
-  AND cisli940.t$fire$l=cisli941.t$fire$l
-  AND whwmd400.t$item=cisli941.T$ITEM$L
-  
-  AND znfmd630.t$pecl$c IN (:Entrega)
+INNER JOIN baandb.tcisli941201 cisli941
+        ON cisli941.T$FIRE$L = znfmd630.t$fire$c
+
+INNER JOIN baandb.tcisli940201 cisli940
+        ON cisli940.t$fire$l = cisli941.t$fire$l
+
+INNER JOIN baandb.twhwmd400201 whwmd400
+        ON whwmd400.t$item = cisli941.T$ITEM$L
+
+WHERE znfmd630.t$pecl$c IN (:Entrega)
