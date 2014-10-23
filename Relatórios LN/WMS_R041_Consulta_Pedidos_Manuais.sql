2de7876d377e1a30d3544d79b56f862ca7182e18
ronaldocs <ronaldo.cristiano@gmail.com>
2014-10-23 10:26:06 -0200

    Relatório WMS Consulta de Pedidos Manuais
    Otimização

--- Relatórios LN/WMS_R041_Consulta_Pedidos_Manuais.sql
+++ Relatórios LN/WMS_R041_Consulta_Pedidos_Manuais.sql
@@ -4,8 +4,9 @@ SELECT
     cisli940.t$docn$l        NF,
     cisli940.t$seri$l        SERIE,
     
-    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(cisli940.t$date$l, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-          AT time zone sessiontimezone) AS DATE) 
+    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(cisli940.t$date$l, 
+      'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+        AT time zone sessiontimezone) AS DATE) 
                              DT_EMISSAO,
     
     cisli940.t$ccfo$l        CFO,
@@ -22,21 +23,21 @@ SELECT
     cisli941.t$ldam$l        VL_DESC_INC,
     
     NVL(( select cisli943.t$amnt$l 
-            from baandb.tcisli943301 cisli943
+            from baandb.tcisli943201 cisli943
            where cisli943.t$fire$l = cisli941.t$fire$l
              and cisli943.t$line$l = cisli941.t$line$l
              and cisli943.t$brty$l = 1 ), 0)
                              VL_ICMS,
     
     NVL(( select cisli943.t$amnt$l 
-            from baandb.tcisli943301 cisli943
+            from baandb.tcisli943201 cisli943
            where cisli943.t$fire$l = cisli941.t$fire$l
              and cisli943.t$line$l = cisli941.t$line$l
              and cisli943.t$brty$l = 5 ), 0)
                              VL_PIS,
     
     NVL(( select cisli943.t$amnt$l 
-            from baandb.tcisli943301 cisli943
+            from baandb.tcisli943201 cisli943
            where cisli943.t$fire$l = cisli941.t$fire$l
              and cisli943.t$line$l = cisli941.t$line$l
              and cisli943.t$brty$l = 6 ), 0)
@@ -52,49 +53,51 @@ SELECT
     tccom130.t$telp          TEL1,
     tccom130.t$telx          TEL2,
     tccom130.t$enfs$l        EMAIL,
-	cisli940.t$fdtc$l		 ID_TIPO_DOC_FIS,
-	(select a.t$dsca$l from baandb.ttcmcs966301 a
-	 where a.t$fdtc$l=cisli940.t$fdtc$l)	DESCR_TIPO_DOC_FIS
+    cisli940.t$fdtc$l        ID_TIPO_DOC_FIS,
+    tcmcs966.t$dsca$l        DESCR_TIPO_DOC_FIS
     
-FROM       baandb.tcisli940301 cisli940
+FROM       baandb.tcisli940201 cisli940
 
 INNER JOIN ( select tcemm030.t$eunt CHAVE_FILIAL,
                     tcemm030.t$dsca DSC_FILIAL,
                     tcemm124.t$cwoc
-               from baandb.ttcemm124301 tcemm124, 
-                    baandb.ttcemm030301 tcemm030
+               from baandb.ttcemm124201 tcemm124, 
+                    baandb.ttcemm030201 tcemm030
               where tcemm030.t$eunt = tcemm124.t$grid
-                and tcemm124.t$loco = 301 ) FILIAL
+                and tcemm124.t$loco = 201 ) FILIAL
         ON FILIAL.t$cwoc = cisli940.t$cofc$l 
 
- LEFT JOIN baandb.ttcmcs940301 tcmcs940 
+ LEFT JOIN baandb.ttcmcs940201 tcmcs940 
         ON tcmcs940.T$OFSO$L = cisli940.t$ccfo$l
 
-INNER JOIN baandb.tcisli941301 cisli941
+INNER JOIN baandb.tcisli941201 cisli941
         ON cisli941.t$fire$l = cisli940.t$fire$l
   
-INNER JOIN baandb.tcisli245301 cisli245
+INNER JOIN baandb.tcisli245201 cisli245
         ON cisli245.t$fire$l = cisli941.t$fire$l
        AND cisli245.t$line$l = cisli941.t$line$l
 
- LEFT JOIN baandb.tznsls401301 znsls401
+ LEFT JOIN baandb.tznsls401201 znsls401
         ON znsls401.t$orno$c = cisli245.t$slso
        AND znsls401.t$pono$c = cisli245.t$pono
 
- LEFT JOIN baandb.tznsls400301 znsls400
+ LEFT JOIN baandb.tznsls400201 znsls400
         ON znsls400.t$ncia$c = znsls401.t$ncia$c
        AND znsls400.t$uneg$c = znsls401.t$uneg$c
        AND znsls400.t$pecl$c = znsls401.t$pecl$c
        AND znsls400.t$sqpd$c = znsls401.t$sqpd$c
   
-INNER JOIN baandb.ttccom130301 tccom130
+INNER JOIN baandb.ttccom130201 tccom130
         ON tccom130.t$cadr   = cisli940.t$itbp$l
   
-INNER JOIN baandb.ttcibd001301 tcibd001
+INNER JOIN baandb.ttcibd001201 tcibd001
         ON tcibd001.t$item   = cisli941.t$item$l
   
-WHERE Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(cisli940.t$date$l, 'DD-MON-YYYY HH24:MI:SS'), 
-     'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+ LEFT JOIN baandb.ttcmcs966201 tcmcs966
+        ON tcmcs966.t$fdtc$l = cisli940.t$fdtc$l
+  
+WHERE Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(cisli940.t$date$l, 
+              'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
                 AT time zone sessiontimezone) AS DATE)) 
       Between :EmissaoDe
           And :EmissaoAte
