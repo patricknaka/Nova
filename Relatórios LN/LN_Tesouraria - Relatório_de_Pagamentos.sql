2212f958331fe9b3c9e690e37b7eb90c14095acc
leonardovianna <leonardo.vianna@novapontocom.com.br>
2014-10-27 12:43:11 -0200

    LN_Tesouraria - Relatório_de_Pagamentos
    Inserção NVL para os parâmetros Modalidade e Situação.

--- Relatórios LN/LN_Tesouraria - Relatório_de_Pagamentos.sql
+++ Relatórios LN/LN_Tesouraria - Relatório_de_Pagamentos.sql
@@ -2,8 +2,9 @@ SELECT
   tfcmg011.t$baoc$l  CODE_BANCO,
   tfcmg011.t$agcd$l  CODE_AGENCIA,
   tfcmg001.t$bano    CODE_CONTA,
-  tfcmg103.t$docd    DATA_PAGTO,
-  tfcmg103.t$paym    CODE_METODO_PGTO, 
+  tfcmg103.t$docd    DATA_PAGTO, 
+  NVL(tfcmg103.t$paym,'- Não Definido -')
+                     CODE_METODO_PGTO, 
   tfcmg003.          t$desc,  
   tfcmg103.t$docn    NUME_TITULO,    
   tfacp200a.t$tpay   TIPO_DOCTO,  
@@ -13,45 +14,46 @@ SELECT
   tfcmg103.t$amnt    VLR_PAGTO,
   tfcmg103.t$ptbp    CODE_FORNECEDOR, 
   tccom100.t$nama    DESC_FORNECEDOR,
-  tfcmg109.t$stpp    SITUACAO_PAGTO, 
+  NVL(tfcmg109.t$stpp,0)  
+                     SITUACAO_PAGTO, 
   iSTPP.             DESC_PAGTO,  
   tfacp200a.t$leac   CTA_CONTABIL,
   tfcmg103.t$btno    NUME_LOTE  
   
-FROM       baandb.ttfacp200201 tfacp200
+FROM       baandb.ttfacp200301 tfacp200
 
-INNER JOIN baandb.ttfacp600201 tfacp600
+INNER JOIN baandb.ttfacp600301 tfacp600
         ON tfacp200.t$tdoc   = tfacp600.t$payt
        AND tfacp200.t$docn   = tfacp600.t$payd
        AND tfacp200.t$lino   = tfacp600.t$payl
  
- LEFT JOIN baandb.ttfcmg109201 tfcmg109  
+ LEFT JOIN baandb.ttfcmg109301 tfcmg109  
         ON tfcmg109.t$btno   = tfacp600.t$pbtn
  
- LEFT JOIN baandb.ttfcmg103201 tfcmg103  
+ LEFT JOIN baandb.ttfcmg103301 tfcmg103  
         ON tfcmg103.T$BTNO   = tfcmg109.T$BTNO
        AND tfcmg103.t$ttyp = tfacp200.t$tdoc 
        AND tfcmg103.t$docn = tfacp200.t$docn
  
- LEFT JOIN baandb.ttccom100201 tccom100
+ LEFT JOIN baandb.ttccom100301 tccom100
         ON tccom100.t$bpid   = tfcmg103.t$ptbp
 
- LEFT JOIN BAANDB.ttfcmg003201 tfcmg003
+ LEFT JOIN BAANDB.ttfcmg003301 tfcmg003
         ON tfcmg003.t$paym = tfcmg103.t$paym
 
- LEFT JOIN baandb.ttfcmg001201 tfcmg001  
+ LEFT JOIN baandb.ttfcmg001301 tfcmg001  
         ON tfcmg001.t$bank   = tfacp600.t$bank
  
- LEFT JOIN baandb.ttfcmg001201 tfcmg001f 
+ LEFT JOIN baandb.ttfcmg001301 tfcmg001f 
         ON tfcmg001f.t$bank  = tfacp600.t$basu
  
- LEFT JOIN baandb.ttfcmg011201 tfcmg011  
+ LEFT JOIN baandb.ttfcmg011301 tfcmg011  
         ON tfcmg011.t$bank   = tfcmg001.t$brch
  
- LEFT JOIN baandb.ttfcmg011201 tfcmg011f 
+ LEFT JOIN baandb.ttfcmg011301 tfcmg011f 
         ON tfcmg011f.t$bank  = tfcmg001f.t$brch
   
- LEFT JOIN baandb.ttfacp200201 tfacp200a 
+ LEFT JOIN baandb.ttfacp200301 tfacp200a 
         ON tfacp200a.t$ttyp  = tfacp200.t$ttyp 
        AND tfacp200a.t$ninv  = tfacp200.t$ninv
 
@@ -86,7 +88,7 @@ INNER JOIN baandb.ttfacp600201 tfacp600
 WHERE tfacp200a.t$lino = 0
   
 AND tfcmg103.t$docd BETWEEN NVL(:DtPagtoDe, tfcmg103.t$docd) AND NVL(:DtPagtoAte, tfcmg103.t$docd)
-AND tfcmg103.t$paym IN (:Modalidade)
-AND tfcmg109.t$stpp IN (:Situacao)
+AND NVL(tfcmg103.t$paym,'- Não Definido -') IN (:Modalidade)
+AND NVL(tfcmg109.t$stpp,0) IN (:Situacao)
 AND tfcmg011.t$baoc$l = NVL(:Banco, tfcmg011.t$baoc$l)
 AND tfcmg103.t$btno = NVL(:Lote, tfcmg103.t$btno)
