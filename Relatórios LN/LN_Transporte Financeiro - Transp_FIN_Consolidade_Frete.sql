1220348d52098dcb27ea20e4acf481f4a7ef056c
ronaldocs <ronaldo.cristiano@gmail.com>
2014-10-29 14:18:59 -0200

    Relatório de Transporte Financeiro Consolidado e Frete
    Alteração da query:
    
    Tipo de Docto. excluído e incluído Tipo de Docto. Fiscal

--- Relatórios LN/LN_Transporte Financeiro - Transp_FIN_Consolidade_Frete.sql
+++ Relatórios LN/LN_Transporte Financeiro - Transp_FIN_Consolidade_Frete.sql
@@ -6,8 +6,8 @@ SELECT
     Trim(tcmcs080.t$dsca)       COD_DESC_TRANS,
     COUNT(znfmd630.t$cfrw$c)    QTDE_ENTREGAS,
     SUM(znfmd630.t$vlfC$c)      FRETE_APAGAR,
-    cisli940.t$doty$l           TIPO_DOCTO, 
-    FGET.                       DESC_TIPO_DOCTO,
+    cisli940.t$fdty$l           TIPO_DOCTO_FISCAL, 
+    FGET.                       DESC_TIPO_DOCTO_FISCAL,
     znfmd630.t$fili$c           FILIAL,
 	
     Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd630.t$date$c, 
@@ -21,13 +21,13 @@ INNER JOIN BAANDB.tcisli940301  cisli940
         ON znfmd630.t$fire$c = cisli940.t$fire$l
 
  LEFT JOIN ( SELECT d.t$cnst CNST,
-                    l.t$desc DESC_TIPO_DOCTO
+                    l.t$desc DESC_TIPO_DOCTO_FISCAL
                FROM BAANDB. tttadv401000 d,
                     BAANDB. tttadv140000 l
-              WHERE d.t$cpac = 'tc'
-                AND d.t$cdom = 'doty.l'
+              WHERE d.t$cpac = 'ci'
+                AND d.t$cdom = 'sli.tdff.l'
                 AND l.t$clan = 'p'
-                AND l.t$cpac = 'tc'
+                AND l.t$cpac = 'ci'
                 AND l.t$clab = d.t$za_clab
                 AND rpad(d.t$vers,4) ||
                     rpad(d.t$rele,2) ||
@@ -46,12 +46,12 @@ INNER JOIN BAANDB.tcisli940301  cisli940
                                           where l1.t$clab = l.t$clab 
                                             and l1.t$clan = l.t$clan 
                                             and l1.t$cpac = l.t$cpac ) ) FGET
-        ON cisli940.t$doty$l = FGET.CNST
+        ON cisli940.t$fdty$l = FGET.CNST
 		  
 INNER JOIN baandb.ttcmcs080301  tcmcs080
         ON tcmcs080.t$cfrw = znfmd630.t$cfrw$c
 		
-WHERE cisli940.t$doty$l IN (:TipoDocto)
+WHERE cisli940.t$fdty$l IN (:TipoDocto)
   AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd630.t$date$c, 
               'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
                 AT time zone sessiontimezone) AS DATE)) 
@@ -60,11 +60,11 @@ WHERE cisli940.t$doty$l IN (:TipoDocto)
 
 GROUP BY znfmd630.t$cfrw$c,
          Trim(tcmcs080.t$dsca),
-         cisli940.t$doty$l,
-         FGET.DESC_TIPO_DOCTO,
+         cisli940.t$fdty$l,
+         FGET.DESC_TIPO_DOCTO_FISCAL
          znfmd630.t$fili$c,
          Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd630.t$date$c, 
                'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-                 AT time zone sessiontimezone) AS DATE))
+                 AT time zone sessiontimezone) AS DATE)),
   
 ORDER BY 9, 8, 2
