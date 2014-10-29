24bf0c5cd1e0092b4579580a03b64d367068738c
Humberto Kasai <humberto@globalti.com.br>
2014-10-29 18:11:42 -0200

    Update LN_Relatorio_Reversa - Preventivo Transportes.sql
    Inclusão de novos campos:
    
    ·         Valor Total do Item
    
    ·         UF (Estado/ Cidade de Coleta)
    
    ·         Contatos do Cliente 3 opções (informações requerida pela transportadora)
    
    ·         Endereço de coleta/ Cep – Bairro -  Referência.
    
    ·         CPF do Cliente
    
    ·         Tipo de Entrega Ex: (Agendada, Retira Loja, Normal)

--- Relatórios LN/LN_Relatorio_Reversa - Preventivo Transportes.sql
+++ Relatórios LN/LN_Relatorio_Reversa - Preventivo Transportes.sql
@@ -26,9 +26,23 @@ SELECT
           'DD-MON-YYYY HH24:MI:SS'), 'GMT') AT time zone sessiontimezone) AS DATE) )
                                               Data_da_Ocorrencia,
                                            
-    NULL                                      Status
+    NULL                                      Status,
+    cisli941.t$amnt$l                         VL_TOTAL_ITEM,
+    znsls401.t$ufen$c                         UF,
+    znsls401.t$cide$c                         CIDADE,
+    znsls401.t$tele$c                         TEL,
+    znsls401.t$te1e$c                         TEL_1,
+    znsls401.t$te2e$c                         TEL_2,
+    znsls401.t$loge$c                         ENDERECO,
+    znsls401.t$nume$c                         NUMERO,
+    znsls401.t$come$c                         COMPL,
+    znsls401.t$baie$c                         BAIRRO,
+    znsls401.t$cepe$c                         CEP,
+    znsls401.t$refe$c                         REFERENCIA,
+    znsls401.t$fovn$c                         CPF,
+    znsls002.t$dsca$c                         TIPO_ENTREGA
         
-FROM       baandb.tznsls401201 znsls401
+FROM       baandb.tznsls401301 znsls401
 
  LEFT JOIN (select r.t$date$c,
                    r.t$ncia$c, 
@@ -39,7 +53,7 @@ FROM       baandb.tznsls401201 znsls401
                    r.t$sequ$c,
                    r.t$orno$c,
                    r.t$pono$c
-              from baandb.tznsls004201 r ) znsls004
+              from baandb.tznsls004301 r ) znsls004
      ON znsls004.t$ncia$c = znsls401.t$ncia$c
     AND znsls004.t$uneg$c = znsls401.t$uneg$c
     AND znsls004.t$pecl$c = znsls401.t$pecl$c
@@ -48,42 +62,43 @@ FROM       baandb.tznsls401201 znsls401
     AND znsls004.t$sequ$c = znsls401.t$sequ$c
     AND ROWNUM = 1
                         
- LEFT JOIN baandb.tcisli245201 cisli245
+ LEFT JOIN baandb.tcisli245301 cisli245
         ON cisli245.t$slso = znsls004.t$orno$c
        AND cisli245.t$pono = znsls004.t$pono$c
 
- LEFT JOIN baandb.tcisli940201 cisli940
+ LEFT JOIN baandb.tcisli940301 cisli940
         ON cisli940.t$fire$l = cisli245.t$fire$l
                   
- LEFT JOIN baandb.tcisli941201 cisli941
-        ON cisli941.t$fire$l = cisli940.t$fire$l
-    
- LEFT JOIN baandb.ttdsls400201 tdsls400
+ LEFT JOIN baandb.tcisli941301 cisli941
+        ON cisli941.t$fire$l = cisli245.t$fire$l
+      AND  cisli941.t$line$l = cisli245.t$line$l
+      
+ LEFT JOIN baandb.ttdsls400301 tdsls400
         ON tdsls400.t$orno = cisli245.t$slso
     
- LEFT JOIN baandb.ttcmcs065201 tcmcs065
+ LEFT JOIN baandb.ttcmcs065301 tcmcs065
         ON tcmcs065.t$cwoc = tdsls400.t$cofc
     
- LEFT JOIN baandb.ttccom130201 tccom130
+ LEFT JOIN baandb.ttccom130301 tccom130
         ON tccom130.t$cadr = tcmcs065.t$cadr
     
- LEFT JOIN baandb.tznfmd001201 znfmd001
+ LEFT JOIN baandb.tznfmd001301 znfmd001
         ON znfmd001.t$fovn$c = tccom130.t$fovn$l
     
- LEFT JOIN baandb.tznfmd630201 znfmd630
+ LEFT JOIN baandb.tznfmd630301 znfmd630
         ON znfmd630.t$orno$c = cisli245.t$slso
     
  LEFT JOIN ( Select znfmd640.t$fili$c,
                     znfmd640.t$etiq$c,
                     max(znfmd640.t$coci$c) OCORR,
                     max(znfmd640.t$date$c) DATA_OCORR
-               from baandb.tznfmd640201 znfmd640
+               from baandb.tznfmd640301 znfmd640
            group by znfmd640.t$fili$c,
                     znfmd640.t$etiq$c ) znfmd640
         ON znfmd640.t$fili$c = znfmd630.t$fili$c
        AND znfmd640.t$etiq$c = znfmd630.t$etiq$c
 
- LEFT JOIN baandb.tznfmd030201 znfmd030
+ LEFT JOIN baandb.tznfmd030301 znfmd030
         ON znfmd030.t$ocin$c = znfmd640.OCORR
 
  LEFT JOIN ( select znsls410.t$ncia$c,
@@ -92,7 +107,7 @@ FROM       baandb.tznsls401201 znsls401
                     znsls410.t$sqpd$c,
                     max(znsls410.t$dtoc$c) DATA_OCORR,
                     max(znsls410.t$poco$c) PT_CONTR
-               from baandb.tznsls410201 znsls410
+               from baandb.tznsls410301 znsls410
            group by znsls410.t$ncia$c,
                     znsls410.t$uneg$c,
                     znsls410.t$pecl$c,
@@ -102,34 +117,37 @@ FROM       baandb.tznsls401201 znsls401
        AND znsls410.t$pecl$c = znsls401.t$pecl$c
        AND znsls410.t$sqpd$c = znsls401.t$sqpd$c
     
- LEFT JOIN baandb.tznmcs002201 znmcs002
+ LEFT JOIN baandb.tznmcs002301 znmcs002
         ON znmcs002.t$poco$c = znsls410.PT_CONTR
     
  LEFT JOIN ( select znsls000.t$indt$c,
                     znsls000.t$itmf$c IT_FRETE,
                     znsls000.t$itmd$c IT_DESP,
                     znsls000.t$itjl$c IT_JUROS
-                from baandb.tznsls000201 znsls000
+                from baandb.tznsls000301 znsls000
                 where rownum = 1 ) PARAM
         ON PARAM.t$indt$c = TO_DATE('01-01-1970','DD-MM-YYYY')
               
  LEFT JOIN ( select cisli941.t$fire$l,
                     cisli941.t$item$l,
                     cisli941.t$amnt$l TOTAL
-               from baandb.tcisli941201 cisli941 ) DESPESA
+               from baandb.tcisli941301 cisli941 ) DESPESA
         ON DESPESA.t$fire$l = cisli940.t$fire$l
        AND TRIM(DESPESA.t$item$l) = TRIM(PARAM.IT_DESP)
      
  LEFT JOIN ( select cisli941.t$fire$l,
                     cisli941.t$item$l,
                     cisli941.t$amnt$l TOTAL
-               from baandb.tcisli941201 cisli941 ) JUROS
+               from baandb.tcisli941301 cisli941 ) JUROS
         ON JUROS.t$fire$l = cisli940.t$fire$l
        AND TRIM(JUROS.t$item$l) = TRIM(PARAM.IT_JUROS)
     
- LEFT JOIN baandb.ttcibd001201 tcibd001
+ LEFT JOIN baandb.ttcibd001301 tcibd001
         ON tcibd001.t$item = cisli941.t$item$l
-  
+
+ LEFT  JOIN  baandb.tznsls002301 znsls002
+         ON  znsls002.t$tpen$c=znsls401.t$itpe$c
+    
 WHERE znsls401.t$idor$c = 'TD'
   AND TRIM(cisli941.t$item$l) !=  TRIM(PARAM.IT_FRETE)
   AND TRIM(cisli941.t$item$l) !=  TRIM(PARAM.IT_DESP)
