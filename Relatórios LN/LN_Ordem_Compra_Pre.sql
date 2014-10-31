2561903a4c2d7612870e07fd4942e0a610330c83
leonardovianna <leonardo.vianna@novapontocom.com.br>
2014-10-31 17:14:01 -0200

    LN_Ordem_Compra_Pre
    Otimização da Query e ajuste dos DateTimes.

--- Relatórios LN/LN_Ordem_Compra_Pre.sql
+++ Relatórios LN/LN_Ordem_Compra_Pre.sql
@@ -8,12 +8,12 @@ SELECT
   CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdpur400.t$ddat, 
     'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
       AT time zone sessiontimezone) AS DATE)  
-                           DATA_PLREC,  
+                           DATA_PLREC,    
   tccom130.t$fovn$l        CNPJ_FORN,
   tccom100.t$nama          DESC_FORN,  
   tcibd001.t$citg          GRUP_ITEM,    
   tcmcs023.t$dsca          DESC_GRUPO,
-  tcibd001.t$item          CODE_ITEM,    
+  Trim(tcibd001.t$item)    CODE_ITEM,    
   tcibd001.t$dsca          DESC_ITEM, 
   whwmd400.t$abcc          CODE_ABC,
   tdpur401.t$qoor *
@@ -31,57 +31,67 @@ SELECT
     WHEN 4 THEN 'A agendar' 
     WHEN 5 THEN 'Agendado' 
    END                     DESC_SITU,
-   
   ' '                      SINA_CANLIB, -- *** AGUARDANDO DUVIDA 3 **** 
   brnfe941.t$qnty$l        QUAN_PREREC,
   brnfe940.t$idat$l        DATA_PREREC
   
-FROM      baandb.tbrnfe940201  brnfe940,  
-          baandb.tbrnfe941201  brnfe941,  
-          baandb.ttdpur400201  tdpur400,
-          baandb.ttdpur401201  tdpur401
+FROM       baandb.ttdpur400301 tdpur400
 
-LEFT JOIN baandb.ttdrec947201   tdrec947
-       ON tdrec947.t$oorg$l = 80 
-      AND tdpur401.t$orno = tdrec947.t$orno$l 
-      AND tdpur401.t$pono = tdrec947.t$pono$l 
-      AND tdpur401.t$sqnb = tdrec947.t$seqn$l
-      
-LEFT JOIN baandb.ttdrec941201  tdrec941 
-       ON tdrec941.t$fire$l = tdrec947.t$fire$l 
-      AND tdrec941.t$line$l = tdrec947.T$LINE$L
-      
-LEFT JOIN baandb.ttdrec940201   tdrec940
-       ON tdrec940.T$FIRE$L=tdrec941.T$FIRE$L,
+INNER JOIN baandb.ttdpur401301 tdpur401
+        ON tdpur401.t$orno = tdpur400.t$orno 
 
-          baandb.tznnfe007201  znnfe007,
-          baandb.ttccom100201  tccom100,         
-          baandb.ttccom130201  tccom130,        
-          baandb.ttcibd001201  tcibd001,  
-          baandb.ttcmcs023201  tcmcs023,    
-          baandb.ttcemm030201  tcemm030,
-          baandb.ttcemm124201  tcemm124,
-          baandb.twhwmd400201  whwmd400
+ LEFT JOIN baandb.ttdrec947301 tdrec947
+        ON tdrec947.t$orno$l = tdpur401.t$orno
+       AND tdrec947.t$pono$l = tdpur401.t$pono
+       AND tdrec947.t$seqn$l = tdpur401.t$sqnb
+    AND tdrec947.t$oorg$l = 80 
 
-WHERE znnfe007.T$ORNO$C=tdpur401.t$orno
-  AND znnfe007.T$PONO$C=tdpur401.t$pono
-  AND znnfe007.T$SEQN$C=tdpur401.T$SQNB
-  AND znnfe007.T$OORG$C=80
-  AND brnfe941.T$FIRE$L=znnfe007.T$FIRE$C
-  AND brnfe941.T$LINE$L=znnfe007.t$line$c
-  AND tdpur401.t$orno = tdpur400.t$orno 
-  AND tcemm124.t$cwoc = tdpur400.t$cofc 
-  AND tcemm124.t$dtyp = 2 
-  AND tcemm030.t$eunt = tcemm124.t$grid
-  AND whwmd400.t$item = tdpur401.t$item
-  AND tcibd001.t$item = tdpur401.t$item  
-  AND tcibd001.t$citg = tcmcs023.t$citg
-  AND tccom100.t$bpid = tdpur400.t$otbp 
-  AND tccom130.t$cadr   = tdpur400.t$otad
-  AND brnfe941.t$fire$l = brnfe940.t$fire$l
+ LEFT JOIN baandb.ttdrec941301 tdrec941 
+        ON tdrec941.t$fire$l = tdrec947.t$fire$l 
+       AND tdrec941.t$line$l = tdrec947.T$LINE$L
+       
+ LEFT JOIN baandb.ttdrec940301 tdrec940
+        ON tdrec940.T$FIRE$L = tdrec941.T$FIRE$L
+ 
+INNER JOIN baandb.tznnfe007301 znnfe007
+        ON znnfe007.T$PONO$C = tdpur401.t$pono
+       AND znnfe007.T$SEQN$C = tdpur401.T$SQNB
+    AND znnfe007.T$ORNO$C = tdpur401.t$orno
+
+INNER JOIN baandb.tbrnfe941301 brnfe941
+        ON brnfe941.T$FIRE$L = znnfe007.T$FIRE$C
+       AND brnfe941.T$LINE$L = znnfe007.t$line$c
+    
+INNER JOIN baandb.tbrnfe940301 brnfe940
+        ON brnfe940.t$fire$l = brnfe941.t$fire$l
+
+INNER JOIN baandb.ttccom100301 tccom100  
+        ON tccom100.t$bpid = tdpur400.t$otbp        
+
+INNER JOIN baandb.ttccom130301 tccom130  
+        ON tccom130.t$cadr = tdpur400.t$otad      
+
+INNER JOIN baandb.ttcibd001301 tcibd001 
+        ON tcibd001.t$item = tdpur401.t$item  
   
-  AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdpur400.t$ddat, 
-    'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-      AT time zone sessiontimezone) AS DATE)) BETWEEN :PreRecebimentoDe AND :PreRecebimentoAte
+INNER JOIN baandb.ttcmcs023301 tcmcs023    
+        ON tcmcs023.t$citg = tcibd001.t$citg
+
+INNER JOIN baandb.ttcemm124301 tcemm124
+        ON tcemm124.t$cwoc = tdpur400.t$cofc 
+  
+INNER JOIN baandb.ttcemm030301 tcemm030
+        ON tcemm030.t$eunt = tcemm124.t$grid
+
+INNER JOIN baandb.twhwmd400301 whwmd400
+        ON whwmd400.t$item = tdpur401.t$item
+    
+WHERE znnfe007.T$OORG$C = 80
+  AND tcemm124.t$dtyp = 2 
+  
+  AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdpur400.t$ddat,'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+      AT time zone sessiontimezone) AS DATE) ) 
+      BETWEEN :PreRecebimentoDe 
+          AND :PreRecebimentoAte
   AND ( (Trim(tccom130.t$fovn$l) like '%' || Trim(:CNPJ) || '%') OR (Trim(:CNPJ) is null) )
   AND Trim(tcibd001.t$citg) IN (:GrupoItem)
