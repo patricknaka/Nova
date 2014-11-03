602223104a5a0e3ee310aab294d929f1a2cfbb58
Humberto Kasai <humberto@globalti.com.br>
2014-11-03 19:04:59 -0200

    Update LN_Fiscal - Contr_Notas_Devolução.sql
    Correção para buscar os dados da Nota Fiscal quando a Ordem de Venda não existe no LN.

--- Relatórios LN/LN_Fiscal - Contr_Notas_Devolução.sql
+++ Relatórios LN/LN_Fiscal - Contr_Notas_Devolução.sql
@@ -20,16 +20,15 @@ SELECT
     cisli940.t$cnfe$l                       CHAVE_NFD,
     cisli940.t$stat$l                       STATUS, iTABLE.DESC_STAT,
     cisli940.t$fire$l                       REF_FISCAL,
-    cisli940d.t$docn$l                      NOTA_NF_VENDA,  
-    cisli940d.t$seri$l                      SERI_NF_VENDA,
-    cisli940d.t$cnfe$l                      CHAVE_NF_VENDA,
+    NVL(cisli940d.t$docn$l, znmcs092.t$docn$c)    NOTA_NF_VENDA,  
+    NVL(cisli940d.t$seri$l, znmcs092.t$seri$c)    SERI_NF_VENDA,
+    NVL(cisli940d.t$cnfe$l, znmcs092.t$cnfe$c)    CHAVE_NF_VENDA,
 
-    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(cisli940d.t$date$l, 
+    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(NVL(cisli940d.t$date$l, znmcs092.t$trdt$c), 
       'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
         AT time zone sessiontimezone) AS DATE)
-                                            DATA_NF_VENDA,
-                                            
-    cisli940d.t$amnt$l                      VALO_NF_VENDA,
+                                            DATA_NF_VENDA,                                        
+    NVL(cisli940d.t$amnt$l, znmcs092.t$amnt$c)    VALO_NF_VENDA,
     cisli245d.t$slso                        NUME_OV,
     cisli940.t$fdty$l                       COD_TIPO_DOC_FISCAL,
     iTIPODOCFIS.DESCR                       DESC_TIPO_DOC_FISCAL,
@@ -58,6 +57,7 @@ INNER JOIN baandb.ttdsls401301  tdsls401
  LEFT JOIN baandb.tcisli245301  cisli245d
         ON cisli245d.t$fire$l = tdsls401.t$fire$l
        AND cisli245d.t$line$l = tdsls401.t$line$l
+       AND cisli245d.t$fire$l != ' '
 
  LEFT JOIN baandb.tcisli940301  cisli940d
         ON cisli940d.t$fire$l = cisli245d.t$fire$l
@@ -201,11 +201,28 @@ INNER JOIN baandb.ttccom130301  tccom130
         
 INNER JOIN baandb.ttccom130301 tccom130B
         ON tccom130B.t$cadr = cisli940.t$sfra$l
- 
+
+LEFT  JOIN  baandb.tznmcs096301   znmcs096
+        ON  znmcs096.t$orno$c=cisli245.t$slso
+       AND  znmcs096.t$pono$c=cisli245.t$pono
+       AND  znmcs096.t$ncmp$c=2
+       AND  ROWNUM=1
+       
+LEFT  JOIN  baandb.tznmcs092301   znmcs092
+        ON  znmcs092.t$ncmp$c=znmcs096.t$ncmp$c
+       AND  znmcs092.t$cref$c=znmcs096.t$cref$c
+       AND  znmcs092.t$cfoc$c=znmcs096.t$cfoc$c
+       AND  znmcs092.t$docn$c=znmcs096.t$docn$c
+       AND  znmcs092.t$seri$c=znmcs096.t$seri$c
+       AND  znmcs092.t$doty$c=znmcs096.t$doty$c
+       AND  znmcs092.t$trdt$c=znmcs096.t$trdt$c
+       AND  znmcs092.t$creg$c=znmcs096.t$creg$c
+       AND  znmcs092.t$cfov$c=znmcs096.t$cfov$c
+       
 WHERE cisli940.t$fdty$l = 14
   AND tcemm124.t$dtyp = 1 
   AND tdrec947.t$oorg$l = 1 --Venda
-  AND cisli245d.t$fire$l != ' '
+  --AND cisli245d.t$fire$l != ' '       --humberto.o
 
   AND tcemm030.T$EUNT IN (:Filial)
   AND tdrec940.t$stat$l IN(:StatusRefFiscal)
