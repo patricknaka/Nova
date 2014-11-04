d493f8d6ad89c0449f295712d22923198a37b5b3
fabioInfor <fabio.ferreira@infor.com>
2014-11-04 15:53:17 -0200

    Relatório R006

--- Relatórios LN/WMS_R006 - Recebimentos em aberto.sql
+++ Relatórios LN/WMS_R006 - Recebimentos em aberto.sql
@@ -1,10 +1,10 @@
 SELECT
-				rd.DATERECEIVED						REDO_DT_ENTRADA,
+				min(rd.DATERECEIVED)						REDO_DT_ENTRADA,
 				rd.RECEIPTKEY						ASN,
 				CASE WHEN SKU.SUSR8 = 'MINUCIOSO' 
 					THEN 'SIM' 
 					ELSE 'NÃO' END					IN_MINUCIOSO,
-				tpnt.LONG_VALUE						TP_NOTA,
+				tpnt.DESCRIPTION					TP_NOTA,
 				stnt.DESCRIPTION					SITUACAO,
 				rc.TRAILERNUMBER					ID_CAMINHAO,
 				rc.TRAILERKEY						PLACA_VEICULO,
@@ -16,21 +16,38 @@ FROM
 					WMWHSE5.RECEIPTDETAIL rd
 		INNER JOIN	WMWHSE5.SKU sku			ON 	sku.SKU = rd.SKU
 		INNER JOIN	WMWHSE5.RECEIPT rc		ON 	rc.RECEIPTKEY = rd.RECEIPTKEY
-		LEFT JOIN	WMWHSE5.CODELKUP tpnt	ON 	TO_CHAR(tpnt.CODE) = TO_CHAR(rc.TYPE)
-											AND tpnt.LISTNAME='RECEIPTYPE'
-		LEFT JOIN	WMWHSE5.CODELKUP stnt	ON 	TO_CHAR(stnt.CODE) = TO_CHAR(rc.STATUS)
-											AND stnt.LISTNAME='RECSTATUS'
+		LEFT JOIN	( select clkp.code         COD, 
+                    trans.description
+                from WMWHSE5.codelkup clkp
+                inner join WMWHSE5.translationlist trans 
+                 on trans.code = clkp.code
+                and trans.joinkey1 = clkp.listname
+                where clkp.listname = 'RECEIPTYPE'
+                and trans.locale = 'pt'
+                and trans.tblname = 'CODELKUP' 
+                and Trim(clkp.code) is not null ) tpnt	ON 	TO_CHAR(tpnt.COD) = TO_CHAR(rc.TYPE)
+		LEFT JOIN	( select clkp.code         COD, 
+                    trans.description
+                from WMWHSE5.codelkup clkp
+                inner join WMWHSE5.translationlist trans 
+                 on trans.code = clkp.code
+                and trans.joinkey1 = clkp.listname
+                where clkp.listname = 'RECSTATUS'
+                and trans.locale = 'pt'
+                and trans.tblname = 'CODELKUP' 
+                and Trim(clkp.code) is not null )stnt	ON 	TO_CHAR(stnt.COD) = TO_CHAR(rc.STATUS)
+
 		INNER JOIN	ENTERPRISE.CODELKUP pl	ON	UPPER(pl.UDF1) = rd.WHSEID
 											AND	pl.LISTNAME = 'SCHEMA'
 WHERE	TO_NUMBER(rc.STATUS) < 9 
 GROUP BY
-	rd.DATERECEIVED,							
+--	rd.DATERECEIVED,							
     rd.RECEIPTKEY,						
     CASE WHEN SKU.SUSR8 = 'MINUCIOSO' 
     	THEN 'SIM' 
     	ELSE 'NÃO' END,					
-    tpnt.LONG_VALUE,					
+    tpnt.DESCRIPTION,					
     stnt.DESCRIPTION,					
     rc.TRAILERNUMBER,					
     rc.TRAILERKEY,						
-    rc.DOOR								
+    rc.DOOR			
