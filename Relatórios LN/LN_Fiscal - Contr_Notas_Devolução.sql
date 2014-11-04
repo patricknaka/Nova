4476bc1406acd29d735c15bf53f15f03e5fc576e
ronaldocs <ronaldo.cristiano@gmail.com>
2014-11-04 17:58:40 -0200

    Relatório de Controle de Notas de Devolução Cliente
    Ajustes

--- Relatórios LN/LN_Fiscal - Contr_Notas_Devolução.sql
+++ Relatórios LN/LN_Fiscal - Contr_Notas_Devolução.sql
@@ -1,49 +1,48 @@
 SELECT 
   DISTINCT
-    301                                     CODE_CIA,
-    tccom130b.t$fovn$l                      NUME_FILIAL,
-    cisli940.t$docn$l                       NUME_NF,
-    cisli940.t$seri$l                       SERI_NF,
+    301                                           CODE_CIA,
+    tccom130b.t$fovn$l                            NUME_FILIAL,
+    cisli940.t$docn$l                             NUME_NF,
+    cisli940.t$seri$l                             SERI_NF,
     
     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(cisli940.t$date$l, 
       'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
         AT time zone sessiontimezone) AS DATE) 
-                                            DATA_EMISSA,
-    cisli940.t$ccfo$l                       COD_CFOP,
-    cisli245.t$slso                         ORDEM,
-    tdsls400.t$sotp                         TIPO_ORD_VENDA_DEV,
-    tccom130.t$fovn$l                       CPF_CNPJ,
-    cisli940.t$itbn$l                       NOME_PARCEI,
-    tccom130.t$cste                         UF_PARCEI,
-    cisli940.t$amnt$l                       VALO_NFD,
-    Usuario.t$logn$c                        USUARIO,
-    cisli940.t$cnfe$l                       CHAVE_NFD,
-    cisli940.t$stat$l                       STATUS, iTABLE.DESC_STAT,
-    cisli940.t$fire$l                       REF_FISCAL,
+                                                  DATA_EMISSA,
+    cisli940.t$ccfo$l                             COD_CFOP,
+    cisli245.t$slso                               ORDEM,
+    tdsls400.t$sotp                               TIPO_ORD_VENDA_DEV,
+    tccom130.t$fovn$l                             CPF_CNPJ,
+    cisli940.t$itbn$l                             NOME_PARCEI,
+    tccom130.t$cste                               UF_PARCEI,
+    cisli940.t$amnt$l                             VALO_NFD,
+    Usuario.t$logn$c                              USUARIO,
+    cisli940.t$cnfe$l                             CHAVE_NFD,
+    cisli940.t$stat$l                             STATUS, iTABLE.DESC_STAT,
+    cisli940.t$fire$l                             REF_FISCAL,
     NVL(cisli940d.t$docn$l, znmcs092.t$docn$c)    NOTA_NF_VENDA,  
     NVL(cisli940d.t$seri$l, znmcs092.t$seri$c)    SERI_NF_VENDA,
     NVL(cisli940d.t$cnfe$l, znmcs092.t$cnfe$c)    CHAVE_NF_VENDA,
 
     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(NVL(cisli940d.t$date$l, znmcs092.t$trdt$c), 
       'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-        AT time zone sessiontimezone) AS DATE)
-                                            DATA_NF_VENDA,                                        
+        AT time zone sessiontimezone) AS DATE)    DATA_NF_VENDA,
+		
     NVL(cisli940d.t$amnt$l, znmcs092.t$amnt$c)    VALO_NF_VENDA,
-    cisli245d.t$slso                        NUME_OV,
-    cisli940.t$fdty$l                       COD_TIPO_DOC_FISCAL,
-    iTIPODOCFIS.DESCR                       DESC_TIPO_DOC_FISCAL,
-    tdrec940.t$stat$l                       CODE_STAT_NF_ORIG,      -- Status ref fiscal
-    iSTATRECFIS.DESCR                       DESCR_STAT_NF_ORIG,     -- Descric Staus Rf fiscal
-    tdrec940.t$fire$l                       REFFIS_REC,
+    cisli245d.t$slso                              NUME_OV,
+    cisli940.t$fdty$l                             COD_TIPO_DOC_FISCAL,
+    iTIPODOCFIS.DESCR                             DESC_TIPO_DOC_FISCAL,
+    tdrec940.t$stat$l                             CODE_STAT_NF_ORIG,      -- Status ref fiscal
+    iSTATRECFIS.DESCR                             DESCR_STAT_NF_ORIG,     -- Descric Staus Rf fiscal
+    tdrec940.t$fire$l                             REFFIS_REC,
 
     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdrec940.t$date$l, 
       'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-        AT time zone sessiontimezone) AS DATE)
-                                            DATA_REC,
+        AT time zone sessiontimezone) AS DATE)    DATA_REC,
                                              
-    tdrec940.t$logn$l                       USER_REC,
-    cisli940d.t$stat$l                      STATUS_NFD,
-    DESCR_STATUS_NFD.DS_SITUACAO_NF         DESCR_STATUS_NFD
+    tdrec940.t$logn$l                             USER_REC,
+    cisli940d.t$stat$l                            STATUS_NFD,
+    DESCR_STATUS_NFD.DS_SITUACAO_NF               DESCR_STATUS_NFD
   
 FROM       baandb.tcisli940301  cisli940 
 
@@ -57,7 +56,7 @@ INNER JOIN baandb.ttdsls401301  tdsls401
  LEFT JOIN baandb.tcisli245301  cisli245d
         ON cisli245d.t$fire$l = tdsls401.t$fire$l
        AND cisli245d.t$line$l = tdsls401.t$line$l
-       AND cisli245d.t$fire$l != ' '
+       AND cisli245d.t$fire$l !=  ' '
 
  LEFT JOIN baandb.tcisli940301  cisli940d
         ON cisli940d.t$fire$l = cisli245d.t$fire$l
@@ -202,30 +201,31 @@ INNER JOIN baandb.ttccom130301  tccom130
 INNER JOIN baandb.ttccom130301 tccom130B
         ON tccom130B.t$cadr = cisli940.t$sfra$l
 
-LEFT  JOIN  baandb.tznmcs096301   znmcs096
-        ON  znmcs096.t$orno$c=cisli245.t$slso
-       AND  znmcs096.t$pono$c=cisli245.t$pono
-       AND  znmcs096.t$ncmp$c=2
-       AND  ROWNUM=1
+ LEFT JOIN baandb.tznmcs096301   znmcs096
+        ON znmcs096.t$orno$c = cisli245.t$slso
+       AND znmcs096.t$pono$c = cisli245.t$pono
+       AND znmcs096.t$ncmp$c = 2
+       AND ROWNUM = 1
        
-LEFT  JOIN  baandb.tznmcs092301   znmcs092
-        ON  znmcs092.t$ncmp$c=znmcs096.t$ncmp$c
-       AND  znmcs092.t$cref$c=znmcs096.t$cref$c
-       AND  znmcs092.t$cfoc$c=znmcs096.t$cfoc$c
-       AND  znmcs092.t$docn$c=znmcs096.t$docn$c
-       AND  znmcs092.t$seri$c=znmcs096.t$seri$c
-       AND  znmcs092.t$doty$c=znmcs096.t$doty$c
-       AND  znmcs092.t$trdt$c=znmcs096.t$trdt$c
-       AND  znmcs092.t$creg$c=znmcs096.t$creg$c
-       AND  znmcs092.t$cfov$c=znmcs096.t$cfov$c
+ LEFT JOIN baandb.tznmcs092301   znmcs092
+        ON znmcs092.t$ncmp$c = znmcs096.t$ncmp$c
+       AND znmcs092.t$cref$c = znmcs096.t$cref$c
+       AND znmcs092.t$cfoc$c = znmcs096.t$cfoc$c
+       AND znmcs092.t$docn$c = znmcs096.t$docn$c
+       AND znmcs092.t$seri$c = znmcs096.t$seri$c
+       AND znmcs092.t$doty$c = znmcs096.t$doty$c
+       AND znmcs092.t$trdt$c = znmcs096.t$trdt$c
+       AND znmcs092.t$creg$c = znmcs096.t$creg$c
+       AND znmcs092.t$cfov$c = znmcs096.t$cfov$c
        
 WHERE cisli940.t$fdty$l = 14
   AND tcemm124.t$dtyp = 1 
-  AND tdrec947.t$oorg$l = 1 --Venda
-  --AND cisli245d.t$fire$l != ' '       --humberto.o
+  AND tdrec947.t$oorg$l = 1
 
   AND tcemm030.T$EUNT IN (:Filial)
   AND tdrec940.t$stat$l IN(:StatusRefFiscal)
   AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(cisli940.t$date$l, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-                  AT time zone sessiontimezone) AS DATE)) BETWEEN :EmissaoDE AND :EmissaoATE
+                  AT time zone sessiontimezone) AS DATE)) 
+      Between :EmissaoDE 
+          And :EmissaoATE
   AND tdrec940.t$opfc$l IN (:COD_CFOP)
