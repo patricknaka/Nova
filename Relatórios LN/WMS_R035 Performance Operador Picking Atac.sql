986de7238004945d629f0fdbc12fe858a85a6320
leonardovianna <leonardo.vianna@novapontocom.com.br>
2014-11-21 11:38:16 -0200

    WMS_R035 Performance Operador Picking Atac
    Troca do campo Entrega por Pedido.

--- Relatórios LN/WMS_R035 Performance Operador Picking Atac.sql
+++ Relatórios LN/WMS_R035 Performance Operador Picking Atac.sql
@@ -1,80 +1,3 @@
-SELECT                                                                    
-  taskdetail.whseid             PLANTA,                                   
-  PL_DB.DB_ALIAS                DSC_PLANTA,                               
-  TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(taskdetail.endtime,            
-    'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')          
-      AT time zone sessiontimezone) AS DATE), 'DD')                       
-                                DATA_COL,                                 
-  OS.SCHEDULEDSHIPDATE          DATA_LIM,                                 
-  (select a.t$pecl$c ENTREGA                                              
-     from baandb.tznsls004301@pln01 a                                     
-    where a.t$orno$c = os.referencedocument                               
-      and rownum=1)             ENTREGA,                                  
-  OS.REFERENCEDOCUMENT          ORDEM_LN,                                 
-  OS.ORDERKEY                   ORDEM_WMS,                                
-  WD.WAVEKEY                    ONDA,                                     
-  CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(taskdetail.STARTTIME,                
-    'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')          
-      AT time zone sessiontimezone) AS DATE)                              
-                                HR_INICIO,                                
-  CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(taskdetail.endtime,                  
-    'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')          
-      AT time zone sessiontimezone) AS DATE)                              
-                                HR_FIM,                                   
-  taskdetail.addwho             OPERADOR,                                 
-  subStr( tu.usr_name, 4,                                                 
-          inStr(tu.usr_name, ',') -4 )                                    
-                                NOME_OP,                                  
-  taskdetail.sku                ITEM,                                     
-  sk.descr                      DESCR_ITEM,                               
-  SUM(taskdetail.qty)           QUANT                                     
-                                                                          
-FROM       WMWHSE5.taskdetail                      
-                                                                          
-INNER JOIN WMWHSE5.ORDERS OS                       
-        ON OS.ORDERKEY = taskdetail.ORDERKEY                              
-                                                                          
-INNER JOIN WMWHSE5.SKU SK                          
-        ON SK.SKU = taskdetail.SKU                                        
-                                                                          
-INNER JOIN (select max(b.WAVEKEY) WAVEKEY,                                
-                       b.ORDERKEY                                         
-                  from WMWHSE5.WAVEDETAIL b        
-              group by b.ORDERKEY) WD                                     
-        ON WD.ORDERKEY = OS.ORDERKEY                                      
-                                                                          
- LEFT JOIN WMWHSE5.taskmanageruser tu              
-        ON tu.userkey = TASKDETAIL.EDITWHO,                               
-                                                                          
-           WMSADMIN.PL_DB                                                 
-                                                                          
-WHERE taskdetail.status = 9                                               
-  AND taskdetail.tasktype = 'PK'                                          
-  AND UPPER(PL_DB.db_logid) = UPPER(taskdetail.whseid)                    
-  AND PL_DB.ISACTIVE = 1                                                  
-  AND PL_DB.DB_ENTERPRISE = 0                                             
-                                                                          
-                          
-                                                                          
-GROUP BY  taskdetail.whseid,                                              
-          PL_DB.DB_ALIAS,                                                 
-          OS.SCHEDULEDSHIPDATE,                                           
-          OS.REFERENCEDOCUMENT,                                           
-          OS.ORDERKEY,                                                    
-          WD.WAVEKEY,                                                     
-          CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(taskdetail.STARTTIME,        
-            'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  
-              AT time zone sessiontimezone) AS DATE),                     
-          CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(taskdetail.endtime,          
-            'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  
-             AT time zone sessiontimezone) AS DATE),                      
-          taskdetail.addwho,                                              
-          subStr( tu.usr_name,4,                                          
-                  inStr(tu.usr_name, ',')-4 ),                            
-          taskdetail.sku,                                                 
-          sk.descr                                                        
-                                                                          
-
 =IIF(Parameters!Table.Value <> "AAA", 
 
 "SELECT                                                                    " &
@@ -85,10 +8,10 @@ GROUP BY  taskdetail.whseid,
 "      AT time zone sessiontimezone) AS DATE), 'DD')                       " &
 "                                DATA_COL,                                 " &
 "  OS.SCHEDULEDSHIPDATE          DATA_LIM,                                 " &
-"  (select a.t$entr$c ENTREGA                                              " &
+"  (select a.t$pecl$c PEDIDO                                               " &
 "     from baandb.tznsls004301@pln01 a                                     " &
 "    where a.t$orno$c = os.referencedocument                               " &
-"      and rownum=1)             ENTREGA,                                  " &
+"      and rownum=1)             PEDIDO,                                   " &
 "  OS.REFERENCEDOCUMENT          ORDEM_LN,                                 " &
 "  OS.ORDERKEY                   ORDEM_WMS,                                " &
 "  WD.WAVEKEY                    ONDA,                                     " &
@@ -157,7 +80,7 @@ GROUP BY  taskdetail.whseid,
 "          taskdetail.sku,                                                 " &
 "          sk.descr                                                        " &
 "                                                                          " &
-"ORDER BY 2, DATA_COL                                                                " 
+"ORDER BY 2, DATA_COL                                                      " 
 
 ,                                                                          
 
@@ -169,10 +92,10 @@ GROUP BY  taskdetail.whseid,
 "      AT time zone sessiontimezone) AS DATE), 'DD')                       " &
 "                                DATA_COL,                                 " &
 "  OS.SCHEDULEDSHIPDATE          DATA_LIM,                                 " &
-"  (select a.t$entr$c ENTREGA                                              " &
+"  (select a.t$pecl$c PEDIDO                                               " &
 "     from baandb.tznsls004301@pln01 a                                     " &
 "    where a.t$orno$c = os.referencedocument                               " &
-"      and rownum=1)             ENTREGA,                                  " &
+"      and rownum=1)             PEDIDO,                                   " &
 "  OS.REFERENCEDOCUMENT          ORDEM_LN,                                 " &
 "  OS.ORDERKEY                   ORDEM_WMS,                                " &
 "  WD.WAVEKEY                    ONDA,                                     " &
@@ -251,10 +174,10 @@ GROUP BY  taskdetail.whseid,
 "      AT time zone sessiontimezone) AS DATE), 'DD')                       " &
 "                                DATA_COL,                                 " &
 "  OS.SCHEDULEDSHIPDATE          DATA_LIM,                                 " &
-"  (select a.t$entr$c ENTREGA                                              " &
+"  (select a.t$pecl$c PEDIDO                                               " &
 "     from baandb.tznsls004301@pln01 a                                     " &
 "    where a.t$orno$c = os.referencedocument                               " &
-"      and rownum=1)             ENTREGA,                                  " &
+"      and rownum=1)             PEDIDO,                                   " &
 "  OS.REFERENCEDOCUMENT          ORDEM_LN,                                 " &
 "  OS.ORDERKEY                   ORDEM_WMS,                                " &
 "  WD.WAVEKEY                    ONDA,                                     " &
@@ -333,10 +256,10 @@ GROUP BY  taskdetail.whseid,
 "      AT time zone sessiontimezone) AS DATE), 'DD')                       " &
 "                                DATA_COL,                                 " &
 "  OS.SCHEDULEDSHIPDATE          DATA_LIM,                                 " &
-"  (select a.t$entr$c ENTREGA                                              " &
+"  (select a.t$pecl$c PEDIDO                                               " &
 "     from baandb.tznsls004301@pln01 a                                     " &
 "    where a.t$orno$c = os.referencedocument                               " &
-"      and rownum=1)             ENTREGA,                                  " &
+"      and rownum=1)             PEDIDO,                                   " &
 "  OS.REFERENCEDOCUMENT          ORDEM_LN,                                 " &
 "  OS.ORDERKEY                   ORDEM_WMS,                                " &
 "  WD.WAVEKEY                    ONDA,                                     " &
@@ -415,10 +338,10 @@ GROUP BY  taskdetail.whseid,
 "      AT time zone sessiontimezone) AS DATE), 'DD')                       " &
 "                                DATA_COL,                                 " &
 "  OS.SCHEDULEDSHIPDATE          DATA_LIM,                                 " &
-"  (select a.t$entr$c ENTREGA                                              " &
+"  (select a.t$pecl$c PEDIDO                                               " &
 "     from baandb.tznsls004301@pln01 a                                     " &
 "    where a.t$orno$c = os.referencedocument                               " &
-"      and rownum=1)             ENTREGA,                                  " &
+"      and rownum=1)             PEDIDO,                                   " &
 "  OS.REFERENCEDOCUMENT          ORDEM_LN,                                 " &
 "  OS.ORDERKEY                   ORDEM_WMS,                                " &
 "  WD.WAVEKEY                    ONDA,                                     " &
@@ -497,10 +420,10 @@ GROUP BY  taskdetail.whseid,
 "      AT time zone sessiontimezone) AS DATE), 'DD')                       " &
 "                                DATA_COL,                                 " &
 "  OS.SCHEDULEDSHIPDATE          DATA_LIM,                                 " &
-"  (select a.t$entr$c ENTREGA                                              " &
+"  (select a.t$pecl$c PEDIDO                                               " &
 "     from baandb.tznsls004301@pln01 a                                     " &
 "    where a.t$orno$c = os.referencedocument                               " &
-"      and rownum=1)             ENTREGA,                                  " &
+"      and rownum=1)             PEDIDO,                                   " &
 "  OS.REFERENCEDOCUMENT          ORDEM_LN,                                 " &
 "  OS.ORDERKEY                   ORDEM_WMS,                                " &
 "  WD.WAVEKEY                    ONDA,                                     " &
@@ -579,10 +502,10 @@ GROUP BY  taskdetail.whseid,
 "      AT time zone sessiontimezone) AS DATE), 'DD')                       " &
 "                                DATA_COL,                                 " &
 "  OS.SCHEDULEDSHIPDATE          DATA_LIM,                                 " &
-"  (select a.t$entr$c ENTREGA                                              " &
+"  (select a.t$pecl$c PEDIDO                                               " &
 "     from baandb.tznsls004301@pln01 a                                     " &
 "    where a.t$orno$c = os.referencedocument                               " &
-"      and rownum=1)             ENTREGA,                                  " &
+"      and rownum=1)             PEDIDO,                                   " &
 "  OS.REFERENCEDOCUMENT          ORDEM_LN,                                 " &
 "  OS.ORDERKEY                   ORDEM_WMS,                                " &
 "  WD.WAVEKEY                    ONDA,                                     " &
@@ -661,10 +584,10 @@ GROUP BY  taskdetail.whseid,
 "      AT time zone sessiontimezone) AS DATE), 'DD')                       " &
 "                                DATA_COL,                                 " &
 "  OS.SCHEDULEDSHIPDATE          DATA_LIM,                                 " &
-"  (select a.t$entr$c ENTREGA                                              " &
+"  (select a.t$pecl$c PEDIDO                                               " &
 "     from baandb.tznsls004301@pln01 a                                     " &
 "    where a.t$orno$c = os.referencedocument                               " &
-"      and rownum=1)             ENTREGA,                                  " &
+"      and rownum=1)             PEDIDO,                                   " &
 "  OS.REFERENCEDOCUMENT          ORDEM_LN,                                 " &
 "  OS.ORDERKEY                   ORDEM_WMS,                                " &
 "  WD.WAVEKEY                    ONDA,                                     " &
@@ -733,6 +656,6 @@ GROUP BY  taskdetail.whseid,
 "          taskdetail.sku,                                                 " &
 "          sk.descr                                                        " &
 "                                                                          " &
-"ORDER BY 2, DATA_COL                                                                "   
+"ORDER BY 2, DATA_COL                                                      "   
 
 )
