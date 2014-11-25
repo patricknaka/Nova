d5d4b332713cd4399c20e83155ecf6079ccc448b
leonardovianna <leonardo.vianna@novapontocom.com.br>
2014-11-25 12:07:15 -0200

    WMS_R048 Estoque Item Kit
    Inserção de cláusula Where para não considerar itens com qtde 0.

--- Relatórios LN/WMS_R048 Estoque Item Kit.sql
+++ Relatórios LN/WMS_R048 Estoque Item Kit.sql
@@ -1,85 +1,3 @@
-SELECT                                                                      
-  LL.WHSEID                         ID_PLANTA,                              
-  CL.UDF2                           DESCR_PLANTA,                           
-  BM.SKU                            TIK_PAI,                                
-  PS.DESCR                          TIK_PAI_DESCR,                          
-  BM.COMPONENTSKU                   TIK_FILHO,                              
-  FS.DESCR                          TIK_FILHO_DESCR,                        
-  IH.STATUS                         RESTRICAO,                              
-  SUM(LL.QTY)                       QTD,                                    
-  ( select asku.altsku                                                      
-      from WMWHSE5.altsku asku                       
-     where asku.sku = BM.SKU                                                
-       and rownum = 1 )             EAN_PAI,                                
-  ( select asku.altsku                                                      
-      from WMWHSE5.altsku asku                       
-     where asku.sku = BM.COMPONENTSKU                                       
-       and rownum = 1 )             EAN_FILHO,                              
-  LC.PUTAWAYZONE                    CLASSE_LOCAL,                           
-  LC.LOC                            LOCAL,                                  
-  Trim(PZ.DESCR)                    DESCR_LOCAL,                            
-  nvl(max(maucLN.mauc),0) *                                                 
-  sum(LL.qty)                       VALOR,                                  
-  SUM(FS.STDCUBE*LL.qty)            CUBAGEM                                 
-                                                                            
-FROM       WMWHSE5.BILLOFMATERIAL BM                 
-                                                                            
-INNER JOIN WMWHSE5.LOTXLOCXID LL                     
-        ON LL.SKU = BM.COMPONENTSKU                                         
-                                                                            
-INNER JOIN WMWHSE5.SKU PS                            
-        ON PS.SKU = BM.SKU                                                  
-                                                                            
-INNER JOIN WMWHSE5.SKU FS                            
-        ON FS.SKU = BM.COMPONENTSKU                                         
-                                                                            
-INNER JOIN WMWHSE5.LOC LC                            
-        ON LC.LOC = LL.LOC                                                  
-                                                                            
- LEFT JOIN WMWHSE5.INVENTORYHOLD IH                  
-        ON IH.LOC = LL.LOC                                                  
-       AND IH.HOLD = 1                                                      
-       AND IH.LOC <> ' '                                                    
-                                                                            
- LEFT JOIN WMWHSE5.PUTAWAYZONE PZ                    
-        ON PZ.PUTAWAYZONE = LC.PUTAWAYZONE                                  
-                                                                            
- LEFT JOIN ENTERPRISE.CODELKUP CL                                           
-        ON UPPER(cl.UDF1) = LL.WHSEID                                       
-       AND CL.LISTNAME = 'SCHEMA'                                           
-                                                                            
- LEFT JOIN ( select trim(whwmd217.t$item) item,                             
-                    whwmd217.t$cwar cwar,                                   
-                    case when sum(a.t$qhnd) = 0                             
-                     then 0                                                 
-                     else round(sum(whwmd217.t$mauc$1) / sum(a.t$qhnd), 4)    
-                    end mauc                                                
-               from baandb.twhwmd217301@pln01 whwmd217                      
-         inner join baandb.twhinr140301@pln01 a                             
-                 on a.t$cwar = whwmd217.t$cwar                              
-                and a.t$item = whwmd217.t$item                              
-              where whwmd217.t$mauc$1 != 0                                  
-           group by whwmd217.t$item,                                        
-                    whwmd217.t$cwar ) maucLN                                 
-        ON maucLN.cwar = subStr(CL.DESCRIPTION,3,6)                         
-        AND maucLN.item = PS.SKU                                            
-
-WHERE LL.QTY>0                                                                            
-GROUP BY  LL.WHSEID,                                                        
-          CL.UDF2,                                                          
-          BM.SKU,                                                           
-          PS.DESCR,                                                         
-          BM.COMPONENTSKU,                                                  
-          FS.DESCR,                                                         
-          IH.STATUS,                                                        
-          LC.PUTAWAYZONE,                                                   
-          LC.LOC,                                                           
-          PZ.DESCR                                                          
-                                                                            
-ORDER BY ID_PLANTA                                                          
-
-
-
 =IIF(Parameters!Table.Value <> "AAA", 
 
 "SELECT                                                                      " &
@@ -147,7 +65,7 @@ ORDER BY ID_PLANTA
 "                    whwmd217.t$cwar ) maucLN                                " & 
 "        ON maucLN.cwar = subStr(CL.DESCRIPTION,3,6)                         " &
 "        AND maucLN.item = PS.SKU                                            " &
-"                                                                            " &
+"WHERE LL.QTY > 0                                                            " &
 "GROUP BY  LL.WHSEID,                                                        " &
 "          CL.UDF2,                                                          " &
 "          BM.SKU,                                                           " &
@@ -228,7 +146,7 @@ ORDER BY ID_PLANTA
 "                    whwmd217.t$cwar ) maucLN                                " &
 "        ON maucLN.cwar = subStr(CL.DESCRIPTION,3,6)                         " &
 "       AND maucLN.item = PS.SKU                                             " &
-"                                                                            " &
+"WHERE LL.QTY > 0                                                            " &
 "GROUP BY  LL.WHSEID,                                                        " &
 "          CL.UDF2,                                                          " &
 "          BM.SKU,                                                           " &
@@ -307,7 +225,7 @@ ORDER BY ID_PLANTA
 "                    whwmd217.t$cwar ) maucLN                                " &
 "        ON maucLN.cwar = subStr(CL.DESCRIPTION,3,6)                         " &
 "       AND maucLN.item = PS.SKU                                             " &
-"                                                                            " &
+"WHERE LL.QTY > 0                                                            " &
 "GROUP BY  LL.WHSEID,                                                        " &
 "          CL.UDF2,                                                          " &
 "          BM.SKU,                                                           " &
@@ -386,7 +304,7 @@ ORDER BY ID_PLANTA
 "                    whwmd217.t$cwar ) maucLN                                " &
 "        ON maucLN.cwar = subStr(CL.DESCRIPTION,3,6)                         " &
 "       AND maucLN.item = PS.SKU                                             " &
-"                                                                            " &
+"WHERE LL.QTY > 0                                                            " &
 "GROUP BY  LL.WHSEID,                                                        " &
 "          CL.UDF2,                                                          " &
 "          BM.SKU,                                                           " &
@@ -465,7 +383,7 @@ ORDER BY ID_PLANTA
 "                    whwmd217.t$cwar ) maucLN                                " &
 "        ON maucLN.cwar = subStr(CL.DESCRIPTION,3,6)                         " &
 "       AND maucLN.item = PS.SKU                                             " &
-"                                                                            " &
+"WHERE LL.QTY > 0                                                            " &
 "GROUP BY  LL.WHSEID,                                                        " &
 "          CL.UDF2,                                                          " &
 "          BM.SKU,                                                           " &
@@ -544,7 +462,7 @@ ORDER BY ID_PLANTA
 "                    whwmd217.t$cwar ) maucLN                                " &
 "        ON maucLN.cwar = subStr(CL.DESCRIPTION,3,6)                         " &
 "       AND maucLN.item = PS.SKU                                             " &
-"                                                                            " &
+"WHERE LL.QTY > 0                                                            " &
 "GROUP BY  LL.WHSEID,                                                        " &
 "          CL.UDF2,                                                          " &
 "          BM.SKU,                                                           " &
@@ -623,7 +541,7 @@ ORDER BY ID_PLANTA
 "                    whwmd217.t$cwar ) maucLN                                " &
 "        ON maucLN.cwar = subStr(CL.DESCRIPTION,3,6)                         " &
 "       AND maucLN.item = PS.SKU                                             " &
-"                                                                            " &
+"WHERE LL.QTY > 0                                                            " &
 "GROUP BY  LL.WHSEID,                                                        " &
 "          CL.UDF2,                                                          " &
 "          BM.SKU,                                                           " &
@@ -702,7 +620,7 @@ ORDER BY ID_PLANTA
 "                    whwmd217.t$cwar ) maucLN                                " &
 "        ON maucLN.cwar = subStr(CL.DESCRIPTION,3,6)                         " &
 "       AND maucLN.item = PS.SKU                                             " &
-"                                                                            " &
+"WHERE LL.QTY > 0                                                            " &
 "GROUP BY  LL.WHSEID,                                                        " &
 "          CL.UDF2,                                                          " &
 "          BM.SKU,                                                           " &
