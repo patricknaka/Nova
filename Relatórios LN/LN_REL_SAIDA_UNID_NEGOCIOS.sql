a108b2c8e53d0a7151020cbb8fa62adfe7e64369
leonardovianna <leonardo.vianna@novapontocom.com.br>
2014-11-26 10:23:26 -0200

    WMS_REL_039 Saida Unineg
    Criação do relatório no SSRS.

--- Relatórios LN/LN_REL_SAIDA_UNID_NEGOCIOS.sql
+++ Relatórios LN/LN_REL_SAIDA_UNID_NEGOCIOS.sql
@@ -1,75 +1,76 @@
-SELECT DISTINCT
+SELECT 
+  DISTINCT
+    wmsCODE.FILIAL               FILIAL,
+    wmsCODE.ID_FILIAL            NOME_FILIAL,
 
-wmsCODE.FILIAL                 FILIAL,
-wmsCODE.ID_FILIAL              NOME_FILIAL,
-TO_CHAR(TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls400.t$odat, 
-                  'DD-MON-YYYY HH24'), 'DD-MON-YYYY HH24'), 'GMT')
-                    AT time zone sessiontimezone) AS DATE),'HH24'), 'DD-MON-YYYY')    DT_EMISSAO,
+    TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls400.t$odat, 
+      'DD-MON-YYYY HH24'), 'DD-MON-YYYY HH24'), 'GMT')
+        AT time zone sessiontimezone) AS DATE),'HH24')  
+                                 DT_EMISSAO,
                     
-       TO_CHAR(TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls400.t$odat, 
+    TO_CHAR(TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls400.t$odat, 
+      'DD-MON-YYYY HH24'), 'DD-MON-YYYY HH24'), 'GMT')
+        AT time zone sessiontimezone) AS DATE),'HH24'), 'HH24:MI')
+    || ' ~ ' 
+    || TO_CHAR(TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls400.t$odat, 
+         'DD-MON-YYYY HH24'), 'DD-MON-YYYY HH24'), 'GMT')
+           AT time zone sessiontimezone) AS DATE)+1/24,'HH24'), 'HH24:MI') 
+                                 PERIODO,
 
-                       'DD-MON-YYYY HH24'), 'DD-MON-YYYY HH24'), 'GMT')
+tdsls400.t$cbrn                  ID_UNINEG,        --Ramo de Atividade (tdsls400)
+tcmcs031.t$dsca                  NOME_UNINEG,
+COUNT(tdsls400.t$orno)           NO_PEDIDOS
 
-                         AT time zone sessiontimezone) AS DATE),'HH24'), 'HH24:MI')
+FROM      baandb.ttdsls400301 tdsls400
 
-       || ' ~ ' 
-
-       || TO_CHAR(TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls400.t$odat, 
-
-                       'DD-MON-YYYY HH24'), 'DD-MON-YYYY HH24'), 'GMT')
-
-                         AT time zone sessiontimezone) AS DATE)+1/24,'HH24'), 'HH24:MI') PERIODO,
-                                --PEDIDOS,
-tdsls400.t$cbrn                   ID_UNINEG,        --Ramo de Atividade (tdsls400)
-tcmcs031.t$dsca                   NOME_UNINEG,
-COUNT(tdsls400.t$orno)            NO_PEDIDOS
-
-FROM  baandb.ttdsls400301   tdsls400
-
-LEFT JOIN baandb.ttcmcs031301   tcmcs031 
-       ON tcmcs031.t$cbrn=tdsls400.t$cbrn
+LEFT JOIN baandb.ttcmcs031301 tcmcs031 
+       ON tcmcs031.t$cbrn = tdsls400.t$cbrn
        
 LEFT JOIN baandb.ttcemm124301 tcemm124
-       ON tcemm124.t$loco=301
-      AND tcemm124.t$dtyp=1
-      AND tcemm124.t$cwoc=tdsls400.t$cofc
+       ON tcemm124.t$loco = 301
+      AND tcemm124.t$dtyp = 1
+      AND tcemm124.t$cwoc = tdsls400.t$cofc
        
 LEFT JOIN ( select upper(wmsCODE.UDF1)  FILIAL,
-                    wmsCODE.UDF2        ID_FILIAL,
-                    b.t$grid
-            from baandb.ttcemm300301 a
-                  inner join baandb.ttcemm112301 b 
-                          on b.t$waid = a.t$code
+                   wmsCODE.UDF2        ID_FILIAL,
+                   b.t$grid
+              from baandb.ttcemm300301 a
+        inner join baandb.ttcemm112301 b 
+                on b.t$waid = a.t$code
 
-                  left join ENTERPRISE.CODELKUP@DL_LN_WMS wmsCode
-                         on upper(trim(wmsCode.DESCRIPTION)) = a.t$lctn
-                        and wmsCode.LISTNAME = 'SCHEMA'
-            where a.t$type = 20
+         left join ENTERPRISE.CODELKUP@DL_LN_WMS wmsCode
+                on upper(trim(wmsCode.DESCRIPTION)) = a.t$lctn
+               and wmsCode.LISTNAME = 'SCHEMA'
+             where a.t$type = 20
 
-            group by  upper(wmsCODE.UDF1),
-                      wmsCODE.UDF2,
-                      b.t$grid )  wmsCODE
-        ON wmsCODE.t$grid = tcemm124.t$grid
+          group by upper(wmsCODE.UDF1),
+                   wmsCODE.UDF2,
+                   b.t$grid )  wmsCODE
+       ON wmsCODE.t$grid = tcemm124.t$grid
+
+WHERE TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls400.t$odat, 
+        'DD-MON-YYYY HH24'), 'DD-MON-YYYY HH24'), 'GMT')
+          AT time zone sessiontimezone) AS DATE),'HH24')
+      BETWEEN :EmissaoDe
+          AND :EmissaoAte
+			
+      AND ( (:Filial = 'AAA') OR (UPPER(wmsCODE.FILIAL) = :Filial) )
         
-GROUP BY  --FILIAL,
-          --NOME_FILIAL
-          --DT_EMISSAO
-          --PERIODO
-          --ID_UNINEG
-          --NOME_UNINEG
-          wmsCODE.FILIAL,
+GROUP BY  wmsCODE.FILIAL,
           wmsCODE.ID_FILIAL,
+          
+          TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls400.t$odat, 
+           'DD-MON-YYYY HH24'), 'DD-MON-YYYY HH24'), 'GMT')
+             AT time zone sessiontimezone) AS DATE),'HH24'),
+          
           TO_CHAR(TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls400.t$odat, 
-                  'DD-MON-YYYY HH24'), 'DD-MON-YYYY HH24'), 'GMT')
-                    AT time zone sessiontimezone) AS DATE),'HH24'), 'DD-MON-YYYY'),
-          TO_CHAR(TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls400.t$odat, 
-                       'DD-MON-YYYY HH24'), 'DD-MON-YYYY HH24'), 'GMT')
-                         AT time zone sessiontimezone) AS DATE),'HH24'), 'HH24:MI')
+            'DD-MON-YYYY HH24'), 'DD-MON-YYYY HH24'), 'GMT')
+              AT time zone sessiontimezone) AS DATE),'HH24'), 'HH24:MI')
           || ' ~ ' 
+          || TO_CHAR(TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls400.t$odat, 
+               'DD-MON-YYYY HH24'), 'DD-MON-YYYY HH24'), 'GMT')
+                 AT time zone sessiontimezone) AS DATE)+1/24,'HH24'), 'HH24:MI'),
 
-          || TO_CHAR(TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls400.t$odat,
-                       'DD-MON-YYYY HH24'), 'DD-MON-YYYY HH24'), 'GMT')
-                         AT time zone sessiontimezone) AS DATE)+1/24,'HH24'), 'HH24:MI'),
           tdsls400.t$cbrn,
           tcmcs031.t$dsca
 
