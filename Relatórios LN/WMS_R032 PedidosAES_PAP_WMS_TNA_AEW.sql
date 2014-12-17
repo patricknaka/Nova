3556b0e2abe5d0afea15be398c139b7309932641
fabioInfor <fabio.ferreira@infor.com>
2014-12-17 10:57:09 -0200

    Relatório R032

--- Relatórios LN/WMS_R032 PedidosAES_PAP_WMS_TNA_AEW.sql
+++ Relatórios LN/WMS_R032 PedidosAES_PAP_WMS_TNA_AEW.sql
@@ -1,23 +1,25 @@
-SELECT wmsCODE.FILIAL            FILIAL,     
-       wmsCODE.ID_FILIAL         DSC_PLANTA,
-       TDSLS400.T$ORNO           PEDIDO_LN,
-       ZNSLS004.T$PECL$C         PEDIDO_SITE,
-       OWMS.ORDERKEY             ORDEM_WMS,
-       ZNSLS004.T$UNEG$C         ID_UNEG,
-       ZNINT002.T$DESC$C         DESCR_UNEG,
-       ZNSLS410.T$POCO$C         ID_ULT_PONTO,
-       ZNMCS002.T$DESC$C         DESCR_ULT_PONTO,
-       TRIM(ZNSLS004.T$ITEM$C)   ITEM
+    SELECT wmsCODE.FILIAL            FILIAL,     
+           wmsCODE.ID_FILIAL         DSC_PLANTA,
+           TDSLS400.T$ORNO           PEDIDO_LN,
+           ZNSLS004.T$PECL$C         PEDIDO_SITE,
+		   OWMS.ORDERKEY			 ORDEM_WMS,
+           ZNSLS004.T$UNEG$C         ID_UNEG,
+           ZNINT002.T$DESC$C         DESCR_UNEG,
+           ZNSLS410.T$POCO$C         ID_ULT_PONTO,
+           ZNMCS002.T$DESC$C         DESCR_ULT_PONTO,
+           TRIM(ZNSLS004.T$ITEM$C)     ITEM
+		   
   
-FROM       BAANDB.TTDSLS400301@PLN01 TDSLS400
+FROM BAANDB.TTDSLS400301@PLN01 TDSLS400
 
  LEFT JOIN ( select A.T$NCIA$C,
                     A.T$UNEG$C,
                     A.T$PECL$C,
                     A.T$SQPD$C,
                     A.T$ORNO$C,
-                    B.T$ITEM$C,
-                    B.T$CWRL$C,
+                    --A.T$PONO$C,
+					B.T$ITEM$C,
+          B.T$CWRL$C,
                     MIN(B.T$DTAP$C) T$DTAP$C
                from BAANDB.TZNSLS004301@PLN01 A
          inner join BAANDB.TZNSLS401301@PLN01 B 
@@ -32,9 +34,11 @@ FROM       BAANDB.TTDSLS400301@PLN01 TDSLS400
                     A.T$PECL$C,
                     A.T$SQPD$C,
                     A.T$ORNO$C,
+                    --A.T$PONO$C,
                     B.T$ITEM$C,
-                    B.T$CWRL$C ) ZNSLS004
+                    B.T$CWRL$C) ZNSLS004
         ON ZNSLS004.T$ORNO$C = TDSLS400.T$ORNO
+       --AND ZNSLS004.T$PONO$C = TDSLS401.T$PONO
     
  LEFT JOIN BAANDB.TZNINT002301@pln01 ZNINT002
         ON ZNINT002.T$NCIA$C = ZNSLS004.T$NCIA$C
@@ -57,25 +61,26 @@ FROM       BAANDB.TTDSLS400301@PLN01 TDSLS400
     
  LEFT JOIN BAANDB.TZNMCS002301@pln01 ZNMCS002
         ON ZNMCS002.T$POCO$C = ZNSLS410.T$POCO$C
-  
-INNER JOIN ( select upper(wmsCODE.UDF1) Filial,
-                   wmsCODE.UDF2 ID_FILIAL,
-                   a.t$code cwar
-              from baandb.ttcemm300301@PLN01 a
-         left join ENTERPRISE.CODELKUP wmsCode
-                on UPPER(TRIM(wmsCode.DESCRIPTION)) = a.t$lctn
-               and wmsCode.LISTNAME = 'SCHEMA'  
-             where a.t$type = 20
-               and wmsCODE.UDF1 is not null
-          group by upper(wmsCODE.UDF1),
-                   wmsCODE.UDF2,
-                   a.t$code )  wmsCODE 
-       ON wmsCODE.CWAR = ZNSLS004.T$CWRL$C
-  
- LEFT JOIN WMWHSE5.ORDERS OWMS 
-        ON OWMS.REFERENCEDOCUMENT = TDSLS400.T$ORNO
 
-WHERE OWMS.STATUS NOT IN ('95', '96,', '97', '98', '99', '100')
+
+  
+ INNER JOIN ( select upper(wmsCODE.UDF1) Filial,
+                    wmsCODE.UDF2 ID_FILIAL,
+                    a.t$code cwar
+               from baandb.ttcemm300301@PLN01 a
+          left join ENTERPRISE.CODELKUP wmsCode
+                 on UPPER(TRIM(wmsCode.DESCRIPTION)) = a.t$lctn
+                and wmsCode.LISTNAME = 'SCHEMA'  
+              where a.t$type = 20
+			  and wmsCODE.UDF1 is not null
+           group by upper(wmsCODE.UDF1),
+                    wmsCODE.UDF2,
+					a.t$code)  wmsCODE 
+        ON wmsCODE.CWAR = ZNSLS004.T$CWRL$C
+		
+ LEFT JOIN WMWHSE5.ORDERS OWMS ON OWMS.REFERENCEDOCUMENT = TDSLS400.T$ORNO
+ WHERE ZNSLS410.T$POCO$C IN ('AES', 'PRD', 'WMS', 'TNA', 'AEW') 
+
   AND wmsCODE.FILIAL :Table
 
 
