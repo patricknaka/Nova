469f32b129d337f8381290d388cab191325743aa
fabioInfor <fabio.ferreira@infor.com>
2014-12-17 14:54:27 -0200

    Relatório WMS R014

--- Relatórios LN/WMS_R014 - Divergencias Inventario.sql
+++ Relatórios LN/WMS_R014 - Divergencias Inventario.sql
@@ -1,12 +1,12 @@
 SELECT
-		CC.CCKEY							ID_INVENTARIO,
+		TO_CHAR(CC.CCKEY)							ID_INVENTARIO,
 		CD.LOC								ID_LOCAL,
 		LC.PUTAWAYZONE						ENDERECO,
 		CD.SKU								ITEM_WMS,
 		TCIBD001.T$ITEM						ITEM_LN,
 		SK.DESCR							NOME_ITEM,
 		( select asku.altsku 	
-		  from WMWHSE5.altsku asku	
+		  from WMWHSE4.altsku asku	
 		  where asku.sku = cd.sku	
 		  and rownum = 1 )           		EAN,		  
 		CD.SYSQTY							QT_LOGICA,
@@ -19,21 +19,21 @@ SELECT
 		CC.WHSEID							ID_PLANTA,
 		CL.UDF2								NOME_PLANTA		
 FROM
-					WMWHSE5.CC			CC
-		INNER JOIN	WMWHSE5.CCDETAIL	CD	ON	CD.CCKEY		=	CC.CCKEY
+					WMWHSE4.CC			CC
+		INNER JOIN	WMWHSE4.CCDETAIL	CD	ON	CD.CCKEY		=	CC.CCKEY
 		INNER JOIN 	ENTERPRISE.CODELKUP CL 	ON 	UPPER(CL.UDF1) 	= 	CC.WHSEID
 											AND CL.LISTNAME 	= 	'SCHEMA'		
-		INNER JOIN	WMWHSE5.LOC			LC	ON	LC.LOC			=	CD.LOC
-		INNER JOIN	WMWHSE5.SKU			SK	ON	SK.SKU			=	CD.SKU
-		INNER JOIN	BAANDB.TTCIBD001301@PLN01 TCIBD001 ON TRIM(TCIBD001.T$ITEM) = CD.SKU
+		INNER JOIN	WMWHSE4.LOC			LC	ON	LC.LOC			=	CD.LOC
+		INNER JOIN	WMWHSE4.SKU			SK	ON	SK.SKU			=	CD.SKU
+		INNER JOIN	BAANDB.TTCIBD001201@DLN01 TCIBD001 ON TRIM(TCIBD001.T$ITEM) = CD.SKU
 		LEFT JOIN ( select trim(whwmd217.t$item) item,                             
 							whwmd217.t$cwar cwar,                                   
 							case when sum(a.t$qhnd) = 0                             
 							 then 0                                                 
 							 else round(sum(whwmd217.t$mauc$1) / sum(a.t$qhnd), 4)    
 							end mauc                                                
-					   from baandb.twhwmd217301@pln01 whwmd217                      
-				 inner join baandb.twhinr140301@pln01 a                             
+					   from baandb.twhwmd217201@DLN01 whwmd217                      
+				 inner join baandb.twhinr140201@DLN01 a                             
 						 on a.t$cwar = whwmd217.t$cwar                              
 						and a.t$item = whwmd217.t$item                              
 					  where whwmd217.t$mauc$1 != 0                                  
@@ -57,12 +57,12 @@ SELECT
 		  from WMWHSE4.altsku asku	
 		  where asku.sku = cd.sku	
 		  and rownum = 1 )           		EAN,		  
-		  LL.QTY							QT_LOGICA,
+		  nvl(LL.QTY,0)							QT_LOGICA,
       CD.QTY					      QT_FISICA,
-      CD.QTY - LL.QTY							VAR_QTDE,
-		  LL.QTY*maucLN.mauc				VL_LOGICO,
+      CD.QTY - nvl(LL.QTY,0)							VAR_QTDE,
+		  nvl(LL.QTY*maucLN.mauc,0)				VL_LOGICO,
       CD.QTY*maucLN.mauc	        VL_FISICO,
-      (CD.QTY - LL.QTY)*maucLN.mauc				VL_DIVERGENCIA,
+      (CD.QTY - nvl(LL.QTY,0))*maucLN.mauc				VL_DIVERGENCIA,
 		' '									SIMULACAO,										--PENDENTE DE CUSTOMIZAÇÃO
 		CD.WHSEID							ID_PLANTA,
 		CL.UDF2								NOME_PLANTA		
