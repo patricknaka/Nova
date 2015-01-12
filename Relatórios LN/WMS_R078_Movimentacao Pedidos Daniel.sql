da10c790267915930f418ac38905000dc264f536
fabioInfor <fabio.ferreira@infor.com>
2015-01-12 12:27:01 -0200

    Update WMS_R078_Movimentacao Pedidos Daniel.sql

--- Relatórios LN/WMS_R078_Movimentacao Pedidos Daniel.sql
+++ Relatórios LN/WMS_R078_Movimentacao Pedidos Daniel.sql
@@ -1,59 +1,53 @@
-SELECT TD.WHSEID               ID_PLANTA,
-       CL.UDF2                 DESCR_PLANTA,
-       TD.ORDERKEY             ORDEM_MOV,
-       TD.WAVEKEY              ONDA,
-       OC.REFERENCEDOCUMENT    ORDEM_LN,
-       ( select a.t$pecl$c
-           from baandb.tznsls004301@pln01 a
-          where a.t$orno$c = OC.REFERENCEDOCUMENT
-            and rownum = 1 )   PEDIDO_SITE,
-       TD.STARTTIME            INICIO_MOV_1,
-       CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(TD.STARTTIME, 
-         'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+SELECT
+		TD.WHSEID							ID_PLANTA,
+		CL.UDF2								DESCR_PLANTA,
+		TD.ORDERKEY							ORDEM_MOV,
+		TD.WAVEKEY							ONDA,
+		OC.REFERENCEDOCUMENT				ORDEM_LN,
+		(select a.t$pecl$c
+		 from baandb.tznsls004301@pln01 a
+		 where a.t$orno$c = OC.REFERENCEDOCUMENT
+		 and rownum=1)						PEDIDO_SITE,
+		TD.STARTTIME						INICIO_MOV,
+		TD.SKU								ID_ITEM,
+		SK.DESCR							DESCR_ITEM,
+		TD.QTY								QUANTID,
+		TD.FROMLOC							LOC_ORG,
+		PO.DESCR							LOCAL_ORIG,
+		LO.PUTAWAYZONE						ZONA_ORIG,
+		TD.TOLOC							LOC_DEST,
+		PD.DESCR							LOCAL_DEST,
+		LD.PUTAWAYZONE						ZONA_DEST,
+		OC.STATUS							ID_SITUACAO,
+		SS.DESCRIPTION						DESCR_SITUACAO,
+		CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(OC.EDITDATE, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
            AT time zone sessiontimezone) AS DATE)
-                               INICIO_MOV,
-       TD.SKU                  ID_ITEM,
-       SK.DESCR                DESCR_ITEM,
-       TD.QTY                  QUANTID,
-       TD.FROMLOC              LOC_ORG,
-       PO.DESCR                LOCAL_ORIG,
-       LO.PUTAWAYZONE          ZONA_ORIG,
-       TD.TOLOC                LOC_DEST,
-       PD.DESCR                LOCAL_DEST,
-       LD.PUTAWAYZONE          ZONA_DEST,
-       OC.STATUS               ID_SITUACAO,
-       SS.DESCRIPTION          DESCR_SITUACAO,
-       OC.EDITDATE             DT_HR_1,
-       CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(OC.EDITDATE, 
-         'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-           AT time zone sessiontimezone) AS DATE)
-                               DT_HR,
-       TD.EDITWHO              ID_OPERADOR,
-       subStr( tu.usr_name, 4, inStr(tu.usr_name, ',') -4 )
-                               NOME_OP  
-FROM       WMWHSE5.TASKDETAIL  TD
-INNER JOIN ENTERPRISE.CODELKUP  CL
-        ON UPPER(CL.UDF1) = TD.WHSEID
-       AND CL.LISTNAME = 'SCHEMA'
-INNER JOIN WMWHSE5.ORDERS  OC 
-        ON OC.ORDERKEY = TD.ORDERKEY
-INNER JOIN WMWHSE5.SKU  SK 
-        ON SK.SKU = TD.SKU
-INNER JOIN WMWHSE5.LOC  LO 
-        ON LO.LOC = TD.FROMLOC
-INNER JOIN WMWHSE5.PUTAWAYZONE  PO 
-        ON PO.PUTAWAYZONE = LO.PUTAWAYZONE
-INNER JOIN WMWHSE5.LOC  LD 
-        ON LD.LOC = TD.TOLOC
-INNER JOIN WMWHSE5.PUTAWAYZONE  PD 
-        ON PD.PUTAWAYZONE = LD.PUTAWAYZONE
-INNER JOIN WMWHSE5.ORDERSTATUSSETUP  SS 
-        ON SS.CODE = OC.STATUS
- LEFT JOIN WMWHSE5.taskmanageruser  tu  
-        ON tu.userkey = TD.EDITWHO
-WHERE TD.TASKTYPE = 'PK'
-  AND TD.STATUS = 9
-  AND CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(TD.STARTTIME, 
+											DT_HR,
+		IT.EDITWHO							ID_OPERADOR,
+		subStr( tu.usr_name,4,
+            inStr(tu.usr_name, ',')-4 )   	NOME_OP		
+FROM
+		WMWHSE5.TASKDETAIL						TD
+		INNER JOIN	WMWHSE5.ITRN				IT	ON	IT.SOURCEKEY	=	TD.SOURCEKEY
+													AND	IT.FROMLOC		=	TD.FROMLOC
+													AND	IT.TOLOC		=	TD.TOLOC
+													AND	IT.SKU			=	TD.SKU
+													AND	IT.LOT			=	TD.LOT
+													AND IT.SOURCETYPE	=	'PICKING'
+		INNER JOIN 	ENTERPRISE.CODELKUP			CL	ON	UPPER(CL.UDF1)	=	TD.WHSEID
+													AND	CL.LISTNAME 	=	'SCHEMA'
+		INNER JOIN	WMWHSE5.ORDERS				OC	ON	OC.ORDERKEY		=	TD.ORDERKEY
+		INNER JOIN	WMWHSE5.SKU					SK	ON	SK.SKU			=	TD.SKU
+		INNER JOIN	WMWHSE5.LOC					LO	ON	LO.LOC			=	TD.FROMLOC
+		INNER JOIN	WMWHSE5.PUTAWAYZONE 		PO	ON	PO.PUTAWAYZONE	=	LO.PUTAWAYZONE
+		INNER JOIN	WMWHSE5.LOC					LD	ON	LD.LOC			=	TD.TOLOC
+		INNER JOIN	WMWHSE5.PUTAWAYZONE 		PD	ON	PD.PUTAWAYZONE	=	LD.PUTAWAYZONE
+		INNER JOIN	WMWHSE5.ORDERSTATUSSETUP	SS	ON	SS.CODE			=	OC.STATUS
+		LEFT JOIN 	WMWHSE5.taskmanageruser 	tu 	ON 	tu.userkey 		= 	IT.EDITWHO
+WHERE
+				TD.TASKTYPE = 'PK'
+		AND		TD.STATUS = 9
+		AND CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(TD.STARTTIME, 
          'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
            AT time zone sessiontimezone) AS DATE)
       BETWEEN :DataMovtoDe
@@ -563,3 +557,4 @@ WHERE TD.TASKTYPE = 'PK'
 "ORDER BY DESCR_PLANTA, INICIO_MOV, ORDEM_MOV                             "
 
 )
+
