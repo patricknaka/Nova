92e7569840e741a818ae8e8f69d0d1f1d52045b5
marcusmsv <marcus_msv@yahoo.com.br>
2015-02-02 13:04:40 -0200

    Alteração VW_CAP_TITULO

--- Views LN/VW_CAP_Titulo.sql
+++ Views LN/VW_CAP_Titulo.sql
@@ -1,53 +1,32 @@
--- 06-mai-2014, Fabio Ferreira, Correção timezone
---	FAF.001 - 07-mai-2014, Fabio Ferreira, 	Retirada conversão de timezone para campos somente data
---											Corrigido campos Taxa Juros, Taxa Mora
---	FAF.003 - 12-mai-2014, Fabio Ferreira, 	Retirada conversão de timezone para campo DATA_LIQUIDACAO_TITULO
---											O campo taxa multa foi alterado apara ler o valor da tabela tfacp201
---	FAF.007 - 12-mai-2014, Fabio Ferreira, 	Alteração valor original do título
---	FAF.116 - 07-jun-2014, Fabio Ferreira, 	Retirado campo NR_NFR
---	FAF.116 - 16-jun-2014, Fabio Ferreira, 	Retirado campo NR_PEDIDO_COMPRA
---	FAF.175 - 25-jun-2014, Fabio Ferreira, 	Correção data liquidação
---	FAF.288 - 18-ago-2014, Fabio Ferreira, 	Inclusão da conta contabil origem e destino
---	21/08/2014 - Correção da instrução timezone
---	FAF.305 - 28-ago-2014, Fabio Ferreira, 	Correção subquery conta destino
---	03/10/2014 - Comentamos a instrução CD_CONTA_ORIGEM e CD_CONTA_DESTINO
---****************************************************************************************************************************************************************
 SELECT DISTINCT
 	'CAP' CD_MODULO,
 	tfacp200.t$ninv NR_TITULO,
-	--tfacp200.t$pcom CD_CIA,
   1 as CD_CIA,
-	--tfacp200.t$dim2 CODIGO_FILIAL,
 	CASE WHEN tfacp200.t$ttyp IN ('AGA', 'GA1') THEN 3 ELSE 2 END CD_FILIAL,
 	tfacp200.t$ttyp CD_TRANSACAO_TITULO,
 	tfacp200.t$tpay CD_TIPO_DOCUMENTO,
 	tfacp200.t$ifbp CD_PARCEIRO,
-	--  tdrec947.t$rcno$l NR_NFR,																		--#FAF.116.o	
 	tfacp200.t$docn$l NR_NF_RECEBIDA,
 	tfacp200.t$seri$l NR_SERIE_NF_RECEBIDA,
 	tfacp200.t$line$l SQ_NF_RECEBIDA,
 	tfacp200.t$docd DT_EMISSAO_TITULO,															--#FAF.001.n
 	tfacp200.t$dued DT_VENCIMENTO,															--#FAF.001.n
 	tfacp201.t$odue$l DT_VENCIMENTO_ORIGINAL,														--#FAF.001.n
---	CAST((FROM_TZ(CAST(TO_CHAR(tfacp201.t$odue$l, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 	--#FAF.001.o
---		AT time zone sessiontimezone) AS DATE) DATA_VENC_ORIG_TITULO,								--#FAF.001.o
 	CASE WHEN tfacp200.t$balh$1=0 THEN																									--#FAF.175.n
 		(select  																														--#FAF.003.sn
 			max(p.t$docd) from baandb.ttfacp200201 p where p.t$ttyp=tfacp200.t$ttyp
-	--		and p.t$ninv=tfacp200.t$ninv and p.t$tpay=2) DT_LIQUIDACAO_TITULO,															--#FAF.003.en	--#FAF.175.o
 			and p.t$ninv=tfacp200.t$ninv) 
 	ELSE NULL
 	END													DT_LIQUIDACAO_TITULO,															--#FAF.175.n
 	tfacp200.t$amnt VL_TITULO,
 	nvl(tdrec940.t$tfda$l, tfacp200.t$amnt)  VL_ORIGINAL,									--#FAF.007.o	
-	CASE WHEN tfacp200.t$afpy=2 THEN 1 ELSE 2 END IN_BLOQUEIO_TITULO,
+	CASE WHEN tfacp200.t$afpy=2 THEN '1' ELSE '2' END IN_BLOQUEIO_TITULO,
 	tfacp201.t$pyst$l CD_PREPARADO_PAGAMENTO,
 	tfacp200.t$stap CD_SITUACAO_TITULO,
 	CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tfacp200.t$rcd_utc, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
 		AT time zone sessiontimezone) AS DATE) DT_SITUACAO_TITULO,
 	tfacp200.t$doty$l CD_TIPO_NF,
 	tfacp200.t$balh$1 VL_SALDO,
---	tdrec947.t$orno$l NR_PEDIDO_COMPRA,																	--#FAF.144.o
 	tfcmg011f.t$baoc$l CD_BANCO_DESTINO,
 	tfcmg011f.t$agcd$l NR_AGENCIA_DESTINO,
 	tfcmg011f.t$agdg$l NR_DIGITO_AGENCIA_DESTINO,
@@ -76,8 +55,6 @@ SELECT DISTINCT
 	 AND	tdrec952.t$trtp$l=2
 	 AND 	tdrec952.t$brty$l=0
 	 and rownum=1)	CD_CONTA_DESTINO																		--#FAF.288.en
-	
-	
 FROM
   baandb.ttfacp200201 tfacp200
   LEFT JOIN baandb.ttdrec940201 tdrec940
