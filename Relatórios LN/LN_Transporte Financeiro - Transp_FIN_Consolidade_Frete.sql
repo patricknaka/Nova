c318e0f3b94f6754eb178fe66f3105235657b792
leonardovianna <leonardo.vianna@novapontocom.com.br>
2015-02-04 15:11:57 -0200

    LN_Transporte_Transp_Fin_Consolid_Frete
    2º Ajuste solicitado no chamado 676234.

--- Relatórios LN/LN_Transporte Financeiro - Transp_FIN_Consolidade_Frete.sql
+++ Relatórios LN/LN_Transporte Financeiro - Transp_FIN_Consolidade_Frete.sql
@@ -10,8 +10,6 @@ select q1.* from (
          CASE WHEN cisli940.t$fdty$l=14 THEN
            'NFE'
          ELSE  'NFS' END               TIPO_NF,
-         cisli940.t$fdty$l             TIPO_DOCTO_FISCAL, 
-         FGET.                         DESC_TIPO_DOCTO_FISCAL,
          znfmd630.t$fili$c             FILIAL
      
      FROM       BAANDB.tznfmd630301  znfmd630
@@ -21,34 +19,6 @@ select q1.* from (
      
      INNER JOIN baandb.ttcmcs080301  tcmcs080
              ON tcmcs080.t$cfrw = znfmd630.t$cfrw$c
-       
-      LEFT JOIN ( SELECT d.t$cnst CNST,
-                         l.t$desc DESC_TIPO_DOCTO_FISCAL
-                    FROM BAANDB. tttadv401000 d,
-                         BAANDB. tttadv140000 l
-                   WHERE d.t$cpac = 'ci'
-                     AND d.t$cdom = 'sli.tdff.l'
-                     AND l.t$clan = 'p'
-                     AND l.t$cpac = 'ci'
-                     AND l.t$clab = d.t$za_clab
-                     AND rpad(d.t$vers,4) ||
-                         rpad(d.t$rele,2) ||
-                         rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
-                                                         rpad(l1.t$rele,2) ||
-                                                         rpad(l1.t$cust,4)) 
-                                                from baandb.tttadv401000 l1 
-                                               where l1.t$cpac = d.t$cpac 
-                                                 and l1.t$cdom = d.t$cdom )
-                     AND rpad(l.t$vers,4) ||
-                         rpad(l.t$rele,2) ||
-                         rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
-                                                         rpad(l1.t$rele,2) || 
-                                                         rpad(l1.t$cust,4)) 
-                                                from baandb.tttadv140000 l1 
-                                               where l1.t$clab = l.t$clab 
-                                                 and l1.t$clan = l.t$clan 
-                                                 and l1.t$cpac = l.t$cpac ) ) FGET
-              ON cisli940.t$fdty$l = FGET.CNST
      
      WHERE cisli940.t$fdty$l IN (1,14,15)
      
@@ -65,9 +35,7 @@ select q1.* from (
               CASE WHEN cisli940.t$fdty$l=14 
                THEN 'NFE'
                ELSE 'NFS' END,
-              znfmd630.t$fili$c,
-              cisli940.t$fdty$l,
-              FGET.DESC_TIPO_DOCTO_FISCAL
-       
-     ORDER BY FILIAL,DESC_TRANS,TIPO_NF,TIPO_DOCTO_FISCAL ) q1
+              znfmd630.t$fili$c
+              
+     ORDER BY FILIAL,DESC_TRANS,TIPO_NF ) q1
 where q1.tipo_nf in (:TipoNF)
