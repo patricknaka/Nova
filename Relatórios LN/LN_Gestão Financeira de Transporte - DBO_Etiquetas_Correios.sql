12a4a427942ccf64c1fa90ae988bcb3e7b4267bd
leonardovianna <leonardo.vianna@novapontocom.com.br>
2015-02-18 17:07:39 -0200

    LN_Transporte_Relatorio_Etiquetas_Correio
    Criação de mais um parâmetro Entrega e ajuste do parâmetro Etiqueta para
    também ser opcional.

--- Relatórios LN/LN_Gestão Financeira de Transporte - DBO_Etiquetas_Correios.sql
+++ Relatórios LN/LN_Gestão Financeira de Transporte - DBO_Etiquetas_Correios.sql
@@ -2,13 +2,12 @@ SELECT
   znfmd630.t$pecl$c  NUME_PEDIDO,
   znfmd630.t$etiq$c  NUME_ETIQUETA
 
-FROM       BAANDB.tznfmd630201 znfmd630
+FROM       BAANDB.tznfmd630301 znfmd630
   
-INNER JOIN BAANDB.tznmcs086201 znmcs086
+INNER JOIN BAANDB.tznmcs086301 znmcs086
         ON znfmd630.t$cfrw$c = znmcs086.t$cfrw$c
        AND znfmd630.t$fili$c = znmcs086.t$bchc$c
   
 WHERE znmcs086.t$padr$c = 2
-  AND znfmd630.t$etiq$c in (:Etiqueta)
-
-
+AND ( (:EtiquetaTodas = 0) or (znfmd630.t$etiq$c IN (:Etiqueta) and (:EtiquetaTodas = 1)) )
+OR  ( (:EntregaTodas = 0) or (znfmd630.t$pecl$c IN (:Entrega) and (:EntregaTodas = 1)) )
