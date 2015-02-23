7fd83fa14534c15766bf893e2fdedef6e420cfaf
fabioInfor <fabio.ferreira@infor.com>
2015-02-23 15:03:13 -0300

    Relatório R016

--- Relatórios LN/WMS_Q_WMS_REL_016_-_ Armazenagem_Detalhe.sql
+++ Relatórios LN/WMS_Q_WMS_REL_016_-_ Armazenagem_Detalhe.sql
@@ -1,7 +1,63 @@
 SELECT
   DISTINCT
     WMSADMIN.DB_ALIAS                    PLANTA,
-    TASKDETAIL.TASKDETAILKEY             ID_OM,
+    TO_CHAR(IT.ITRNKEY)             ID_OM,
+    IT.EDITWHO                   ROMA_ID_OPERADOR,  
+    subStr( tu.usr_name,4,
+            inStr(tu.usr_name, ',')-4 )  USUA_NOME,  
+    IT.SKU                       ID_ITEM,
+    SKU.DESCR                            DESC_ITEM,
+    DEPARTSECTORSKU.SECTOR_NAME          SETOR,  
+    DEPARTSECTORSKU.DEPART_NAME          DEPARTAMENTO,  
+    IT.QTY                       QT,
+    LOC.PUTAWAYZONE                      CLA_ORIGEM,
+    IT.FROMLOC                   ID_LOC_ORIG,
+    LOC.LOGICALLOCATION                  ID_DESC_LOC_ORIG,      -- RETIRAR DO RELATÓRIO  
+    IT.TOLOC                     ID_LOCAL,
+    aTOLOC.LOGICALLOCATION               DESC_LOC,              -- RETIRAR DO RELATÓRIO
+    aTOLOC.PUTAWAYZONE                   ID_CLA_LOC,
+    'Concluído'                 ID_SIT,
+    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(IT.EDITDATE, 
+      'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+        AT time zone sessiontimezone) AS DATE)                 
+                                         DT_SIT,
+    CASE WHEN IT.FROMLOC IN ('STAGE', 'RETURN')
+      THEN 'PUTAWAY'
+      ELSE 'MOVE' END     TIPO
+  
+FROM      
+
+  
+          WMWHSE5.ITRN          IT
+
+INNER JOIN          WMWHSE5.SKU
+    ON SKU.SKU = IT.SKU
+
+LEFT JOIN ENTERPRISE.DEPARTSECTORSKU  DEPARTSECTORSKU
+       ON TO_CHAR(ENTERPRISE.DEPARTSECTORSKU.ID_DEPART) = TO_CHAR(SKU.SKUGROUP)
+                            
+LEFT JOIN WMWHSE5.taskmanageruser tu 
+       ON tu.userkey = IT.EDITWHO
+
+LEFT JOIN WMWHSE5.LOC                 aTOLOC
+       ON aTOLOC.LOC = IT.TOLOC
+	   
+LEFT JOIN WMWHSE5.LOC
+      ON LOC.LOC = IT.FROMLOC,
+	   
+          WMSADMIN.PL_DB              WMSADMIN
+
+WHERE 
+   UPPER(WMSADMIN.DB_LOGID) = IT.WHSEID
+  
+AND IT.TRANTYPE='MV'
+AND SOURCETYPE!='PICKING'
+
+UNION
+SELECT
+  DISTINCT
+    WMSADMIN.DB_ALIAS                    PLANTA,
+    TO_CHAR(TASKDETAIL.TASKDETAILKEY)             ID_OM,
     TASKDETAIL.EDITWHO                   ROMA_ID_OPERADOR,  
     subStr( tu.usr_name,4,
             inStr(tu.usr_name, ',')-4 )  USUA_NOME,  
@@ -12,31 +68,32 @@ SELECT
     TASKDETAIL.QTY                       QT,
     LOC.PUTAWAYZONE                      CLA_ORIGEM,
     TASKDETAIL.FROMLOC                   ID_LOC_ORIG,
-    LOC.LOGICALLOCATION                  ID_DESC_LOC_ORIG,   
+    LOC.LOGICALLOCATION                  ID_DESC_LOC_ORIG,    -- RETIRAR DO RELATÓRIO  
     TASKDETAIL.TOLOC                     ID_LOCAL,
-    aTOLOC.LOGICALLOCATION               DESC_LOC,
+    aTOLOC.LOGICALLOCATION               DESC_LOC,            -- RETIRAR DO RELATÓRIO
     aTOLOC.PUTAWAYZONE                   ID_CLA_LOC,
-    CODELKUP.DESCRIPTION                 ID_SIT,
+    TO_CHAR(CODELKUP.DESCRIPTION)                 ID_SIT,
     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(TASKDETAIL.EDITDATE, 
       'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
         AT time zone sessiontimezone) AS DATE)                 
-                                         DT_SIT
+                                         DT_SIT,
+    'PUTAWAY'                            TIPO
   
-FROM      WMWHSE4.LOC,
-          WMWHSE4.SKU
+FROM      WMWHSE5.LOC,
+          WMWHSE5.SKU
 
 LEFT JOIN ENTERPRISE.DEPARTSECTORSKU  DEPARTSECTORSKU
        ON TO_CHAR(ENTERPRISE.DEPARTSECTORSKU.ID_DEPART) = TO_CHAR(SKU.SKUGROUP),
   
-          WMWHSE4.TASKDETAIL          TASKDETAIL
+          WMWHSE5.TASKDETAIL          TASKDETAIL
                             
 LEFT JOIN WMWHSE5.taskmanageruser tu 
        ON tu.userkey = TASKDETAIL.EDITWHO
 
-LEFT JOIN WMWHSE4.LOC                 aTOLOC
+LEFT JOIN WMWHSE5.LOC                 aTOLOC
        ON aTOLOC.LOC = TASKDETAIL.TOLOC
 	   
-LEFT JOIN WMWHSE4.CODELKUP
+LEFT JOIN WMWHSE5.CODELKUP
        ON CODELKUP.CODE = TASKDETAIL.STATUS
 	  AND CODELKUP.LISTNAME = 'TMSTATUS',
 	   
@@ -45,11 +102,12 @@ LEFT JOIN WMWHSE4.CODELKUP
 WHERE SKU.SKU = TASKDETAIL.SKU
   AND UPPER(WMSADMIN.DB_LOGID) = TASKDETAIL.WHSEID
   AND LOC.LOC = TASKDETAIL.FROMLOC
-  AND TASKDETAIL.TASKTYPE = 'PA' 
-  AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(TASKDETAIL.EDITDATE, 
-      'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-        AT time zone sessiontimezone) AS DATE)) Between :DataSituacaoDe
-  AND :DataSituacaoAte
+  AND TASKDETAIL.TASKTYPE = 'PA'
+  AND TASKDETAIL.STATUS != 9  
+  -- AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(TASKDETAIL.EDITDATE, 
+      -- 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+        -- AT time zone sessiontimezone) AS DATE)) Between :DataSituacaoDe
+  -- AND :DataSituacaoAte
   
 "SELECT                                                                                    " &
 "  DISTINCT                                                                                " &
