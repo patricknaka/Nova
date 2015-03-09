ebbeece15ce7598ed62991f63e30cb4bfe427d07
marcusmsv <marcus_msv@yahoo.com.br>
2015-03-09 17:52:34 -0300

    Ajustes View - VW_DES_RAZAO_GD

--- Views LN/VW_DES_RAZAO_GD.sql
+++ Views LN/VW_DES_RAZAO_GD.sql
@@ -1,20 +1,11 @@
 SELECT
-  tfgld106.t$dcdt                      DATA_TRANS,
-  tfgld106.t$oyer                      ANO_FISCAL,
-  tfgld106.T$FPRD                      PERI_FISCAL,
-  Filial.COD_FILIAL                    NUME_FILIAL,
-  
-  CASE WHEN Filial.COD_FILIAL IS NULL 
-         THEN NULL
-       ELSE ( SELECT tfgld010_2.t$desc
-                FROM baandb.ttfgld010301 tfgld010_2
-               WHERE tfgld010_2.t$dimx = Filial.COD_FILIAL
-                 AND tfgld010_2.t$dtyp = 2 )      
-   END                                 NOME_FILIAL,
-  
-  tfgld106.t$dbcr                      NATU_LANCTO,
-  tfgld106.t$obat                      LOTE,
-  tfgld106.t$otyp                      TIPO_TRANSACAO,
+  tfgld106.t$dcdt                      DT_TRANSACAO,
+  tfgld106.t$oyer                      NR_ANO_FISCAL,
+  tfgld106.T$FPRD                      NR_MES_FISCAL,
+  Filial.COD_FILIAL                    NR_FILIAL,
+  tfgld106.t$dbcr                      NR_NATUREZA_LANCAMENTO,
+  tfgld106.t$obat                      NR_LOTE,
+  tfgld106.t$otyp                      NR_TIPO_TRANSACAO,
   tfgld106.t$odoc					   NR_DOCUMENTO,				
   tfgld106.t$olin					   NR_LINHA_DOCUMENTO,
   CASE WHEN ( select count(*) 
@@ -27,19 +18,10 @@ SELECT
                    and a2.t$odoc = tfgld106.t$odoc
                    and a2.t$dbcr != tfgld106.t$dbcr )
        ELSE NULL 
-   END                                 ID_CONTA_CONTRAPARTIDA,
+   END                                 NR_CONTA_CONTRAPARTIDA,
   
-  tfgld106.t$leac                      ID_CONTA_PRINCIPAL,
-  tfgld008.t$desc                      NOME_DA_CONTA,
-  tfgld106.t$dim1                      ID_CCUSTO,
-
-   CASE WHEN tfgld106.t$dim1 = ' ' 
-         THEN ' '
-       ELSE ( SELECT tfgld010_1.t$desc
-                FROM baandb.ttfgld010301 tfgld010_1
-               WHERE tfgld010_1.t$dimx = tfgld106.t$dim1
-                 AND tfgld010_1.t$dtyp = 1 )      
-   END                                 NOME_CCUSTO,
+  tfgld106.t$leac                      NR_CONTA_PRINCIPAL,
+   tfgld106.t$dim1                      NR_CENTRO_CUSTO,
    
    CASE WHEN tfgld106.t$dim3 = ' ' 
          THEN ' '
@@ -47,7 +29,7 @@ SELECT
                 FROM baandb.ttfgld010301 tfgld010_3
                WHERE tfgld010_3.t$dimx = tfgld106.t$dim3
                  AND tfgld010_3.t$dtyp = 3 )      
-   END                                 UN_NEGOCIO,
+   END                                 NM_UNIDADE_NEGOCIO,
    
    CASE WHEN tfgld106.t$dim5 = ' ' 
          THEN ' '
@@ -55,22 +37,22 @@ SELECT
                 FROM baandb.ttfgld010301 tfgld010_5
                WHERE tfgld010_5.t$dimx = tfgld106.t$dim5
                  AND tfgld010_5.t$dtyp = 5 )      
-   END                                 BANDEIRA,
+   END                                 NM_BANDEIRA,
    
-  tfgld106.t$dim6                      PROJETO,
+  tfgld106.t$dim6                      NM_PROJETO,
   
-  tfgld106.t$desc$l                    HIST_COMPLETO,
-  tfgld106.t$refr                      REFERENCIA,
+  tfgld106.t$desc$l                    DS_HISTORICO,
+  tfgld106.t$refr                      DS_REFERENCIA,
   
   CASE WHEN tfgld106.t$dbcr = 1 
          THEN tfgld106.t$amnt 
        ELSE 0 
-   END                                 VLR_DEBITO,
+   END                                 VL_DEBITO,
    
   CASE WHEN tfgld106.t$dbcr = 2 
          THEN tfgld106.t$amnt 
        ELSE 0 
-   END                                 VLR_CREDITO
+   END                                 VL_CREDITO
 FROM       baandb.ttfgld106301  tfgld106
 
  LEFT JOIN ( select distinct 
