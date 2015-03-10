25fbc1c35b6cb9be6b1da8ae84f8c140c5e14005
marcusmsv <marcus_msv@yahoo.com.br>
2015-03-10 11:10:49 -0300

    VW_EST_REFERENCIA - Atulização Filial

--- Views LN/VW_EST_Referencia.sql
+++ Views LN/VW_EST_Referencia.sql
@@ -1,11 +1,11 @@
 ﻿-- #FAF.283 - 18-ago-2014, Fabio Ferreira, Correção QT_FISICA
 --************************************************************************************************************************************************************
 SELECT  1 CD_CIA,
-        tcemm030.t$euca CD_FILIAL,
+        case when tcemm030.t$euca = ' ' then SUBSTR(tcemm112.t$grid,5,2) else tcemm030.t$euca end CD_FILIAL,
         whina112.t$cwar CD_DEPOSITO,
         ltrim(rtrim(whina112.t$item)) CD_ITEM,
         tcmcs003.t$tpar$l CD_MODALIDADE,
-        sum(whina112.t$qstk) QT_FISICA,
+        sum(whina112.t$qskt) QT_FISICA,													--#FAF.283.n
 		tdrec947.t$rcno$l NR_NFR,
 		(SELECT 
 		 case when (max(whwmd215.t$qhnd) - max(whwmd215.t$qchd) - max(whwmd215.t$qnhd))=0 then 0
@@ -23,17 +23,16 @@ FROM    baandb.twhina112201 whina112,
         baandb.ttcemm112201 tcemm112,
         baandb.ttcemm030201 tcemm030,
         baandb.ttcmcs003201 tcmcs003,
---        baandb.twhwmd200201 whwmd200,
+        baandb.twhinr110201 whinr110,
         baandb.ttdrec947201 tdrec947,
         baandb.ttdrec940201 tdrec940
 WHERE   tcemm112.t$loco = 201
 AND     tcemm112.t$waid = whina112.t$cwar
 AND 	tcemm030.t$eunt = tcemm112.t$grid
---AND		whwmd200.t$cwar = tcmcs003.t$cwar
 AND     tcmcs003.t$cwar = whina112.t$cwar
-AND		tdrec947.t$orno$l=whina112.t$orno
-AND		tdrec947.t$pono$l=whina112.t$pono
-AND		tdrec947.t$seqn$l=whina112.t$srnb
+AND   whina112.t$itid = whinr110.t$itid
+AND   whinr110.t$rcno = tdrec947.t$rcno$l
+AND   whinr110.t$rcln = tdrec947.t$rcln$l
 AND		tdrec940.t$fire$l=tdrec947.t$fire$l
 AND		(tdrec940.t$stat$l=4 OR tdrec940.t$stat$l=5)
 AND		tdrec940.t$rfdt$l not in (3,5,8,13,16,22,33)
@@ -42,6 +41,5 @@ GROUP BY tcemm030.t$euca,
          whina112.t$item, 
          tcmcs003.t$tpar$l,
          tdrec940.t$fire$l,
---         whwmd200.t$wvgr,
          tcemm112.t$grid,
          tdrec947.t$rcno$l
