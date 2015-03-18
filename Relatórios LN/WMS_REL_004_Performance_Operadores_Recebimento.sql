c8247f37ba04c9e7373df8ba2cc3ea1db6624343
fabioInfor <fabio.ferreira@infor.com>
2015-03-18 11:42:16 -0300

    Relatório R004

--- Relatórios LN/WMS_REL_004_Performance_Operadores_Recebimento.sql
+++ Relatórios LN/WMS_REL_004_Performance_Operadores_Recebimento.sql
@@ -1,46 +1,59 @@
 SELECT
   DISTINCT
-    WMSADMIN.PL_DB.DB_ALIAS                           PLANTA,
-    TASKDETAIL.EDITWHO                                USUARIO,
+    WMSADMIN.PL_DB.DB_ALIAS                           	PLANTA,
+    TI.EDITWHO                                			USUARIO,
     subStr( tu.usr_name,4,
-            inStr(tu.usr_name, ',')-4 )               NOME_USUARIO,
-    count(distinct SUBSTR(TASKDETAIL.SOURCEKEY,0,10)) NU_RECBTOS,
-    count(distinct RECEIPT.SUPPLIERCODE)              NU_FORNECS,
-    count(TASKDETAIL.SKU)                             NU_ITENS,
-    sum(TASKDETAIL.UOMQTY)                            QTDE,
+            inStr(tu.usr_name, ',')-4 )               	NOME_USUARIO,
+    count(distinct SUBSTR(TI.RECEIPTKEY,0,10)) 			NU_RECBTOS,
+    count(distinct RECEIPT.SUPPLIERCODE)              	NU_FORNECS,
+    count(distinct TI.SKU)   		                          	NU_ITENS,
+    sum(TI.QTY)											QTDE,
     CASE WHEN SKU.SUSR8 = 'MINUCIOSO' THEN 'SIM' 
          ELSE 'NÃO' 
-     END                                              IN_MINUCIOSO,
-    CODELKUP.LONG_VALUE                               TP_NOTA
+     END                                              	IN_MINUCIOSO,
+    TIPO_PEDIDO.DSC_TIPO_PEDIDO                         TP_NOTA
 	
-FROM      WMWHSE1.TASKDETAIL
+FROM      WMWHSE5.ITRN TI
 
-LEFT JOIN WMWHSE1.taskmanageruser tu 
-       ON tu.userkey=TASKDETAIL.EDITWHO,
-	   
+LEFT JOIN WMWHSE5.taskmanageruser tu 
+       ON tu.userkey=TI.EDITWHO,
+	
           WMSADMIN.PL_DB,
-          WMWHSE1.RECEIPT,
-          WMWHSE1.CODELKUP,
-          WMWHSE1.SKU
+          WMWHSE5.RECEIPT
+          
+ LEFT JOIN ( select clkp.code              COD_TIPO_PEDIDO, 
+                    NVL(trans.description, 
+                        clkp.description)  DSC_TIPO_PEDIDO
+               from WMWHSE5.codelkup clkp
+          left join WMWHSE5.translationlist trans 
+                 on trans.code = clkp.code
+                and trans.joinkey1 = clkp.listname
+                and trans.locale = 'pt'
+                and trans.tblname = 'CODELKUP' 
+              where clkp.listname = 'RECEIPTYPE'
+                and Trim(clkp.code) is not null ) TIPO_PEDIDO
+        ON TIPO_PEDIDO.COD_TIPO_PEDIDO = RECEIPT.TYPE,
+		
+          WMWHSE5.SKU
 	 
-WHERE TASKDETAIL.STATUS = '9'
-  AND TASKDETAIL.TASKTYPE = 'PA'
-  AND SUBSTR(TASKDETAIL.SOURCEKEY,0,10) = RECEIPT.RECEIPTKEY
-  AND RECEIPT.TYPE = CODELKUP.CODE
-  AND CODELKUP.LISTNAME = 'RECEIPTYPE'
-  AND UPPER(WMSADMIN.PL_DB.DB_LOGID) = TASKDETAIL.WHSEID
-  AND SKU.SKU=TASKDETAIL.SKU
-  AND Trunc(RECEIPT.RECEIPTDATE) Between :DataRectoDe
-  AND :DataRectoAte
+WHERE TI.TRANTYPE 						=	'DP'
+  AND TI.SOURCETYPE 					= 	'ntrReceiptDetailAdd'
+  AND RECEIPT.RECEIPTKEY				=	TI.RECEIPTKEY
+  AND UPPER(WMSADMIN.PL_DB.DB_LOGID)	= 	TI.WHSEID
+  AND SKU.SKU							=	TI.SKU
 
-GROUP BY TASKDETAIL.STATUS, TASKDETAIL.EDITWHO,
+
+  -- AND Trunc(RECEIPT.RECEIPTDATE) Between :DataRectoDe
+  -- AND :DataRectoAte
+
+GROUP BY TI.EDITWHO,
          subStr( tu.usr_name,4, inStr(tu.usr_name, ',')-4 ), 
-         CODELKUP.LONG_VALUE,
+         TIPO_PEDIDO.DSC_TIPO_PEDIDO,
          WMSADMIN.PL_DB.DB_ALIAS,
          CASE WHEN SKU.SUSR8 = 'MINUCIOSO' THEN 'SIM' 
               ELSE 'NÃO' 
           END
-ORDER BY USUARIO
+--ORDER BY USUARIO
 
 "SELECT                                                                             " &
 "  DISTINCT                                                                         " &
@@ -103,15 +116,15 @@ ORDER BY USUARIO
 "     END                                              IN_MINUCIOSO,                " &
 "    CODELKUP.LONG_VALUE                               TP_NOTA                      " &
 "	                                                                                " &
-"FROM      WMWHSE1.TASKDETAIL                                                       " &
+"FROM      WMWHSE5.TASKDETAIL                                                       " &
 "                                                                                   " &
-"LEFT JOIN WMWHSE1.taskmanageruser tu                                               " &
+"LEFT JOIN WMWHSE5.taskmanageruser tu                                               " &
 "       ON tu.userkey=TASKDETAIL.EDITWHO,                                           " &
 "	                                                                                " &
 "          WMSADMIN.PL_DB,                                                          " &
-"          WMWHSE1.RECEIPT,                                                         " &
-"          WMWHSE1.CODELKUP,                                                        " &
-"          WMWHSE1.SKU                                                              " &
+"          WMWHSE5.RECEIPT,                                                         " &
+"          WMWHSE5.CODELKUP,                                                        " &
+"          WMWHSE5.SKU                                                              " &
 "	                                                                                " &
 "WHERE TASKDETAIL.STATUS = '9'                                                      " &
 "  AND TASKDETAIL.TASKTYPE = 'PA'                                                   " &
