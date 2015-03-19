90ba7b4615f29c33150097415befb1732d02aae1
leonardovianna <leonardo.vianna@novapontocom.com.br>
2015-03-19 17:35:26 -0300

    Transporte_Relatório_DBO_Painel_Ajustada_Todos
    Criaçaõ do campo Unid. Negócio e Atualização no SSRS (Chamado 704757).

--- Relatórios LN/LN_Planejamento Logistico - DBO_Painel_Pendentes_Ajustada_Todos.sql
+++ Relatórios LN/LN_Planejamento Logistico - DBO_Painel_Pendentes_Ajustada_Todos.sql
@@ -3,7 +3,7 @@ SELECT
     tcmcs080.t$dsca    DESC_TRANSP, 
     znfmd060.t$cdes$c  DESC_CONTRATO,
     znsls401.t$entr$c  NUME_ENTREGA,
-	znfmd630.t$etiq$c  NUME_ETIQUETA,
+    znfmd630.t$etiq$c  NUME_ETIQUETA,
     znfmd630.t$fili$c  NUME_FILIAL, 
     cisli940.t$docn$l  NUME_NOTA,  
     cisli940.t$seri$l  NUME_SERIE,     
@@ -68,43 +68,49 @@ SELECT
                        DATA_PREVISTA,
     
     cisli940.t$amnt$l  VALOR,
-	
-	( select znfmd061.t$dzon$c
-	from 	baandb.tznfmd062301 znfmd062, 
-			baandb.tznfmd061301 znfmd061
-	where 	znfmd062.t$cfrw$c = znfmd630.t$cfrw$c 
-	and 	znfmd062.t$cono$c = znfmd630.t$cono$c
-	and 	znfmd062.t$cepd$c <= tccom130.t$pstc
-	and 	znfmd062.t$cepa$c >= tccom130.t$pstc
-	and 	znfmd061.t$cfrw$c = znfmd062.t$cfrw$c
-	and 	znfmd061.t$cono$c = znfmd062.t$cono$c
-	and 	znfmd061.t$creg$c = znfmd062.t$creg$c
-	and 	rownum = 1 )          	REGIAO,
-	znsls400.t$tped$c				ID_TIPO_PEDIDO,
-	znsls407.t$dscs$c				DESCR_TIPO_PEDIDO,
-	znfmd630.t$orno$c				ORDEM_VENDA
+ 
+ ( select znfmd061.t$dzon$c
+     from baandb.tznfmd062301 znfmd062, 
+          baandb.tznfmd061301 znfmd061
+    where znfmd062.t$cfrw$c = znfmd630.t$cfrw$c 
+      and znfmd062.t$cono$c = znfmd630.t$cono$c
+      and znfmd062.t$cepd$c <= tccom130.t$pstc
+      and znfmd062.t$cepa$c >= tccom130.t$pstc
+      and znfmd061.t$cfrw$c = znfmd062.t$cfrw$c
+      and znfmd061.t$cono$c = znfmd062.t$cono$c
+      and znfmd061.t$creg$c = znfmd062.t$creg$c
+      and rownum = 1 ) REGIAO,
+    znsls400.t$tped$c  ID_TIPO_ENTREGA,
+    znsls407.t$dscs$c  DESCR_TIPO_ENTREGA,
+    znfmd630.t$orno$c  ORDEM_VENDA,
+    znint002.t$uneg$c  UNIDADE_NEG,
+    znint002.t$desc$c  DESCR_UNIDADE_NEG
  
 FROM       BAANDB.tznfmd630301 znfmd630
 
 INNER JOIN BAANDB.tznsls004301 znsls004
         ON znsls004.t$orno$c = znfmd630.t$orno$c
-		
+  
 INNER JOIN BAANDB.tznsls401301 znsls401
-        ON 	znsls401.t$ncia$c = znsls004.t$ncia$c
-		AND znsls401.t$uneg$c = znsls004.t$uneg$c
-		AND znsls401.t$pecl$c = znsls004.t$pecl$c
-		AND znsls401.t$sqpd$c = znsls004.t$sqpd$c
-		AND znsls401.t$entr$c = znsls004.t$entr$c
-		AND znsls401.t$sequ$c = znsls004.t$sequ$c
-		
+        ON znsls401.t$ncia$c = znsls004.t$ncia$c
+       AND znsls401.t$uneg$c = znsls004.t$uneg$c
+       AND znsls401.t$pecl$c = znsls004.t$pecl$c
+       AND znsls401.t$sqpd$c = znsls004.t$sqpd$c
+       AND znsls401.t$entr$c = znsls004.t$entr$c
+       AND znsls401.t$sequ$c = znsls004.t$sequ$c
+
+INNER JOIN BAANDB.tznint002301 znint002
+        ON znint002.t$ncia$c = znsls401.t$ncia$c
+       AND znint002.t$uneg$c = znsls401.t$uneg$c
+  
 INNER JOIN BAANDB.tznsls400301 znsls400
-        ON 	znsls400.t$ncia$c = znsls004.t$ncia$c
-		AND znsls400.t$uneg$c = znsls004.t$uneg$c
-		AND znsls400.t$pecl$c = znsls004.t$pecl$c
-		AND znsls400.t$sqpd$c = znsls004.t$sqpd$c
+        ON znsls400.t$ncia$c = znsls004.t$ncia$c
+      AND znsls400.t$uneg$c = znsls004.t$uneg$c
+      AND znsls400.t$pecl$c = znsls004.t$pecl$c
+      AND znsls400.t$sqpd$c = znsls004.t$sqpd$c
 
 INNER JOIN BAANDB.tznsls407301 znsls407
-        ON 	znsls407.t$tpst$c = znsls400.t$tped$c
+        ON znsls407.t$tpst$c = znsls400.t$tped$c
   
  LEFT JOIN BAANDB.tznfmd060301 znfmd060
         ON znfmd060.t$cfrw$c = znfmd630.t$cfrw$c 
@@ -119,17 +125,16 @@ INNER JOIN baandb.tcisli940301 cisli940
        AND cisli940.t$seri$l = znfmd630.t$seri$c  
 
 LEFT JOIN baandb.ttccom130301 tccom130       
-		ON	tccom130.t$cadr = cisli940.t$stoa$l
-		
-		
+  ON tccom130.t$cadr = cisli940.t$stoa$l
+  
 INNER JOIN (SELECT d.t$cnst CNST, 
                    l.t$desc DESC_TIPO_ORDEM
               FROM baandb.tttadv401000 d, baandb.tttadv140000 l 
-             WHERE d.t$cpac='ci' 
-               AND d.t$cdom='sli.tdff.l'
-               AND l.t$clan='p'
-               AND l.t$cpac='ci'
-               AND l.t$clab=d.t$za_clab
+             WHERE d.t$cpac = 'ci' 
+               AND d.t$cdom = 'sli.tdff.l'
+               AND l.t$clan = 'p'
+               AND l.t$cpac = 'ci'
+               AND l.t$clab = d.t$za_clab
                AND rpad(d.t$vers,4) ||
                    rpad(d.t$rele,2) ||
                    rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
@@ -149,18 +154,18 @@ INNER JOIN (SELECT d.t$cnst CNST,
                                            and l1.t$cpac = l.t$cpac)) FGET
         ON FGET.CNST = cisli940.t$fdty$l
          
-WHERE  ( SELECT znfmd640.t$coci$c 
-           FROM BAANDB.tznfmd640301 znfmd640
-          WHERE znfmd640.t$coct$c = 'ETR'          
-            AND znfmd640.t$fili$c = znfmd630.t$fili$c
-            AND znfmd640.t$etiq$c = znfmd630.t$etiq$c
-            AND ROWNUM = 1 ) IS NOT NULL
+WHERE ( SELECT znfmd640.t$coci$c 
+          FROM BAANDB.tznfmd640301 znfmd640
+         WHERE znfmd640.t$coct$c = 'ETR'          
+           AND znfmd640.t$fili$c = znfmd630.t$fili$c
+           AND znfmd640.t$etiq$c = znfmd630.t$etiq$c
+           AND ROWNUM = 1 ) IS NOT NULL
   
-    AND cisli940.t$fdty$l = 1
+  AND cisli940.t$fdty$l = 1
   
-    AND TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znsls401.t$dtep$c, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-    AT time zone 'America/Sao_Paulo') AS DATE))
-    BETWEEN :DataPlanejadaDe
-        AND :DataPlanejadaAte
-	AND tcmcs080.t$cfrw IN (:Transportadora)
-	AND NVL(TRIM(znfmd630.t$stat$c),'P') IN (:Situacao)
+  AND TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znsls401.t$dtep$c, 'DD-MON-YYYY HH24:MI:SS'), 
+        'DD-MON-YYYY HH24:MI:SS'), 'GMT') AT time zone 'America/Sao_Paulo') AS DATE))
+      BETWEEN :DataPlanejadaDe
+          AND :DataPlanejadaAte
+  AND tcmcs080.t$cfrw IN (:Transportadora)
+  AND NVL(TRIM(znfmd630.t$stat$c),'P') IN (:Situacao)
