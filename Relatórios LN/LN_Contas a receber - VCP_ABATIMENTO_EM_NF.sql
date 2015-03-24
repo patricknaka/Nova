476c3cfbacfc3c65a4285a68beed48b09b0ae851
ronaldocs <ronaldo.cristiano@gmail.com>
2015-03-24 16:20:52 -0300

    Relatório VCP Abatimento em NF
    Tratamento, having, para exibir apenas registros liquidados

--- Relatórios LN/LN_Contas a receber - VCP_ABATIMENTO_EM_NF.sql
+++ Relatórios LN/LN_Contas a receber - VCP_ABATIMENTO_EM_NF.sql
@@ -17,7 +17,7 @@ select Q1.* from ( SELECT
                      
                        CASE WHEN sum(tfacp200t.t$balc) = 0 
                               THEN ( select max(a.t$docd) 
-                                       from baandb.ttfacp200201 a 
+                                       from baandb.ttfacp200301 a 
                                       where a.t$ttyp = tfacp200t.t$ttyp 
                                         and a.t$ninv = tfacp200t.t$ninv )
                             ELSE NULL END      liquidacao,
@@ -38,36 +38,33 @@ select Q1.* from ( SELECT
                         
                         znrec007.t$cvpc$c      NUM_CARTA,
                         znrec007.t$amnt$c      VALOR_CARTA,
-                        tfacp200t.t$balc        SALDO_CAP,
-                        tfacp200.t$line         PARCELA
+                        tfacp200t.t$balc       SALDO_CAP,
+                        tfacp200.t$line        PARCELA
                          
-          FROM baandb.ttfacr200201 tfacr200
+                   FROM baandb.ttfacr200301 tfacr200
                  
-             INNER JOIN baandb.tznrec007201 znrec007
+             INNER JOIN baandb.tznrec007301 znrec007
                      ON znrec007.t$ttyp$c = tfacr200.t$ttyp 
                     AND znrec007.t$docn$c = tfacr200.t$ninv   
              
-             INNER JOIN baandb.ttccom100201 tccom100
+             INNER JOIN baandb.ttccom100301 tccom100
                      ON tccom100.t$bpid = tfacr200.t$itbp
                
-             INNER JOIN baandb.ttccom130201 tccom130
+             INNER JOIN baandb.ttccom130301 tccom130
                      ON tccom130.t$cadr = tccom100.t$cadr
              
-             LEFT JOIN baandb.ttfacr201201 tfacr301
+              LEFT JOIN baandb.ttfacr201301 tfacr301
                      ON tfacr301.t$ttyp = tfacr200.t$ttyp
                     AND tfacr301.t$ninv = tfacr200.t$ninv
              
-             INNER JOIN baandb.ttfacp200201 tfacp200
+             INNER JOIN baandb.ttfacp200301 tfacp200
                      ON tfacp200.t$tdoc = tfacr200.t$tdoc
                     AND tfacp200.t$docn = tfacr200.t$docn
              
-             INNER JOIN baandb.ttfacp200201 tfacp200t
+             INNER JOIN baandb.ttfacp200301 tfacp200t
                      ON tfacp200t.t$ttyp = tfacp200.t$ttyp
                     AND tfacp200t.t$ninv = tfacp200.t$ninv
              
---             LEFT JOIN baandb.tcisli940201 cisli940
---                     ON cisli940.t$fire$l = znrec007.t$fire$c
-            
              
               LEFT JOIN ( select l.t$desc Descr_ID_Transacao,
                                  d.t$cnst
@@ -100,13 +97,13 @@ select Q1.* from ( SELECT
                   WHERE tfacr200.t$tdoc = 'ENC'
                     AND tfacr200.t$amnt < 0
                     AND tfacp200t.t$lino = 0
+					
+                 HAVING SUM(tfacp200t.t$balc) = 0 
 
                GROUP BY tfacr200.t$ttyp, 
                         tfacr200.t$ninv, 
-
                         tfacr200.t$tdoc,
                         tfacr200.t$docn,                         
-
                         tccom130.T$FOVN$L,
                         tfacr200.t$itbp,
                         tccom100.t$nama,
@@ -122,7 +119,7 @@ select Q1.* from ( SELECT
                         znrec007.t$cvpc$c,
                         znrec007.t$amnt$c,
                         tfacp200t.t$balc,
-                        tfacp200.t$line) Q1
+                        tfacp200.t$line ) Q1
                  
 where Q1.Vencimento between nvl(:DataVenctoDe, Q1.Vencimento) and nvl(:DataVenctoAte, Q1.Vencimento)
   and Q1.Emissao between nvl(:DataEmissaoDe, Q1.Emissao) and nvl(:DataEmissaoAte, Q1.Emissao)
