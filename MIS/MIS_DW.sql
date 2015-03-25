cae2e7d4bc27c67473d396ab535c84684b2d23ce
marcusmsv <marcus_msv@yahoo.com.br>
2015-03-25 13:39:42 -0300

    Alteração Scrips

---
+++ MIS/MIS_DW.sql
@@ -0,0 +1,3 @@
+--Adaptação LN
+alter table dbo.stg_sige_garantia_estendida
+alter column nr_id_entrega_pai nvarchar(15)
---
+++ MIS/MIS_ODS.sql
@@ -0,0 +1,7 @@
+--Adaptação LN
+
+alter table mis_ods.ln.ods_gar_estendida
+alter column NR_PEDIDO nvarchar(10)
+
+alter table mis_ln.dbo.stg_gar_estendida
+alter column NR_PEDIDO nvarchar(10)
