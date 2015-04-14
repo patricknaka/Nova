5fb2e5826f6a9d8cff92b4a7bcaa4f8bf562af3b
rosanaprignolato <rosana.prignolato@iteris.com.br>
2015-04-14 16:38:06 -0300

    Atualização documentação

--- MIS/MIS_ODS.sql
+++ MIS/MIS_ODS.sql
@@ -5,3 +5,9 @@ alter column NR_PEDIDO nvarchar(10)
 
 alter table mis_ln.dbo.stg_gar_estendida
 alter column NR_PEDIDO nvarchar(10)
+
+alter table mis_ods.log.ods_volumetria_detalhe
+alter column nr_deposito nvarchar(9) null
+
+alter table mis_ods.log.ods_volumetria
+alter column nr_id_deposito nvarchar(9) null
