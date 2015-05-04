2e4c81af496aef364f722ef7b4707e31faefa0de
fabioInfor <fabio.ferreira@infor.com>
2015-05-04 19:44:48 -0300

    Relatório perda logistica

--- Relatórios LN/LN_Logística Reversa - Perda Logística Logística Reversa.sql
+++ Relatórios LN/LN_Logística Reversa - Perda Logística Logística Reversa.sql
@@ -1,7 +1,7 @@
 SELECT 
   WSOR.WHSEID                  ID_FILIAL,
   wmsCODE.UDF2                 NOME_FILIAL,
-  WHINH200.T$ORNO              PEDIDO_LN,
+  WSOR.REFERENCEDOCUMENT       PEDIDO_LN,
   WSOR.ORDERKEY                PEDIDO_WMS,
   CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(WSOR.ADDDATE, 
     'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
@@ -12,167 +12,124 @@ SELECT
     'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
       AT time zone 'America/Sao_Paulo') AS DATE)
                                DATA_EVENTO,
-  CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(cisli940.t$DATE$L, 
-    'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-      AT time zone 'America/Sao_Paulo') AS DATE)
-                               DATA_FATURAMENTO,
+   CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(LNRF.t$DATE$L, 
+     'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+       AT time zone 'America/Sao_Paulo') AS DATE)
+                                DATA_FATURAMENTO,
   WSOR.INVOICENUMBER           NF,
   WSOR.LANE                    NF_SERIE,
-  NVL(TCMCS966.T$DSCA$L,
-      TCMCS966x.T$DSCA$L)      DESCR_TP_DOC_FISCAL,
-  WHINH010.T$DSCA              NOME_OPERADOR,
-  TRIM(WHINH220.T$ITEM)        ITEM,
-  TCIBD001.T$DSCA              DESCRICAO,
+  LNRF.T$DSCA$L      	DESCR_TP_DOC_FISCAL,													
+  TIPO_PEDIDO.DSC_TIPO_PEDIDO  	NOME_OPERADOR,																	
+  TRIM(WSOD.SKU)        		ITEM,	
+  TCIBD001.T$DSCA              DESCRICAO,																		
   TCMCS023.T$DSCA              DEPARTAMENTO,
   LNCF.T$FOVN$L                CNPJ_FABRICANTE,
   LNFB.T$DSCA                  NOME_FABRICANTE,
-  LNCC.T$FOVN$L                CNPJ_CLIENTE,
-  LNCC.T$NAMA                  NOME_CLIENTE,
-  WHWMD400.T$HGHT * 
-  WHWMD400.T$WDTH * 
-  WHWMD400.T$DPTH              M3_UNITARIO,
-  SUM(WHINH220.T$QORO)         QTD_PEDIDO,
-  SUM(WHINH220.T$QORO)         QTD_CANCELADA,     -- Será sempre o mesmo que a quantidade do pedido pois no Ln não temos entrega parcial
-  NVL(SUM(CISLI941.T$DQUA$L),0)QTD_FATURADA,
-  ABS(MAX(CISLI941.T$PRIC$L))  VALOR_NF_UNITARIO,
-  ABS(SUM(CISLI941.T$AMNT$L))  VALOR_NF_TOTAL,
+  NVL(LNCC.T$FOVN$L, 
+	  SUBSTR(WSOR.C_COMPANY,0, INSTR(WSOR.C_COMPANY,' ')))                CNPJ_CLIENTE,
+  CASE WHEN WSOR.CONSIGNEEKEY=' '
+	THEN SUBSTR(WSOR.C_COMPANY, INSTR(WSOR.C_COMPANY,' ')+1,20)
+	ELSE WSOR.C_COMPANY END		NOME_CLIENTE,               
+  WSKU.STDCUBE					M3_UNITARIO,																
+  WSOD.ORIGINALQTY   		QTD_PEDIDO,																		
+  WSOD.ORIGINALQTY    		QTD_CANCELADA,     -- Será sempre o mesmo que a quantidade do pedido pois no Ln não temos entrega parcial
+  NVL(LNRF.T$DQUA$L,0)	QTD_FATURADA,
+  ABS(LNRF.T$PRIC$L)  	VALOR_NF_UNITARIO,
+  ABS(LNRF.T$AMNT$L)  	VALOR_NF_TOTAL,
   WSOR.EDITWHO                 USUARIO_CANCELAMENTO,
   DIVS.DESCRIPTION             SITUACAO_FATUTAMENTO
 
-FROM       BAANDB.TWHINH200301     WHINH200
+FROM 		WMWHSE5.ORDERS   	WSOR
 
-INNER JOIN WMWHSE5.ORDERS@DL_LN_WMS   WSOR
-        ON CASE WHEN INSTR(WSOR.REFERENCEDOCUMENT, '_') = 0
-                  THEN WSOR.REFERENCEDOCUMENT
-                ELSE   SUBSTR(WSOR.REFERENCEDOCUMENT, 4, 9)
-            END  = WHINH200.T$ORNO
+INNER JOIN 	( SELECT A.ORDERKEY,
+                     A.SKU,
+                     SUM(A.ORIGINALQTY) ORIGINALQTY
+              FROM WMWHSE5.ORDERDETAIL A
+              GROUP BY  A.ORDERKEY,
+                        A.SKU)  WSOD
+		ON	WSOD.ORDERKEY	=	WSOR.ORDERKEY
 
+INNER JOIN 	WMWHSE5.SKU WSKU
+		ON	WSKU.SKU = WSOD.SKU
+		
 INNER JOIN ( select A.LONG_VALUE,
                     UPPER(A.UDF1) UDF1,
                     A.UDF2
-               from ENTERPRISE.CODELKUP@DL_LN_WMS A
+               from ENTERPRISE.CODELKUP A
               where A.LISTNAME = 'SCHEMA') wmsCODE
         ON wmsCODE.UDF1 = WSOR.WHSEID
   
-INNER JOIN WMWHSE5.ORDERSTATUSSETUP@DL_LN_WMS WOSS
+INNER JOIN WMWHSE5.ORDERSTATUSSETUP WOSS
         ON WOSS.CODE = WSOR.STATUS
 
-INNER JOIN BAANDB.TWHINH010301  WHINH010
-        ON WHINH010.T$OTYP = WHINH200.T$OTYP
-    
-INNER JOIN BAANDB.TWHINH220301  WHINH220
-        ON WHINH220.T$OORG = WHINH200.T$OORG
-       AND WHINH220.T$ORNO = WHINH200.T$ORNO
-       AND WHINH220.T$SEQN = WHINH200.T$OSET  
+LEFT JOIN ( select clkp.code          COD_TIPO_PEDIDO, 
+                    NVL(trans.description, 
+                    clkp.description)  DSC_TIPO_PEDIDO
+               from WMWHSE5.codelkup clkp
+          left join WMWHSE5.translationlist trans 
+                 on trans.code = clkp.code
+                and trans.joinkey1 = clkp.listname
+                and trans.locale = 'pt'
+                and trans.tblname = 'CODELKUP' 
+              where clkp.listname = 'ORDERTYPE'
+                and Trim(clkp.code) is not null  ) TIPO_PEDIDO
+        ON TIPO_PEDIDO.COD_TIPO_PEDIDO = WSOR.type
 
-INNER JOIN BAANDB.TTCIBD001301  TCIBD001
-        ON TCIBD001.T$ITEM = WHINH220.T$ITEM
+INNER JOIN BAANDB.TTCIBD001301@PLN01  TCIBD001
+        ON TRIM(TCIBD001.T$ITEM) = WSOD.SKU
 
- LEFT JOIN BAANDB.TWHWMD400301  WHWMD400
-        ON WHWMD400.T$ITEM = Trim(WHINH220.T$ITEM)
-    
- LEFT JOIN (       Select  3 koor,  1 oorg From DUAL
-             Union Select  7 koor,  2 oorg From DUAL
-             Union Select 34 koor,  3 oorg From DUAL
-             Union Select  2 koor, 80 oorg From DUAL
-             Union Select  6 koor, 81 oorg From DUAL
-             Union Select 33 koor, 82 oorg From DUAL
-             Union Select 17 koor, 11 oorg From DUAL
-             Union Select 35 koor, 12 oorg From DUAL
-             Union Select 37 koor, 31 oorg From DUAL
-             Union Select 39 koor, 32 oorg From DUAL
-             Union Select 38 koor, 33 oorg From DUAL
-             Union Select 42 koor, 34 oorg From DUAL
-             Union Select  1 koor, 50 oorg From DUAL
-             Union Select 32 koor, 51 oorg From DUAL
-             Union Select 56 koor, 53 oorg From DUAL
-             Union Select  9 koor, 55 oorg From DUAL
-             Union Select 46 koor, 56 oorg From DUAL
-             Union Select 57 koor, 58 oorg From DUAL
-             Union Select 22 koor, 71 oorg From DUAL
-             Union Select 36 koor, 72 oorg From DUAL
-             Union Select 58 koor, 75 oorg From DUAL
-             Union Select 59 koor, 76 oorg From DUAL
-             Union Select 60 koor, 90 oorg From DUAL
-             Union Select 21 koor, 61 oorg From DUAL ) KOOR2OORG
-        ON KOOR2OORG.OORG = WHINH220.T$OORG    
-
- LEFT JOIN BAANDB.TCISLI245301  CISLI245 
-        ON CISLI245.T$SLCP = 301       
-       AND CISLI245.T$ORTP = CASE WHEN WHINH220.T$OORG=1
-                                    THEN 1
-                                  ELSE 2 
-                              END
-       AND CISLI245.T$KOOR = KOOR2OORG.KOOR       
-       AND CISLI245.T$SLSO = WHINH220.T$ORNO       
-       AND CISLI245.T$OSET = WHINH200.T$OSET                  
-       AND CISLI245.T$PONO = WHINH220.T$PONO
-     
- LEFT JOIN BAANDB.TCISLI941301 CISLI941 
-        ON CISLI941.T$FIRE$L = CISLI245.T$FIRE$L
-       AND CISLI941.T$LINE$L = CISLI245.T$LINE$L
-     
- LEFT JOIN BAANDB.TCISLI940301 CISLI940 
-        ON CISLI940.T$FIRE$L = CISLI941.T$FIRE$L
- 
- LEFT JOIN BAANDB.TTCMCS966301 TCMCS966
-        ON TCMCS966.T$FDTC$L = CISLI940.T$FDTC$L  
-
- LEFT JOIN ( select A.T$FIRE$L,
-                    A.T$DOCN$L,
-                    A.T$SERI$L,
-                    A.T$FDTC$L
-               from BAANDB.TCISLI940301 A
-              where A.T$DATG$L = ( SELECT MIN(B.T$DATG$L) 
-                                     FROM BAANDB.TCISLI940301 B
-                                    WHERE B.T$DOCN$L = A.T$DOCN$L
-                                      AND B.T$SERI$L = A.T$SERI$L ) ) CISLI940x 
-        ON CISLI940x.T$DOCN$L = WSOR.INVOICENUMBER
-       AND CISLI940x.T$SERI$L = WSOR.LANE
-
- LEFT JOIN BAANDB.TTCMCS966301 TCMCS966x
-        ON TCMCS966x.T$FDTC$L = CISLI940x.T$FDTC$L
-      
- LEFT JOIN BAANDB.TTCMCS023301  TCMCS023
+ LEFT JOIN BAANDB.TTCMCS023301@PLN01  TCMCS023
         ON TCMCS023.T$CITG = TCIBD001.T$CITG
      
- LEFT JOIN BAANDB.TTCMCS060301  LNFB 
+ LEFT JOIN BAANDB.TTCMCS060301@PLN01  LNFB 
         ON LNFB.T$CMNF = TCIBD001.T$CMNF
     
- LEFT JOIN BAANDB.TTCCOM130301  LNCF
+ LEFT JOIN BAANDB.TTCCOM130301@PLN01  LNCF
         ON LNCF.T$CADR = LNFB.T$CADR
 
- LEFT JOIN BAANDB.TTDSLS400301  TDSLS400
-        ON TDSLS400.T$ORNO = WHINH200.T$ORNO
-    
- LEFT JOIN BAANDB.TTCCOM130301  LNCC
-        ON LNCC.T$CADR = NVL(TDSLS400.T$OFAD, WHINH200.T$STAD)
-
- LEFT JOIN WMWHSE5.codelkup@DL_LN_WMS DIVS
+ LEFT JOIN	BAANDB.TTCCOM100301@PLN01	LNCP
+		ON	LNCP.T$BPID	=	TRIM(WSOR.CONSIGNEEKEY)
+		
+ LEFT JOIN BAANDB.TTCCOM130301@PLN01  LNCC
+        ON LNCC.T$CADR = LNCP.T$CADR
+        
+ LEFT JOIN WMWHSE5.codelkup DIVS
         ON DIVS.CODE = WSOR.INVOICESTATUS
        AND DIVS.LISTNAME = 'INVSTATUS'
-  
+
+LEFT JOIN ( SELECT  A.T$CNFE$L,
+                    B.T$ITEM$L,
+                    A.t$DATE$L,
+                    C.T$DSCA$L,
+                    SUM(B.T$DQUA$L) T$DQUA$L,
+                    MAX(B.T$PRIC$L) T$PRIC$L,
+                    SUM(B.T$AMNT$L) T$AMNT$L
+            FROM  BAANDB.TCISLI940301@PLN01 A
+            INNER JOIN BAANDB.TCISLI941301@PLN01 B ON B.T$FIRE$L = A.T$FIRE$L
+            INNER JOIN BAANDB.TTCMCS966301@PLN01 C ON C.T$FDTC$L = A.T$FDTC$L
+            WHERE A.T$CNFE$L != ' '
+            GROUP BY  A.T$CNFE$L,
+                      B.T$ITEM$L,
+                      A.t$DATE$L,
+                      C.T$DSCA$L) LNRF
+          ON  LNRF.T$CNFE$L = WSOR.LOCATOR
+          AND TRIM(LNRF.T$ITEM$L) = WSOD.SKU
+
+
 WHERE WSOR.STATUS = '100'
   AND ( select count(x.orderkey)
-          from WMWHSE5.orders@DL_LN_WMS x
+          from WMWHSE5.orders x
          where x.referencedocument = WSOR.referencedocument
-           and x.adddate > WSOR.adddate ) = 0  
-          
-  AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(WSOR.ADDDATE, 
-              'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-                AT time zone 'America/Sao_Paulo') AS DATE))
-      Between :DataRegistroDe
-          And :DataRegistroAte
-  AND ((:PedidoTodos = 1)OR(WHINH200.T$ORNO IN (:Pedido) AND :PedidoTodos = 0))
-  
-GROUP BY WSOR.WHSEID, wmsCODE.UDF2, WHINH200.T$ORNO, WSOR.ORDERKEY, WSOR.ADDDATE, 
-         WOSS.DESCRIPTION, WSOR.EDITDATE, cisli940.t$DATE$L, WSOR.INVOICENUMBER,
-         WSOR.LANE,NVL(TCMCS966.T$DSCA$L, TCMCS966x.T$DSCA$L), WHINH010.T$DSCA,
-         TRIM(WHINH220.T$ITEM), TCIBD001.T$DSCA, TCMCS023.T$DSCA, LNCF.T$FOVN$L,
-         LNFB.T$DSCA, LNCC.T$FOVN$L, LNCC.T$NAMA, WHWMD400.T$HGHT * WHWMD400.T$WDTH *
-         WHWMD400.T$DPTH, WSOR.EDITWHO, DIVS.DESCRIPTION
+           and x.adddate > WSOR.adddate ) = 0
 
-ORDER BY PEDIDO_WMS, DATA_REGISTRO
+          
+  -- AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(WSOR.ADDDATE, 
+              -- 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                -- AT time zone 'America/Sao_Paulo') AS DATE))
+      -- Between :DataRegistroDe
+          -- And :DataRegistroAte
+  -- AND ((:PedidoTodos = 1)OR(WHINH200.T$ORNO IN (:Pedido) AND :PedidoTodos = 0))
+ 
 
 
 
