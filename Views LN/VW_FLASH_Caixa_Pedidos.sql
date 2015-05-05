cf6f173b86756d6cc76efe7858466c609dde9a5b
fabioInfor <fabio.ferreira@infor.com>
2015-05-05 18:16:44 -0300

    VW_FLASH_Caixa_pedidos

--- Views LN/VW_FLASH_Caixa_Pedidos.sql
+++ Views LN/VW_FLASH_Caixa_Pedidos.sql
@@ -33,7 +33,7 @@ FROM
         sum(znsls401.t$vlfr$c) - 
         sum(znsls401.t$vldi$c)   VL_TOTAL,
         sum(znsls401.t$qtve$c)   QUANTIDADE
-FROM baandb.tznsls401201 znsls401
+FROM baandb.tznsls401301 znsls401
 GROUP BY
         znsls401.T$NCIA$C,
                 znsls401.T$UNEG$C,
@@ -42,7 +42,7 @@ GROUP BY
                 znsls401.T$ENTR$C,
         znsls401.t$itml$c) Q_ENTREGA
 
-INNER JOIN baandb.tznsls400201 znsls400    ON  znsls400.T$NCIA$C = Q_ENTREGA.T$NCIA$C
+INNER JOIN baandb.tznsls400301 znsls400    ON  znsls400.T$NCIA$C = Q_ENTREGA.T$NCIA$C
                                             AND znsls400.T$UNEG$C = Q_ENTREGA.T$UNEG$C
                                             AND znsls400.T$PECL$C = Q_ENTREGA.T$PECL$C
                                             AND znsls400.T$SQPD$C = Q_ENTREGA.T$SQPD$C
@@ -55,30 +55,29 @@ INNER JOIN
           znsls410.T$SQPD$C,
           znsls410.T$ENTR$C,
           znsls410.t$ORNO$C,
-          MAX(znsls410.T$POCO$C) 
-            KEEP (DENSE_RANK LAST ORDER BY znsls410.T$DTOC$C, 
-                           znsls410.T$SEQN$C)      ID_ULTIMO_PONTO
-  FROM  baandb.tznsls410201 znsls410
-  GROUP BY        znsls410.T$NCIA$C,
-          znsls410.T$UNEG$C,
-          znsls410.T$PECL$C,
-          znsls410.T$SQPD$C,
-          znsls410.T$ENTR$C,
-          znsls410.t$ORNO$C) Q_UPONTO
+          znsls410.T$POCO$C ID_ULTIMO_PONTO,
+          ROW_NUMBER() OVER (PARTITION BY znsls410.T$NCIA$C,
+                                          znsls410.T$UNEG$C,
+                                          znsls410.T$PECL$C,
+                                          znsls410.T$SQPD$C,
+                                          znsls410.T$ENTR$C
+                            ORDER BY znsls410.T$DTOC$C DESC, znsls410.T$SEQN$C DESC) RN
+	FROM  baandb.tznsls410301 znsls410) Q_UPONTO
                       ON  Q_UPONTO.T$NCIA$C = Q_ENTREGA.T$NCIA$C
                       AND Q_UPONTO.T$UNEG$C = Q_ENTREGA.T$UNEG$C
                       AND Q_UPONTO.T$PECL$C = Q_ENTREGA.T$PECL$C
                       AND Q_UPONTO.T$SQPD$C = Q_ENTREGA.T$SQPD$C
                       AND Q_UPONTO.T$ENTR$C = Q_ENTREGA.T$ENTR$C
         
-INNER JOIN  baandb.ttdsls400201 tdsls400  ON  tdsls400.t$orno = Q_UPONTO.t$ORNO$C
-INNER JOIN  baandb.ttcmcs065201 tcmcs065  ON  tcmcs065.t$cwoc = tdsls400.t$cofc
-INNER JOIN  baandb.ttccom130201 tccom130  ON  tccom130.t$cadr = tcmcs065.t$cadr
-INNER JOIN  baandb.tznfmd001201 znfmd001  ON  znfmd001.t$fovn$c = tccom130.t$fovn$l
-INNER JOIN  baandb.twhwmd400201 whwmd400  ON  whwmd400.t$item = Q_ENTREGA.t$itml$c
-INNER JOIN  baandb.ttcibd001201 tcibd001   ON  tcibd001.t$item = Q_ENTREGA.t$itml$c
-INNER JOIN  baandb.ttcmcs023201  tcmcs023  ON  tcmcs023.t$citg = tcibd001.t$citg
+INNER JOIN  baandb.ttdsls400301 tdsls400  ON  tdsls400.t$orno = Q_UPONTO.t$ORNO$C
+INNER JOIN  baandb.ttcmcs065301 tcmcs065  ON  tcmcs065.t$cwoc = tdsls400.t$cofc
+INNER JOIN  baandb.ttccom130301 tccom130  ON  tccom130.t$cadr = tcmcs065.t$cadr
+INNER JOIN  baandb.tznfmd001301 znfmd001  ON  znfmd001.t$fovn$c = tccom130.t$fovn$l
+INNER JOIN  baandb.twhwmd400301 whwmd400  ON  whwmd400.t$item = Q_ENTREGA.t$itml$c
+INNER JOIN  baandb.ttcibd001301 tcibd001   ON  tcibd001.t$item = Q_ENTREGA.t$itml$c
+INNER JOIN  baandb.ttcmcs023301  tcmcs023  ON  tcmcs023.t$citg = tcibd001.t$citg
 
 WHERE Q_UPONTO.ID_ULTIMO_PONTO='WMS'
+AND Q_UPONTO.RN=1
 AND Q_ENTREGA.QUANTIDADE>0
 AND tdsls400.t$hdst!=35
