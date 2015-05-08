8eab3069e47498b32a92b24c6a3b8a785d572057
ronaldocs <ronaldo.cristiano@gmail.com>
2015-05-08 19:50:51 -0300

    Relatório Tipo Aconselhamento Adiantamento
    Ajuste nos parâmetros

--- Relatórios LN/LN_Contas a pagar - Tipo de Aconselhamento Adiantamento.sql
+++ Relatórios LN/LN_Contas a pagar - Tipo de Aconselhamento Adiantamento.sql
@@ -17,46 +17,49 @@ SELECT
     tfcmg101.t$paym               METODO_PAGTO,                             --14
     tfcmg001.t$desc               RELACAO_BANCARIA,                         --15
     tfcmg101.t$amnt               VALOR_ADIANTAMENTO,                       --16
-    tfcmg101.t$ptyp || '-' ||
+    tfcmg101.t$ptyp ||
     TO_CHAR(tfcmg101.t$pdoc)      DOCUMENTO_PAGAMENTO,                      --17
     ADIANTAMENTO.                 DT_EMISSAO,                               --18
     ADIANTAMENTO.                 VL_ADIANTADO,                             --19
     ADIANTAMENTO.BALC -
     NVL(ADIANTAMENTO.BALA,0)      VL_ADIANTADO_SALDO,                       --20
     TIPO_DOC.DESCR                TIPO_DOCUMENTO,                           --21
-    PREST_CONTAS.TRANS || '-' ||
+    PREST_CONTAS.TRANS ||
     TO_CHAR(PREST_CONTAS.DOCTO)   LINK_ADIANTAMETNO,                        --22
     PREST_CONTAS.PN               COD_PN_PREST_CONTAS,                      --23
     tccom130pt.t$fovn$l           CNPJ_PN_PREST_CONTAS,                     --24
     tccom100pt.t$nama             PN_PREST_CONTAS,                          --25
-    TRANS_PREST_CONTAS.TRANS || '-' ||
+    TRANS_PREST_CONTAS.TRANS ||
     TRANS_PREST_CONTAS.DOCTO      TRANS_PREST_CONTAS,                       --26
     PREST_CONTAS.TIPO_DOC         TIPO_DOC_PREST_CONTAS,                    --27
     TIPO_DOCTO.DESCR              DSC_TIPO_DOC_PREST_CONTAS,                --28
     PREST_CONTAS.VALOR            VALOR_TRANS_PREST_CONTAS,                 --29
     PREST_CONTAS.BALC -
     NVL(PREST_CONTAS.BALA,0)      SALDO_TRANS_PREST_CONTAS,                 --30
-    tfcmg101.t$refr               REFERENCIA,                               --EXCEL
-    TRANS_PREST_CONTAS.DT_LINK    EMISSAO_LINK,
-    tfgld011_LINK.t$desc          DESCRICAO_LINK,
+    tfcmg101.t$refr               REFERENCIA,                               --31
+    TRANS_PREST_CONTAS.DT_LINK    EMISSAO_LINK,                             --32
+    tfgld011_LINK.t$desc          DESCRICAO_LINK,                           --33
 
-    CASE WHEN GLD102.USUARIO IS NULL THEN
-          GLD106.USUARIO
-    ELSE  GLD102.USUARIO END      ULT_MODIF,
+    CASE WHEN GLD102.USUARIO IS NULL 
+           THEN GLD106.USUARIO
+         ELSE  GLD102.USUARIO 
+     END                          ULT_MODIF,                                --34
     
-    CASE WHEN GLD102.DT_ALT IS NULL THEN
-          GLD106.DT_ALT
-    ELSE  GLD102.DT_ALT END       DATA_ULT_MOD,
-    
-    CASE WHEN GLD102.HR_ALT IS NULL THEN
-          TO_CHAR(TRUNC(GLD106.HR_ALT/3600),'FM9900') || ':' ||
-          TO_CHAR(TRUNC(MOD(GLD106.HR_ALT,3600)/60),'FM00') || ':' ||
-          TO_CHAR(MOD(GLD106.HR_ALT,60),'FM00')
-    ELSE  
-          TO_CHAR(TRUNC(GLD102.HR_ALT/3600),'FM9900') || ':' ||
-          TO_CHAR(TRUNC(MOD(GLD102.HR_ALT,3600)/60),'FM00') || ':' ||
-          TO_CHAR(MOD(GLD102.HR_ALT,60),'FM00')
-    END                         HORA_ULT_MOD
+    CASE WHEN GLD102.DT_ALT IS NULL 
+           THEN GLD106.DT_ALT
+         ELSE  GLD102.DT_ALT 
+     END                          DATA_ULT_MOD,                             --35
+
+    CASE WHEN TRIM(GLD102.HR_ALT) IS NULL AND TRIM(GLD106.HR_ALT) IS NOT NULL
+           THEN TO_CHAR(TRUNC(GLD106.HR_ALT/3600),'FM9900') || ':' ||
+                TO_CHAR(TRUNC(MOD(GLD106.HR_ALT,3600)/60),'FM00') || ':' ||
+                TO_CHAR(MOD(GLD106.HR_ALT,60),'FM00')
+         WHEN TRIM(GLD102.HR_ALT) IS NOT NULL       
+           THEN TO_CHAR(TRUNC(GLD102.HR_ALT/3600),'FM9900') || ':' ||
+                TO_CHAR(TRUNC(MOD(GLD102.HR_ALT,3600)/60),'FM00') || ':' ||
+                TO_CHAR(MOD(GLD102.HR_ALT,60),'FM00')
+         ELSE NULL
+     END                          HORA_ULT_MOD                              --36
  
 FROM       baandb.ttccom100201  tccom100
 
@@ -99,7 +102,7 @@ FROM       baandb.ttccom100201  tccom100
                     a.t$ninv
                from baandb.ttfacp200201 a
               where a.t$tpay = 14 
-              and   a.t$amnt != 0) PREST_CONTAS
+                and a.t$amnt != 0 ) PREST_CONTAS
         ON PREST_CONTAS.t$ttyp = tfcmg101.t$ptyp
        AND PREST_CONTAS.t$ninv = tfcmg101.t$pdoc
        
@@ -136,26 +139,26 @@ FROM       baandb.ttccom100201  tccom100
        AND GLD102.t$ttyp = TRANS_PREST_CONTAS.t$tdoc
        AND GLD102.t$docn = TRANS_PREST_CONTAS.t$docn
        
-  LEFT JOIN ( select a.t$user   USUARIO,
+ LEFT JOIN ( select a.t$user    USUARIO,
                     a.t$date    DT_ALT,
                     a.t$time    HR_ALT,
                     a.t$otyp,
                     a.t$odoc
-              from  baandb.ttfgld106201  a
+               from baandb.ttfgld106201  a
               where a.t$olin = 1 )  GLD106
         ON GLD106.t$otyp = TRANS_PREST_CONTAS.t$tdoc
        AND GLD106.t$odoc = TRANS_PREST_CONTAS.t$docn
  
- LEFT JOIN ( SELECT iDOMAIN.t$cnst CODE_MODAL, 
+ LEFT JOIN ( select iDOMAIN.t$cnst CODE_MODAL, 
                     iLABEL.t$desc DESC_MODAL  
-               FROM baandb.tttadv401000 iDOMAIN, 
+               from baandb.tttadv401000 iDOMAIN, 
                     baandb.tttadv140000 iLABEL 
-              WHERE iDOMAIN.t$cpac = 'tf' 
-                AND iDOMAIN.t$cdom = 'cmg.mopa.d'
-                AND iLABEL.t$clan = 'p'
-                AND iLABEL.t$cpac = 'tf'
-                AND iLABEL.t$clab = iDOMAIN.t$za_clab
-                AND rpad(iDOMAIN.t$vers,4) ||
+              where iDOMAIN.t$cpac = 'tf' 
+                and iDOMAIN.t$cdom = 'cmg.mopa.d'
+                and iLABEL.t$clan = 'p'
+                and iLABEL.t$cpac = 'tf'
+                and iLABEL.t$clab = iDOMAIN.t$za_clab
+                and rpad(iDOMAIN.t$vers,4) ||
                     rpad(iDOMAIN.t$rele,2) ||
                     rpad(iDOMAIN.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
                                                           rpad(l1.t$rele,2) ||
@@ -163,7 +166,7 @@ FROM       baandb.ttccom100201  tccom100
                                                  from baandb.tttadv401000 l1 
                                                 where l1.t$cpac = iDOMAIN.t$cpac 
                                                   and l1.t$cdom = iDOMAIN.t$cdom )
-                AND rpad(iLABEL.t$vers,4) ||
+                and rpad(iLABEL.t$vers,4) ||
                     rpad(iLABEL.t$rele,2) ||
                     rpad(iLABEL.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
                                                          rpad(l1.t$rele,2) ||
@@ -174,16 +177,16 @@ FROM       baandb.ttccom100201  tccom100
                                                  and l1.t$cpac = iLABEL.t$cpac ) ) iTABLE
         ON tfcmg101.t$mopa$d = iTABLE.CODE_MODAL
  
- LEFT JOIN ( SELECT l.t$desc DESCR_TIPO_ACONSELHAMENTO,
+ LEFT JOIN ( select l.t$desc DESCR_TIPO_ACONSELHAMENTO,
                     d.t$cnst
-               FROM baandb.tttadv401000 d,
+               from baandb.tttadv401000 d,
                     baandb.tttadv140000 l
-              WHERE d.t$cpac = 'tf'
-                AND d.t$cdom = 'cmg.tadv'
-                AND l.t$clan = 'p'
-                AND l.t$cpac = 'tf'
-                AND l.t$clab = d.t$za_clab
-                AND rpad(d.t$vers,4) ||
+              where d.t$cpac = 'tf'
+                and d.t$cdom = 'cmg.tadv'
+                and l.t$clan = 'p'
+                and l.t$cpac = 'tf'
+                and l.t$clab = d.t$za_clab
+                and rpad(d.t$vers,4) ||
                     rpad(d.t$rele,2) ||
                     rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
                                                     rpad(l1.t$rele,2) ||
@@ -191,7 +194,7 @@ FROM       baandb.ttccom100201  tccom100
                                            from baandb.tttadv401000 l1 
                                           where l1.t$cpac = d.t$cpac 
                                             and l1.t$cdom = d.t$cdom )
-                AND rpad(l.t$vers,4) ||
+                and rpad(l.t$vers,4) ||
                     rpad(l.t$rele,2) ||
                     rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
                                                     rpad(l1.t$rele,2) ||
@@ -202,16 +205,16 @@ FROM       baandb.ttccom100201  tccom100
                                             and l1.t$cpac = l.t$cpac ) ) ACONSELHAMENTO
         ON ACONSELHAMENTO.t$cnst = tfcmg101.t$tadv
  
- LEFT JOIN ( SELECT iDOMAIN.t$cnst CODE, 
+ LEFT JOIN ( select iDOMAIN.t$cnst CODE, 
                     iLABEL.t$desc DESCR  
-               FROM baandb.tttadv401000 iDOMAIN, 
+               from baandb.tttadv401000 iDOMAIN, 
                     baandb.tttadv140000 iLABEL 
-              WHERE iDOMAIN.t$cpac = 'tf' 
-                AND iDOMAIN.t$cdom = 'acp.tpay'
-                AND iLABEL.t$clan = 'p'
-                AND iLABEL.t$cpac = 'tf'
-                AND iLABEL.t$clab = iDOMAIN.t$za_clab
-                AND rpad(iDOMAIN.t$vers,4) ||
+              where iDOMAIN.t$cpac = 'tf' 
+                and iDOMAIN.t$cdom = 'acp.tpay'
+                and iLABEL.t$clan = 'p'
+                and iLABEL.t$cpac = 'tf'
+                and iLABEL.t$clab = iDOMAIN.t$za_clab
+                and rpad(iDOMAIN.t$vers,4) ||
                     rpad(iDOMAIN.t$rele,2) ||
                     rpad(iDOMAIN.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
                                                           rpad(l1.t$rele,2) ||
@@ -219,7 +222,7 @@ FROM       baandb.ttccom100201  tccom100
                                                  from baandb.tttadv401000 l1 
                                                 where l1.t$cpac = iDOMAIN.t$cpac 
                                                   and l1.t$cdom = iDOMAIN.t$cdom )
-                AND rpad(iLABEL.t$vers,4) ||
+                and rpad(iLABEL.t$vers,4) ||
                     rpad(iLABEL.t$rele,2) ||
                     rpad(iLABEL.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
                                                          rpad(l1.t$rele,2) ||
@@ -230,16 +233,16 @@ FROM       baandb.ttccom100201  tccom100
                                                  and l1.t$cpac = iLABEL.t$cpac ) ) TIPO_DOC
         ON TIPO_DOC.CODE = ADIANTAMENTO.TP_DOCTO
         
- LEFT JOIN ( SELECT iDOMAIN.t$cnst CODE, 
+ LEFT JOIN ( select iDOMAIN.t$cnst CODE, 
                     iLABEL.t$desc    DESCR  
-               FROM baandb.tttadv401000 iDOMAIN, 
+               from baandb.tttadv401000 iDOMAIN, 
                     baandb.tttadv140000 iLABEL 
-              WHERE iDOMAIN.t$cpac = 'tf' 
-                AND iDOMAIN.t$cdom = 'cmg.adty'
-                AND iLABEL.t$clan = 'p'
-                AND iLABEL.t$cpac = 'tf'
-                AND iLABEL.t$clab = iDOMAIN.t$za_clab
-                AND rpad(iDOMAIN.t$vers,4) ||
+              where iDOMAIN.t$cpac = 'tf' 
+                and iDOMAIN.t$cdom = 'cmg.adty'
+                and iLABEL.t$clan = 'p'
+                and iLABEL.t$cpac = 'tf'
+                and iLABEL.t$clab = iDOMAIN.t$za_clab
+                and rpad(iDOMAIN.t$vers,4) ||
                     rpad(iDOMAIN.t$rele,2) ||
                     rpad(iDOMAIN.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
                                                           rpad(l1.t$rele,2) ||
@@ -247,7 +250,7 @@ FROM       baandb.ttccom100201  tccom100
                                                  from baandb.tttadv401000 l1 
                                                 where l1.t$cpac = iDOMAIN.t$cpac 
                                                   and l1.t$cdom = iDOMAIN.t$cdom )
-                AND rpad(iLABEL.t$vers,4) ||
+                and rpad(iLABEL.t$vers,4) ||
                     rpad(iLABEL.t$rele,2) ||
                     rpad(iLABEL.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
                                                          rpad(l1.t$rele,2) ||
@@ -257,17 +260,17 @@ FROM       baandb.ttccom100201  tccom100
                                                  and l1.t$clan = iLABEL.t$clan 
                                                  and l1.t$cpac = iLABEL.t$cpac ) ) CATEGORIA
         ON CATEGORIA.CODE = tfcmg101.t$adty
-		
- LEFT JOIN ( SELECT iDOMAIN.t$cnst   CODE, 
+  
+ LEFT JOIN ( select iDOMAIN.t$cnst   CODE, 
                     iLABEL.t$desc    DESCR  
-               FROM baandb.tttadv401000 iDOMAIN, 
+               from baandb.tttadv401000 iDOMAIN, 
                     baandb.tttadv140000 iLABEL 
-              WHERE iDOMAIN.t$cpac = 'tf' 
-                AND iDOMAIN.t$cdom = 'acp.tpay'
-                AND iLABEL.t$clan = 'p'
-                AND iLABEL.t$cpac = 'tf'
-                AND iLABEL.t$clab = iDOMAIN.t$za_clab
-                AND rpad(iDOMAIN.t$vers,4) ||
+              where iDOMAIN.t$cpac = 'tf' 
+                and iDOMAIN.t$cdom = 'acp.tpay'
+                and iLABEL.t$clan = 'p'
+                and iLABEL.t$cpac = 'tf'
+                and iLABEL.t$clab = iDOMAIN.t$za_clab
+                and rpad(iDOMAIN.t$vers,4) ||
                     rpad(iDOMAIN.t$rele,2) ||
                     rpad(iDOMAIN.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
                                                           rpad(l1.t$rele,2) ||
@@ -275,7 +278,7 @@ FROM       baandb.ttccom100201  tccom100
                                                  from baandb.tttadv401000 l1 
                                                 where l1.t$cpac = iDOMAIN.t$cpac 
                                                   and l1.t$cdom = iDOMAIN.t$cdom )
-                AND rpad(iLABEL.t$vers,4) ||
+                and rpad(iLABEL.t$vers,4) ||
                     rpad(iLABEL.t$rele,2) ||
                     rpad(iLABEL.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
                                                          rpad(l1.t$rele,2) ||
@@ -285,8 +288,11 @@ FROM       baandb.ttccom100201  tccom100
                                                  and l1.t$clan = iLABEL.t$clan 
                                                  and l1.t$cpac = iLABEL.t$cpac ) ) TIPO_DOCTO
         ON TIPO_DOCTO.CODE = PREST_CONTAS.TIPO_DOC
-		
+  
 Where tfcmg101.t$tadv = 5   --Pagamento Adiantado
         
   and tfcmg101.t$plan Between :DataPlanejadoPagtoDe And :DataPlanejadoPagtoAte
   and tfcmg101.t$ifbp IN (:ParceiroNegocio)
+
+  and ((Trim(:TransPConta) is null) OR (Concat(UPPER(TRANS_PREST_CONTAS.TRANS), TRANS_PREST_CONTAS.DOCTO) = UPPER(:TransPConta)))
+  and ((Trim(:DoctoPagto) is null) OR (Concat(tfcmg101.t$ptyp, TO_CHAR(tfcmg101.t$pdoc)) = :DoctoPagto))
