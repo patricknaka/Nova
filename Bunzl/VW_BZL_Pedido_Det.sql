2d0b68b63dad26c15b80aa6b52e1e9fb3044618f
rosanaprignolato <rosana.prignolato@iteris.com.br>
2015-05-22 15:41:44 -0300

    Correção View Bunzl Pedido Det

--- Bunzl/VW_BZL_Pedido_Det.sql
+++ Bunzl/VW_BZL_Pedido_Det.sql
@@ -1,11 +1,10 @@
+SELECT
 --*********************************************************************************************************************
 --	LISTA TODOS AS ENTREGAS INTEGRADAS E OS PRODUTOS IDEPENDDDENTE DO STATUS
 --*********************************************************************************************************************
-SELECT
-
-			ZNSLS401.T$ENTR$C													PEDIDO_WEB,
-			ZNSLS401.T$SEQU$C													SEQUENCIAL,
-			ZNSLS401.T$ITEM$C													ITEM,
+			to_char(ZNSLS401.T$ENTR$C)								PEDIDO_WEB,
+			to_char(ZNSLS401.T$SEQU$C)								SEQUENCIAL,
+			to_char(ZNSLS401.T$ITEM$C)								ITEM,
 			ZNSLS401.T$QTVE$C													QTD,
 			ZNSLS401.T$VLUN$C													PRIC_UNIT,
 			CASE WHEN COUNTDT.CTDT>1
@@ -15,12 +14,11 @@ SELECT
 				AT time zone 'America/Sao_Paulo') AS DATE)
 				ELSE NULL END													DT_ENTREGA
 
-
 FROM
 
-			BAANDB.TZNSLS400301	ZNSLS400
+			BAANDB.TZNSLS400201	ZNSLS400
 			
-INNER JOIN	BAANDB.TZNSLS401301 ZNSLS401	ON	ZNSLS401.T$NCIA$C	=	ZNSLS400.T$NCIA$C
+INNER JOIN	BAANDB.TZNSLS401201 ZNSLS401	ON	ZNSLS401.T$NCIA$C	=	ZNSLS400.T$NCIA$C
                                             AND ZNSLS401.T$UNEG$C   =	ZNSLS400.T$UNEG$C
                                             AND ZNSLS401.T$PECL$C   =	ZNSLS400.T$PECL$C
                                             AND ZNSLS401.T$SQPD$C   =	ZNSLS400.T$SQPD$C
@@ -29,12 +27,14 @@ INNER JOIN (SELECT	C.T$NCIA$C,
                     C.T$UNEG$C,
                     C.T$PECL$C,
                     C.T$SQPD$C,
+                    C.T$ENTR$C,
 					COUNT(DISTINCT TRUNC(C.T$DTEP$C)) CTDT
-			FROM	BAANDB.TZNSLS401301 C
+			FROM	BAANDB.TZNSLS401201 C
 			GROUP BY C.T$NCIA$C,
 			         C.T$UNEG$C,
 			         C.T$PECL$C,
-			         C.T$SQPD$C) COUNTDT	ON	COUNTDT.T$NCIA$C	=	ZNSLS400.T$NCIA$C
+			         C.T$SQPD$C,
+               			 C.T$ENTR$C) COUNTDT	ON	COUNTDT.T$NCIA$C	=	ZNSLS400.T$NCIA$C
 					                        AND COUNTDT.T$UNEG$C   =	ZNSLS400.T$UNEG$C
 					                        AND COUNTDT.T$PECL$C   =	ZNSLS400.T$PECL$C
 					                        AND COUNTDT.T$SQPD$C   =	ZNSLS400.T$SQPD$C	
