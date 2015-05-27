fe5135550b8fb3310d4768c452125361d53f9b71
ronaldocs <ronaldo.cristiano@gmail.com>
2015-05-27 14:12:12 -0300

    Relatório Circularização LN
    Inclusão dos parâmetros e do campo nro. da programação

--- Relatórios LN/LN_Contas a pagar - Circularização.sql
+++ Relatórios LN/LN_Contas a pagar - Circularização.sql
@@ -1,6 +1,42 @@
 SELECT
   DISTINCT
-    301                                         NUME_CIA,
+    tfacp200.t$ttyp                             TIPO_TRANSACAO,
+    to_char(tfacp200.t$docd, 'YYYY')            ANO_EMISSAO, 
+    to_char(tfacp200.t$docd, 'MM')              MES_EMISSAO, 
+    tfacp200.t$docd                             DATA_EMISSAO, 
+    tfacp201.t$odue$l                           DATA_VENCTO_ORIGINAL,
+    tfacp200.t$dued                             DATA_VENCTO,
+    tccom130.t$fovn$l                           CNPJ_FORNECEDOR,  
+    tfacp200.t$ifbp                             NUME_FORNECEDOR,    
+    tccom100.t$nama                             DESC_FORNECEDOR,        
+    tfacp200.t$ninv                             NUME_DOCUMENTO,
+    tfacp201.t$schn                             NUME_PROGRAMACAO,
+    tfacp200.t$docn$l                           NUME_NFISCAL,
+    tfacp200.t$seri$l                           SERIE_NFISCAL,
+    tdrec940.t$fire$l                           NUME_RFISCAL,
+    tfacp200.t$ninv                             NUME_TITULO,
+    tfacp200.t$amnt                             VLR_TITULO,
+    tfacp200.t$balc                             SALD_TITULO,
+    CASE WHEN tfcmg101.t$amnt IS NULL or tfcmg101.t$amnt = 0 
+           THEN NVL(tfacp201.t$amnt,0)                             
+         ELSE  tfcmg101.t$amnt 
+     END                                        VALOR_APAGAR,
+    tfacp200.t$amnt+tfacp200.t$ramn$l           VALOR_COM_RETIMP,
+    CASE WHEN tfacp201.t$pyst$l = 5 
+           THEN 'Pago'
+         ELSE CASE WHEN tfacp200.t$dued >= TRUNC(SYSDATE) 
+                     THEN 'Sim' 
+                   ELSE   'Não' 
+               END
+     END                                        A_VENCER,
+    CASE WHEN tfacp201.t$pyst$l = 5 
+           THEN 'Pago'
+         ELSE CASE WHEN tfacp200.t$dued < TRUNC(SYSDATE) 
+                     THEN 'Sim' 
+                   ELSE   'Não' 
+               END
+     END                                        EM_ATRASO,
+
     tfcmg011.t$baoc$l                           NUME_BANCO,
     tfcmg011.t$agcd$l                           NUME_AGENCIA,
     tfcmg011.t$agdg$l                           DIGI_AGENCIA,
@@ -12,35 +48,6 @@ SELECT
     tfcmg011b.t$agdg$l                          DIGI_AGENCIA_PARC,
     tccom125.t$bano                             NUME_CONTA_PARC,
     tccom125.t$dacc$d                           DIGI_CONTA_PARC,
-  
-    tccom130.t$fovn$l                           CNPJ_FORNECEDOR,  
-    tfacp200.t$ifbp                             NUME_FORNECEDOR,    
-    tccom100.t$nama                             DESC_FORNECEDOR,        
-    tfacp200.t$ttyp                             TIPO_TRANSACAO,
-    tfacp200.t$ninv                             NUME_DOCUMENTO,
-    tdrec940.t$fire$l                           NUME_RFISCAL,
-    tfacp200.t$ninv                             NUME_TITULO,
-    tfacp200.t$docn$l                           NUME_NFISCAL,
-    tfacp200.t$seri$l                           SERIE_NFISCAL,
-    to_char(tfacp200.t$docd, 'YYYY')            ANO_EMISSAO, 
-    to_char(tfacp200.t$docd, 'MM')              MES_EMISSAO, 
-    tfacp200.t$docd                             DATA_EMISSAO, 
-    tfacp201.t$odue$l                           DATA_VENCTO_ORIGINAL,
-    tfacp200.t$dued                             DATA_VENCTO,
-    tfacp200.t$amnt                             VLR_TITULO,
-    tfacp200.t$balc                             SALD_TITULO,
-    CASE WHEN tfcmg101.t$amnt IS NULL or tfcmg101.t$amnt = 0 THEN
-          NVL(tfacp201.t$amnt,0)                             
-    ELSE  tfcmg101.t$amnt END                   VALOR_APAGAR,
-    tfacp200.t$amnt+tfacp200.t$ramn$l           VALOR_COM_RETIMP,
- 
-    CASE WHEN tfacp201.t$pyst$l = 3 
-           THEN 'SIM' 
-         ELSE   'NÃO' 
-     END                                        STATUS_PREPARADO_PAGTO,
-  
-    tfacp201.t$pyst$l                           CODE_STATUS,
- 
     ( SELECT l.t$desc DESCR
         FROM baandb.tttadv401000 d,
              baandb.tttadv140000 l
@@ -66,29 +73,22 @@ SELECT
                                    where l1.t$clab = l.t$clab 
                                      and l1.t$clan = l.t$clan 
                                      and l1.t$cpac = l.t$cpac )
-         AND d.t$cnst = tfacp201.t$pyst$l )      DESCR_STATUS,
-   
-    tfacp201.t$paym                              CODE_METODO_PGTO,
+         AND d.t$cnst = tfacp201.t$pyst$l )     DESCR_STATUS,  
  
+    tfacp201.t$pyst$l                           CODE_STATUS,
+    CASE WHEN tfacp201.t$pyst$l = 3 
+           THEN 'Sim' 
+         ELSE   'Não' 
+     END                                        STATUS_PREPARADO_PAGTO,
+  
+    tfacp201.t$paym                             CODE_METODO_PGTO,
     ( SELECT a.t$desc DS_METODO_PAGAMENTO
         FROM baandb.ttfcmg003301 a
-       WHERE a.t$paym=tfacp201.t$paym )          DESCR_METODO_PGTO,
-    
-    CMG101.LOTE                                  LOTE_PGTO,
-    CASE WHEN tfacp201.t$pyst$l = 5 THEN  --PAGO
-      'PAGO'
-    ELSE
-      CASE WHEN tfacp200.t$dued >= TRUNC(SYSDATE) THEN
-        'SIM' 
-      ELSE 'NAO' END                      
-    END                                          A_VENCER,
-    CASE WHEN tfacp201.t$pyst$l = 5 THEN  --PAGO
-      'PAGO'
-    ELSE
-      CASE WHEN tfacp200.t$dued < TRUNC(SYSDATE) THEN
-        'SIM' 
-      ELSE 'NAO' END                     
-    END                                          EM_ATRASO
+       WHERE a.t$paym = tfacp201.t$paym )       DESCR_METODO_PGTO,
+ 
+    CMG101.LOTE                                 LOTE_PGTO,
+    301                                         NUME_CIA
+
 
 FROM       baandb.ttfacp200301  tfacp200
 
@@ -132,41 +132,41 @@ INNER JOIN baandb.ttfacp201301  tfacp201
        AND CMG101.t$ttyp = tfacp201.t$ttyp 
        AND CMG101.t$ninv = tfacp201.t$ninv
        
-  LEFT JOIN baandb.ttfcmg101301 tfcmg101
-         ON tfcmg101.t$btno = CMG101.LOTE
-        AND tfcmg101.t$ifbp = tfacp201.t$ifbp
-        AND tfcmg101.t$ttyp = tfacp201.t$ttyp
-        AND tfcmg101.t$ninv = tfacp201.t$ninv
+ LEFT JOIN baandb.ttfcmg101301 tfcmg101
+        ON tfcmg101.t$btno = CMG101.LOTE
+       AND tfcmg101.t$ifbp = tfacp201.t$ifbp
+       AND tfcmg101.t$ttyp = tfacp201.t$ttyp
+       AND tfcmg101.t$ninv = tfacp201.t$ninv
   
-  LEFT JOIN ( SELECT d.t$cnst iCODE, l.t$desc STATUS_PAGAMENTO 
-             FROM baandb.tttadv401000 d, 
-                  baandb.tttadv140000 l 
-            WHERE d.t$cpac = 'tf' 
-              AND d.t$cdom = 'acp.stap'
-              AND l.t$clan = 'p'
-              AND l.t$cpac = 'tf'
-              AND l.t$clab = d.t$za_clab
-              AND rpad(d.t$vers,4) ||
-                  rpad(d.t$rele,2) ||
-                  rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
-                                                  rpad(l1.t$rele,2) ||
-                                                  rpad(l1.t$cust,4)) 
-                                         from baandb.tttadv401000 l1 
-                                        where l1.t$cpac = d.t$cpac 
-                                          and l1.t$cdom = d.t$cdom )
-              AND rpad(l.t$vers,4) ||
-                  rpad(l.t$rele,2) ||
-                  rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
-                                                  rpad(l1.t$rele,2) ||
-                                                  rpad(l1.t$cust,4)) 
-                                         from baandb.tttadv140000 l1 
-                                        where l1.t$clab = l.t$clab 
-                                          and l1.t$clan = l.t$clan 
-                                          and l1.t$cpac = l.t$cpac ) ) DSTAP
-    ON tfacp200.t$stap   = DSTAP.iCODE
+ LEFT JOIN ( select d.t$cnst iCODE, l.t$desc STATUS_PAGAMENTO 
+               from baandb.tttadv401000 d, 
+                    baandb.tttadv140000 l 
+              where d.t$cpac = 'tf' 
+                and d.t$cdom = 'acp.stap'
+                and l.t$clan = 'p'
+                and l.t$cpac = 'tf'
+                and l.t$clab = d.t$za_clab
+                and rpad(d.t$vers,4) ||
+                    rpad(d.t$rele,2) ||
+                    rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
+                                                    rpad(l1.t$rele,2) ||
+                                                    rpad(l1.t$cust,4)) 
+                                           from baandb.tttadv401000 l1 
+                                          where l1.t$cpac = d.t$cpac 
+                                            and l1.t$cdom = d.t$cdom )
+                and rpad(l.t$vers,4) ||
+                    rpad(l.t$rele,2) ||
+                    rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
+                                                    rpad(l1.t$rele,2) ||
+                                                    rpad(l1.t$cust,4)) 
+                                           from baandb.tttadv140000 l1 
+                                          where l1.t$clab = l.t$clab 
+                                            and l1.t$clan = l.t$clan 
+                                            and l1.t$cpac = l.t$cpac ) ) DSTAP
+        ON tfacp200.t$stap   = DSTAP.iCODE
             
 WHERE tfacp200.t$lino = 0
-  AND Trunc(tfacp200.t$docd) BETWEEN NVL(:EmissaoDe, tfacp200.t$docd) AND NVL(:EmissaoAte, tfacp200.t$docd)
-  AND Trunc(tfacp200.t$dued) BETWEEN NVL(:VectoDe, tfacp200.t$dued) AND NVL(:VectoAte, tfacp200.t$dued)
-  AND tfacp200.t$ifbp IN (:PN)
+  AND Trunc(tfacp200.t$docd) <= :EmissaoAte
   AND tfacp201.t$pyst$l IN (:Situacao)
+  AND tfacp200.t$ttyp IN (:TipoTransacao)
+  AND tfacp200.t$ifbp IN (:PN)
