734cb1022bfeb5b46dd57fd32633afffbb331dd8
Humberto Kasai <humberto@globalti.com.br>
2015-05-28 18:18:22 -0300

    Update LN_Contas a pagar - Atendimento RN x LN.sql
    [Fwd: ##724405## : [LN/RN] RELATÓRIO ATENDIMENTOS RN X LN]  
    ServiceDesk [servicedesk@suanova.com]

--- Relatórios LN/LN_Contas a pagar - Atendimento RN x LN.sql
+++ Relatórios LN/LN_Contas a pagar - Atendimento RN x LN.sql
@@ -1,8 +1,8 @@
 SELECT
   DISTINCT
     znsls412.t$pecl$c                           PEDIDO,
-                                                ENTREGA,
-                                                ITEM,
+    znsls401.t$entr$c                           ENTREGA,
+    znsls401.t$item$c                           ITEM,
     tfacp200.t$ifbp                             CODE_PN,
     tccom100.t$nama                             DESC_PN,
     regexp_replace(tccom130.t$fovn$l, '[^0-9]', '')
@@ -18,7 +18,7 @@ SELECT
     tccom125.t$bano                             NUME_CONTA,
     tccom125.t$dacc$d                           DIGI_CONTA,  
 	 
-    TENT.RN                                     TENTATIVA_PAGTO
+    TENT.RN                                     TENTATIVA_PAGTO,
     tfcmg101.t$plan                             DATA_TENTATIVA_PAGTO,
     tfcmg101.t$mopa$d                           TENTATIVA_MOD_PAGTO,
     DESC_MODAL_PGTO_2                           DESC_MOD_PAGTO,
@@ -27,7 +27,7 @@ SELECT
     tfacp200.t$ninv                             NUME_TITULO,
     tfacp200.t$docn$l                           NUME_NF,
     tfacp200.t$seri$l                           SERI_NF,
-    tfacp200.t$docd                             DATA_EMISSAO,
+    tfacp200.t$docd                             DATA_EMISSAO
 
 FROM       baandb.ttfacp200301  tfacp200  
 
@@ -51,6 +51,12 @@ INNER JOIN baandb.ttccom130301 tccom130
        AND znsls412.t$ninv$c = tfacp200.t$ninv
        AND znsls412.t$type$c = 3
 
+ LEFT JOIN baandb.tznsls401301  znsls401
+        ON znsls401.t$ncia$c = znsls412.t$ncia$c
+       AND znsls401.t$uneg$c = znsls412.t$uneg$c
+       AND znsls401.t$pecl$c = znsls412.t$pecl$c
+       AND znsls401.t$sqpd$c = znsls412.t$sqpd$c
+        
  LEFT JOIN baandb.ttccom125301  tccom125
         ON tccom125.t$ptbp = tfacp201.t$ifbp
        AND tccom125.t$cban = tfacp201.t$bank
@@ -110,7 +116,6 @@ INNER JOIN baandb.ttccom130301 tccom130
                     D.T$NINV$D,
                     D.T$PTYP$D,
                     D.T$DOCN$D,
-                    D.T$$D,
                     ROW_NUMBER() OVER 
                     ( PARTITION BY D.T$TTYP$D,
                                    D.T$NINV$D
@@ -121,6 +126,34 @@ INNER JOIN baandb.ttccom130301 tccom130
        AND TENT.T$PTYP$D = CMG103.T$TTYP
        AND TENT.T$DOCN$D = CMG103.T$DOCN
        
+ LEFT JOIN ( select l.t$desc DESC_MODAL_PGTO_2,
+                    d.t$cnst COD_MODAL_PGTO_2
+               from baandb.tttadv401000 d,
+                    baandb.tttadv140000 l
+              where d.t$cpac = 'tf'
+                and d.t$cdom = 'cmg.mopa.d'
+                and l.t$clan = 'p'
+                and l.t$cpac = 'tf'
+                and l.t$clab = d.t$za_clab
+                and rpad(d.t$vers,4) ||
+                    rpad(d.t$rele,2) ||
+                    rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
+                                                    rpad(l1.t$rele,2) ||
+                                                    rpad(l1.t$cust,4)) 
+                                           from baandb.tttadv401000 l1 
+                                          where l1.t$cpac = d.t$cpac 
+                                            and l1.t$cdom = d.t$cdom )
+                and rpad(l.t$vers,4) ||
+                    rpad(l.t$rele,2) ||
+                    rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
+                                                    rpad(l1.t$rele,2) ||
+                                                    rpad(l1.t$cust,4)) 
+                                           from baandb.tttadv140000 l1 
+                                          where l1.t$clab = l.t$clab 
+                                            and l1.t$clan = l.t$clan 
+                                            and l1.t$cpac = l.t$cpac ) )  DESC_MODAL_PGTO_2
+        ON DESC_MODAL_PGTO_2.COD_MODAL_PGTO_2 = tfacp201.t$mopa$d 
+        
 WHERE tfacp200.t$docn = 0 
   AND tfacp200.t$ttyp in ('PKB','PKC','PKD','PKE','PKF','PRB','PRW','PKG')
   
