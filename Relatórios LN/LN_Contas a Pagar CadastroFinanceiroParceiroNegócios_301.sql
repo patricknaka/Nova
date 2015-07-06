f515c4db160ff8853d5e3d01de9ae51e6ec6843a
ronaldocs <ronaldo.cristiano@gmail.com>
2015-07-06 19:43:29 -0300

    Ajuste nas tabelas compartilhadas
    Ajuste nas tabelas compartilhadas

--- Relatórios LN/LN_Contas a Pagar CadastroFinanceiroParceiroNegócios_301.sql
+++ Relatórios LN/LN_Contas a Pagar CadastroFinanceiroParceiroNegócios_301.sql
@@ -228,3 +228,217 @@ WHERE tccom100.t$bprl = 4   --Cliente e Fornecedor
   AND tccom124.t$cbtp IN (:TipoNegocioPnCredor)
   
 ORDER BY DATA_CRIACAO
+
+
+=
+
+"SELECT                              " &
+"  DISTINCT                          " &
+" " + Parameters!Compania.Value +  "      COMPANHIA,  " &
+" regexp_replace(tccom130.t$fovn$l, '[^0-9]', '')  " &
+"                                         ENTIDADE_FISCAL,  " &
+" tccom100.t$bpid                         COD_PARCEIRO_NEGOCIOS,  " &
+" tccom100.t$nama                         DSC_PARCEIRO_NEGOCIOS,  " &
+" tccom125.t$cban                         COD_CONTA,  " &
+" tccom125.t$brch                         FILIAL_BANCARIA,  " &
+" tfcmg011.t$baoc$l                       CODE_BANCO,  " &
+" tfcmg011.t$bnam                         NOME_BANCO,  " &
+" CONCAT(CONCAT(tfcmg011.t$baoc$l, '  '),  " &
+"               tfcmg011.t$bnam)          BANCO,  " &
+" tccom125.t$bano                         COD_CONTA_BANCARIA,  " &
+" tccom125.t$dacc$d                       DIG_CONTA_BANCARIA,  " &
+" tccom125.t$toac                         COD_TIPO_CONTA_BANCARIA,  " &
+" iTOAC.ToacDesc                          DSC_TIPO_CONTA_BANCARIA,  " &
+" tccom122.t$paym                         COD_METODO_PAGAMENTO,  " &
+" tfcmg003.t$desc                         DSC_METODO_PAGAMENTO,  " &
+" tccom122.t$mopa$d                       COD_MODALIDADE_PAGAMENTO,  " &
+" iMOPA.MopaDesc                          DSC_MODALIDADE_PAGAMENTO,  " &
+" CONCAT(CONCAT(tccom130.t$namc, ', '),  " &
+"               tccom130.t$hono)          ENDERECO,  " &
+" tccom130.t$dist$l                       BAIRRO,  " &
+" tccom130.t$pstc                         CEP,  " &
+" tccom130.t$ccit                         COD_CIDADE,  " &
+" tccom139.t$dsca                         CIDADE,  " &
+" tccom130.t$cste                         ESTADO,  " &
+" tccom130.t$ccty                         COD_PAIS,  " &
+" tccom130.t$telp                         TELEFONE,  " &
+" tccom100.t$bprl                         COD_FUNCAO,  " &
+" iBPRL.BprlDesc                          DSC_FUNCAO,  " &
+" tccom100.t$prst                         COD_STATUS,  " &
+" iPRST.PrstDesc                          DSC_STATUS,  " &
+" CASE WHEN tccom100.t$btbv = 1  " &
+"        THEN 'Sim'  " &
+"      ELSE   'Não'  " &
+"  END                                    A_SER_VERIFICADO,  " &
+" tccom100.t$prbp                         PARCEIRO_NEGOCIOS_PAI,  " &
+" tccom130.t$ftyp$l                       TIPO_IDENTIFICADOR_FISCAL,  " &
+" tccom100.t$seak                         CHAVE_BUSCA,  " &
+" CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tccom100.t$crdt,  " &
+"   'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  " &
+"     AT time zone 'America/Sao_Paulo') AS DATE)  " &
+"                                         DATA_CRIACAO,  " &
+" CASE WHEN tccom124.t$cbtp IS NOT NULL  " &
+"        THEN tccom124.t$cbtp || ' - ' ||  " &
+"             tcmcs029cr.t$dsca    " &
+"      ELSE NULL                   " &
+" END                                     TIPO_NEG_PN_CREDOR,  " &
+" CASE WHEN tccom122.t$cbtp IS NOT NULL  " &
+"        THEN tccom122.t$cbtp || ' - ' ||  " &
+"             tcmcs029ft.t$dsca    " &
+"      ELSE NULL                   " &
+" END                                     TIPO_NEG_PN_FATURADOR,  " &
+" CASE WHEN tccom100.t$okfi$c = 1  " &
+"        THEN 'Sim'                " &
+"      ELSE   'Não'                " &
+"  END                                    OK_FISCAL,  " &
+" CASE WHEN tccom100.t$okfe$c = 1  " &
+"        THEN 'Sim'                " &
+"      ELSE   'Não'                " &
+"  END                                    OK_FISCAL_EXCECAO,  " &
+" tcmcs013.t$dsca                         CONDICAO_PAGTO  " &
+"FROM       baandb.ttccom100" + Parameters!Compania.Value +  "   tccom100  " &
+" LEFT JOIN baandb.ttccom122" + Parameters!Compania.Value +  "   tccom122  " &
+"        ON tccom122.t$ifbp = tccom100.t$bpid  " &
+" LEFT JOIN baandb.ttcmcs013301 tcmcs013  " &
+"        ON tcmcs013.t$cpay = tccom122.t$cpay  " &
+" LEFT JOIN baandb.ttccom125301  tccom125  " &
+"        ON tccom100.t$bpid = tccom125.t$ptbp  " &
+" LEFT JOIN baandb.ttfcmg003301 tfcmg003  " &
+"        ON tfcmg003.t$paym = tccom122.t$paym  " &
+" LEFT JOIN baandb.ttccom130" + Parameters!Compania.Value +  " tccom130  " &
+"        ON tccom100.t$cadr = tccom130.t$cadr  " &
+" LEFT JOIN baandb.ttcmcs010301 tcmcs010  " &
+"        ON tcmcs010.t$ccty = tccom130.t$ccty  " &
+" LEFT JOIN baandb.ttccom139301 tccom139  " &
+"        ON tccom139.t$city = tccom130.t$ccit  " &
+" LEFT JOIN baandb.ttfcmg011301 tfcmg011  " &
+"        ON tfcmg011.t$bank = tccom125.t$brch  " &
+" LEFT JOIN baandb.ttccom124" + Parameters!Compania.Value +  "  tccom124  " &
+"        ON tccom124.t$ptbp = tccom100.t$bpid  " &
+" LEFT JOIN baandb.ttcmcs029301 tcmcs029cr  " &
+"        ON tcmcs029cr.t$cbtp = tccom124.t$cbtp  " &
+" LEFT JOIN baandb.ttcmcs029301 tcmcs029ft  " &
+"        ON tcmcs029ft.t$cbtp = tccom122.t$cbtp  " &
+" LEFT JOIN ( SELECT d.t$cnst  ToacCode,  " &
+"                    l.t$desc  ToacDesc  " &
+"               FROM baandb.tttadv401000 d,  " &
+"                    baandb.tttadv140000 l  " &
+"              WHERE d.t$cpac = 'tc'  " &
+"                AND d.t$cdom = 'com.toac'  " &
+"                AND l.t$clan = 'p'   " &
+"                AND l.t$cpac = 'tc'  " &
+"                AND l.t$clab = d.t$za_clab  " &
+"                AND rpad(d.t$vers,4) ||  " &
+"                    rpad(d.t$rele,2) ||  " &
+"                    rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"                                                rpad(l1.t$rele,2) ||  " &
+"                                                rpad(l1.t$cust,4))  " &
+"                                                from baandb.tttadv401000 l1  " &
+"                                          where l1.t$cpac = d.t$cpac  " &
+"                                            and l1.t$cdom = d.t$cdom )  " &
+"                   AND rpad(l.t$vers,4) ||  " &
+"                       rpad(l.t$rele,2) ||  " &
+"                       rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"                                                   rpad(l1.t$rele,2) ||  " &
+"                                                   rpad(l1.t$cust,4))  " &
+"                                              from baandb.tttadv140000 l1  " &
+"                                             where l1.t$clab = l.t$clab  " &
+"                                               and l1.t$clan = l.t$clan  " &
+"                                               and l1.t$cpac = l.t$cpac ) ) iTOAC  " &
+"        ON tccom125.t$toac = iTOAC.ToacCode  " &
+" LEFT JOIN ( SELECT d.t$cnst MopaCode,  " &
+"                    l.t$desc MopaDesc  " &
+"               FROM baandb.tttadv401000 d,  " &
+"                    baandb.tttadv140000 l  " &
+"              WHERE d.t$cpac = 'tf'  " &
+"                AND d.t$cdom = 'cmg.mopa.d'  " &
+"                AND l.t$clan = 'p'   " &
+"                AND l.t$cpac = 'tf'  " &
+"                AND l.t$clab = d.t$za_clab  " &
+"                AND rpad(d.t$vers,4) ||  " &
+"                    rpad(d.t$rele,2) ||  " &
+"                    rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"                                                rpad(l1.t$rele,2) ||  " &
+"                                                rpad(l1.t$cust,4))  " &
+"                                           from baandb.tttadv401000 l1  " &
+"                                          where l1.t$cpac = d.t$cpac  " &
+"                                            and l1.t$cdom = d.t$cdom )  " &
+"                AND rpad(l.t$vers,4) ||  " &
+"                    rpad(l.t$rele,2) ||  " &
+"                    rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"                                                rpad(l1.t$rele,2) ||  " &
+"                                                rpad(l1.t$cust,4))  " &
+"                                           from baandb.tttadv140000 l1  " &
+"                                          where l1.t$clab = l.t$clab  " &
+"                                            and l1.t$clan = l.t$clan  " &
+"                                            and l1.t$cpac = l.t$cpac ) ) iMOPA  " &
+"        ON tccom122.t$mopa$d = iMOPA.MopaCode  " &
+" LEFT JOIN ( SELECT d.t$cnst BprlCode,  " &
+"                    l.t$desc BprlDesc  " &
+"               FROM baandb.tttadv401000 d,  " &
+"                    baandb.tttadv140000 l  " &
+"              WHERE d.t$cpac = 'tc'  " &
+"                AND d.t$cdom = 'bprl'  " &
+"                AND l.t$clan = 'p'   " &
+"                AND l.t$cpac = 'tc'  " &
+"                AND l.t$clab = d.t$za_clab  " &
+"                AND rpad(d.t$vers,4) ||  " &
+"                    rpad(d.t$rele,2) ||  " &
+"                    rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"                                                rpad(l1.t$rele,2) ||  " &
+"                                                rpad(l1.t$cust,4))  " &
+"                                           from baandb.tttadv401000 l1  " &
+"                                          where l1.t$cpac = d.t$cpac  " &
+"                                            and l1.t$cdom = d.t$cdom )  " &
+"                AND rpad(l.t$vers,4) ||  " &
+"                    rpad(l.t$rele,2) ||  " &
+"                    rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"                                                rpad(l1.t$rele,2) ||  " &
+"                                                rpad(l1.t$cust,4))  " &
+"                                           from baandb.tttadv140000 l1  " &
+"                                          where l1.t$clab = l.t$clab  " &
+"                                            and l1.t$clan = l.t$clan  " &
+"                                            and l1.t$cpac = l.t$cpac ) ) iBPRL  " &
+"        ON tccom100.t$bprl = iBPRL.BprlCode  " &
+" LEFT JOIN ( SELECT d.t$cnst PrstCode,  " &
+"                    l.t$desc PrstDesc  " &
+"               FROM baandb.tttadv401000 d,  " &
+"                    baandb.tttadv140000 l  " &
+"              WHERE d.t$cpac = 'tc'  " &
+"                AND d.t$cdom = 'com.prst'  " &
+"                AND l.t$clan = 'p'   " &
+"                AND l.t$cpac = 'tc'  " &
+"                AND l.t$clab = d.t$za_clab  " &
+"                AND rpad(d.t$vers,4) ||  " &
+"                    rpad(d.t$rele,2) ||  " &
+"                    rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"                                                rpad(l1.t$rele,2) ||  " &
+"                                                rpad(l1.t$cust,4))  " &
+"                                           from baandb.tttadv401000 l1  " &
+"                                          where l1.t$cpac = d.t$cpac  " &
+"                                            and l1.t$cdom = d.t$cdom )  " &
+"                AND rpad(l.t$vers,4) ||  " &
+"                    rpad(l.t$rele,2) ||  " &
+"                    rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"                    rpad(l1.t$rele,2) ||  " &
+"                    rpad(l1.t$cust,4))  " &
+"               from baandb.tttadv140000 l1  " &
+"              where l1.t$clab = l.t$clab  " &
+"                and l1.t$clan = l.t$clan  " &
+"                and l1.t$cpac = l.t$cpac ) ) iPRST  " &
+"        ON tccom100.t$prst = iPRST.PrstCode  " &
+"WHERE tccom100.t$bprl = 4           " &
+"  AND (tccom125.t$toac = 1 OR tccom125.t$toac IS NULL)  " &
+"  AND ( (tccom100.t$bpid IN  " &
+"        ( " + IIF(Trim(Parameters!PN.Value) = "", "''", "'" + Replace(Replace(Parameters!PN.Value, " ", ""), ",", "','") + "'")  + " )  " &
+"          AND (" + IIF(Parameters!PN.Value Is Nothing, "1", "0") + " = 0))  " &
+"           OR (" + IIF(Parameters!PN.Value Is Nothing, "1", "0") + " = 1) ) " &
+"  AND ((regexp_replace(tccom130.t$fovn$l, '[^0-9]', '')  like '%' || Trim(:CNPJ) || '%') OR (:CNPJ is null))  " &
+"  AND tccom130.t$ftyp$l IN (" + Replace(("'" + JOIN(Parameters!IdentificadorFiscal.Value, "',") + "'"),",",",'") + ") " &
+"  AND NVL(Trim(tfcmg011.t$baoc$l), '0') IN (" + Replace(("'" + JOIN(Parameters!Banco.Value, "',") + "'"),",",",'") + ") " &
+"  AND tccom124.t$cbtp IN (" + Replace(("'" + JOIN(Parameters!TipoNegocioPnCredor.Value, "',") + "'"),",",",'") + ") " &
+"  AND CASE WHEN tccom100.t$okfi$c = 1" &
+"             THEN 1  " &
+"           ELSE   0  " &
+"      END IN (" + Replace(("'" + JOIN(Parameters!OkFiscal.Value, "',") + "'"),",",",'") + ") " &
+"ORDER BY DATA_CRIACAO "
--- Relatórios LN/LN_Contas a pagar - Tipo de Aconselhamento Adiantamento.sql
+++ Relatórios LN/LN_Contas a pagar - Tipo de Aconselhamento Adiantamento.sql
@@ -296,3 +296,306 @@ Where tfcmg101.t$tadv = 5   --Pagamento Adiantado
 
   and ((Trim(:TransPConta) is null) OR (Concat(UPPER(TRANS_PREST_CONTAS.TRANS), TRANS_PREST_CONTAS.DOCTO) = UPPER(:TransPConta)))
   and ((Trim(:DoctoPagto) is null) OR (Concat(tfcmg101.t$ptyp, TO_CHAR(tfcmg101.t$pdoc)) = :DoctoPagto))
+  
+
+  =
+"	SELECT  " &
+"	  DISTINCT  " &
+"		tfcmg101.t$btno               LOTE_PAGTO,   " &
+"		tfcmg101.t$plan               DT_PLANEJADO_PAGTO,   " &
+"		regexp_replace(tccom130.t$fovn$l, '[^0-9]', '')   " &
+"									  CNPJ_PN,  " &
+"		tfcmg101.t$ifbp               COD_PN,   " &
+"		Trim(tccom100.t$nama)         PARCEIRO_NEGOCIOS,  " &
+"		tfcmg101.t$adty               COD_CATEGORIA,  " &
+"		CATEGORIA.DESCR               DSC_CATEGORIA,  " &
+"		ACONSELHAMENTO.               DESCR_TIPO_ACONSELHAMENTO,  " &
+"		tfcmg002.t$desc               MOTIVO_PAGTO,   " &
+"		tfcmg101.t$basu               CODE_CONTA_BANCO_PN,  " &
+"		tccom125.t$brch               FILIAL_BANCARIA_BANCO_PN,   " &
+"		tccom125.t$bano               CONTA_BANCARIA_BANCO_PN,  " &
+"		tccom125.t$dacc$d             DIGITO_CONTA_BANCARIA_BANCO_PN,   " &
+"		tfcmg101.t$paym               METODO_PAGTO,   " &
+"		tfcmg001.t$desc               RELACAO_BANCARIA,   " &
+"		tfcmg101.t$amnt               VALOR_ADIANTAMENTO,   " &
+"		tfcmg101.t$ptyp ||  " &
+"		TO_CHAR(tfcmg101.t$pdoc)      DOCUMENTO_PAGAMENTO,  " &
+"		ADIANTAMENTO.                 DT_EMISSAO,   " &
+"		ADIANTAMENTO.                 VL_ADIANTADO,   " &
+"		ADIANTAMENTO.BALC -   " &
+"		NVL(ADIANTAMENTO.BALA,0)      VL_ADIANTADO_SALDO,   " &
+"		TIPO_DOC.DESCR                TIPO_DOCUMENTO,   " &
+"		PREST_CONTAS.TRANS ||   " &
+"		TO_CHAR(PREST_CONTAS.DOCTO)   LINK_ADIANTAMETNO,  " &
+"		PREST_CONTAS.PN               COD_PN_PREST_CONTAS,  " &
+"		tccom130pt.t$fovn$l           CNPJ_PN_PREST_CONTAS,   " &
+"		tccom100pt.t$nama             PN_PREST_CONTAS,  " &
+"		TRANS_PREST_CONTAS.TRANS ||   " &
+"		TRANS_PREST_CONTAS.DOCTO      TRANS_PREST_CONTAS,   " &
+"		PREST_CONTAS.TIPO_DOC         TIPO_DOC_PREST_CONTAS,  " &
+"		TIPO_DOCTO.DESCR              DSC_TIPO_DOC_PREST_CONTAS,  " &
+"		PREST_CONTAS.VALOR            VALOR_TRANS_PREST_CONTAS,   " &
+"		PREST_CONTAS.BALC -   " &
+"		NVL(PREST_CONTAS.BALA,0)      SALDO_TRANS_PREST_CONTAS,   " &
+"		tfcmg101.t$refr               REFERENCIA,   " &
+"		TRANS_PREST_CONTAS.DT_LINK    EMISSAO_LINK,   " &
+"		tfgld011_LINK.t$desc          DESCRICAO_LINK,   " &
+"   " &
+"		CASE WHEN GLD102.USUARIO IS NULL  " &
+"			   THEN GLD106.USUARIO  " &
+"			 ELSE  GLD102.USUARIO   " &
+"		 END                          ULT_MODIF,  " &
+"		  " &
+"		CASE WHEN GLD102.DT_ALT IS NULL   " &
+"			   THEN GLD106.DT_ALT   " &
+"			 ELSE  GLD102.DT_ALT  " &
+"		 END                          DATA_ULT_MOD,   " &
+"   " &
+"		CASE WHEN TRIM(GLD102.HR_ALT) IS NULL AND TRIM(GLD106.HR_ALT) IS NOT NULL   " &
+"			   THEN TO_CHAR(TRUNC(GLD106.HR_ALT/3600),'FM9900') || ':' ||   " &
+"					TO_CHAR(TRUNC(MOD(GLD106.HR_ALT,3600)/60),'FM00') || ':' ||   " &
+"					TO_CHAR(MOD(GLD106.HR_ALT,60),'FM00')   " &
+"			 WHEN TRIM(GLD102.HR_ALT) IS NOT NULL   " &
+"			   THEN TO_CHAR(TRUNC(GLD102.HR_ALT/3600),'FM9900') || ':' ||   " &
+"					TO_CHAR(TRUNC(MOD(GLD102.HR_ALT,3600)/60),'FM00') || ':' ||   " &
+"					TO_CHAR(MOD(GLD102.HR_ALT,60),'FM00')   " &
+"			 ELSE NULL  " &
+"		 END                          HORA_ULT_MOD  " &
+"	  " &
+"	FROM       baandb.ttccom100" + Parameters!Compania.Value +  "   tccom100  " &
+"   " &
+"	 LEFT JOIN baandb.ttccom130" + Parameters!Compania.Value +  "   tccom130  " &
+"			ON tccom130.t$cadr = tccom100.t$cadr  " &
+"	  " &
+"	 LEFT JOIN baandb.ttfcmg101" + Parameters!Compania.Value +  "   tfcmg101  " &
+"			ON tccom100.T$BPID = tfcmg101.t$ifbp  " &
+"	  " &
+"	 LEFT JOIN baandb.ttccom125" + Parameters!Compania.Value +  "   tccom125   " &
+"			ON tccom125.t$ptbp = tfcmg101.t$ifbp  " &
+"		   AND tccom125.t$cban = tfcmg101.t$basu  " &
+"	  " &
+"	 LEFT JOIN baandb.ttfcmg001" + Parameters!Compania.Value +  "   tfcmg001   " &
+"			ON tfcmg001.t$bank = tfcmg101.t$bank  " &
+"			  " &
+"	 LEFT JOIN baandb.ttfcmg002301 tfcmg002   " &
+"			ON tfcmg002.t$reas = tfcmg101.t$reas  " &
+"	  " &
+"	 LEFT JOIN ( select a.t$docd DT_EMISSAO,  " &
+"						a.t$tpay TP_DOCTO,  " &
+"						a.t$amnt VL_ADIANTADO,  " &
+"						a.t$balc BALC,  " &
+"						a.t$bala BALA,  " &
+"						a.t$ttyp,   " &
+"						a.t$ninv  " &
+"				   from baandb.ttfacp200" + Parameters!Compania.Value +  "   a   " &
+"				  where a.t$tpay = 9 ) ADIANTAMENTO   " &
+"			ON ADIANTAMENTO.t$ttyp = tfcmg101.t$ptyp  " &
+"		   AND ADIANTAMENTO.t$ninv = tfcmg101.t$pdoc  " &
+"	  " &
+"	 LEFT JOIN ( select a.t$tdoc TRANS,   " &
+"						a.t$docn DOCTO,   " &
+"						a.t$ifbp PN,  " &
+"						a.t$tpay TIPO_DOC,  " &
+"						a.t$amnt VALOR,   " &
+"						a.t$balc BALC,  " &
+"						a.t$bala BALA,  " &
+"						a.t$ttyp,   " &
+"						a.t$ninv  " &
+"				   from baandb.ttfacp200" + Parameters!Compania.Value +  "   a   " &
+"				  where a.t$tpay = 14   " &
+"					and a.t$amnt != 0 ) PREST_CONTAS  " &
+"			ON PREST_CONTAS.t$ttyp = tfcmg101.t$ptyp  " &
+"		   AND PREST_CONTAS.t$ninv = tfcmg101.t$pdoc  " &
+"		  " &
+"	 LEFT JOIN baandb.ttfgld011" + Parameters!Compania.Value +  "   tfgld011_LINK  " &
+"			ON tfgld011_LINK.t$ttyp = PREST_CONTAS.TRANS  " &
+"	  " &
+"	 LEFT JOIN baandb.ttccom100" + Parameters!Compania.Value +  "   tccom100pt   " &
+"			ON tccom100pt.t$bpid = PREST_CONTAS.PN  " &
+"			  " &
+"	 LEFT JOIN baandb.ttccom130" + Parameters!Compania.Value +  "   tccom130pt   " &
+"			ON tccom130pt.t$cadr = tccom100pt.t$cadr  " &
+"	  " &
+"	 LEFT JOIN ( select a.t$ttyp TRANS,   " &
+"						a.t$ninv DOCTO,   " &
+"						a.t$docd DT_LINK,   " &
+"						a.t$tdoc,   " &
+"						a.t$docn  " &
+"				   from baandb.ttfacp200" + Parameters!Compania.Value +  "   a   " &
+"				  where a.t$tpay = 14 ) TRANS_PREST_CONTAS  " &
+"			ON TRANS_PREST_CONTAS.t$tdoc = PREST_CONTAS.TRANS   " &
+"		   AND TRANS_PREST_CONTAS.t$docn = PREST_CONTAS.DOCTO   " &
+"		   AND TRANS_PREST_CONTAS.TRANS != PREST_CONTAS.t$ttyp  " &
+"		   AND TRANS_PREST_CONTAS.DOCTO != PREST_CONTAS.t$ninv  " &
+"	  " &
+"	 LEFT JOIN ( select a.t$user  USUARIO,  " &
+"						a.t$date  DT_ALT,   " &
+"						a.t$time  HR_ALT,   " &
+"						a.t$cono,   " &
+"						a.t$ttyp,   " &
+"						a.t$docn  " &
+"				  from  baandb.ttfgld102301   a   " &
+"				  where a.t$lino = 1 )  GLD102  " &
+"			ON GLD102.t$cono = 301  " &
+"		   AND GLD102.t$ttyp = TRANS_PREST_CONTAS.t$tdoc  " &
+"		   AND GLD102.t$docn = TRANS_PREST_CONTAS.t$docn  " &
+"		  " &
+"	 LEFT JOIN ( select a.t$user    USUARIO,  " &
+"						a.t$date    DT_ALT,   " &
+"						a.t$time    HR_ALT,   " &
+"						a.t$otyp,   " &
+"						a.t$odoc  " &
+"				   from baandb.ttfgld106" + Parameters!Compania.Value +  "   a  " &
+"				  where a.t$olin = 1 )  GLD106  " &
+"			ON GLD106.t$otyp = TRANS_PREST_CONTAS.t$tdoc  " &
+"		   AND GLD106.t$odoc = TRANS_PREST_CONTAS.t$docn  " &
+"	  " &
+"	 LEFT JOIN ( select iDOMAIN.t$cnst CODE_MODAL,  " &
+"						iLABEL.t$desc DESC_MODAL  " &
+"				   from baandb.tttadv401000 iDOMAIN,  " &
+"						baandb.tttadv140000 iLABEL  " &
+"				  where iDOMAIN.t$cpac = 'tf'   " &
+"					and iDOMAIN.t$cdom = 'cmg.mopa.d'   " &
+"					and iLABEL.t$clan = 'p'   " &
+"					and iLABEL.t$cpac = 'tf'  " &
+"					and iLABEL.t$clab = iDOMAIN.t$za_clab   " &
+"					and rpad(iDOMAIN.t$vers,4) ||   " &
+"						rpad(iDOMAIN.t$rele,2) ||   " &
+"						rpad(iDOMAIN.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"															  rpad(l1.t$rele,2) ||  " &
+"															  rpad(l1.t$cust,4))  " &
+"													 from baandb.tttadv401000 l1  " &
+"													where l1.t$cpac = iDOMAIN.t$cpac  " &
+"													  and l1.t$cdom = iDOMAIN.t$cdom )  " &
+"					and rpad(iLABEL.t$vers,4) ||  " &
+"						rpad(iLABEL.t$rele,2) ||  " &
+"						rpad(iLABEL.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||   " &
+"															 rpad(l1.t$rele,2) ||   " &
+"															 rpad(l1.t$cust,4))   " &
+"													from baandb.tttadv140000 l1   " &
+"												   where l1.t$clab = iLABEL.t$clab  " &
+"													 and l1.t$clan = iLABEL.t$clan  " &
+"													 and l1.t$cpac = iLABEL.t$cpac ) ) iTABLE   " &
+"			ON tfcmg101.t$mopa$d = iTABLE.CODE_MODAL  " &
+"	  " &
+"	 LEFT JOIN ( select l.t$desc DESCR_TIPO_ACONSELHAMENTO,   " &
+"						d.t$cnst  " &
+"				   from baandb.tttadv401000 d,  " &
+"						baandb.tttadv140000 l   " &
+"				  where d.t$cpac = 'tf'   " &
+"					and d.t$cdom = 'cmg.tadv'   " &
+"					and l.t$clan = 'p'  " &
+"					and l.t$cpac = 'tf'   " &
+"					and l.t$clab = d.t$za_clab  " &
+"					and rpad(d.t$vers,4) ||   " &
+"						rpad(d.t$rele,2) ||   " &
+"						rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"														rpad(l1.t$rele,2) ||  " &
+"														rpad(l1.t$cust,4))  " &
+"											   from baandb.tttadv401000 l1  " &
+"											  where l1.t$cpac = d.t$cpac  " &
+"												and l1.t$cdom = d.t$cdom )  " &
+"					and rpad(l.t$vers,4) ||   " &
+"						rpad(l.t$rele,2) ||   " &
+"						rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"														rpad(l1.t$rele,2) ||  " &
+"														rpad(l1.t$cust,4))  " &
+"											   from baandb.tttadv140000 l1  " &
+"											  where l1.t$clab = l.t$clab  " &
+"												and l1.t$clan = l.t$clan  " &
+"												and l1.t$cpac = l.t$cpac ) ) ACONSELHAMENTO   " &
+"			ON ACONSELHAMENTO.t$cnst = tfcmg101.t$tadv  " &
+"	  " &
+"	 LEFT JOIN ( select iDOMAIN.t$cnst CODE,  " &
+"						iLABEL.t$desc DESCR   " &
+"				   from baandb.tttadv401000 iDOMAIN,  " &
+"						baandb.tttadv140000 iLABEL  " &
+"				  where iDOMAIN.t$cpac = 'tf'   " &
+"					and iDOMAIN.t$cdom = 'acp.tpay'   " &
+"					and iLABEL.t$clan = 'p'   " &
+"					and iLABEL.t$cpac = 'tf'  " &
+"					and iLABEL.t$clab = iDOMAIN.t$za_clab   " &
+"					and rpad(iDOMAIN.t$vers,4) ||   " &
+"						rpad(iDOMAIN.t$rele,2) ||   " &
+"						rpad(iDOMAIN.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"															  rpad(l1.t$rele,2) ||  " &
+"															  rpad(l1.t$cust,4))  " &
+"													 from baandb.tttadv401000 l1  " &
+"													where l1.t$cpac = iDOMAIN.t$cpac  " &
+"													  and l1.t$cdom = iDOMAIN.t$cdom )  " &
+"					and rpad(iLABEL.t$vers,4) ||  " &
+"						rpad(iLABEL.t$rele,2) ||  " &
+"						rpad(iLABEL.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||   " &
+"															 rpad(l1.t$rele,2) ||   " &
+"															 rpad(l1.t$cust,4))   " &
+"													from baandb.tttadv140000 l1   " &
+"												   where l1.t$clab = iLABEL.t$clab  " &
+"													 and l1.t$clan = iLABEL.t$clan  " &
+"													 and l1.t$cpac = iLABEL.t$cpac ) ) TIPO_DOC   " &
+"			ON TIPO_DOC.CODE = ADIANTAMENTO.TP_DOCTO  " &
+"			  " &
+"	 LEFT JOIN ( select iDOMAIN.t$cnst CODE,  " &
+"						iLABEL.t$desc    DESCR  " &
+"				   from baandb.tttadv401000 iDOMAIN,  " &
+"						baandb.tttadv140000 iLABEL  " &
+"				  where iDOMAIN.t$cpac = 'tf'   " &
+"					and iDOMAIN.t$cdom = 'cmg.adty'   " &
+"					and iLABEL.t$clan = 'p'   " &
+"					and iLABEL.t$cpac = 'tf'  " &
+"					and iLABEL.t$clab = iDOMAIN.t$za_clab   " &
+"					and rpad(iDOMAIN.t$vers,4) ||   " &
+"						rpad(iDOMAIN.t$rele,2) ||   " &
+"						rpad(iDOMAIN.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"															  rpad(l1.t$rele,2) ||  " &
+"															  rpad(l1.t$cust,4))  " &
+"													 from baandb.tttadv401000 l1  " &
+"													where l1.t$cpac = iDOMAIN.t$cpac  " &
+"													  and l1.t$cdom = iDOMAIN.t$cdom )  " &
+"					and rpad(iLABEL.t$vers,4) ||  " &
+"						rpad(iLABEL.t$rele,2) ||  " &
+"						rpad(iLABEL.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||   " &
+"															 rpad(l1.t$rele,2) ||   " &
+"															 rpad(l1.t$cust,4))   " &
+"													from baandb.tttadv140000 l1   " &
+"												   where l1.t$clab = iLABEL.t$clab  " &
+"													 and l1.t$clan = iLABEL.t$clan  " &
+"													 and l1.t$cpac = iLABEL.t$cpac ) ) CATEGORIA  " &
+"			ON CATEGORIA.CODE = tfcmg101.t$adty   " &
+"	  " &
+"	 LEFT JOIN ( select iDOMAIN.t$cnst   CODE,  " &
+"						iLABEL.t$desc    DESCR  " &
+"				   from baandb.tttadv401000 iDOMAIN,  " &
+"						baandb.tttadv140000 iLABEL  " &
+"				  where iDOMAIN.t$cpac = 'tf'   " &
+"					and iDOMAIN.t$cdom = 'acp.tpay'   " &
+"					and iLABEL.t$clan = 'p'   " &
+"					and iLABEL.t$cpac = 'tf'  " &
+"					and iLABEL.t$clab = iDOMAIN.t$za_clab   " &
+"					and rpad(iDOMAIN.t$vers,4) ||   " &
+"						rpad(iDOMAIN.t$rele,2) ||   " &
+"						rpad(iDOMAIN.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"															  rpad(l1.t$rele,2) ||  " &
+"															  rpad(l1.t$cust,4))  " &
+"													 from baandb.tttadv401000 l1  " &
+"													where l1.t$cpac = iDOMAIN.t$cpac  " &
+"													  and l1.t$cdom = iDOMAIN.t$cdom )  " &
+"					and rpad(iLABEL.t$vers,4) ||  " &
+"						rpad(iLABEL.t$rele,2) ||  " &
+"						rpad(iLABEL.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||   " &
+"															 rpad(l1.t$rele,2) ||   " &
+"															 rpad(l1.t$cust,4))   " &
+"													from baandb.tttadv140000 l1   " &
+"												   where l1.t$clab = iLABEL.t$clab  " &
+"													 and l1.t$clan = iLABEL.t$clan  " &
+"													 and l1.t$cpac = iLABEL.t$cpac ) ) TIPO_DOCTO   " &
+"			ON TIPO_DOCTO.CODE = PREST_CONTAS.TIPO_DOC  " &
+"	Where tfcmg101.t$tadv = 5  " &
+"	  and tfcmg101.t$plan Between :DataPlanejadoPagtoDe And :DataPlanejadoPagtoAte  " &
+
+"   AND ( (tfcmg101.t$ifbp IN  " &
+"         ( " + IIF(Trim(Parameters!ParceiroNegocio.Value) = "", "''", "'" + Replace(Replace(Parameters!ParceiroNegocio.Value, " ", ""), ",", "','") + "'")  + " )  " &
+"           AND (" + IIF(Parameters!ParceiroNegocio.Value Is Nothing, "1", "0") + " = 0))  " &
+"            OR (" + IIF(Parameters!ParceiroNegocio.Value Is Nothing, "1", "0") + " = 1) ) " &
+
+"	  and ((Trim(:TransPConta) is null) OR (Concat(UPPER(TRANS_PREST_CONTAS.TRANS), TRANS_PREST_CONTAS.DOCTO) = UPPER(:TransPConta))) "&
+"	  and ((Trim(:DoctoPagto) is null) OR (Concat(tfcmg101.t$ptyp, TO_CHAR(tfcmg101.t$pdoc)) = :DoctoPagto))  "
