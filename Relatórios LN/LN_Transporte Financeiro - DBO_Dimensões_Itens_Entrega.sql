c1c2a44b39568bcf8f616425dbf6e91bb08236e7
Humberto Kasai <humberto@globalti.com.br>
2015-07-10 09:03:43 -0300

    Update LN_Transporte Financeiro - DBO_Dimensões_Itens_Entrega.sql
    [Fwd: ##749212## : [LN/RN] INCLUIR COLUNA NO Relatório Dimensões Itens Entregue]
    
    Incluir campo "Forçado (S/N)". Indica se a devolução foi forçada, ou seja, devolução de valor ao cliente antes que a mercadoria chegue ao depósito.

--- Relatórios LN/LN_Transporte Financeiro - DBO_Dimensões_Itens_Entrega.sql
+++ Relatórios LN/LN_Transporte Financeiro - DBO_Dimensões_Itens_Entrega.sql
@@ -13,7 +13,10 @@ SELECT
     whwmd400.t$dpth                    COMPRIMENTO,
     znfmd630.t$wght$c                  PESO,
     whwmd400.t$hght * whwmd400.t$wdth *  
-    whwmd400.t$dpth * cisli941.t$dqua$l  M3
+    whwmd400.t$dpth * cisli941.t$dqua$l  M3,
+    CASE WHEN znsls409.t$dved$c = 1 THEN
+      'Sim'
+    ELSE 'Não' END                    Forçado
 
 FROM       baandb.tznfmd630301 znfmd630
 
@@ -25,5 +28,8 @@ INNER JOIN baandb.tcisli940301 cisli940
 
 INNER JOIN baandb.twhwmd400301 whwmd400
         ON whwmd400.t$item = cisli941.T$ITEM$L
+        
+LEFT JOIN baandb.tznsls409301 znsls409
+       ON TRIM(TO_CHAR(znsls409.t$entr$c)) = TRIM(znfmd630.t$pecl$c)
 
 WHERE znfmd630.t$pecl$c IN (:Entrega)
