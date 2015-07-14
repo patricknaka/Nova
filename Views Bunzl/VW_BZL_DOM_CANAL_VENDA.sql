3cfd3288d872e85c58b8bb3e245b14402ec64401
rosanaprignolato <rosana.prignolato@iteris.com.br>
2015-07-14 16:17:39 -0300

    Nova View  - Bunzl

---
+++ Views Bunzl/VW_BZL_DOM_CANAL_VENDA.sql
@@ -0,0 +1,13 @@
+﻿
+SELECT
+-- O campo CD_CIA foi incluido para diferenciar NIKE(13) E BUNZL(15)
+--**********************************************************************************************************************************************************
+-- a tabela ttcmcs066 é compartilhada com a 201
+
+  tcmcs066.t$chan CD_CANAL_VENDA,  
+  tcmcs066.t$dsca DS_CANAL_VENDA,
+  cast(15 as int) CD_CIA
+  
+FROM
+  baandb.ttcmcs066201 tcmcs066
+order by 1
