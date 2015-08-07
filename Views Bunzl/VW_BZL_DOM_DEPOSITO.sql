3370e63148beb8d6e28a5964ce4d76ac11d6330e
rosanaprignolato <rosana.prignolato@iteris.com.br>
2015-08-07 07:27:45 -0300

    Atualização querys Bunzl

--- Views Bunzl/VW_BZL_DOM_DEPOSITO.sql
+++ Views Bunzl/VW_BZL_DOM_DEPOSITO.sql
@@ -1,8 +1,8 @@
 ﻿SELECT
 -- O campo CD_CIA foi incluido para diferenciar NIKE(13) E BUNZL(15)
---*******************************************************************
+--*********************************************************************
         CAST(15 AS INT) CD_CIA,
-        case when tcemm030.t$euca = ' ' then null else tcemm030.t$euca end CD_FILIAL,
+        case when tcemm030.t$euca = ' ' then '1' else tcemm030.t$euca end CD_FILIAL,
         tcmcs003.t$cwar CD_DEPOSITO,
         tcmcs003.t$dsca DS_DEPOSITO,
         tcemm112.t$grid CD_UNIDADE_EMPRESARIAL
