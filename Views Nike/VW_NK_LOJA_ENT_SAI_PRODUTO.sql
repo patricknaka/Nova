8be00b11393171b4a8dd69e904daaac6f78f7740
Humberto Kasai <humberto@globalti.com.br>
2015-08-19 17:13:15 -0300

    Update VW_NK_LOJA_ENT_SAI_PRODUTO.sql
    Equalização dos filtros das notas

--- Views Nike/VW_NK_LOJA_ENT_SAI_PRODUTO.sql
+++ Views Nike/VW_NK_LOJA_ENT_SAI_PRODUTO.sql
@@ -14,6 +14,7 @@ SELECT
 		CISLI941.T$AMNT$L										          VALOR ,
 		CISLI941.T$PRIC$L										          PRECO1,
 		CISLI941.T$DQUA$L										          QTDE_ITEM,
+    2														                  TIPO_TRANSACAO,     --Saídas
     cisli940.t$fire$l                             REF_FISCAL,
     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(cisli940.t$rcd_utc, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT') 
 		AT time zone 'America/Sao_Paulo') AS DATE)    DT_ULT_ALTERACAO
@@ -26,10 +27,18 @@ INNER JOIN	BAANDB.TTCIBD001601	TCIBD001	ON	TCIBD001.T$ITEM		=	CISLI941.T$ITEM$L
 LEFT JOIN	BAANDB.TTCIBD004601	TCIBD004	ON	TCIBD004.T$CITT		=	'000'
 											AND	TCIBD004.T$BPID		=	' '
 											AND	TCIBD004.T$ITEM		=	CISLI941.T$ITEM$L
-WHERE
-			CISLI940.T$FDTY$L IN (4,5,9,17,18,19,22,23,26,32,33)
-AND	  CISLI940.T$STAT$L IN (5, 6)			-- IMPRESSO, LANÇADO			
-			
+--WHERE
+--			CISLI940.T$FDTY$L IN (4,5,9,17,18,19,22,23,26,32,33)
+--AND	  CISLI940.T$STAT$L IN (5, 6)			-- IMPRESSO, LANÇADO			
+WHERE CISLI940.T$STAT$L IN (2, 5, 6, 101)			-- 2-CANCELADA, 5-IMPRESSO, 6-LANÇADO, 101-ESTORNADA
+AND   cisli940.t$cnfe$l != ' '
+AND   exists (select *
+                  from  baandb.tznnfe011601 znnfe011
+                  where znnfe011.t$oper$c = 1         
+                  and   znnfe011.t$fire$c = cisli940.t$fire$l
+                  and   znnfe011.t$stfa$c = 5
+                  and   (znnfe011.t$nfes$c = 2 or znnfe011.t$nfes$c = 5))
+AND      cisli940.t$fdty$l != 2     --venda sem pedido			
 			
 			
 			
@@ -52,6 +61,7 @@ SELECT
 		TDREC941.T$TAMT$L										          VALOR ,
 		TDREC941.T$PRIC$L										          PRECO1,
 		TDREC941.T$QNTY$L										          QTDE_ITEM,
+    1														                  TIPO_TRANSACAO,   --Entradas
     tdrec940.t$fire$l                             REF_FISCAL,
     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdrec940.t$rcd_utc, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT') 
 		AT time zone 'America/Sao_Paulo') AS DATE)    DT_ULT_ALTERACAO
@@ -65,6 +75,10 @@ INNER JOIN	BAANDB.TTCIBD001601	TCIBD001	ON	TCIBD001.T$ITEM		=	TDREC941.T$ITEM$L
 LEFT JOIN	BAANDB.TTCIBD004601	TCIBD004	ON	TCIBD004.T$CITT		=	'000'
 											AND	TCIBD004.T$BPID		=	' '
 											AND	TCIBD004.T$ITEM		=	TDREC941.T$ITEM$L
-WHERE
-			TDREC940.T$RFDT$L IN (1,2,4,5,10,26,27,28,32,33,35,36,37,40)
-AND   TDREC940.T$STAT$L IN (4,5)    --APROVADO, APROVADO COM PROBLEMAS
+--WHERE
+--			TDREC940.T$RFDT$L IN (1,2,4,5,10,26,27,28,32,33,35,36,37,40)
+--AND   TDREC940.T$STAT$L IN (4,5)    --APROVADO, APROVADO COM PROBLEMAS
+WHERE TDREC940.T$STAT$L IN (4,5,6)  --4-Aprovado, 5-Aprovado com Problemas, 6-estornada
+  AND	tdrec940.t$cnfe$l != ' '
+  
+  ORDER BY TIPO_TRANSACAO, REF_FISCAL
