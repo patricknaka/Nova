bc8e2cd2290d4141a3dd807a3a3265bc92b56623
Humberto Kasai <humberto.kasai@outlook.com>
2015-09-18 19:52:25 -0300

    CORREÇÃO DUPLICIDADE PEDIDO 280

--- Views Bunzl/VW_BZL_Pedido_Cab.sql
+++ Views Bunzl/VW_BZL_Pedido_Cab.sql
@@ -1,4 +1,4 @@
-﻿SELECT
+SELECT
 --*********************************************************************************************************************
 --	LISTA TODOS OS PEDIDOS INTEGRADOS INCLUSIVE TROCAS E DEVOLUÇÕES INDEPENDENTE DO STATUS
 --*********************************************************************************************************************
@@ -34,35 +34,50 @@ INNER JOIN (SELECT	C.T$NCIA$C,
                     C.T$PECL$C,
                     C.T$SQPD$C,
                     C.T$ENTR$C,
-					C.T$LOGE$C,
-					C.T$NUME$C,
-					C.T$COME$C,
-					C.T$BAIE$C,
-					C.T$CIDE$C,
-					C.T$UFEN$C,
-					C.T$CEPE$C,
-					C.T$DTEP$C,
-					C.T$IDTR$C
+                    C.T$LOGE$C,
+                    C.T$NUME$C,
+                    C.T$COME$C,
+                    C.T$BAIE$C,
+                    C.T$CIDE$C,
+                    C.T$UFEN$C,
+                    C.T$CEPE$C,
+                    C.T$DTEP$C,
+                    C.T$IDTR$C
 			FROM	BAANDB.TZNSLS401602 C
 			GROUP BY C.T$NCIA$C,
 			         C.T$UNEG$C,
 			         C.T$PECL$C,
 			         C.T$SQPD$C,
 			         C.T$ENTR$C,
-					 C.T$LOGE$C,
-					 C.T$NUME$C,
-					 C.T$COME$C,
-					 C.T$BAIE$C,
-					 C.T$CIDE$C,
-					 C.T$UFEN$C,
-					 C.T$CEPE$C,
-					 C.T$DTEP$C,
-					 C.T$IDTR$C) ZNSLS401	ON	ZNSLS401.T$NCIA$C	=	ZNSLS400.T$NCIA$C
+               C.T$LOGE$C,
+               C.T$NUME$C,
+               C.T$COME$C,
+               C.T$BAIE$C,
+               C.T$CIDE$C,
+               C.T$UFEN$C,
+					     C.T$CEPE$C,
+					     C.T$DTEP$C,
+					     C.T$IDTR$C) ZNSLS401	ON	ZNSLS401.T$NCIA$C	=	ZNSLS400.T$NCIA$C
 					                        AND ZNSLS401.T$UNEG$C   =	ZNSLS400.T$UNEG$C
 					                        AND ZNSLS401.T$PECL$C   =	ZNSLS400.T$PECL$C
 					                        AND ZNSLS401.T$SQPD$C   =	ZNSLS400.T$SQPD$C	
-GROUP BY 
-		to_char(ZNSLS401.T$ENTR$C),ZNSLS400.T$DTEM$C, to_char(ZNSLS400.T$ICLF$C), ZNSLS401.T$LOGE$C,
-		ZNSLS401.T$NUME$C, ZNSLS401.T$COME$C, ZNSLS401.T$BAIE$C, ZNSLS401.T$CIDE$C, ZNSLS401.T$UFEN$C,	
-		to_char(ZNSLS401.T$CEPE$C), ZNSLS400.T$VLFR$C, ZNSLS401.T$DTEP$C, to_char(ZNSLS401.T$IDTR$C), 
-		to_char(ZNSLS400.T$PEEX$C)
+                                  
+--GROUP BY 
+--		to_char(ZNSLS401.T$ENTR$C),
+-- 		CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ZNSLS400.T$DTEM$C, 
+--			'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+--			AT time zone 'America/Sao_Paulo') AS DATE),
+--    to_char(ZNSLS400.T$ICLF$C),
+--    ZNSLS401.T$LOGE$C,
+--		ZNSLS401.T$NUME$C, 
+--    ZNSLS401.T$COME$C, 
+--    ZNSLS401.T$BAIE$C, 
+--    ZNSLS401.T$CIDE$C, 
+--    ZNSLS401.T$UFEN$C,	
+--		to_char(ZNSLS401.T$CEPE$C), 
+--    ZNSLS400.T$VLFR$C, 
+--    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ZNSLS401.T$DTEP$C, 
+--			'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+--			AT time zone 'America/Sao_Paulo') AS DATE), 
+--    to_char(ZNSLS401.T$IDTR$C), 
+--		to_char(ZNSLS400.T$PEEX$C)
