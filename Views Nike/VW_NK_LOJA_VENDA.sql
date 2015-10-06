6d9dec5984c40c6f40a7bd721f9b9ac4d68a6a8d
Humberto Kasai <humberto.kasai@outlook.com>
2015-10-06 20:47:41 -0300

    Retirada da Rotina de Instância

--- Views Nike/VW_NK_LOJA_VENDA.sql
+++ Views Nike/VW_NK_LOJA_VENDA.sql
@@ -287,127 +287,127 @@ AND 	TDREC940.T$RFDT$L = 10        --10-retorno de mercadoria
 --				INSTANCIA
 --***************************************************************************************************************************
 
-UNION
-SELECT
-		'NIKE.COM'												FILIAL,
-    TO_CHAR(znsls401.t$entr$c)                 TICKET,
-		CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ZNSLS400.T$DTEM$C, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT') --#FAF.004.sn
-		AT time zone 'America/Sao_Paulo') AS DATE) 				DATA_VENDA,
-		nvl(to_char(ZNSLS400.T$ICLF$C),' ')										CPF_CGC,		-- VERIFICAR FORMATAÇÃO
-		''														PERIODO,
-		''														VENDEDOR,
-		'005'													OPERACAO_VENDA,
-		''														CODIGO_TAB_PRECO,
-		0														COMISSAO,
-		0														DESCONTO,
-		1														QTDE_TOTAL,
-		ZNSLS401.VL_MERCD 
-		+ ZNSLS401.VL_FRETE 
-		- ZNSLS401.VL_DSCONT									VALOR_TIKET,
-		ZNSLS402.T$VLMR$C										VALOR_PAGO,
-		ZNSLS401.VL_MERCD 
-		+ ZNSLS401.VL_FRETE 									VALOR_VENDA_BRUTA,
-		0														VALOR_TROCA,
-		0														QTDE_TROCA_TOTAL,
-		'' 													TICKET_IMPRESSO,
-		''														TERMINAL,
-		''														GERENTE_LOJA,
-		''														GERENTE_PERIODO,
-		''														LANCAMENTO_CAIXA,
-		0														VALOR_CANCELADO,
-		0														TOTAL_QTDE_CANCELADA,
-		1														QTDE_ITEM,
-		''														MOTIVO_CANCELAMENTO,
-		CASE WHEN ZNSLS400.T$FTYP$C='PF'			
-		 THEN nvl(to_char(ZNSLS400.T$ICLF$C),' ')			
-		 ELSE NULL END											CPF_CGC_ECF,
-		''														DATA_HORA_CANCELAMENTO,
-		''														SUGESTAO_COD_FORMA_PGTO,
-		0														VALOR_IPI,
-		CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ZNSLS400.T$DTIN$C, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT') --#FAF.004.sn
-		AT time zone 'America/Sao_Paulo') AS DATE) 				EMISSAO,
-		TO_CHAR(0)														TRANSIT_TIME,
-    'I'                         TP_MOVTO,                 -- Criado para separar na tabela as entradas e saídas
-    CASE WHEN CISLI940_FAT.T$FDTY$L = 15 THEN
-          CISLI940_FAT.T$FIRE$L
-    ELSE  CISLI940.T$FIRE$L END          REF_FISCAL,
-    CASE WHEN CISLI940_FAT.T$FDTY$L = 15 THEN
-        CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(CISLI940_FAT.T$SADT$L, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT') --#FAF.004.sn
-        AT time zone 'America/Sao_Paulo') AS DATE)
-    ELSE
-        CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(CISLI940.T$SADT$L, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT') --#FAF.004.sn
-        AT time zone 'America/Sao_Paulo') AS DATE) 	END			DT_ULT_ALTERACAO
-		
-FROM
-			BAANDB.TZNSLS400601	ZNSLS400
-											
-INNER JOIN (SELECT	C.T$NCIA$C,
-                    C.T$UNEG$C,
-                    C.T$PECL$C,
-                    C.T$SQPD$C,
-                    C.T$ENTR$C,
-					C.T$ORNO$C,
-                    SUM(C.T$QTVE$C) T$QTVE$C,
-					SUM(C.T$VLUN$C*C.T$QTVE$C)	VL_MERCD,
-					SUM(C.T$VLDI$C) VL_DSCONT,
-					SUM(C.T$VLFR$C) VL_FRETE,
-					MAX(C.T$PZTR$C) T$PZTR$C
-			FROM	BAANDB.TZNSLS401601 C
-			GROUP BY C.T$NCIA$C,
-			         C.T$UNEG$C,
-			         C.T$PECL$C,
-			         C.T$SQPD$C,
-			         C.T$ENTR$C,
-               C.T$ORNO$C) ZNSLS401			ON	ZNSLS401.T$NCIA$C	=	ZNSLS400.T$NCIA$C
-					                        AND ZNSLS401.T$UNEG$C   =	ZNSLS400.T$UNEG$C
-					                        AND ZNSLS401.T$PECL$C   =	ZNSLS400.T$PECL$C
-					                        AND ZNSLS401.T$SQPD$C   =	ZNSLS400.T$SQPD$C
-
-INNER JOIN	BAANDB.TTDSLS400601 TDSLS400	ON	TDSLS400.T$ORNO		=	ZNSLS401.T$ORNO$C
-											
-INNER JOIN (SELECT	D.T$NCIA$C,
-                    D.T$UNEG$C,
-                    D.T$PECL$C,
-                    D.T$SQPD$C,
-                    SUM(D.T$VLMR$C) T$VLMR$C
-			FROM	BAANDB.TZNSLS402601 D
-			GROUP BY D.T$NCIA$C,
-			         D.T$UNEG$C,
-			         D.T$PECL$C,
-			         D.T$SQPD$C) ZNSLS402	ON	ZNSLS402.T$NCIA$C	=	ZNSLS400.T$NCIA$C
-					                        AND ZNSLS402.T$UNEG$C   =	ZNSLS400.T$UNEG$C
-					                        AND ZNSLS402.T$PECL$C   =	ZNSLS400.T$PECL$C
-					                        AND ZNSLS402.T$SQPD$C   =	ZNSLS400.T$SQPD$C
-
-LEFT JOIN ( 	SELECT	A.T$FIRE$L,
-                A.T$SLSO
-        FROM	BAANDB.TCISLI245601 A
-        WHERE	A.T$SLCP=601
-        AND		A.T$ORTP=1
-        AND		A.T$KOOR=3
-        GROUP BY A.T$FIRE$L,
-		             A.T$SLSO ) SLS245
-      ON SLS245.T$SLSO = TDSLS400.T$ORNO
-      
-LEFT JOIN BAANDB.TCISLI940601 CISLI940
-       ON CISLI940.T$FIRE$L = SLS245.T$FIRE$L
-
-INNER JOIN (SELECT	E.T$FIRE$L,
-                    E.T$REFR$L
-			FROM	BAANDB.TCISLI941601 E
-			LEFT JOIN BAANDB.TCISLI941601 E1 
-						ON	E1.T$FIRE$L=E.T$REFR$L
-						AND	E1.T$LINE$L=E.T$RFDL$L
-			GROUP BY E.T$FIRE$L,
-					 E.T$REFR$L) CISLI941	ON	CISLI941.T$FIRE$L	=	CISLI940.T$FIRE$L
-           
-LEFT JOIN	BAANDB.TCISLI940601	CISLI940_FAT 
-       ON CISLI940_FAT.T$FIRE$L =	CISLI941.T$REFR$L
-      AND	CISLI940_FAT.T$FDTY$L =	16
-      
-WHERE
-			ZNSLS400.T$IDPO$C	=		'TD'
-		AND	TDSLS400.T$HDST		=		35
-		AND TDSLS400.T$FDTY$L 	NOT IN (0,2,14)
+--UNION
+--SELECT
+--		'NIKE.COM'												FILIAL,
+--    TO_CHAR(znsls401.t$entr$c)                 TICKET,
+--		CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ZNSLS400.T$DTEM$C, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT') --#FAF.004.sn
+--		AT time zone 'America/Sao_Paulo') AS DATE) 				DATA_VENDA,
+--		nvl(to_char(ZNSLS400.T$ICLF$C),' ')										CPF_CGC,		-- VERIFICAR FORMATAÇÃO
+--		''														PERIODO,
+--		''														VENDEDOR,
+--		'005'													OPERACAO_VENDA,
+--		''														CODIGO_TAB_PRECO,
+--		0														COMISSAO,
+--		0														DESCONTO,
+--		1														QTDE_TOTAL,
+--		ZNSLS401.VL_MERCD 
+--		+ ZNSLS401.VL_FRETE 
+--		- ZNSLS401.VL_DSCONT									VALOR_TIKET,
+--		ZNSLS402.T$VLMR$C										VALOR_PAGO,
+--		ZNSLS401.VL_MERCD 
+--		+ ZNSLS401.VL_FRETE 									VALOR_VENDA_BRUTA,
+--		0														VALOR_TROCA,
+--		0														QTDE_TROCA_TOTAL,
+--		'' 													TICKET_IMPRESSO,
+--		''														TERMINAL,
+--		''														GERENTE_LOJA,
+--		''														GERENTE_PERIODO,
+--		''														LANCAMENTO_CAIXA,
+--		0														VALOR_CANCELADO,
+--		0														TOTAL_QTDE_CANCELADA,
+--		1														QTDE_ITEM,
+--		''														MOTIVO_CANCELAMENTO,
+--		CASE WHEN ZNSLS400.T$FTYP$C='PF'			
+--		 THEN nvl(to_char(ZNSLS400.T$ICLF$C),' ')			
+--		 ELSE NULL END											CPF_CGC_ECF,
+--		''														DATA_HORA_CANCELAMENTO,
+--		''														SUGESTAO_COD_FORMA_PGTO,
+--		0														VALOR_IPI,
+--		CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ZNSLS400.T$DTIN$C, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT') --#FAF.004.sn
+--		AT time zone 'America/Sao_Paulo') AS DATE) 				EMISSAO,
+--		TO_CHAR(0)														TRANSIT_TIME,
+--    'I'                         TP_MOVTO,                 -- Criado para separar na tabela as entradas e saídas
+--    CASE WHEN CISLI940_FAT.T$FDTY$L = 15 THEN
+--          CISLI940_FAT.T$FIRE$L
+--    ELSE  CISLI940.T$FIRE$L END          REF_FISCAL,
+--    CASE WHEN CISLI940_FAT.T$FDTY$L = 15 THEN
+--        CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(CISLI940_FAT.T$SADT$L, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT') --#FAF.004.sn
+--        AT time zone 'America/Sao_Paulo') AS DATE)
+--    ELSE
+--        CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(CISLI940.T$SADT$L, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT') --#FAF.004.sn
+--        AT time zone 'America/Sao_Paulo') AS DATE) 	END			DT_ULT_ALTERACAO
+--		
+--FROM
+--			BAANDB.TZNSLS400601	ZNSLS400
+--											
+--INNER JOIN (SELECT	C.T$NCIA$C,
+--                    C.T$UNEG$C,
+--                    C.T$PECL$C,
+--                    C.T$SQPD$C,
+--                    C.T$ENTR$C,
+--					C.T$ORNO$C,
+--                    SUM(C.T$QTVE$C) T$QTVE$C,
+--					SUM(C.T$VLUN$C*C.T$QTVE$C)	VL_MERCD,
+--					SUM(C.T$VLDI$C) VL_DSCONT,
+--					SUM(C.T$VLFR$C) VL_FRETE,
+--					MAX(C.T$PZTR$C) T$PZTR$C
+--			FROM	BAANDB.TZNSLS401601 C
+--			GROUP BY C.T$NCIA$C,
+--			         C.T$UNEG$C,
+--			         C.T$PECL$C,
+--			         C.T$SQPD$C,
+--			         C.T$ENTR$C,
+--               C.T$ORNO$C) ZNSLS401			ON	ZNSLS401.T$NCIA$C	=	ZNSLS400.T$NCIA$C
+--					                        AND ZNSLS401.T$UNEG$C   =	ZNSLS400.T$UNEG$C
+--					                        AND ZNSLS401.T$PECL$C   =	ZNSLS400.T$PECL$C
+--					                        AND ZNSLS401.T$SQPD$C   =	ZNSLS400.T$SQPD$C
+--
+--INNER JOIN	BAANDB.TTDSLS400601 TDSLS400	ON	TDSLS400.T$ORNO		=	ZNSLS401.T$ORNO$C
+--											
+--INNER JOIN (SELECT	D.T$NCIA$C,
+--                    D.T$UNEG$C,
+--                    D.T$PECL$C,
+--                    D.T$SQPD$C,
+--                    SUM(D.T$VLMR$C) T$VLMR$C
+--			FROM	BAANDB.TZNSLS402601 D
+--			GROUP BY D.T$NCIA$C,
+--			         D.T$UNEG$C,
+--			         D.T$PECL$C,
+--			         D.T$SQPD$C) ZNSLS402	ON	ZNSLS402.T$NCIA$C	=	ZNSLS400.T$NCIA$C
+--					                        AND ZNSLS402.T$UNEG$C   =	ZNSLS400.T$UNEG$C
+--					                        AND ZNSLS402.T$PECL$C   =	ZNSLS400.T$PECL$C
+--					                        AND ZNSLS402.T$SQPD$C   =	ZNSLS400.T$SQPD$C
+--
+--LEFT JOIN ( 	SELECT	A.T$FIRE$L,
+--                A.T$SLSO
+--        FROM	BAANDB.TCISLI245601 A
+--        WHERE	A.T$SLCP=601
+--        AND		A.T$ORTP=1
+--        AND		A.T$KOOR=3
+--        GROUP BY A.T$FIRE$L,
+--		             A.T$SLSO ) SLS245
+--      ON SLS245.T$SLSO = TDSLS400.T$ORNO
+--      
+--LEFT JOIN BAANDB.TCISLI940601 CISLI940
+--       ON CISLI940.T$FIRE$L = SLS245.T$FIRE$L
+--
+--INNER JOIN (SELECT	E.T$FIRE$L,
+--                    E.T$REFR$L
+--			FROM	BAANDB.TCISLI941601 E
+--			LEFT JOIN BAANDB.TCISLI941601 E1 
+--						ON	E1.T$FIRE$L=E.T$REFR$L
+--						AND	E1.T$LINE$L=E.T$RFDL$L
+--			GROUP BY E.T$FIRE$L,
+--					 E.T$REFR$L) CISLI941	ON	CISLI941.T$FIRE$L	=	CISLI940.T$FIRE$L
+--           
+--LEFT JOIN	BAANDB.TCISLI940601	CISLI940_FAT 
+--       ON CISLI940_FAT.T$FIRE$L =	CISLI941.T$REFR$L
+--      AND	CISLI940_FAT.T$FDTY$L =	16
+--      
+--WHERE
+--			ZNSLS400.T$IDPO$C	=		'TD'
+--		AND	TDSLS400.T$HDST		=		35
+--		AND TDSLS400.T$FDTY$L 	NOT IN (0,2,14)
 
 ORDER BY TP_MOVTO, REF_FISCAL
