eea630b93f8bbe3349c8396080925dcfc66c36cd
Humberto Kasai <humberto.kasai@outlook.com>
2015-10-15 16:42:55 -0300

    Correção status NFe

--- Views Nike/VW_NK_LOJA_NF.sql
+++ Views Nike/VW_NK_LOJA_NF.sql
@@ -572,7 +572,7 @@ FROM  baandb.tcisli940601  cisli940
                   from  baandb.tznnfe011601 znnfe011
                   where znnfe011.t$oper$c = 1
                   and   znnfe011.t$fire$c = cisli940.t$fire$l
-                  and   znnfe011.t$stfa$c = 5
-                  and   (znnfe011.t$nfes$c = 2 or znnfe011.t$nfes$c = 5))
+                  and   znnfe011.t$stfa$c = 5   --status nota impressa
+                  and   znnfe011.t$nfes$c = 5)  --status nfe processada
    AND      cisli940.t$fdty$l NOT IN (2,14)     --venda sem pedido, retorno mercadoria cliente
 order by REF_FISCAL
