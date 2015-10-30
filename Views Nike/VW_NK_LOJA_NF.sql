831c21d3b7fd09fba691bfed731444fc688649c2
Humberto Kasai <humberto.kasai@outlook.com>
2015-10-30 12:10:24 -0200

    Retirar Notas de Débito do recebimento

--- Views Nike/VW_NK_LOJA_NF.sql
+++ Views Nike/VW_NK_LOJA_NF.sql
@@ -199,8 +199,7 @@ FROM  baandb.ttdrec940601  tdrec940
 
     WHERE tdrec940.t$stat$l IN (4,5,6)
     AND	  tdrec940.t$cnfe$l != ' '
-    AND   tdrec940.t$rfdt$l NOT IN (19,20,21,22,23) --Conhecimento de Frete Aéreo-19, Ferroviário-20, Aquaviário-21, Rodoviário--22, Multimodal-23
-
+    AND   tdrec940.t$rfdt$l NOT IN (14,19,20,21,22,23) --Nota de Débito-14, Conhecimento de Frete Aéreo-19, Ferroviário-20, Aquaviário-21, Rodoviário--22, Multimodal-23
 UNION
 
 SELECT DISTINCT
