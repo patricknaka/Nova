5b7d7486c75a93df0cf59110d4948ed23e7ae8c2
Humberto Kasai <humberto.kasai@outlook.com>
2015-10-30 12:32:14 -0200

    Excluir notas de débito no recebimento

--- Views Nike/VW_NK_LOJA_NF_ITEM.sql
+++ Views Nike/VW_NK_LOJA_NF_ITEM.sql
@@ -137,7 +137,7 @@ WHERE tdrec940.t$stat$l IN (4,5,6)
    AND tdrec941.t$item$l != znsls000.t$itmd$c      --ITEM DESPESAS
    AND tdrec941.t$item$l != znsls000.t$itjl$c      --ITEM JUROS
    AND tdrec940.t$cnfe$l != ' '
-   AND   tdrec940.t$rfdt$l NOT IN (19,20,21,22,23) --Conhecimento de Frete Aéreo-19, Ferroviário-20, Aquaviário-21, Rodoviário--22, Multimodal-23
+   AND   tdrec940.t$rfdt$l NOT IN (14,19,20,21,22,23) --Nota de Débito-14, Conhecimento de Frete Aéreo-19, Ferroviário-20, Aquaviário-21, Rodoviário--22, Multimodal-23
 UNION
 
 SELECT DISTINCT
