c119d78bc02589ca44a8652de1c2cb0c1780de46
rosanaprignolato <rosana.prignolato@iteris.com.br>
2015-12-11 17:32:52 -0200

    Correção da DT_VENCIMENTO E DT_VENCIMENTO_ORIGINAL

--- Views LN/VW_CAR_Titulo.sql
+++ Views LN/VW_CAR_Titulo.sql
@@ -15,8 +15,8 @@
 -- #FAF.282 - 23-ago-2014, Fabio Ferreira, 	Inclusão do campo conta controle
 --****************************************************************************************************************************************************************
 SELECT DISTINCT	
-  CONCAT(tfacr200.t$ttyp, TO_CHAR(tfacr200.t$ninv)) CD_CHAVE_PRIMARIA,											--#FAF.193.n
-  tfacr200.t$ttyp CD_TRANSACAO_TITULO,																		--#FAF.193.o
+  CONCAT(tfacr200.t$ttyp, TO_CHAR(tfacr200.t$ninv)) CD_CHAVE_PRIMARIA,											
+  tfacr200.t$ttyp CD_TRANSACAO_TITULO,																		
     1 CD_CIA,
 	CASE WHEN nvl((	select c.t$styp from baandb.tcisli205201 c
 					where c.t$styp='BL ATC'
@@ -32,58 +32,49 @@ SELECT DISTINCT
 	tfacr200.t$docn$l NR_NF,
 	tfacr200.t$seri$l NR_SERIE_NF,
 	tfacr200.t$docd DT_EMISSAO_TITULO,
-  tfacr200.t$liqd dt_vencimento,
---	TO_DATE(REGEXP_REPLACE(tfacr200.T$LIQD,',''[[:punct:]]',''), 'DD-MM-YY HH:MI:SS AM') DT_VENCIMENTO			--#FAF.001.n
-  tfacr200.t$liqd   DT_VENCIMENTO,
-	CASE WHEN tfacr200.t$balc=0 THEN																--#FAF.146.n														 													
+--	TO_DATE(REGEXP_REPLACE(tfacr200.T$LIQD,',''[[:punct:]]',''), 'DD-MM-YY HH:MI:SS AM') DT_VENCIMENTO,			
+  case when to_date(tfacr200.t$liqd) = to_date('4712-01-01','yyyy-mm-dd HH24:MI:SS')
+    then null else to_date(tfacr200.t$liqd) end DT_VENCIMENTO,
+	CASE WHEN tfacr200.t$balc=0 THEN																													 													
 	(select max(p.t$docd) 
 		from baandb.ttfacr200201 p
 		where p.t$ttyp=tfacr200.t$ttyp
 		and p.t$ninv=tfacr200.t$ninv) 
-	ELSE NULL END DT_LIQUIDACAO_TITULO,																			--#FAF.146.n
+	ELSE NULL END DT_LIQUIDACAO_TITULO,																			
 	tfacr200.t$amnt VL_TITULO,
 	tfcmg011.t$baoc$l CD_BANCO,
 	tfcmg011.t$agcd$l NR_AGENCIA,
 	tfcmg001.t$bano NR_CONTA_CORRENTE,
 	tfacr200.t$dim1 CD_CENTRO_CUSTO,
---	tfcmg401.t$btno NR_REMESSA,																						--#FAF.203.o
---	tfcmg409.t$date DT_REMESSA,																						--#FAF.203.o
---#FAF.002.o
-	nvl((select min(a.t$rpst$l) from baandb.ttfacr201201 a																	--#FAF.002.sn
+	nvl((select min(a.t$rpst$l) from baandb.ttfacr201201 a																	
 	 where a.t$ttyp=tfacr200.t$ttyp
 	 and a.t$ninv=tfacr200.t$ninv),1) CD_SITUACAO_TITULO,
 	(select min(a.t$docd) from baandb.ttfacr200201 a
 	 where a.t$ttyp=tfacr200.t$ttyp 
 	 and a.t$ninv=tfacr200.t$ninv
-	 and a.t$docn!=0) DT_SITUACAO_TITULO,																			--#FAF.002.en
---	TO_DATE(REGEXP_REPLACE(tfacr200.t$dued,',''[[:punct:]]',''), 'DD-MM-YY HH:MI:SS AM') DT_VENCIMENTO_ORIGINAL,		--#FAF.001.n
-  tfacr200.t$dued DT_VENCIMENTO_ORIGINAL,
+	 and a.t$docn!=0) DT_SITUACAO_TITULO,																			
+--	TO_DATE(REGEXP_REPLACE(tfacr200.t$dued,',''[[:punct:]]',''), 'DD-MM-YY HH:MI:SS AM') DT_VENCIMENTO_ORIGINAL,		
+  case when to_date(tfacr200.t$dued) = to_date('4712-01-01','yyyy-mm-dd HH24:MI:SS') 
+    then null else to_date(tfacr200.t$dued) end  DT_VENCIMENTO_ORIGINAL,
 	tfacr200.t$bank NR_BANCARIO,
 	tfacr200.t$balc VL_SALDO,
---	tfacr200.t$amti VL_DESCONTO,																						--#FAF.146.1.o
-	tfacr200.t$dc1h$1 + tfacr200.t$dc2h$1 +tfacr200.t$dc3h$1 VL_DESCONTO,														--#FAF.146.1.n
---	tfacr200.t$ninv NR_DOCUMENTO,																						--#FAF.193.o
-	tfacr200.t$ninv NR_TITULO,																							--#FAF.193.n
-    nvl((select t.t$text from baandb.ttttxt010201 t 
+	tfacr200.t$dc1h$1 + tfacr200.t$dc2h$1 +tfacr200.t$dc3h$1 VL_DESCONTO,														
+	tfacr200.t$ninv NR_TITULO,																							
+  nvl((select t.t$text from baandb.ttttxt010201 t 
 	where t$clan='p'
 	AND t.t$ctxt=tfacr200.t$text
 	and rownum=1),' ') DS_OBSERVACAO_TITULO,
 	tfgld100.t$user DS_USUARIO_GERACAO_TITULO,
---	CAST((FROM_TZ(CAST(TO_CHAR(tfacr200.t$rcd_utc, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') --#MAR.273.so
---		AT time zone 'America/Sao_Paulo') AS DATE) DT_ATUALIZACAO,                                        --#MAR.273.eo
- 
-  GREATEST(                                                                                         --#MAR.273.sn
+  GREATEST(                                                                                         
 	nvl(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tfacr200.t$rcd_utc, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
 		AT time zone 'America/Sao_Paulo') AS DATE), TO_DATE('01-JAN-1970', 'DD-MON-YYYY')),
---    nvl(CAST((FROM_TZ(CAST(TO_CHAR(tfacr201.t$rcd_utc, 'DD-MON-YYYY HH:MI:SS AM') AS TIMESTAMP), 'GMT') 								--#FAF.282.o
-	nvl(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(																										--#FAF.282.sn
+	nvl(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(																										
 		(select max(a.t$rcd_utc) from baandb.ttfacr201201 a
 			where a.t$ttyp=tfacr200.t$ttyp
 			and a.t$ninv=tfacr200.t$ninv), 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
 			AT time zone 'America/Sao_Paulo') AS DATE), TO_DATE('01-JAN-1970', 'DD-MON-YYYY')),
 	nvl(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tfcmg001.t$rcd_utc, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
 		AT time zone 'America/Sao_Paulo') AS DATE), TO_DATE('01-JAN-1970', 'DD-MON-YYYY'))) DT_ULT_ATUALIZACAO,                                     --#MAR.273.en
-	
 	(Select u.t$eunt From baandb.ttcemm030201 u
 		where u.t$euca!=' '
 		AND TO_NUMBER(u.t$euca)=CASE WHEN tfacr200.t$dim2=' ' then 999
@@ -97,18 +88,15 @@ SELECT DISTINCT
 	 and znsls401.t$orno$c=ro.t$slso
 	 and znsls401.t$pono$c=ro.t$pono
 	 and rownum=1) NR_PEDIDO,
---	 tfacr200.t$paym CD_METODO_RECEBIMENTO,	--#FAF.008.n  --#MAR.273.o
---	 tfacr201.t$paym CD_METODO_RECEBIMENTO,	--#MAR.273.n	--#FAF.282.o
 	(select a.t$send$l from baandb.ttfcmg948201 a
 	 where a.t$ttyp$l=tfacr200.t$ttyp
 	 and a.t$ninv$l=tfacr200.t$ninv
-	 and rownum=1																						--#FAF.141.n
+	 and rownum=1																						
 	 and a.t$lach$l = (select max(b.t$lach$l) from baandb.ttfcmg948201 b
 					 where b.t$ttyp$l=a.t$ttyp$l
-					 and a.t$ninv$l=b.t$ninv$l)) CD_SITUACAO_PAGAMENTO,									--#FAF.108.n
-	tfacr200.t$leac								 CD_CONTA_CONTROLE										--#FAF.294.n	
+					 and a.t$ninv$l=b.t$ninv$l)) CD_SITUACAO_PAGAMENTO,									
+	tfacr200.t$leac								 CD_CONTA_CONTROLE											
 FROM
---	baandb.ttfacr201201 tfacr201,             --#MAR.273.n	--#FAF.282.o
 	baandb.ttfacr200201 tfacr200
 	LEFT JOIN baandb.ttfcmg001201 tfcmg001
 	ON  tfcmg001.t$bank=tfacr200.t$bank
@@ -117,7 +105,6 @@ FROM
 	LEFT JOIN baandb.ttfcmg401201 tfcmg401
 	ON tfcmg401.t$ttyp=tfacr200.t$ttyp
 	AND tfcmg401.t$ninv=tfacr200.t$ninv
-	--AND tfcmg401.t$ttyp=tfacr200.t$ttyp
 	LEFT JOIN baandb.ttfcmg409201 tfcmg409
 	ON  tfcmg409.t$btno=tfcmg401.t$btno,
   baandb.ttfgld100201 tfgld100
@@ -125,5 +112,4 @@ WHERE tfgld100.t$btno=tfacr200.t$btno
 AND tfgld100.t$year=tfacr200.t$year
 AND tfacr200.t$docn=0
 AND tfacr200.t$trec!=9
---AND tfacr201.t$ttyp = tfacr200.t$ttyp               --#MAR.273.sn		--#FAF.282.o
---AND tfacr201.t$ninv = tfacr200.t$ninv               --#MAR.273.en		--#FAF.282.o
+
