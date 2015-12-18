c309585c37539220b19aa33968bbc4cd9047026e
rosanaprignolato <rosana.prignolato@iteris.com.br>
2015-12-18 17:23:17 -0200

    Correção da view VW_DOM_WMS_PLANTA

--- Views LN/VW_DOM_WMS_Planta.sql
+++ Views LN/VW_DOM_WMS_Planta.sql
@@ -1,34 +1,30 @@
-SELECT DISTINCT
-        TFGLD010.T$SKEY CD_PLANTA,
-        ' ' CD_TERCEIRO, -- NÃO EXISITE NO WMS/LN
-        TFGLD010.T$DESC NM_PLANTA,
-        WMSWHS.WHSEID CD_ARMAZEM,
-  tcemm030.t$euca CD_FILIAL,
-        CASE WHEN regexp_replace(tccom130.t$fovn$l, '[^0-9]', '') IS NULL
-  THEN '00000000000000' 
-  WHEN LENGTH(regexp_replace(tccom130.t$fovn$l, '[^0-9]', ''))<11
-  THEN '00000000000000'
-  ELSE regexp_replace(tccom130.t$fovn$l, '[^0-9]', '') END NR_CNPJ_FILIAL,       --#FAF.151.n  
-  
---        TCCOM130.T$FOVN$L CGC_FILIAL,                 --#FAF.151.o
-        1 CD_CIA,
-        (SELECT C.T$NAMA FROM baandb.TTCCOM000201 C 
+select DISTINCT
+  znfmd001.t$dsca$c CD_PLANTA,
+  CASE WHEN regexp_replace(znfmd001.t$fovn$c, '[^0-9]', '') IS NULL
+    THEN '00000000000000' 
+      WHEN LENGTH(regexp_replace(znfmd001.t$fovn$c, '[^0-9]', ''))<11
+        THEN '00000000000000'
+          ELSE regexp_replace(znfmd001.t$fovn$c, '[^0-9]', '') END CD_TERCEIRO,
+  znfmd001.t$dsca$c NM_PLANTA,
+  znfmd001.t$fili$c CD_FILIAL,
+  CASE WHEN regexp_replace(znfmd001.t$fovn$c, '[^0-9]', '') IS NULL
+    THEN '00000000000000' 
+      WHEN LENGTH(regexp_replace(znfmd001.t$fovn$c, '[^0-9]', ''))<11
+        THEN '00000000000000'
+          ELSE regexp_replace(znfmd001.t$fovn$c, '[^0-9]', '') END NR_CNPJ_FILIAL,
+  WMSWHS.WHSEID CD_ARMAZEM,
+  1 AS CD_CIA,
+  (SELECT C.T$NAMA FROM baandb.TTCCOM000201 C 
          WHERE C.T$INDT<TO_DATE('2000-01-01', 'YYYY-DD-MM') AND C.T$NCMP=201) NM_CIA
-FROM
-        baandb.TTCEMM112201 TCEMM112,
-        baandb.TTCMCS003201 TCMCS003,
-        baandb.TTCEMM030201 TCEMM030,
-        baandb.ttccom130201 TCCOM130,
-        baandb.ttcemm300201 TCEMM300,
-        (SELECT DISTINCT w.DESCRIPTION lctn, UPPER(w.LONG_VALUE) WHSEID 
+
+from baandb.tznfmd001201 znfmd001,
+      baandb.TTCEMM112201 TCEMM112, 
+      baandb.ttcemm300201 TCEMM300    
+
+left JOIN (SELECT DISTINCT w.DESCRIPTION lctn, UPPER(w.LONG_VALUE) WHSEID 
          FROM ENTERPRISE.CODELKUP@DL_LN_WMS w 
-         WHERE w.LISTNAME = 'SCHEMA') WMSWHS,
-        baandb.TTFGLD010201 TFGLD010
-WHERE   
-        TCMCS003.T$CWAR=TCEMM112.T$WAID
-  AND   TCEMM030.T$EUNT=TCEMM112.T$GRID
-  AND   TCCOM130.T$CADR=TCMCS003.T$CADR
-  AND   TCEMM300.t$code=TCEMM112.T$WAID
-  AND   WMSWHS.lctn=TCEMM300.T$LCTN
-  AND   TFGLD010.T$DIMX=tcemm030.t$euca
-  AND   TFGLD010.T$DTYP=2
+         WHERE w.LISTNAME = 'SCHEMA') WMSWHS
+ON WMSWHS.lctn=TCEMM300.T$LCTN
+
+WHERE znfmd001.T$COFC$C = TCEMM112.T$GRID  
+AND   TCEMM112.T$WAID = TCEMM300.t$code
