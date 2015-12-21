66532f63d9c8f9c8cb21f69084df0586d34c0129
rosanaprignolato <rosana.prignolato@iteris.com.br>
2015-12-21 15:36:20 -0200

    Criação de view NIKE

---
+++ Views Nike/VW_NK_DOM_WMS_TRANSPORTADORA.sql
@@ -0,0 +1,21 @@
+
+SELECT
+--**********************************************************************************************************************************************************
+-- O campo CD_CIA foi incluido para diferenciar NIKE(13) E BUNZL(15)
+
+          cast(13 as int) CD_CIA,
+          CASE WHEN regexp_replace(e.T$FOVN$L, '[^0-9]', '') IS NULL
+          THEN '00000000000000' 
+          WHEN LENGTH(regexp_replace(e.T$FOVN$L, '[^0-9]', ''))<11
+          THEN '00000000000000'
+          ELSE regexp_replace(e.T$FOVN$L, '[^0-9]', '') END NR_CNPJ_TRANSPORTADORA,
+          t.T$DSCA NM_TRANSPORTADORA,
+          t.T$SEAK NM_APELIDO_TRANSPORTADORA,
+          t.T$CFRW CD_TRANSPORTADORA
+FROM
+          ttcmcs080201 t,
+          ttccom100201 p,
+          ttccom130201 e
+WHERE     t.t$suno!=' '
+AND       p.T$BPID=t.T$SUNO
+AND       e.T$CADR=p.T$CADR
