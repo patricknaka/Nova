4abd3ca1a8e3c0809357c246f330420d51830b90
marcos-matias <mmferreira@gmail.com>
2015-12-28 09:03:54 -0200

    Update VW_CAR_Titulo_Remessa.sql
    A parcela não estava sendo considerada. e os registros estavam se duplicando.

--- Views LN/VW_CAR_Titulo_Remessa.sql
+++ Views LN/VW_CAR_Titulo_Remessa.sql
@@ -1,29 +1,34 @@
-﻿-- #FAF.204 - 04-jul-2014, Fabio Ferreira, 	Adicionado campo CD_MODULO
+-- #FAF.204 - 04-jul-2014, Fabio Ferreira, 	Adicionado campo CD_MODULO
 -- #FAF.204 - 04-jul-2014, Fabio Ferreira, 	Campo Nr. do banco tfcmg948
 -- 01/09/2014 - inclusão do CD_STATUS_ARQUIVOM, CD_STATUS_ENVIO e NR_CONTA
 -- #MAR.315 - 08-set-2014, Marcia Amador R. Torres, Trazer o ultimo registro da tfcmg948.
 --***************************************************************************************************************************************************
 
-SELECT distinct   1 CD_CIA,
-    tfcmg401.t$ttyp || tfcmg401.t$ninv CD_CHAVE_PRIMARIA,
-          tfcmg401.t$btno NR_REMESSA,
-          tfcmg409.t$date DT_REMESSA,
-          tfcmg401.t$schn NR_PARCELA,
-          tfacr201.t$liqd DT_PREVISTA_RECEBIMENTO,
-          tfcmg401.t$paym CD_METODO_RECEBIMENTO,
-          tfacr201.t$rpst$l CD_SITUACAO_TITULO,
-    'CAR' CD_MODULO,
-    nvl(tfcmg948.t$banu$l,' ') NR_BANCO,
-    tfcmg948.t$stat$l CD_STATUS_ARQUIVO,
-      tfcmg948.t$send$l CD_STATUS_ENVIO,
-      tfcmg948.t$acco$l NR_CONTA                        --#FAF.294.n.
+SELECT distinct   
+    	1 						CD_CIA,
+	tfcmg401.t$ttyp || tfcmg401.t$ninv 
+							    CD_CHAVE_PRIMARIA,
+	tfcmg401.t$btno				NR_REMESSA,
+	tfcmg409.t$date				DT_REMESSA,
+	tfcmg401.t$schn				NR_PARCELA,
+	tfacr201.t$liqd				DT_PREVISTA_RECEBIMENTO,
+	tfcmg401.t$paym				CD_METODO_RECEBIMENTO,
+	tfacr201.t$rpst$l			CD_SITUACAO_TITULO,
+	'CAR'					    CD_MODULO,
+	nvl(tfcmg948.t$banu$l,' ')	NR_BANCO,
+	tfcmg948.t$stat$l			CD_STATUS_ARQUIVO,
+	tfcmg948.t$send$l			CD_STATUS_ENVIO,
+	tfcmg948.t$acco$l			NR_CONTA                        --#FAF.294.n.
 FROM        baandb.ttfcmg401201 tfcmg401
-INNER JOIN  baandb.ttfcmg409201 tfcmg409 ON  tfcmg409.t$btno=tfcmg401.t$btno
-INNER JOIN  baandb.ttfacr201201 tfacr201 ON  tfacr201.t$ttyp=tfcmg401.t$ttyp
-                                         AND tfacr201.t$ninv=tfcmg401.t$ninv
-                                         AND tfacr201.t$schn=tfcmg401.t$schn
-LEFT JOIN  baandb.ttfcmg948201 tfcmg948 ON  tfcmg948.t$ttyp$l=tfcmg401.t$ttyp    --#FAF.294.sn
-                     AND tfcmg948.t$ninv$l=tfcmg401.t$ninv
-                     AND tfcmg948.t$btno$l=tfcmg401.t$btno    --#FAF.294.en
-
+INNER JOIN  baandb.ttfcmg409201 tfcmg409 
+	ON  		tfcmg409.t$btno=tfcmg401.t$btno
+INNER JOIN  baandb.ttfacr201201 tfacr201 
+	ON  		tfacr201.t$ttyp=tfcmg401.t$ttyp
+	AND 		tfacr201.t$ninv=tfcmg401.t$ninv
+	AND 		tfacr201.t$schn=tfcmg401.t$schn
+LEFT JOIN  baandb.ttfcmg948201 tfcmg948 		       --#FAF.294.sn
+	ON  	tfcmg948.t$ttyp$l=tfcmg401.t$ttyp   
+	AND 	tfcmg948.t$ninv$l=tfcmg401.t$ninv
+	AND 	tfcmg948.t$btno$l=tfcmg401.t$btno    	--#FAF.294.en
+	AND	tfcmg948.t$sern$l=tfcmg401.t$schn	        --#MMF.478.n
 ORDER BY 2,5
