2e140b0b39cbde473d7b2f45fdd1c5dc75b4a0b2
Humberto Kasai <humberto.kasai@outlook.com>
2016-01-04 19:57:25 -0200

    Correção para vales válidos

--- Relatórios LN/LN_Relatorio_Vales_Troca_Abertos_eHub.sql
+++ Relatórios LN/LN_Relatorio_Vales_Troca_Abertos_eHub.sql
@@ -1,13 +1,16 @@
 SELECT
-        pt_vale.dtoc                         DT_EMISSAO_VALE,
+        CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(NVL(pt_vale.dtoc,znsls400.t$dtem$c), 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+        AT time zone 'America/Sao_Paulo') AS DATE)                         
+                                             DT_EMISSAO_VALE,
         ABS(znsls402.t$vlmr$c)               VALOR_VALE,
         tfacr201.t$liqd                      DATA_VALIDADE,
         znsls400.t$fovn$c                    CPF_CLIENTE,
-        CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znsls410.DATA_OCORR, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+        CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tfacr200.t$rcd_utc, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
           AT time zone 'America/Sao_Paulo') AS DATE)
                                              ULTIMA_ALTERACAO,
         znsls402.t$pecl$c                    PEDIDO_CLIENTE,
-        znsls402.t$sqpd$c                    PEDIDO_SEQ
+        znsls402.t$sqpd$c                    PEDIDO_SEQ,
+        tfacr200.t$balc
 
 FROM    baandb.tznsls402601 znsls402
 
@@ -16,22 +19,6 @@ FROM    baandb.tznsls402601 znsls402
          AND znsls400.t$uneg$c = znsls402.t$uneg$c
          AND znsls400.t$pecl$c = znsls402.t$pecl$c
          AND znsls400.t$sqpd$c = znsls402.t$sqpd$c
-       
- LEFT JOIN ( select a.t$ncia$c,
-                    a.t$uneg$c,
-                    a.t$pecl$c,
-                    a.t$sqpd$c,
-                    max(a.t$dtoc$c) DATA_OCORR,
-                    MAX(a.t$poco$c) KEEP (DENSE_RANK LAST ORDER BY a.T$DTOC$C,  a.T$SEQN$C) PT_CONTR
-               from baandb.tznsls410601 a
-           group by a.t$ncia$c,
-                    a.t$uneg$c,
-                    a.t$pecl$c,
-                    a.t$sqpd$c ) znsls410
-        ON znsls410.t$ncia$c = znsls402.t$ncia$c
-       AND znsls410.t$uneg$c = znsls402.t$uneg$c
-       AND znsls410.t$pecl$c = znsls402.t$pecl$c
-       AND znsls410.t$sqpd$c = znsls402.t$sqpd$c
 
  LEFT JOIN ( select a.t$ncia$c,
                     a.t$uneg$c,
@@ -49,9 +36,22 @@ FROM    baandb.tznsls402601 znsls402
        AND pt_vale.t$pecl$c = znsls402.t$pecl$c
        AND pt_vale.t$sqpd$c = znsls402.t$sqpd$c
        
- LEFT JOIN baandb.tznmcs002301 znmcs002
-        ON znmcs002.t$poco$c = znsls410.PT_CONTR
-   
+   LEFT JOIN ( select a.t$ncia$c,
+                    a.t$uneg$c,
+                    a.t$pecl$c,
+                    a.t$sqpd$c,
+                    max(a.t$dtoc$c) dtoc
+               from baandb.tznsls410601 a
+               where a.t$poco$c = 'CAN'
+           group by a.t$ncia$c,
+                    a.t$uneg$c,
+                    a.t$pecl$c,
+                    a.t$sqpd$c ) pt_cancel
+        ON pt_cancel.t$ncia$c = znsls402.t$ncia$c
+       AND pt_cancel.t$uneg$c = znsls402.t$uneg$c
+       AND pt_cancel.t$pecl$c = znsls402.t$pecl$c
+       AND pt_cancel.t$sqpd$c = znsls402.t$sqpd$c
+       
   LEFT JOIN baandb.tznsls412601 znsls412
          ON znsls412.t$ncia$c = znsls402.t$ncia$c
         AND znsls412.t$uneg$c = znsls402.t$uneg$c
@@ -63,8 +63,19 @@ FROM    baandb.tznsls402601 znsls402
          ON tfacr201.t$ttyp = znsls412.t$ttyp$c
         AND tfacr201.t$ninv = znsls412.t$ninv$c
 
-WHERE znsls402.t$idmp$c = 4   --Vale
-  AND znsls402.t$vlmr$c < 0   --Reembolso
-  AND pt_vale.dtoc IS NOT NULL  --somente pedidos com vale emitido
+  LEFT JOIN ( select a.t$ttyp,
+                     a.t$ninv,
+                     a.t$amnt,
+                     a.t$balc,
+                     a.t$rcd_utc
+              from  baandb.ttfacr200601 a
+              where a.t$docn = 0 ) tfacr200
+         ON tfacr200.t$ttyp = znsls412.t$ttyp$c
+        AND tfacr200.t$ninv = znsls412.t$ninv$c
+        
+WHERE znsls402.t$idmp$c = 4       --Meio de Pagto tipo Vale
+  AND (znsls402.t$vlmr$c > 0 OR   --Vales utilizados
+      (znsls402.t$vlmr$c < 0 AND pt_vale.dtoc IS NOT NULL  AND tfacr200.t$balc = tfacr200.t$amnt ))  --Vales emitidos e não utilizados
+  AND pt_cancel.dtoc IS NULL      --restrição para pedidos cancelados
      
-order by znsls400.t$DTEM$c, ZNSLS402.T$PECL$C
+order by DT_EMISSAO_VALE, ZNSLS402.T$PECL$C
