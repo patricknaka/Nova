3d20736ba67dffb71a15b1b94ec740801b328ef0
marcusmsv <marcus_msv@yahoo.com.br>
2016-01-07 16:05:14 -0200

    Update VW_TRK_Status_Pedido.sql
    Exclusão das colunas Filial e Unidade Empresarial, pois não há contexto para estas colunas no modulo de Tracking do Pedido.

--- Views LN/VW_TRK_Status_Pedido.sql
+++ Views LN/VW_TRK_Status_Pedido.sql
@@ -1,9 +1,6 @@
--- VIEW: VW_TRK_Status_Pedido
--- #FAF.050, 23-mai-2014, Fabio Ferreira,	NUM_ENTREGA_PEDIDO convertido para string
---************************************************************************************************************************************************
 SELECT
     1 CD_CIA,
-    tcemm030.t$euca CD_FILIAL,
+    --tcemm030.t$euca CD_FILIAL,
 	TO_CHAR(znsls410.t$entr$c) NR_ENTREGA_PEDIDO,					--#FAF.050.n
     znsls410.t$pecl$c NR_PEDIDO,
     znsls410.t$poco$c CD_STATUS,
@@ -11,14 +8,17 @@ SELECT
 	CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znsls410.t$dtoc$c, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
     AT time zone 'America/Sao_Paulo') AS DATE) DT_ULT_ATUALIZACAO,
     znsls410.t$nmre$c NM_USUARIO_MODIFICACAO_STATUS,
-    tcemm112.t$grid CD_UNIDADE_EMPRESARIAL,
+    --tcemm112.t$grid CD_UNIDADE_EMPRESARIAL,
     znsls410.T$UNEG$C CD_UNIDADE_NEGOCIO,
-    znsls410.t$orno$c NR_ORDEM_VENDA
+    znsls410.t$orno$c NR_ORDEM_VENDA,
+    znsls410.t$seqn$c as SQ_PONTO_CONTROLE,
+    znsls410.t$sqpd$c as SQ_PEDIDO,
+    tdsls400.t$cwar
 FROM
-    baandb.tznsls410201 znsls410,
-    baandb.ttdsls400201 tdsls400,
-    baandb.ttcemm112201 tcemm112,
-    baandb.ttcemm030201 tcemm030
-WHERE 	tdsls400.t$orno=znsls410.t$orno$c
-AND     tdsls400.t$cwar=tcemm112.t$waid
-AND		tcemm030.t$eunt=tcemm112.t$grid
+    baandb.tznsls410201 znsls410
+    inner join baandb.ttdsls400201 tdsls400
+    on tdsls400.t$orno=znsls410.t$orno$c
+    /*left join  baandb.ttcemm112201 tcemm112
+    on tdsls400.t$cwar=tcemm112.t$waid
+    left join    baandb.ttcemm030201 tcemm030
+    on tcemm030.t$eunt=tcemm112.t$grid*/
