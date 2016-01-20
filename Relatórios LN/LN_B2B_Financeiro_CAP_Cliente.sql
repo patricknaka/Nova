09f381832b03f321d281a09b4c4fcda4f4a36b0a
marcos-matias <mmferreira@gmail.com>
2016-01-20 16:14:07 -0200

    Update LN_B2B_Financeiro_CAP_Cliente.sql
    Algumas equalizacoes para a queri ficar igual a LN_B2B_FINANCEIRO_CAR.
    Notas de operação triangular não estavam sendo impressas
    Novo campo: VALOR_TOTAL

--- Relatórios LN/LN_B2B_Financeiro_CAP_Cliente.sql
+++ Relatórios LN/LN_B2B_Financeiro_CAP_Cliente.sql
@@ -1,3 +1,7 @@
+-- ID MMF:  Algumas equalizacoes para a querie ficar igual a LN_B2B_FINANCEIRO_CAR.
+--          Notas de operação triangular não estavam sendo impressas
+--          Novo campo: VALOR_TOTAL
+
 SELECT
   DISTINCT
     znsls400b.t$idco$c        CONTRATO,
@@ -5,7 +9,7 @@ SELECT
     znsls400b.t$fovn$c        CNPJ,
     tccom130a.t$fovn$l        CNPJ_FILIAL_EMISSORA,    
     tccom130a.t$nama          NOME,
---  N�o tem no LN             JOGO_NF,
+--  Não tem no LN             JOGO_NF,
     tfacr200r.t$docn$l        NF,
     tfacr200r.t$seri$l        SERIE,  
     tfacr200r.t$docd          DTA_EMISSAO_TITULO,  
@@ -26,9 +30,12 @@ SELECT
     cisli941.t$tldm$l         VLR_DESC_INCL_TOTAL,
     cisli941.t$fght$l         VLR_TOTAL_FRETE,
     cisli941.t$iprt$l         VLR_TOTAL_ITEM,
+    ( znsls401.t$qtve$c * znsls401.t$vlun$c ) - cisli941.t$tldm$l + 
+        cisli941.t$fght$l     VALOR_TOTAL,
+
     TO_CHAR(cisli940b.t$cnfe$l)  
                               ID_CHAVE_ACESSO,
---  N�o tem no LN             DESC_CAMPANHA,
+--  Não tem no LN             DESC_CAMPANHA,
     znsls400b.t$peex$c        PEDIDO_PARCEIRO,
  
     ( SELECT cisli943.t$sbas$l 
@@ -58,29 +65,42 @@ SELECT
 FROM       baandb.ttfacr200201  tfacr200r 
 
  LEFT JOIN baandb.tcisli940201  cisli940b
-        ON cisli940b.t$ityp$l = tfacr200r.t$ttyp
+--        ON cisli940b.t$ityp$l = tfacr200r.t$ttyp
+        ON CISLI940b.T$SFCP$L = tfacr200r.t$loco$l 	-- MMF	equalizando com LN_B2B_FINANCEIRO_CAR	
+       AND cisli940b.t$ityp$l = tfacr200r.t$ttyp
        AND cisli940b.t$idoc$l = tfacr200r.t$ninv
        AND cisli940b.t$docn$l = tfacr200r.t$docn$l
+       AND CISLI940b.T$SERI$L = tfacr200r.t$seri$l 	-- MMF	equalizando com LN_B2B_FINANCEIRO_CAR
 
 INNER JOIN baandb.ttcemm124201  tcemm124
         ON tcemm124.t$cwoc    = cisli940b.t$cofc$l
- 
+
 INNER JOIN baandb.ttcemm030201  tcemm030
         ON tcemm030.t$eunt    = tcemm124.t$grid
- 
+
  LEFT JOIN baandb.ttfacr200201  tfacr200p
         ON tfacr200p.t$ttyp   = tfacr200r.t$ttyp
        AND tfacr200p.t$ninv   = tfacr200r.t$ninv
        AND tfacr200p.t$docn$l = tfacr200r.t$docn$l
        AND tfacr200p.t$trec   = 4
-       
-LEFT JOIN baandb.tcisli941201   cisli941
-       ON cisli941.t$fire$l   = cisli940b.t$fire$l           
 
-LEFT JOIN baandb.tcisli245201   cisli245b
-       ON cisli245b.t$fire$l  = cisli941.t$fire$l
-      AND cisli245b.t$line$l  = cisli941.t$line$l
-	  
+       -- MMF.sn
+LEFT JOIN baandb.tcisli941201   cisli941
+       ON cisli941.t$fire$l   = cisli940b.t$fire$l   
+
+LEFT JOIN BAANDB.TCISLI941201 CISLI941_REM                      -- REMESSA OP TRIANGULAR
+       ON CISLI941_REM.T$FIRE$L = CISLI941.T$REFR$L
+      AND CISLI941_REM.T$LINE$L = CISLI941.T$RFDL$L
+       -- MMF.en
+       
+--LEFT JOIN baandb.tcisli245201   cisli245b			-- MMF.so
+--       ON cisli245b.t$fire$l  = cisli941.t$fire$l
+--      AND cisli245b.t$line$l  = cisli941.t$line$l 		-- MMF.eo
+
+LEFT JOIN BAANDB.TCISLI245201 cisli245b 			           -- MMF.sn
+       ON CISLI245b.T$FIRE$L = CISLI941_REM.T$FIRE$L
+      AND CISLI245b.T$LINE$L = CISLI941_REM.T$LINE$L 		-- MMF.enl
+
 LEFT JOIN baandb.tcisli943201   cisli943b
        ON cisli943b.t$fire$l  = cisli941.t$fire$l
       AND cisli943b.t$line$l  = cisli941.t$line$l
@@ -104,7 +124,7 @@ INNER JOIN baandb.tznsls400201  znsls400b
       AND znsls400b.t$uneg$c   = znsls004.t$uneg$c
       AND znsls400b.t$pecl$c   = znsls004.t$pecl$c
       AND znsls400b.t$sqpd$c   = znsls004.t$sqpd$c
-	  
+
 LEFT JOIN baandb.tznsls407201   znsls407
        ON znsls407.t$tpst$c   = znsls400b.t$tped$c
 
@@ -114,10 +134,17 @@ LEFT JOIN baandb.tznsls407201   znsls407
  LEFT JOIN baandb.ttccom130201  tccom130b
         ON tccom130b.t$cadr   = cisli940b.t$stoa$l
 
-WHERE  tfacr200r.t$balc <> 0.00 AND tfacr200r.t$lino = 0
+INNER JOIN baandb.tznsls000201 znsls000                  -- Excluir itens frete / seguro / outras despesas
+        ON znsls000.t$indt$c = TO_DATE('01-01-1970','DD-MM-YYYY')
+       AND cisli941.t$item$l != znsls000.t$itmf$c      --ITEM FRETE
+       AND cisli941.t$item$l != znsls000.t$itmd$c      --ITEM DESPESAS
+       AND cisli941.t$item$l != znsls000.t$itjl$c      --ITEM JUROS
+
+--WHERE  tfacr200r.t$balc <> 0.00 AND tfacr200r.t$lino = 0  
+WHERE  tfacr200r.t$lino = 0                             -- MMF  equalizando com LN_B2B_FINANCEIRO_CAR 
    AND tfacr200r.t$trec <> 4
    AND tcemm124.t$dtyp   = 1 
    AND znsls400b.t$idca$c = 'B2B'
 
    AND TRUNC(tfacr200r.t$docd) BETWEEN :EmissaoDe AND :EmissaoAte
-   AND ((tccom130a.t$fovn$l like '%' || Trim(:CNPJ) || '%') OR (:CNPJ is null))
+   AND ((tccom130a.t$fovn$l like '%' || Trim(:CNPJ) || '%') OR (:CNPJ is null)) 
