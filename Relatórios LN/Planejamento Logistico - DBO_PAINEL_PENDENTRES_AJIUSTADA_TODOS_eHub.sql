137847a3e19fbcbbdc59de52dec1a59f3284e0ce
ronaldocs <ronaldo.cristiano@gmail.com>
2016-01-21 18:32:09 -0200

    Relatório DBO Painel Pendentes Ajustada Todos eHub
    Ajuste nos pontos de controle que o relatório deve exibir e exclusão da
    restriação por Documento Fiscal

--- Relatórios LN/Planejamento Logistico - DBO_PAINEL_PENDENTRES_AJIUSTADA_TODOS_eHub.sql
+++ Relatórios LN/Planejamento Logistico - DBO_PAINEL_PENDENTRES_AJIUSTADA_TODOS_eHub.sql
@@ -222,6 +222,7 @@ where Q1.OCORRENCIA IS NOT NULL
   
   
 =
+
 " select Q1.*  " &
 "   from ( SELECT  " &
 "            DISTINCT  " &
@@ -238,49 +239,14 @@ where Q1.OCORRENCIA IS NOT NULL
 "              cisli940.t$gamt$l  VL_MERCADORIA,  " &
 "              znsls401.t$uneg$c  UNINEG,  " &
 "  " &
-"              ( SELECT znfmd640.t$coci$c  " &
-"                  FROM BAANDB.tznfmd640" + Parameters!Compania.Value + " znfmd640  " &
-"                 WHERE znfmd640.t$coci$c = ( select max(znfmd640.t$coci$c) KEEP (DENSE_RANK LAST ORDER BY znfmd640.t$date$C,  znfmd640.t$udat$C)  " &
-"                                               from BAANDB.tznfmd640" + Parameters!Compania.Value + " znfmd640  " &
-"                                              where znfmd640.t$fili$c = znfmd630.t$fili$c  " &
-"                                                and znfmd640.t$etiq$c = znfmd630.t$etiq$c )  " &
-"                   AND ROWNUM = 1  " &
-"                   AND znfmd640.t$coci$c NOT IN ('ENT', 'EXT', 'ROU', 'AVA', 'DEV', 'EXF', 'RIE', 'RTD')  " &
-"                   AND znfmd640.t$fili$c = znfmd630.t$fili$c  " &
-"                   AND znfmd640.t$etiq$c = znfmd630.t$etiq$c )  " &
-"                                 OCORRENCIA,  " &
-"  " &
-"              ( SELECT znfmd040d.t$dotr$c  " &
-"                  FROM BAANDB.tznfmd640" + Parameters!Compania.Value + " znfmd640d,  " &
-"                       BAANDB.tznfmd040" + Parameters!Compania.Value + " znfmd040d  " &
-"                 WHERE znfmd640d.t$coci$c = ( select max(znfmd640x.t$coci$c) KEEP (DENSE_RANK LAST ORDER BY znfmd640x.t$date$C,  znfmd640x.t$udat$C)  " &
-"                                                from BAANDB.tznfmd640" + Parameters!Compania.Value + " znfmd640x  " &
-"                                               where znfmd640x.t$fili$c = znfmd630.t$fili$c  " &
-"                                                 and znfmd640x.t$etiq$c = znfmd630.t$etiq$c  " &
-"                                                 and znfmd040d.t$cfrw$c = znfmd630.t$cfrw$c  " &
-"                                                 and znfmd040d.t$ocin$c = znfmd640d.t$coci$c )  " &
-"                   AND ROWNUM = 1  " &
-"                   AND znfmd640d.t$fili$c = znfmd630.t$fili$c  " &
-"                   AND znfmd640d.t$etiq$c = znfmd630.t$etiq$c )  " &
-"                                 DESCRICAO,  " &
-"  " &
-"              ( SELECT CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd640.t$date$c, 'DD-MON-YYYY HH24:MI:SS'),  " &
-"                         'DD-MON-YYYY HH24:MI:SS'), 'GMT') AT time zone 'America/Sao_Paulo') AS DATE)  " &
-"                  FROM BAANDB.tznfmd640" + Parameters!Compania.Value + " znfmd640  " &
-"                 WHERE znfmd640.t$date$c = ( select max(znfmd640.t$date$c)  " &
-"                                               from BAANDB.tznfmd640301 znfmd640  " &
-"                                              where znfmd640.t$fili$c = znfmd630.t$fili$c  " &
-"                                                and znfmd640.t$etiq$c = znfmd630.t$etiq$c )  " &
-"                   AND ROWNUM = 1  " &
-"                   AND znfmd640.t$fili$c = znfmd630.t$fili$c  " &
-"                   AND znfmd640.t$etiq$c = znfmd630.t$etiq$c )  " &
-"                                 DATA_OCORRENCIA,  " &
+"             znfmd640.t$coci$c   OCORRENCIA,  " &
+"             znfmd640.t$dotr$c   DESCRICAO,  " &
+"             znfmd640.t$date$c   DATA_OCORRENCIA,  " &
 "  " &
 "              CASE WHEN znfmd630.t$stat$c = 2  " &
 "                     THEN 'F'  " &
 "                   ELSE   'P'  " &
 "              END                SITUACAO,  " &
-"  " &
 "              znsls401.t$cide$c  CIDADE,  " &
 "              znsls401.t$cepe$c  CEP,  " &
 "              znsls401.t$ufen$c  UF,  " &
@@ -288,7 +254,7 @@ where Q1.OCORRENCIA IS NOT NULL
 "  " &
 "              CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls400.t$prdt, 'DD-MON-YYYY HH24:MI:SS'),  " &
 "                'DD-MON-YYYY HH24:MI:SS'), 'GMT') AT time zone 'America/Sao_Paulo') AS DATE)  " &
-"                  DATA_PROMETIDA,  " &
+"                                 DATA_PROMETIDA,  " &
 "  " &
 "              ( SELECT MAX(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(A.t$date$c,  " &
 "                        'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  " &
@@ -318,6 +284,7 @@ where Q1.OCORRENCIA IS NOT NULL
 "                   and znfmd061.t$creg$c = znfmd062.t$creg$c  " &
 "                   and rownum = 1 )  " &
 "                                 REGIAO,  " &
+"  " &
 "              znsls401.t$itpe$c  ID_TIPO_ENTREGA,  " &
 "              znsls002.t$dsca$c  DESCR_TIPO_ENTREGA,  " &
 "              znfmd630.t$orno$c  ORDEM_VENDA,  " &
@@ -343,98 +310,107 @@ where Q1.OCORRENCIA IS NOT NULL
 "                   ELSE   'Não'  " &
 "               END               PEDIDO_SIGE,  " &
 "  " &
-"              ( SELECT CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd640.t$udat$c, 'DD-MON-YYYY HH24:MI:SS'),  " &
-"                         'DD-MON-YYYY HH24:MI:SS'), 'GMT') AT time zone 'America/Sao_Paulo') AS DATE)  " &
-"                 FROM BAANDB.tznfmd640" + Parameters!Compania.Value + " znfmd640  " &
-"                WHERE znfmd640.t$date$c = ( select max(znfmd640.t$date$c)  " &
-"                                              from BAANDB.tznfmd640" + Parameters!Compania.Value + " znfmd640  " &
-"                                             where znfmd640.t$fili$c = znfmd630.t$fili$c  " &
-"                                               and znfmd640.t$etiq$c = znfmd630.t$etiq$c )  " &
-"                  AND ROWNUM = 1  " &
-"                  AND znfmd640.t$fili$c = znfmd630.t$fili$c  " &
-"                  AND znfmd640.t$etiq$c = znfmd630.t$etiq$c )  " &
-"                                 DT_INSERCAO  " &
+"              znfmd640.t$udat$c  DT_INSERCAO  " &
 "  " &
-" FROM       BAANDB.tznfmd630" + Parameters!Compania.Value + " znfmd630  " &
+"   FROM       BAANDB.tznfmd630" + Parameters!Compania.Value + " znfmd630  " &
 "  " &
-" INNER JOIN BAANDB.tznsls004" + Parameters!Compania.Value + " znsls004  " &
-"         ON znsls004.t$orno$c = znfmd630.t$orno$c  " &
+"   INNER JOIN BAANDB.tznsls004" + Parameters!Compania.Value + " znsls004  " &
+"           ON znsls004.t$orno$c = znfmd630.t$orno$c  " &
 "  " &
-" INNER JOIN BAANDB.tznsls401" + Parameters!Compania.Value + " znsls401  " &
-"         ON znsls401.t$ncia$c = znsls004.t$ncia$c  " &
-"        AND znsls401.t$uneg$c = znsls004.t$uneg$c  " &
-"        AND znsls401.t$pecl$c = znsls004.t$pecl$c  " &
-"        AND znsls401.t$sqpd$c = znsls004.t$sqpd$c  " &
-"        AND znsls401.t$entr$c = znsls004.t$entr$c  " &
-"        AND znsls401.t$sequ$c = znsls004.t$sequ$c  " &
+"   INNER JOIN BAANDB.tznsls401" + Parameters!Compania.Value + " znsls401  " &
+"           ON znsls401.t$ncia$c = znsls004.t$ncia$c  " &
+"          AND znsls401.t$uneg$c = znsls004.t$uneg$c  " &
+"          AND znsls401.t$pecl$c = znsls004.t$pecl$c  " &
+"          AND znsls401.t$sqpd$c = znsls004.t$sqpd$c  " &
+"          AND znsls401.t$entr$c = znsls004.t$entr$c  " &
+"          AND znsls401.t$sequ$c = znsls004.t$sequ$c  " &
 "  " &
-"  LEFT JOIN baandb.ttdsls400" + Parameters!Compania.Value + " tdsls400  " &
-"         ON tdsls400.t$orno = znsls401.t$orno$c  " &
+"    LEFT JOIN baandb.ttdsls400" + Parameters!Compania.Value + " tdsls400  " &
+"           ON tdsls400.t$orno = znsls401.t$orno$c  " &
 "  " &
-" INNER JOIN BAANDB.tznsls400" + Parameters!Compania.Value + " znsls400  " &
-"         ON znsls400.t$ncia$c = znsls004.t$ncia$c  " &
-"        AND znsls400.t$uneg$c = znsls004.t$uneg$c  " &
-"        AND znsls400.t$pecl$c = znsls004.t$pecl$c  " &
-"        AND znsls400.t$sqpd$c = znsls004.t$sqpd$c  " &
+"   INNER JOIN BAANDB.tznsls400" + Parameters!Compania.Value + " znsls400  " &
+"           ON znsls400.t$ncia$c = znsls004.t$ncia$c  " &
+"          AND znsls400.t$uneg$c = znsls004.t$uneg$c  " &
+"          AND znsls400.t$pecl$c = znsls004.t$pecl$c  " &
+"          AND znsls400.t$sqpd$c = znsls004.t$sqpd$c  " &
 "  " &
-" INNER JOIN BAANDB.tznint002" + Parameters!Compania.Value + " znint002  " &
-"         ON znint002.t$ncia$c = znsls401.t$ncia$c  " &
-"        AND znint002.t$uneg$c = znsls401.t$uneg$c  " &
+"   INNER JOIN BAANDB.tznint002" + Parameters!Compania.Value + " znint002  " &
+"           ON znint002.t$ncia$c = znsls401.t$ncia$c  " &
+"          AND znint002.t$uneg$c = znsls401.t$uneg$c  " &
 "  " &
-" INNER JOIN BAANDB.tznsls002" + Parameters!Compania.Value + " znsls002  " &
-"         ON znsls002.t$tpen$c = znsls401.t$itpe$c  " &
+"   INNER JOIN BAANDB.tznsls002" + Parameters!Compania.Value + " znsls002  " &
+"           ON znsls002.t$tpen$c = znsls401.t$itpe$c  " &
 "  " &
-"  LEFT JOIN BAANDB.tznfmd060" + Parameters!Compania.Value + " znfmd060  " &
-"         ON znfmd060.t$cfrw$c = znfmd630.t$cfrw$c  " &
-"        AND znfmd060.t$cono$c = znfmd630.t$cono$c  " &
+"    LEFT JOIN BAANDB.tznfmd060" + Parameters!Compania.Value + " znfmd060  " &
+"           ON znfmd060.t$cfrw$c = znfmd630.t$cfrw$c  " &
+"          AND znfmd060.t$cono$c = znfmd630.t$cono$c  " &
 "  " &
-"  LEFT JOIN BAANDB.ttcmcs080" + Parameters!Compania.Value + " tcmcs080  " &
-"         ON tcmcs080.t$cfrw = znfmd630.t$cfrw$c  " &
+"    LEFT JOIN BAANDB.ttcmcs080" + Parameters!Compania.Value + " tcmcs080  " &
+"           ON tcmcs080.t$cfrw = znfmd630.t$cfrw$c  " &
 "  " &
-" INNER JOIN baandb.tcisli940" + Parameters!Compania.Value + " cisli940  " &
-"         ON cisli940.t$fire$l = znfmd630.t$fire$c  " &
-"        AND cisli940.t$docn$l = znfmd630.t$docn$c  " &
-"        AND cisli940.t$seri$l = znfmd630.t$seri$c  " &
+"   INNER JOIN baandb.tcisli940" + Parameters!Compania.Value + " cisli940  " &
+"           ON cisli940.t$fire$l = znfmd630.t$fire$c  " &
+"          AND cisli940.t$docn$l = znfmd630.t$docn$c  " &
+"          AND cisli940.t$seri$l = znfmd630.t$seri$c  " &
 "  " &
-"  LEFT JOIN baandb.ttccom130" + Parameters!Compania.Value + " tccom130  " &
-"         ON tccom130.t$cadr = cisli940.t$stoa$l  " &
+"    LEFT JOIN baandb.ttccom130" + Parameters!Compania.Value + " tccom130  " &
+"           ON tccom130.t$cadr = cisli940.t$stoa$l  " &
 "  " &
-" INNER JOIN ( SELECT d.t$cnst CNST,  " &
-"                     l.t$desc DESC_TIPO_ORDEM  " &
-"                FROM baandb.tttadv401000 d, baandb.tttadv140000 l  " &
-"               WHERE d.t$cpac = 'ci'  " &
-"                 AND d.t$cdom = 'sli.tdff.l'  " &
-"                 AND l.t$clan = 'p'  " &
-"                 AND l.t$cpac = 'ci'  " &
-"                 AND l.t$clab = d.t$za_clab  " &
-"                 AND rpad(d.t$vers,4) ||  " &
-"                     rpad(d.t$rele,2) ||  " &
-"                     rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
-"                                                     rpad(l1.t$rele,2) ||  " &
-"                                                     rpad(l1.t$cust,4))  " &
-"                                            from baandb.tttadv401000 l1  " &
-"                                           where l1.t$cpac = d.t$cpac  " &
-"                                             and l1.t$cdom = d.t$cdom )  " &
-"                 AND rpad(l.t$vers,4) ||  " &
-"                     rpad(l.t$rele,2) ||  " &
-"                     rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
-"                                                     rpad(l1.t$rele,2) ||  " &
-"                                                     rpad(l1.t$cust,4))  " &
-"                                            from baandb.tttadv140000 l1  " &
-"                                           where l1.t$clab = l.t$clab  " &
-"                                             and l1.t$clan = l.t$clan  " &
-"                                             and l1.t$cpac = l.t$cpac ) ) FGET  " &
-"   ON FGET.CNST = cisli940.t$fdty$l  " &
+"   INNER JOIN ( SELECT d.t$cnst CNST,  " &
+"                       l.t$desc DESC_TIPO_ORDEM  " &
+"                  FROM baandb.tttadv401000 d, baandb.tttadv140000 l  " &
+"                 WHERE d.t$cpac = 'ci'  " &
+"                   AND d.t$cdom = 'sli.tdff.l'  " &
+"                   AND l.t$clan = 'p'  " &
+"                   AND l.t$cpac = 'ci'  " &
+"                   AND l.t$clab = d.t$za_clab  " &
+"                   AND rpad(d.t$vers,4) ||  " &
+"                       rpad(d.t$rele,2) ||  " &
+"                       rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"                                                       rpad(l1.t$rele,2) ||  " &
+"                                                       rpad(l1.t$cust,4))  " &
+"                                              from baandb.tttadv401000 l1  " &
+"                                             where l1.t$cpac = d.t$cpac  " &
+"                                               and l1.t$cdom = d.t$cdom )  " &
+"                   AND rpad(l.t$vers,4) ||  " &
+"                       rpad(l.t$rele,2) ||  " &
+"                       rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||  " &
+"                                                       rpad(l1.t$rele,2) ||  " &
+"                                                       rpad(l1.t$cust,4))  " &
+"                                              from baandb.tttadv140000 l1  " &
+"                                             where l1.t$clab = l.t$clab  " &
+"                                               and l1.t$clan = l.t$clan  " &
+"                                               and l1.t$cpac = l.t$cpac ) ) FGET  " &
+"     ON FGET.CNST = cisli940.t$fdty$l  " &
 "  " &
-" INNER JOIN ( SELECT znfmd640.t$coci$c,  " &
-"                     znfmd640.t$fili$c,  " &
-"                     znfmd640.t$etiq$c  " &
-"                FROM BAANDB.tznfmd640601 znfmd640  " &
-"               WHERE znfmd640.t$coci$c = 'ETR' ) fmd640  " &
-"         ON fmd640.t$fili$c = znfmd630.t$fili$c  " &
-"        AND fmd640.t$etiq$c = znfmd630.t$etiq$c  " &
+"   INNER JOIN ( SELECT znfmd640.t$coci$c,  " &
+"                       znfmd640.t$fili$c,  " &
+"                       znfmd640.t$etiq$c  " &
+"                  FROM BAANDB.tznfmd640" + Parameters!Compania.Value + " znfmd640  " &
+"                 WHERE znfmd640.t$coci$c = 'ETR' ) fmd640  " &
+"           ON fmd640.t$fili$c = znfmd630.t$fili$c  " &
+"          AND fmd640.t$etiq$c = znfmd630.t$etiq$c  " &
 "  " &
-" WHERE cisli940.t$fdty$l = 1 ) Q1  " &
+"  " &
+"   INNER JOIN ( select znfmd640.t$fili$c,  " &
+"                       znfmd640.t$etiq$c,  " &
+"                       znfmd640.t$coci$c,  " &
+"                       znfmd040.t$cfrw$c,  " &
+"                       znfmd040.t$dotr$c,  " &
+"                       CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd640.t$udat$c, 'DD-MON-YYYY HH24:MI:SS'),			  " &
+"                         'DD-MON-YYYY HH24:MI:SS'), 'GMT') AT time zone 'America/Sao_Paulo') AS DATE)	 t$udat$c,  " &
+"                       CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd640.t$date$c, 'DD-MON-YYYY HH24:MI:SS'),			  " &
+"                         'DD-MON-YYYY HH24:MI:SS'), 'GMT') AT time zone 'America/Sao_Paulo') AS DATE)	 t$date$c		  " &
+"                   from BAANDB.tznfmd640" + Parameters!Compania.Value + " znfmd640  " &
+"              left join BAANDB.tznfmd040" + Parameters!Compania.Value + " znfmd040  " &
+"                     on znfmd040.t$ocin$c = znfmd640.t$coci$c  " &
+"                  where znfmd640.t$coci$c = ( select max(fmd640.t$coci$c) KEEP (DENSE_RANK LAST ORDER BY fmd640.t$date$C, fmd640.t$udat$C)  " &
+"                                                from BAANDB.tznfmd640" + Parameters!Compania.Value + " fmd640  " &
+"                                               where znfmd640.t$fili$c = fmd640.t$fili$c  " &
+"                                                 and znfmd640.t$etiq$c = fmd640.t$etiq$c )  " &
+"                    and znfmd640.t$coci$c IN ('ARE','ARO','FIS','ROT','DDL','CME','ENL','ETR','POT','DIE','PNR','PRT','TRN','REE','SEF','EA1','EA2','EA3','CXP','AGE','NAP','RET','PRE','DRE','TRD','FER','CHU','MPD' ) ) znfmd640  " &
+"           ON znfmd640.t$fili$c = znfmd630.t$fili$c  " &
+"          AND znfmd640.t$etiq$c = znfmd630.t$etiq$c ) Q1  " &
 "  " &
 " where Q1.OCORRENCIA IS NOT NULL  " &
 "   and Q1.DATA_PROMETIDA  " &
