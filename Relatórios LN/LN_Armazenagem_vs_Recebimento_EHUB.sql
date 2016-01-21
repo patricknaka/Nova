d2be1a2295dfe7eea80fbe774e8e3e5d0400f1aa
marcos-matias <mmferreira@gmail.com>
2016-01-21 16:03:24 -0200

    Create LN_Armazenagem_vs_Recebimento_EHUB.sql
    Solicitação por email.

---
+++ Relatórios LN/LN_Armazenagem_vs_Recebimento_EHUB.sql
@@ -0,0 +1,42 @@
+
+SELECT	
+	trim(whinh312.t$item)			ITEM,
+	tdrec941.t$qnty$l				QUANTIDADE_PLANEJADA_NR,
+	whinh312.t$qrec					QUANTIDADE_FISICA_RECEBIDA,
+	
+	TO_CHAR(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdrec941.t$tdat$l, 
+	'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+	AT time zone 'America/Sao_Paulo') AS DATE), 'dd/MM/yyyy') 
+										DATA_RECEBIMENTO,
+	TO_CHAR(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(whinh312.t$ardt, 
+	'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+	AT time zone 'America/Sao_Paulo') AS DATE), 'dd/MM/yyyy') 	
+										DATA_RECEBIMENTO_FISICO
+
+FROM	baandb.twhinh312601 whinh312
+
+INNER JOIN baandb.ttdpur406601 tdpur406
+		ON whinh312.t$rcno = tdpur406.t$rcno
+	   AND whinh312.t$rcln = tdpur406.t$rseq
+  
+LEFT JOIN baandb.ttdrec947601 tdrec947  
+		ON tdrec947.t$oorg$l = whinh312.t$oorg
+	   AND tdrec947.t$orno$l = tdpur406.t$orno
+	   AND tdrec947.t$pono$l = tdpur406.t$pono
+	   AND tdrec947.t$seqn$l = tdpur406.t$sqnb  
+
+LEFT JOIN baandb.ttdrec941601 tdrec941  
+		ON tdrec941.t$fire$l = tdrec947.t$fire$l
+	   AND tdrec941.t$line$l = tdrec947.t$line$l
+
+WHERE	TO_CHAR(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdrec941.t$tdat$l, 
+			'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+			AT time zone 'America/Sao_Paulo') AS DATE), 'dd/MM/yyyy') 
+		BETWEEN		:DataRecDe
+		AND 		:DataRecAte
+AND		TO_CHAR(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(whinh312.t$ardt, 
+			'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+			AT time zone 'America/Sao_Paulo') AS DATE), 'dd/MM/yyyy')
+		BETWEEN		:DataArmDe
+		AND			:DataArmAte
+AND		( trim(whinh312.t$item) = (trim(:Item)) or :Item is null )
