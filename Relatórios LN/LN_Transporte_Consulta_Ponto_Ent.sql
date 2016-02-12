df83ca05a8bba27ab4eef64886648c148706f2ec
ronaldocs <ronaldo.cristiano@gmail.com>
2016-02-12 15:54:14 -0200

    Relatório - Consulta Ponto ENT
    Ajuste no parametro Ponto

--- Relatórios LN/LN_Transporte_Consulta_Ponto_Ent.sql
+++ Relatórios LN/LN_Transporte_Consulta_Ponto_Ent.sql
@@ -3,11 +3,11 @@ SELECT
     znfmd630.t$pecl$c  ENTREGA,
     znfmd640.t$coci$c  PONTO,
       
-    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd640.t$date$c, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+ CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd640.t$date$c, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
           AT time zone 'America/Sao_Paulo') AS DATE)
                        DATA_PONTO,
 
-    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd640.t$udat$c, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+ CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd640.t$udat$c, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
           AT time zone 'America/Sao_Paulo') AS DATE)
                        DATA_PROCESSAMENTO
 
@@ -16,18 +16,18 @@ FROM       BAANDB.tznfmd630301 znfmd630
 INNER JOIN BAANDB.tznfmd640301 znfmd640
         ON znfmd640.t$fili$c = znfmd630.t$fili$c 
        AND znfmd640.t$etiq$c = znfmd630.t$etiq$c
-WHERE
-      znfmd640.t$coci$c = 'ENT'
-
-      AND TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd640.t$date$c, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-          AT time zone 'America/Sao_Paulo') AS DATE))
+WHERE TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd640.t$date$c, 
+              'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                AT time zone 'America/Sao_Paulo') AS DATE))
       BETWEEN :DataPontoDe
           AND :DataPontoAte
     
-     AND TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd640.t$udat$c, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-          AT time zone 'America/Sao_Paulo') AS DATE))
+  AND TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd640.t$udat$c, 
+              'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                AT time zone 'America/Sao_Paulo') AS DATE))
       BETWEEN :DataProceDe
           AND :DataProceAte
-       
      AND ( (znfmd630.t$pecl$c IN (:Entrega) and :EntregaTodos = 1 ) or (:EntregaTodos = 0) )
-     AND ( (Upper(znfmd640.t$coci$c) IN (:Ponto) and :PontoTodos = 1 ) or (:PontoTodos = 0) )
+     AND znfmd640.t$coci$c IN (:Ponto)
+     
+ORDER BY ENTREGA
