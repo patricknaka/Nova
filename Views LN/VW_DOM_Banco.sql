67b25de9e6a94ebd0c105d9bf408b475c810f4af
rosanaprignolato <rosana.prignolato@iteris.com.br>
2016-02-12 11:15:50 -0200

    Atualização VW_DOM_BANCO

--- Views LN/VW_DOM_Banco.sql
+++ Views LN/VW_DOM_Banco.sql
@@ -1,15 +1,24 @@
--- #FAF.064 - 22-mai-2014, Fabio Ferreira, 	Adicionados os códigos do banco do site
---**********************************************************************************************************************************************************
-Select distinct * from																--#FAF.064.n 
-(select cast(a.t$baoc$l as varchar(3)) CD_Banco,
-        ( select b.t$bnam from  baandb.ttfcmg011201 b
+Select distinct * 
+  from 
+    (select 
+      cast(to_char(a.t$baoc$l) as varchar(3))	CD_BANCO,
+    (select b.t$bnam 
+          from baandb.ttfcmg011201 b
           where b.t$baoc$l=a.t$baoc$l
-          and rownum=1) DS_Banco
-from baandb.ttfcmg011201 a
-UNION select cast(TO_CHAR(c.t$idbc$c) as varchar(3)) Cod_Banco,											--#FAF.064.sn
-		(select d.t$desc$c from BAANDB.tzncmg020201 d
-		 where d.t$idbc$c=c.t$idbc$c
-		 and rownum=1) Ds_Banco
-from BAANDB.tzncmg020201 c)
-UNION Select '0' CD_Banco,  'Não Associado' DS_Banco from dual						--#FAF.064.en
+          and rownum=1)				DS_BANCO
+    from baandb.ttfcmg011201 a
+
+UNION 
+    select 
+      cast(to_char(c.t$idbc$c) as varchar(3))	CD_BANCO,
+		(select d.t$desc$c 
+        from baandb.tzncmg020201 d
+        where d.t$idbc$c=c.t$idbc$c
+        and rownum=1)				DS_BANCO
+    from baandb.tzncmg020201 c)
+
+UNION 
+  Select '0'					CD_BANCO,  
+  'Não Associado'				DS_BANCO 
+  from dual
 order by 1
