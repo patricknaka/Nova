62f500ecda7a7b35eb018ecb3891d3b72c237f2c
Humberto Kasai <humberto.kasai@outlook.com>
2016-03-01 15:12:22 -0300

    Correções referente ao chamado "1074090"

--- Relatórios LN/WMS_REL_007 - Pedidos Cancelados pelo SAC.sql
+++ Relatórios LN/WMS_REL_007 - Pedidos Cancelados pelo SAC.sql
@@ -4,75 +4,76 @@ ORDERS.REFERENCEDOCUMENT                    PED_TERC,
 ORDERS.ORDERKEY                             ID_PED,
 TASKDETAIL.ASSIGNMENTNUMBER                 PROG,
 WAVEDETAIL.WAVEKEY                          ONDA,
-orderstatussetup.description                EVENTO,
-ULTIMO_EVENTO.ADDWHO                        USUARIO,
-    subStr( tu.usr_name,4,
-            inStr(tu.usr_name, ',')-4 )      NOME,
-ULTIMO_EVENTO.ADDDATE                       DT_EVENTO,
-STATUSCANCELADO.ADDDATE                     DT_CANC,
+--CASE WHEN OSET1.CODE IS NULL THEN
+--    OSET2.CODE
+--ELSE OSET1.CODE END                         COD_EVENTO,
+CASE WHEN OSET1.DESCRIPTION IS NULL THEN
+  OSET2.DESCRIPTION
+ELSE OSET1.description END                  EVENTO,
+CASE WHEN ULTIMO_EVENTO.ADDWHO IS NULL THEN
+    ORDERS.EDITWHO
+ELSE ULTIMO_EVENTO.ADDWHO END               USUARIO,
+CASE WHEN tu_1.usr_name IS NULL THEN
+     subStr( tu_2.usr_name,4,
+     inStr(tu_2.usr_name, ',')-4 )
+ELSE subStr( tu_1.usr_name,4,
+     inStr(tu_1.usr_name, ',')-4 ) END       NOME,
+CASE WHEN ULTIMO_EVENTO.ADDDATE IS NULL THEN
+    ORDERS.EDITDATE
+ELSE ULTIMO_EVENTO.ADDDATE END              DT_EVENTO,
+CASE WHEN STATUSCANCELADO.ADDDATE IS NULL THEN
+     ORDERS.EDITDATE
+ELSE STATUSCANCELADO.ADDDATE END            DT_CANC,
 ORDERS.WHSEID                               PLANTA,
 CODELKUP.UDF2                               DESC_PLANTA
 
-<<<<<<< HEAD
-FROM      WMWHSE5.ORDERS
+FROM      WMWHSE6.ORDERS
 
-LEFT JOIN WMWHSE5.WAVEDETAIL
-=======
-FROM      WMWHSE4.ORDERS
-
-LEFT JOIN WMWHSE4.WAVEDETAIL
->>>>>>> parent of cf75d17... Relatório R078
-       ON WAVEDETAIL.ORDERKEY=ORDERS.ORDERKEY
+LEFT JOIN WMWHSE6.WAVEDETAIL
+       ON WAVEDETAIL.ORDERKEY = ORDERS.ORDERKEY
        
-LEFT JOIN TASKDETAIL
-       ON TASKDETAIL.ORDERKEY=ORDERS.ORDERKEY
-      AND TASKDETAIL.WAVEKEY=WAVEDETAIL.WAVEKEY
+LEFT JOIN WMWHSE6.TASKDETAIL
+       ON TASKDETAIL.ORDERKEY = ORDERS.ORDERKEY
+      AND TASKDETAIL.WAVEKEY = WAVEDETAIL.WAVEKEY
              
  LEFT JOIN ( SELECT a.ORDERKEY, 
                     a.ORDERLINENUMBER , 
                     max(a.STATUS)   STATUS, 
                     max(a.ADDDATE)  ADDDATE, 
                     max(a.ADDWHO)   ADDWHO 
-<<<<<<< HEAD
-              FROM WMWHSE5.ORDERSTATUSHISTORY a
+              FROM WMWHSE6.ORDERSTATUSHISTORY a
               WHERE a.ADDDATE = ( select  max(b.adddate) 
-                                  from    WMWHSE5.ORDERSTATUSHISTORY b
-=======
-              FROM WMWHSE4.ORDERSTATUSHISTORY a
-              WHERE a.ADDDATE = ( select  max(b.adddate) 
-                                  from    WMWHSE4.ORDERSTATUSHISTORY b
->>>>>>> parent of cf75d17... Relatório R078
+                                  from    WMWHSE6.ORDERSTATUSHISTORY b
                                   where   b.ORDERKEY = a.ORDERKEY
                                   and     b.ORDERLINENUMBER = a.ORDERLINENUMBER )
               GROUP BY a.ORDERKEY, 
                        a.ORDERLINENUMBER ) ULTIMO_EVENTO
               ON ULTIMO_EVENTO.ORDERKEY = ORDERS.ORDERKEY 
      
-LEFT JOIN ( SELECT MAX(C.ADDDATE)  ADDDATE,
-                    C.ORDERKEY
-<<<<<<< HEAD
-            FROM WMWHSE5.ORDERSTATUSHISTORY C
-            WHERE C.STATUS=99
-=======
-            FROM WMWHSE4.ORDERSTATUSHISTORY C
-            WHERE C.STATUS=98
->>>>>>> parent of cf75d17... Relatório R078
-            GROUP BY C.ORDERKEY)  STATUSCANCELADO
-       ON STATUSCANCELADO.ORDERKEY=ORDERS.ORDERKEY
-            
-LEFT JOIN ORDERSTATUSSETUP  
-       ON ORDERSTATUSSETUP.CODE=ULTIMO_EVENTO.STATUS
+LEFT JOIN ( SELECT MAX(C.ADDDATE)  ADDDATE, C.ORDERKEY
+            FROM WMWHSE6.ORDERSTATUSHISTORY C
+            WHERE C.STATUS = 98 or c.status = 99    --Cancelado Externamente ou Cancelado Internamente
 
+            GROUP BY C.ORDERKEY)  STATUSCANCELADO
+       ON STATUSCANCELADO.ORDERKEY = ORDERS.ORDERKEY
+            
+LEFT JOIN ORDERSTATUSSETUP OSET1 
+       ON OSET1.CODE = ULTIMO_EVENTO.STATUS
+
+LEFT JOIN ORDERSTATUSSETUP OSET2 
+       ON OSET2.CODE = ORDERS.STATUS
+       
 LEFT JOIN ( SELECT a.UDF1, a.UDF2
             FROM  CODELKUP a
-            WHERE LISTNAME='SCHEMA')  CODELKUP
-       ON   UPPER(CODELKUP.UDF1)=UPPER(ORDERS.WHSEID)
+            WHERE LISTNAME = 'SCHEMA')  CODELKUP
+       ON   UPPER(CODELKUP.UDF1) = UPPER(ORDERS.WHSEID)
 
-<<<<<<< HEAD
-LEFT JOIN WMWHSE5.taskmanageruser tu 
-=======
-LEFT JOIN WMWHSE4.taskmanageruser tu 
->>>>>>> parent of cf75d17... Relatório R078
-       ON tu.userkey = ULTIMO_EVENTO.ADDWHO
+LEFT JOIN WMWHSE6.taskmanageruser tu_1 
+       ON tu_1.userkey = ULTIMO_EVENTO.ADDWHO
+
+LEFT JOIN WMWHSE6.taskmanageruser tu_2
+       ON tu_2.userkey = ORDERS.EDITWHO
        
+where (orders.status = 98 or orders.status = 99) --Cancelado Externamente ou Cancelado Internamente
+
 order by ORDERS.ORDERKEY
