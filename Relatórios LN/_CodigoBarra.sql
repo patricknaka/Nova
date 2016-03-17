fff31181f3113a67fe99b0d79f8a0d949fc3cc2f
rosanaprignolato <rosana.prignolato@iteris.com.br>
2016-03-17 16:44:10 -0300

    Atualização view do relatório _CodigoBarra

---
+++ Relatórios LN/_CodigoBarra.sql
@@ -0,0 +1,7 @@
+﻿select tcibd001.t$item
+      from baandb.ttcibd001301 tcibd001
+inner join ( select distinct whinp100.t$item 
+               from baandb.twhinp100301 whinp100 ) whinp100
+        on tcibd001.t$item = whinp100.t$item 
+     where ROWNUM <= 10
+  order by tcibd001.t$item
