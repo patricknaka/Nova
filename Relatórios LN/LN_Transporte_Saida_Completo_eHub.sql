32ca77443bce2bd6fa49ebd454dca1ee06bcb9b8
ronaldocs <ronaldo.cristiano@gmail.com>
2016-03-17 14:47:34 -0300

    Relatório Transporte Saída Completo
    Ajustes

--- Relatórios LN/LN_Transporte_Saida_Completo_eHub.sql
+++ Relatórios LN/LN_Transporte_Saida_Completo_eHub.sql
@@ -10,6 +10,7 @@ SELECT
     ZNSLS401.T$PECL$C      PEDIDO,
     SHO.INVOICENUMBER      NOTA,
     SHO.LANE               SERIE,
+    ZNFMD630.T$ORNO$C      ORDEM_VENDA,
     OXV.NOTES1             ETIQUETA,
     ZNSLS401.T$NOME$C      CLIENTE,
     ZNSLS401.T$ICLE$C      CPF_CNPJ,
@@ -21,10 +22,7 @@ SELECT
     ZNSLS401.T$TELE$C      TELEFONE_1,
     ZNSLS401.T$TE1E$C      TELEFONE_2,
     ZNSLS401.T$TE2E$C      TELEFONE_3,
-    ZNSLS410.T$CONO$C      CONTRATO,
     SHD.ORIGINALQTY        QTD_VOL,
-    ZNSLS401.T$ITPE$C      ID_TIPO_ENTREGA,
-    ZNSLS002.T$DSCA$C      DESCR_TIPO_ENTREGA,
     SHD.PRODUCT_WEIGHT     PESO,
     SHD.PRODUCT_CUBE       VOLUME,
     ZNSLS401.T$VLUN$C *
@@ -65,9 +63,16 @@ SELECT
                   AT time zone 'America/Sao_Paulo') AS DATE)
     END                    DT_CORRIGIDA,
 
+    CASE WHEN TRUNC(znfmd630.t$dtpe$c) <= TO_DATE('01/01/1970','DD/MM/YYYY')
+           THEN NULL
+         ELSE   CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd630.t$dtpe$c, 'DD-MON-YYYY HH24:MI:SS'),
+                  'DD-MON-YYYY HH24:MI:SS'), 'GMT') AT time zone 'America/Sao_Paulo') AS DATE)
+    END                    DT_PREVISTA,
+
     ZNFMD630.T$NCAR$C      CARGA,
     znfmd062.t$creg$c      CAPITAL_INTERIOR,
     znfmd061.t$dzon$c      REGIAO,
+
     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ZNSLS410.T$DTOC$C,
       'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
         AT time zone 'America/Sao_Paulo') AS DATE)
@@ -86,24 +91,24 @@ SELECT
            THEN 'Noite'
          ELSE Null
     END                    PERIODO
-	
+
 FROM       WMWHSE9.ORDERDETAIL SHD
 
 INNER JOIN WMWHSE9.ORDERDETAILXVAS OXV
         ON OXV.ORDERKEY = SHD.ORDERKEY
        AND OXV.ORDERLINENUMBER = SHD.ORDERLINENUMBER
-	
+
 INNER JOIN WMWHSE9.ORDERS SHO
         ON SHO.ORDERKEY = SHD.ORDERKEY
-		
+
  LEFT JOIN BAANDB.TWHINH431601@PLN01 WHINH431
         ON WHINH431.T$SHPM = SUBSTR(SHD.EXTERNORDERKEY,5,9)
        AND TO_CHAR(WHINH431.T$PONO) = TO_CHAR(SHD.EXTERNLINENO*10)
-	
+
  LEFT JOIN BAANDB.TZNSLS004601@PLN01 ZNSLS004
         ON ZNSLS004.T$ORNO$C = WHINH431.T$WORN
        AND ZNSLS004.T$PONO$C = WHINH431.T$WPON
-	
+
  LEFT JOIN BAANDB.TZNSLS401601@PLN01 ZNSLS401
         ON ZNSLS401.T$NCIA$C = ZNSLS004.T$NCIA$C
        AND ZNSLS401.T$UNEG$C = ZNSLS004.T$UNEG$C
@@ -111,13 +116,13 @@ INNER JOIN WMWHSE9.ORDERS SHO
        AND ZNSLS401.T$SQPD$C = ZNSLS004.T$SQPD$C
        AND ZNSLS401.T$ENTR$C = ZNSLS004.T$ENTR$C
        AND ZNSLS401.T$SEQU$C = ZNSLS004.T$SEQU$C
-	
+
  LEFT JOIN BAANDB.TZNSLS400601@PLN01 ZNSLS400
         ON ZNSLS400.T$NCIA$C = ZNSLS004.T$NCIA$C
        AND ZNSLS400.T$UNEG$C = ZNSLS004.T$UNEG$C
        AND ZNSLS400.T$PECL$C = ZNSLS004.T$PECL$C
        AND ZNSLS400.T$SQPD$C = ZNSLS004.T$SQPD$C
-	
+
  LEFT JOIN ( select A.T$NCIA$C,
                     A.T$UNEG$C,
                     A.T$PECL$C,
@@ -137,27 +142,24 @@ INNER JOIN WMWHSE9.ORDERS SHO
        AND ZNSLS410.T$PECL$C = ZNSLS004.T$PECL$C
        AND ZNSLS410.T$SQPD$C = ZNSLS004.T$SQPD$C
        AND ZNSLS410.T$ENTR$C = ZNSLS004.T$ENTR$C
-	
+
  LEFT JOIN BAANDB.TZNFMD630601@PLN01 ZNFMD630
         ON ZNFMD630.T$ORNO$C = WHINH431.T$WORN
-		
+
  LEFT JOIN BAANDB.TZNFMD001601@PLN01 ZNFMD001
         ON ZNFMD001.T$FILI$C = ZNFMD630.T$FILI$C
-		
+
  LEFT JOIN BAANDB.TZNINT002601@PLN01 ZNINT002
         ON ZNINT002.T$NCIA$C = ZNSLS401.T$NCIA$C
        AND ZNINT002.T$UNEG$C = ZNSLS401.T$UNEG$C
-	
- LEFT JOIN BAANDB.TZNSLS002601@PLN01 ZNSLS002
-        ON ZNSLS002.T$TPEN$C = ZNSLS401.T$ITPE$C
-		
+
  LEFT JOIN BAANDB.TTCMCS080601@PLN01 TCMCS080
         ON TCMCS080.T$CFRW = ZNFMD630.T$CFRW$C
-		
+
 INNER JOIN ENTERPRISE.CODELKUP CL
         ON UPPER(CL.UDF1) = SHO.WHSEID
        AND CL.LISTNAME = 'SCHEMA'
-	
+
  LEFT JOIN ( select znfmd062.t$creg$c,
                     znfmd062.t$cfrw$c,
                     znfmd062.t$cono$c,
@@ -173,28 +175,19 @@ INNER JOIN ENTERPRISE.CODELKUP CL
         ON znfmd061.t$cfrw$c = znfmd630.t$cfrw$c
        AND znfmd061.t$cono$c = znfmd630.t$cono$c
        AND znfmd061.t$creg$c = znfmd062.t$creg$c
-	
+
 WHERE ZNSLS410.T$DTOC$C IS NOT NULL
   AND OXV.NOTES1 IS NOT NULL
   AND OXV.UDF1 = 'SHIPPINGID'
-	
---  AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ZNSLS410.T$DTOC$C,
---      'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
---        AT time zone 'America/Sao_Paulo') AS DATE))
---      Between :DataETRDe
---          AND :DataETRAte
---  AND NVL(TCMCS080.t$cfrw, 'N/A') IN (+ Replace(('+ JOIN(Parameters!Transportadora.Value, ',) + '),,,,') + )
---  AND ZNSLS401.T$ITPE$C IN (+ JOIN(Parameters!TipoEntrega.Value, , ) + )
---  AND ZNSLS401.T$UNEG$C IN (+ JOIN(Parameters!UnidNegocio.Value, , ) + ) 
-  
---  AND NVL(TCMCS080.t$cfrw, 'N/A') IN (:Transportadora)
---  AND TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ZNSLS410.T$DTOC$C, 
---              'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT') 
---                AT time zone 'America/Sao_Paulo') AS DATE))
---      BETWEEN :DataETRDe
---          AND :DataETRAte
---  AND ZNSLS401.T$ITPE$C IN (:TipoEntrega)
-    
+
+  AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ZNSLS410.T$DTOC$C,
+              'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                AT time zone 'America/Sao_Paulo') AS DATE))
+      Between :DataETRDe
+          AND :DataETRAte
+  AND NVL(TCMCS080.t$cfrw, 'N/A') IN (:Transportadora)
+  AND ZNSLS401.T$UNEG$C IN (:UnidNegocio)
+
 ORDER BY DT_ETR
 
 
@@ -213,6 +206,7 @@ ORDER BY DT_ETR
 "     ZNSLS401.T$PECL$C      PEDIDO,  " &
 "     SHO.INVOICENUMBER      NOTA,  " &
 "     SHO.LANE               SERIE,  " &
+"     ZNFMD630.T$ORNO$C      ORDEM_VENDA,  " &
 "     OXV.NOTES1             ETIQUETA,  " &
 "     ZNSLS401.T$NOME$C      CLIENTE,  " &
 "     ZNSLS401.T$ICLE$C      CPF_CNPJ,  " &
@@ -224,10 +218,7 @@ ORDER BY DT_ETR
 "     ZNSLS401.T$TELE$C      TELEFONE_1,  " &
 "     ZNSLS401.T$TE1E$C      TELEFONE_2,  " &
 "     ZNSLS401.T$TE2E$C      TELEFONE_3,  " &
-"     ZNSLS410.T$CONO$C      CONTRATO,  " &
 "     SHD.ORIGINALQTY        QTD_VOL,  " &
-"     ZNSLS401.T$ITPE$C      ID_TIPO_ENTREGA,  " &
-"     ZNSLS002.T$DSCA$C      DESCR_TIPO_ENTREGA,  " &
 "     SHD.PRODUCT_WEIGHT     PESO,  " &
 "     SHD.PRODUCT_CUBE       VOLUME,  " &
 "     ZNSLS401.T$VLUN$C *  " &
@@ -268,9 +259,16 @@ ORDER BY DT_ETR
 "                   AT time zone 'America/Sao_Paulo') AS DATE)  " &
 "     END                    DT_CORRIGIDA,  " &
 "  " &
+"     CASE WHEN TRUNC(znfmd630.t$dtpe$c) <= TO_DATE('01/01/1970','DD/MM/YYYY')  " &
+"            THEN NULL  " &
+"          ELSE   CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd630.t$dtpe$c, 'DD-MON-YYYY HH24:MI:SS'),  " &
+"                   'DD-MON-YYYY HH24:MI:SS'), 'GMT') AT time zone 'America/Sao_Paulo') AS DATE)  " &
+"     END                    DT_PREVISTA,  " &
+"  " &
 "     ZNFMD630.T$NCAR$C      CARGA,  " &
 "     znfmd062.t$creg$c      CAPITAL_INTERIOR,  " &
 "     znfmd061.t$dzon$c      REGIAO,  " &
+"  " &
 "     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ZNSLS410.T$DTOC$C,  " &
 "       'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  " &
 "         AT time zone 'America/Sao_Paulo') AS DATE)  " &
@@ -289,24 +287,24 @@ ORDER BY DT_ETR
 "            THEN 'Noite'  " &
 "          ELSE Null  " &
 "     END                    PERIODO  " &
-" 	  " &
+"  " &
 " FROM       " + Parameters!Compania.Value + ".ORDERDETAIL SHD  " &
 "  " &
 " INNER JOIN " + Parameters!Compania.Value + ".ORDERDETAILXVAS OXV  " &
 "         ON OXV.ORDERKEY = SHD.ORDERKEY  " &
 "        AND OXV.ORDERLINENUMBER = SHD.ORDERLINENUMBER  " &
-" 	  " &
+"  " &
 " INNER JOIN " + Parameters!Compania.Value + ".ORDERS SHO  " &
 "         ON SHO.ORDERKEY = SHD.ORDERKEY  " &
-" 		  " &
+"  " &
 "  LEFT JOIN BAANDB.TWHINH431" + IIF(Parameters!Compania.Value = "WMWHSE9","601","602") + "@PLN01 WHINH431  " &
 "         ON WHINH431.T$SHPM = SUBSTR(SHD.EXTERNORDERKEY,5,9)  " &
 "        AND TO_CHAR(WHINH431.T$PONO) = TO_CHAR(SHD.EXTERNLINENO*10)  " &
-" 	  " &
+"  " &
 "  LEFT JOIN BAANDB.TZNSLS004" + IIF(Parameters!Compania.Value = "WMWHSE9","601","602") + "@PLN01 ZNSLS004  " &
 "         ON ZNSLS004.T$ORNO$C = WHINH431.T$WORN  " &
 "        AND ZNSLS004.T$PONO$C = WHINH431.T$WPON  " &
-" 	  " &
+"  " &
 "  LEFT JOIN BAANDB.TZNSLS401" + IIF(Parameters!Compania.Value = "WMWHSE9","601","602") + "@PLN01 ZNSLS401  " &
 "         ON ZNSLS401.T$NCIA$C = ZNSLS004.T$NCIA$C  " &
 "        AND ZNSLS401.T$UNEG$C = ZNSLS004.T$UNEG$C  " &
@@ -314,13 +312,13 @@ ORDER BY DT_ETR
 "        AND ZNSLS401.T$SQPD$C = ZNSLS004.T$SQPD$C  " &
 "        AND ZNSLS401.T$ENTR$C = ZNSLS004.T$ENTR$C  " &
 "        AND ZNSLS401.T$SEQU$C = ZNSLS004.T$SEQU$C  " &
-" 	  " &
+"  " &
 "  LEFT JOIN BAANDB.TZNSLS400" + IIF(Parameters!Compania.Value = "WMWHSE9","601","602") + "@PLN01 ZNSLS400  " &
 "         ON ZNSLS400.T$NCIA$C = ZNSLS004.T$NCIA$C  " &
 "        AND ZNSLS400.T$UNEG$C = ZNSLS004.T$UNEG$C  " &
 "        AND ZNSLS400.T$PECL$C = ZNSLS004.T$PECL$C  " &
 "        AND ZNSLS400.T$SQPD$C = ZNSLS004.T$SQPD$C  " &
-" 	  " &
+"  " &
 "  LEFT JOIN ( select A.T$NCIA$C,  " &
 "                     A.T$UNEG$C,  " &
 "                     A.T$PECL$C,  " &
@@ -340,27 +338,24 @@ ORDER BY DT_ETR
 "        AND ZNSLS410.T$PECL$C = ZNSLS004.T$PECL$C  " &
 "        AND ZNSLS410.T$SQPD$C = ZNSLS004.T$SQPD$C  " &
 "        AND ZNSLS410.T$ENTR$C = ZNSLS004.T$ENTR$C  " &
-" 	  " &
+"  " &
 "  LEFT JOIN BAANDB.TZNFMD630" + IIF(Parameters!Compania.Value = "WMWHSE9","601","602") + "@PLN01 ZNFMD630  " &
 "         ON ZNFMD630.T$ORNO$C = WHINH431.T$WORN  " &
-" 		  " &
+"  " &
 "  LEFT JOIN BAANDB.TZNFMD001" + IIF(Parameters!Compania.Value = "WMWHSE9","601","602") + "@PLN01 ZNFMD001  " &
 "         ON ZNFMD001.T$FILI$C = ZNFMD630.T$FILI$C  " &
-" 		  " &
+"  " &
 "  LEFT JOIN BAANDB.TZNINT002" + IIF(Parameters!Compania.Value = "WMWHSE9","601","602") + "@PLN01 ZNINT002  " &
 "         ON ZNINT002.T$NCIA$C = ZNSLS401.T$NCIA$C  " &
 "        AND ZNINT002.T$UNEG$C = ZNSLS401.T$UNEG$C  " &
-" 	  " &
-"  LEFT JOIN BAANDB.TZNSLS002" + IIF(Parameters!Compania.Value = "WMWHSE9","601","602") + "@PLN01 ZNSLS002  " &
-"         ON ZNSLS002.T$TPEN$C = ZNSLS401.T$ITPE$C  " &
-" 		  " &
+"  " &
 "  LEFT JOIN BAANDB.TTCMCS080" + IIF(Parameters!Compania.Value = "WMWHSE9","601","602") + "@PLN01 TCMCS080  " &
 "         ON TCMCS080.T$CFRW = ZNFMD630.T$CFRW$C  " &
-" 		  " &
+"  " &
 " INNER JOIN ENTERPRISE.CODELKUP CL  " &
 "         ON UPPER(CL.UDF1) = SHO.WHSEID  " &
 "        AND CL.LISTNAME = 'SCHEMA'  " &
-" 	  " &
+"  " &
 "  LEFT JOIN ( select znfmd062.t$creg$c,  " &
 "                     znfmd062.t$cfrw$c,  " &
 "                     znfmd062.t$cono$c,  " &
@@ -376,17 +371,18 @@ ORDER BY DT_ETR
 "         ON znfmd061.t$cfrw$c = znfmd630.t$cfrw$c  " &
 "        AND znfmd061.t$cono$c = znfmd630.t$cono$c  " &
 "        AND znfmd061.t$creg$c = znfmd062.t$creg$c  " &
-" 	  " &
+"  " &
 " WHERE ZNSLS410.T$DTOC$C IS NOT NULL  " &
 "   AND OXV.NOTES1 IS NOT NULL  " &
 "   AND OXV.UDF1 = 'SHIPPINGID'  " &
+"  " &
 "   AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(ZNSLS410.T$DTOC$C,  " &
-"       'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  " &
-"         AT time zone 'America/Sao_Paulo') AS DATE))  " &
+"               'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  " &
+"                 AT time zone 'America/Sao_Paulo') AS DATE))  " &
 "       Between :DataETRDe  " &
 "           AND :DataETRAte  " &
 "   AND NVL(TCMCS080.t$cfrw, 'N/A') IN (" + Replace(("'" + JOIN(Parameters!Transportadora.Value, "',") + "'"),",",",'") + ")  " &
-"   AND ZNSLS401.T$ITPE$C IN (" + JOIN(Parameters!TipoEntrega.Value, ", ") + ") " &
 "   AND ZNSLS401.T$UNEG$C IN (" + JOIN(Parameters!UnidNegocio.Value, ", ") + ") " &
 "  " &
 " ORDER BY DT_ETR  "
+
