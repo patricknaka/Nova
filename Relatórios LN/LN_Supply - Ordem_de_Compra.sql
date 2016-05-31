41f44bc5ee61ce2cbd60b1c2eed0f84ccf2d9be6
YuriFialh0 <yuri.fialho@cnova.com>
2016-05-31 15:56:06 -0300

    Atualização de query(PRD)

--- Relatórios LN/LN_Supply - Ordem_de_Compra.sql
+++ Relatórios LN/LN_Supply - Ordem_de_Compra.sql
@@ -1,24 +1,30 @@
-SELECT
-  ( select CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(MIN(tdpur450.t$trdt), 
-             'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-               AT time zone 'America/Sao_Paulo') AS DATE) 
-      from baandb.ttdpur450301 tdpur450
-     where tdpur450.t$orno = tdpur400.t$orno) 
+SELECT 
+  CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdpur450.t$trdt, 
+    'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+      AT time zone 'America/Sao_Paulo') AS DATE)
                                        DATA_GERACAO,
+  tdpur450.t$logn                      LOGIN,
+									   
   tccom130.t$fovn$l                    CNPJ_FORNECEDOR,
   tccom130.t$nama                      NOME_FORNECEDOR,  
   tcemm030.t$euca                      NUME_FILIAL,
   tdpur400.t$orno                      NUME_ORDEM,
+  tdrec940.t$fire$l                    REFE_FISCAL,
+  tdpur400.t$cotp                      COD_TIPO_ORDEM_COMPRA,
+  tdpur094.t$dsca                      DSC_TIPO_ORDEM_COMPRA,
   tcmcs041.t$dsca                      TIPO_GER,
   Trim(tdpur401.t$item)                NUME_ITEM,
   tcibd001.t$dsca                      DESC_ITEM,  
   tcibd001.t$citg                      NUME_GRUPO_ITEM,  
   tcmcs023.t$dsca                      DESC_GRUPO_ITEM,  
   
-  CASE WHEN tdpur401.t$fire = 1 THEN tdpur401.t$qoor - tdpur401.t$qidl 
+  CASE WHEN tdpur401.t$clyn = 1 
+         THEN tdpur401.t$qoor
        ELSE 0 
-   END                                 QTDE_CANCELADA,
-   
+  END                                  QTDE_CANCELADA,
+  tdrec947.t$qnty$l                    QTDE_RECEBIDA,
+  tdpur401.t$disc$1                    PERCE_DESCONTO,
+  tdpur401.t$ldam$1                    VALOR_DESCONTO,
   tdpur401.t$qibo                      QTDE_REPOSICAO,
   tdpur401.t$pric                      PRECO_UNITARIO,
   tdpur401.t$qoor                      QTDE_ORDENADA,
@@ -40,60 +46,117 @@ SELECT
   Trim(tdrec941.t$item$l)              ITEM_NOTA,
   whwmd400.t$abcc                      CODE_ABC, 
   tdpur400.t$sorn                      ORDEM_PN_FORNECEDOR,
-  ( SELECT tdpur450.t$LOGN 
-      FROM baandb.ttdpur450301 tdpur450 
-     WHERE tdpur450.t$orno = tdpur400.t$orno 
-       AND tdpur450.t$trdt  = ( select min(a.t$trdt) 
-                                  from baandb.ttdpur450301 a 
-                                 where a.t$orno = tdpur450.t$orno) 
-                                   and ROWNUM = 1 )
-                                       LOGIN,
-
   tdpur401.t$crcd                      LIN_RAZAO_ALTERAC,
-  tdpur401.t$ctcd                      LIN_TIPO_ALTERAC
+  tdpur401.t$ctcd                      LIN_TIPO_ALTERAC,
+  
+  tdpur400.t$hdst                      COD_STATUS_PEDIDO,
+  StatusPedido.DESCR                   DSC_STATUS_PEDIDO
 
-FROM     baandb.ttccom130301  tccom130,
-         baandb.ttcemm030301  tcemm030,
-         baandb.ttcemm124301  tcemm124,
-         baandb.ttdpur400301  tdpur400
-  
-LEFT JOIN baandb.ttcmcs041301  tcmcs041  
-       ON tcmcs041.t$cdec = tdpur400.t$cdec, 
-          
-          baandb.ttdpur401301 tdpur401
- 
-LEFT JOIN baandb.ttdrec947301  tdrec947  
-       ON tdrec947.t$oorg$l = 80 
-      AND tdrec947.t$orno$l = tdpur401.t$orno
-      AND tdrec947.t$pono$l = tdpur401.t$pono
-      AND tdrec947.t$seqn$l = tdpur401.t$sqnb
-  
-LEFT JOIN baandb.ttdrec940301  tdrec940  
-       ON tdrec940.t$fire$l = tdrec947.t$fire$l
+FROM       baandb.ttdpur400301 tdpur400
+
+INNER JOIN baandb.ttdpur401301 tdpur401
+        ON tdpur401.t$orno = tdpur400.t$orno
+        
+INNER JOIN baandb.ttcibd001301 tcibd001
+        ON tcibd001.t$item = tdpur401.t$item
+
+INNER JOIN ( select tdpur450.t$orno,
+                    tdpur450.t$logn,
+                    tdpur450.t$trdt
+               from baandb.ttdpur450301 tdpur450
+              where tdpur450.t$trdt  = ( select min(a.t$trdt) 
+                                           from baandb.ttdpur450301 a 
+                                          where a.t$orno = tdpur450.t$orno 
+                                            and rownum = 1 ) ) tdpur450
+        ON tdpur450.t$orno = tdpur400.t$orno
+
+ LEFT JOIN ( select rec947.t$orno$l,
+                    rec947.t$pono$l,
+                    rec947.t$seqn$l,
+                    rec947.t$oorg$l,
+                    rec947.t$fire$l, 
+                    rec947.t$line$l,
+                    rec947.t$qnty$l
+               from baandb.ttdrec947301 rec947
+              where rec947.t$oorg$l = 80 ) tdrec947 --Ordem de Compra 
+        ON tdrec947.t$orno$l = tdpur401.t$orno
+       AND tdrec947.t$pono$l = tdpur401.t$pono
+       AND tdrec947.t$seqn$l = tdpur401.t$sqnb
    
-LEFT JOIN baandb.ttdrec941301 tdrec941  
-       ON tdrec941.t$fire$l = tdrec947.t$fire$l 
-      AND tdrec941.t$line$l = tdrec947.t$line$l,  
+ LEFT JOIN baandb.ttdrec940301 tdrec940
+        ON tdrec940.t$fire$l = tdrec947.t$fire$l
+    
+ LEFT JOIN baandb.ttdrec941301 tdrec941
+        ON tdrec941.t$fire$l = tdrec947.t$fire$l 
+       AND tdrec941.t$line$l = tdrec947.t$line$l
   
-          baandb.ttcibd001301 tcibd001   
-		  
-LEFT JOIN baandb.twhwmd400301 whwmd400  
-       ON whwmd400.t$item = tcibd001.t$item
+ LEFT JOIN baandb.ttdpur094301 tdpur094
+        ON tdpur094.t$potp = tdpur400.t$cotp
+        
+ LEFT JOIN baandb.ttcemm124301 tcemm124
+        ON tcemm124.t$cwoc = tdpur400.t$cofc 
+       AND tcemm124.t$dtyp = 2
+
+ LEFT JOIN baandb.ttcemm030301 tcemm030
+        ON tcemm030.t$eunt = tcemm124.t$grid 
+
+ LEFT JOIN baandb.ttccom130301 tccom130
+        ON tccom130.t$cadr = tdpur400.t$sfad
   
-LEFT JOIN baandb.ttcmcs023301 tcmcs023
-       ON tcmcs023.t$citg = tcibd001.t$citg
-          
-WHERE tdpur400.t$orno = tdpur401.t$orno
-  AND tcibd001.t$item = tdpur401.t$item
-  AND tccom130.t$cadr = tdpur400.t$sfad
-  AND tcemm124.t$cwoc = tdpur400.t$cofc 
-  AND tcemm124.t$dtyp = 2
-  AND tcemm030.t$eunt = tcemm124.t$grid 
-  
-  AND tcemm030.t$euca = NVL(:Filial, tcemm030.t$euca)
-  AND Trunc( (SELECT CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(MIN(tdpur450.t$trdt), 
-                'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT') AT time zone 'America/Sao_Paulo') AS DATE) 
-                FROM baandb.ttdpur450301 tdpur450
-                WHERE tdpur450.t$orno = tdpur400.t$orno)) BETWEEN :DtGeraOCDe AND :DtGeraOCAte
-  AND tcibd001.t$csig = (CASE WHEN :Situacao = 'T' THEN tcibd001.t$csig ELSE :Situacao END)
+ LEFT JOIN baandb.ttcmcs041301 tcmcs041
+        ON tcmcs041.t$cdec = tdpur400.t$cdec
+
+ LEFT JOIN baandb.twhwmd400301 whwmd400
+        ON whwmd400.t$item = tcibd001.t$item
+   
+ LEFT JOIN baandb.ttcmcs023301 tcmcs023
+        ON tcmcs023.t$citg = tcibd001.t$citg
+		
+ LEFT JOIN ( SELECT d.t$cnst CODE,
+                    l.t$desc DESCR
+               FROM baandb.tttadv401000 d,      
+                    baandb.tttadv140000 l
+              WHERE d.t$cpac = 'td'      
+                AND d.t$cdom = 'pur.hdst'     
+                AND l.t$clan = 'p'
+                AND l.t$cpac = 'td'      
+                AND l.t$clab = d.t$za_clab      
+                AND rpad(d.t$vers,4) ||  
+                    rpad(d.t$rele,2) ||  
+                    rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||    
+                                                    rpad(l1.t$rele,2) ||    
+                                                    rpad(l1.t$cust,4))      
+                                           from baandb.tttadv401000 l1      
+                                          where l1.t$cpac = d.t$cpac 
+                                            and l1.t$cdom = d.t$cdom )      
+                AND rpad(l.t$vers,4) ||  
+                    rpad(l.t$rele,2) ||  
+                    rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||    
+                                                    rpad(l1.t$rele,2) ||    
+                                                    rpad(l1.t$cust,4))      
+                                           from baandb.tttadv140000 l1      
+                                          where l1.t$clab = l.t$clab 
+                                            and l1.t$clan = l.t$clan
+                                            and l1.t$cpac = l.t$cpac ) ) StatusPedido  
+      ON StatusPedido.CODE = tdpur400.t$hdst
+      
+WHERE tcibd001.t$citg != '001'
+  AND tdpur400.t$cotp != '200'
+
+  AND (          (:ValData = 0) 
+        OR (   ( (:ValData = 1) AND ( Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdpur450.t$trdt, 
+                                              'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                                                AT time zone 'America/Sao_Paulo') AS DATE)) = :DtGeraOCDe ) ) 
+            OR ( (:ValData = 2) AND ( Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdpur450.t$trdt, 
+                                              'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                                                AT time zone 'America/Sao_Paulo') AS DATE)) = :DtGeraOCAte ) ) 
+            OR ( (:ValData = 3) AND ( Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdpur450.t$trdt, 
+                                              'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                                                AT time zone 'America/Sao_Paulo') AS DATE)) 
+                                      Between :DtGeraOCDe
+                                          And :DtGeraOCAte ) ) ) )
+
+  AND NVL(Trim(tcibd001.t$csig), '000') IN (:Situacao)
   AND Trim(tcibd001.t$citg) IN (:GrupoItem)
+  AND tdpur400.t$hdst IN (:StatusPedido)
+  AND tcemm030.t$euca IN (:Filial)
