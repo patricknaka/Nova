69986adb0fe2c1ba876f03da91169a36141fdd28
marcos-matias <mmferreira@gmail.com>
2016-06-24 13:42:30 -0300

    SDP 1153676  Pedidos integrados com taxa do cartao
    Com base no relatório de pedidos integrados, solicito um relatório incluindo o valor da tarifa de cartão de crédito.

---
+++ Relatórios LN/LN_Contabilidade_Relatorio_Pedidos_Integrados_Taxa_Cartao.sql
@@ -0,0 +1,88 @@
+SELECT
+  DISTINCT
+    znsls400.t$ncia$c  CIA,
+    znsls400.t$uneg$c  UNID_NEGOCIO,
+    znsls400.t$pecl$c  PEDIDO,      
+    znsls400.t$sqpd$c  SEQ_PEDIDO,
+    znsls402.t$sequ$c  SEQ_PAGAMENTO,
+    znsls402.t$cccd$c  NUM_BANDEIRA,
+    nvl(zncmg009.t$desc$c, ' ')  DSC_BANDEIRA,
+    znsls402.t$idad$c  ID_ADQUIRENTE, 
+    znsls402.t$idmp$c  NUM_MEIO_PAGTO,  
+    zncmg007.t$desc$c  DSC_MEIO_PAGTO, 
+    nvl(TO_CHAR(zncmg010.t$trbd$c, '990.99'), ' ') 	TAXA_CARTAO,
+    znsls400.t$nomf$c  NOME_CLIENTE, 
+    znsls402.t$cpft$c  CPF_CLIENTE, 
+    znsls402.t$vlmr$c  VALOR_MEIO_PGTO,
+    znsls412.t$ttyp$c  TIPO_TRANSACAO,
+    znsls412.t$ninv$c  DOC_TRANSACAO,
+ 
+    CASE WHEN tfgld018.t$dcdt < TO_DATE('01/01/1990','DD/MM/YYYY') THEN NULL
+         ELSE tfgld018.t$dcdt END AS DATA_DOC
+ 
+FROM      baandb.tznsls400301 znsls400
+
+LEFT JOIN baandb.tznsls402301 znsls402 
+       ON znsls402.t$ncia$c = znsls400.t$ncia$c
+      AND znsls402.t$uneg$c = znsls400.t$uneg$c
+      AND znsls402.t$pecl$c = znsls400.t$pecl$c
+      AND znsls402.t$sqpd$c = znsls400.t$sqpd$c
+
+LEFT JOIN baandb.tznsls412301 znsls412 
+       ON znsls412.t$ncia$c = znsls402.t$ncia$c
+      AND znsls412.t$uneg$c = znsls402.t$uneg$c
+      AND znsls412.t$pecl$c = znsls402.t$pecl$c
+      AND znsls412.t$sqpd$c = znsls402.t$sqpd$c
+      AND znsls412.t$sequ$c = znsls402.t$sequ$c
+       
+LEFT JOIN baandb.ttfgld018301 tfgld018
+       ON tfgld018.t$ttyp = znsls412.t$ttyp$c
+      AND tfgld018.t$docn = znsls412.t$ninv$c
+
+LEFT JOIN baandb.tzncmg007301 zncmg007 
+--       ON zncmg007.t$mpgt$c = znsls402.t$idmp$c
+       ON zncmg007.t$mpgs$c = znsls402.t$idmp$c		-- buscar pelo codigo do site
+       
+LEFT JOIN baandb.tzncmg007301 zncmg007 
+--       ON zncmg007.t$mpgt$c = znsls402.t$idmp$c
+       ON zncmg007.t$mpgs$c = znsls402.t$idmp$c		-- buscar pelo codigo do site
+       
+LEFT JOIN baandb.tzncmg009301 zncmg009
+       ON zncmg009.t$band$c = znsls402.t$cccd$c
+
+LEFT JOIN	baandb.tzncmg008301 zncmg008			-- BP do banco
+	ON	zncmg008.t$adqs$c = znsls402.t$idad$c
+       AND	zncmg008.t$cias$c = znsls402.t$ncia$c
+
+LEFT JOIN (								-- data de inicio da vigencia
+		select	zncmg010_.t$band$c,
+				zncmg010_.t$adqu$c,
+				zncmg010_.t$cias$c,
+				max(zncmg010_.t$dtin$c) DATA_INICIO
+		from		baandb.tzncmg010301 zncmg010_		
+		group by 	zncmg010_.t$band$c, zncmg010_.t$adqu$c, zncmg010_.t$cias$c) zncmg010_dt
+on		zncmg010_dt.t$band$c = zncmg009.t$band$c
+and 		zncmg010_dt.t$adqu$c = zncmg008.t$adqu$c
+and		zncmg010_dt.t$cias$c = znsls402.t$ncia$c
+ 
+LEFT JOIN (								-- numero de parcelas
+		select	zncmg010_.t$band$c,
+				zncmg010_.t$adqu$c,
+				zncmg010_.t$cias$c,
+				zncmg010_.t$dtin$c,
+				min(zncmg010_.t$prct$c) NUM_PARC
+		from		baandb.tzncmg010301 zncmg010_		
+		group by 	zncmg010_.t$band$c, zncmg010_.t$adqu$c, zncmg010_.t$cias$c, zncmg010_.t$dtin$c) zncmg010_np
+on		zncmg010_np.t$band$c = zncmg009.t$band$c
+and 		zncmg010_np.t$adqu$c = zncmg008.t$adqu$c
+and		zncmg010_np.t$cias$c = znsls402.t$ncia$c
+and   	zncmg010_np.t$dtin$c = zncmg010_dt.DATA_INICIO
+
+LEFT JOIN baandb.tzncmg010301 zncmg010			
+on		zncmg010.t$band$c = zncmg009.t$band$c
+and 		zncmg010.t$adqu$c = zncmg008.t$adqu$c
+and		zncmg010.t$cias$c = znsls402.t$ncia$c
+and		zncmg010.t$prct$c = zncmg010_np.NUM_PARC
+and		zncmg010.t$dtin$c = zncmg010_dt.DATA_INICIO
+
+WHERE NVL(znsls412.t$ttyp$c,' ') NOT IN ('LPJ', 'FAT', 'RWC')
