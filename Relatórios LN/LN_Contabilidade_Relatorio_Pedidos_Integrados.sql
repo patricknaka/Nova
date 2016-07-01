4c6c4a7534d039617308fbe2094056b7cfe6eac2
ronaldocs <ronaldo.cristiano@gmail.com>
2016-07-01 11:32:32 -0300

    Relatorio de Pedidos Integrados - SDP 1153676
    Inclusao da Taxa e do Valor calculado sob a taxa

--- Relatórios LN/LN_Contabilidade_Relatorio_Pedidos_Integrados.sql
+++ Relatórios LN/LN_Contabilidade_Relatorio_Pedidos_Integrados.sql
@@ -1,23 +1,30 @@
 SELECT
   DISTINCT
-    znsls400.t$ncia$c  CIA,
-    znsls400.t$uneg$c  UNID_NEGOCIO,
-    znsls400.t$pecl$c  PEDIDO,      
-    znsls400.t$sqpd$c  SEQ_PEDIDO,
-    znsls402.t$sequ$c  SEQ_PAGAMENTO,
-    znsls402.t$cccd$c  NUM_BANDEIRA,
-    zncmg009.t$desc$c  DSC_BANDEIRA,
-    znsls402.t$idad$c  ID_ADQUIRENTE, 
-    znsls402.t$idmp$c  NUM_MEIO_PAGTO,  
-    zncmg007.t$desc$c  DSC_MEIO_PAGTO, 
-    znsls400.t$nomf$c  NOME_CLIENTE, 
-    znsls402.t$cpft$c  CPF_CLIENTE, 
-    znsls402.t$vlmr$c  VALOR_MEIO_PGTO,
-    znsls412.t$ttyp$c  TIPO_TRANSACAO,
-    znsls412.t$ninv$c  DOC_TRANSACAO,
+    znsls400.t$ncia$c                                  CIA,
+    znsls400.t$uneg$c                                  UNID_NEGOCIO,
+    znsls400.t$pecl$c                                  PEDIDO,      
+    znsls400.t$sqpd$c                                  SEQ_PEDIDO,
+    znsls402.t$sequ$c                                  SEQ_PAGAMENTO,
+    znsls402.t$cccd$c                                  NUM_BANDEIRA,
+    nvl(zncmg009.t$desc$c, ' ')                        DSC_BANDEIRA,
+    znsls402.t$idad$c                                  ID_ADQUIRENTE, 
+    znsls402.t$idmp$c                                  NUM_MEIO_PAGTO,  
+    zncmg007.t$desc$c                                  DSC_MEIO_PAGTO, 
+    znsls400.t$nomf$c                                  NOME_CLIENTE, 
+    znsls402.t$cpft$c                                  CPF_CLIENTE, 
+    znsls412.t$ttyp$c                                  TIPO_TRANSACAO,
+    znsls412.t$ninv$c                                  DOC_TRANSACAO,
+    ROUND(zncmg010.t$trbd$c, 2)                        TAXA_CARTAO,
+    ABS(znsls402.t$vlmr$c)                             VALOR_MEIO_PGTO,
+    CASE WHEN zncmg010.t$trbd$c IS NULL
+           THEN NULL
+         ELSE ROUND((ABS(znsls402.t$vlmr$c) * zncmg010.t$trbd$c / 100), 2)
+    END                                                VALOR_TAXA_CARTAO,
  
-    CASE WHEN tfgld018.t$dcdt < TO_DATE('01/01/1990','DD/MM/YYYY') THEN NULL
-         ELSE tfgld018.t$dcdt END AS DATA_DOC
+    CASE WHEN tfgld018.t$dcdt < TO_DATE('01/01/1990','DD/MM/YYYY') 
+           THEN NULL
+         ELSE tfgld018.t$dcdt 
+    END                                                DATA_DOC
  
 FROM      baandb.tznsls400301 znsls400
 
@@ -39,9 +46,54 @@ LEFT JOIN baandb.ttfgld018301 tfgld018
       AND tfgld018.t$docn = znsls412.t$ninv$c
 
 LEFT JOIN baandb.tzncmg007301 zncmg007 
-       ON zncmg007.t$mpgt$c = znsls402.t$idmp$c
-  
+       ON zncmg007.t$mpgs$c = znsls402.t$idmp$c        -- buscar pelo codigo do site
+       
+LEFT JOIN baandb.tzncmg007301 zncmg007 
+       ON zncmg007.t$mpgs$c = znsls402.t$idmp$c        -- buscar pelo codigo do site
+       
 LEFT JOIN baandb.tzncmg009301 zncmg009
        ON zncmg009.t$band$c = znsls402.t$cccd$c
 
-WHERE NVL(znsls412.t$ttyp$c,' ') NOT IN ('LPJ', 'FAT', 'RWC')
+LEFT JOIN baandb.tzncmg008301 zncmg008                 -- BP do banco
+       ON zncmg008.t$adqs$c = znsls402.t$idad$c
+      AND zncmg008.t$cias$c = znsls402.t$ncia$c
+
+LEFT JOIN ( select zncmg010_.t$band$c,                 -- data de inicio da vigencia
+                   zncmg010_.t$adqu$c,
+                   zncmg010_.t$cias$c,
+                   max(zncmg010_.t$dtin$c) DATA_INICIO
+              from baandb.tzncmg010301 zncmg010_        
+          group by zncmg010_.t$band$c, 
+                   zncmg010_.t$adqu$c, 
+                   zncmg010_.t$cias$c ) zncmg010_dt
+      ON zncmg010_dt.t$band$c = zncmg009.t$band$c
+     AND zncmg010_dt.t$adqu$c = zncmg008.t$adqu$c
+     AND zncmg010_dt.t$cias$c = znsls402.t$ncia$c
+ 
+LEFT JOIN ( select zncmg010_.t$band$c,                 -- numero de parcelas
+                   zncmg010_.t$adqu$c,
+                   zncmg010_.t$cias$c,
+                   zncmg010_.t$dtin$c,
+                   min(zncmg010_.t$prct$c) NUM_PARC
+              from baandb.tzncmg010301 zncmg010_        
+          group by zncmg010_.t$band$c, 
+                   zncmg010_.t$adqu$c, 
+                   zncmg010_.t$cias$c, 
+                   zncmg010_.t$dtin$c ) zncmg010_np
+       ON zncmg010_np.t$band$c = zncmg009.t$band$c
+      AND zncmg010_np.t$adqu$c = zncmg008.t$adqu$c
+      AND zncmg010_np.t$cias$c = znsls402.t$ncia$c
+      AND zncmg010_np.t$dtin$c = zncmg010_dt.DATA_INICIO
+
+LEFT JOIN baandb.tzncmg010301 zncmg010            
+       ON zncmg010.t$band$c = zncmg009.t$band$c
+      AND zncmg010.t$adqu$c = zncmg008.t$adqu$c
+      AND zncmg010.t$cias$c = znsls402.t$ncia$c
+      AND zncmg010.t$prct$c = zncmg010_np.NUM_PARC
+      AND zncmg010.t$dtin$c = zncmg010_dt.DATA_INICIO
+
+WHERE NVL(Trim(znsls412.t$ttyp$c), '000') in (:Transacao)
+  AND NVL(tfgld018.t$dcdt, :DataDocDe) 
+      Between :DataDocDe 
+          And :DataDocAte
+  AND NVL(znsls412.t$ttyp$c,' ') NOT IN ('LPJ', 'FAT', 'RWC')
