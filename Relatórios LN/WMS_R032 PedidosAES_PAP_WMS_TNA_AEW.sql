3f9ba85aa7951750e16e69640f337296bf457138
rarruda1988 <rarruda1988@gmail.com>
2016-07-14 09:01:23 -0300

    Update WMS_R032 PedidosAES_PAP_WMS_TNA_AEW.sql

--- Relatórios LN/WMS_R032 PedidosAES_PAP_WMS_TNA_AEW.sql
+++ Relatórios LN/WMS_R032 PedidosAES_PAP_WMS_TNA_AEW.sql
@@ -1,4 +1,6 @@
-SELECT
+--b2c
+
+SELECT 
   DISTINCT
     wmsCODE.UDF1                         FILIAL,
     wmsCODE.UDF2                         DSC_PLANTA,
@@ -8,34 +10,34 @@ SELECT
     TDSLS400.T$CBRN                      ID_UNEG,
     TCMCS031.T$DSCA                      DESCR_UNEG,
     NVL(TDSLS420.T$HREA,
-        CASE WHEN (    CISLI245.T$FIRE$L IS NULL
-                   AND OWMS.ORDERKEY IS NULL )
-               THEN 'NEN'
-             ELSE NULL
+        CASE WHEN (    CISLI245.T$FIRE$L IS NULL 
+                   AND OWMS.ORDERKEY IS NULL ) 
+               THEN 'NEN' 
+             ELSE NULL 
          END)                            ID_ULT_PONTO,
     NVL(TDSLS090.T$DSCA,
-        CASE WHEN (    CISLI245.T$FIRE$L IS NULL
-                   AND OWMS.ORDERKEY IS NULL)
-               THEN 'NÃO ENVIADO PARA WMS E NF NÃO EMITIDA'
-             ELSE NULL
+        CASE WHEN (    CISLI245.T$FIRE$L IS NULL 
+                   AND OWMS.ORDERKEY IS NULL) 
+               THEN 'NÃO ENVIADO PARA WMS E NF NÃO EMITIDA' 
+             ELSE NULL 
          END)                            DESCR_ULT_PONTO,
-    TRIM(ZNSLS004.T$ITEM$C)              ITEM,
+    TRIM(ZNSLS004.T$ITEM$C)              ITEM, 
     SYSDATE -
     ( SELECT MAX(A.T$DTBL)
-        FROM BAANDB.TTDSLS421601 A
+        FROM BAANDB.TTDSLS421301 A
        WHERE A.T$ORNO = TDSLS420.T$ORNO
          AND A.T$PONO = TDSLS420.T$PONO
          AND A.T$SQNB = TDSLS420.T$SQNB
          AND A.T$CSQN = TDSLS420.T$CSQN
          AND A.T$HREA = TDSLS420.T$HREA )  TEMPO_STAUS
+     
+FROM       BAANDB.TTDSLS400301 TDSLS400
 
-FROM       BAANDB.TTDSLS400601 TDSLS400
-
-INNER JOIN BAANDB.TTDSLS401601 TDSLS401
+INNER JOIN BAANDB.TTDSLS401301 TDSLS401
         ON TDSLS401.T$ORNO = TDSLS400.T$ORNO
-
-INNER JOIN BAANDB.TTDSLS094601 TDSLS094
-        ON TDSLS094.T$SOTP = TDSLS400.T$SOTP
+  
+INNER JOIN BAANDB.TTDSLS094301 TDSLS094
+        ON TDSLS094.T$SOTP = TDSLS400.T$SOTP  
 
  LEFT JOIN ( select A.T$NCIA$C,
                     A.T$UNEG$C,
@@ -46,8 +48,8 @@ INNER JOIN BAANDB.TTDSLS094601 TDSLS094
                     B.T$ITEM$C,
                     B.T$CWRL$C,
                     MIN(B.T$DTAP$C) T$DTAP$C
-               from BAANDB.TZNSLS004601 A
-         inner join BAANDB.TZNSLS401601 B
+               from BAANDB.TZNSLS004301 A
+         inner join BAANDB.TZNSLS401301 B 
                  on B.T$NCIA$C = A.T$NCIA$C
                 and B.T$UNEG$C = A.T$UNEG$C
                 and B.T$PECL$C = A.T$PECL$C
@@ -63,50 +65,60 @@ INNER JOIN BAANDB.TTDSLS094601 TDSLS094
                     B.T$ITEM$C,
                     B.T$CWRL$C ) ZNSLS004
         ON ZNSLS004.T$ORNO$C = TDSLS400.T$ORNO
-
+    
  LEFT JOIN BAANDB.TTCMCS031301 TCMCS031
         ON TCMCS031.T$CBRN = TDSLS400.T$CBRN
-
- LEFT JOIN BAANDB.TTDSLS420601 TDSLS420
+    
+ LEFT JOIN BAANDB.TTDSLS420301 TDSLS420
         ON TDSLS420.T$ORNO = TDSLS401.T$ORNO
-
+    
  LEFT JOIN BAANDB.TTDSLS090301 TDSLS090
         ON TDSLS090.T$HREA = TDSLS420.T$HREA
-
+ 
+INNER JOIN BAANDB.TTCEMM300301 TCEMM300
+        ON TCEMM300.T$COMP = 301
+       AND TCEMM300.T$TYPE = 20
+       AND TRIM(TCEMM300.T$CODE) = TRIM(TDSLS401.T$CWAR)
+ 
 INNER JOIN ( select A.LONG_VALUE,
                     UPPER(A.UDF1) UDF1,
                     A.UDF2
                from ENTERPRISE.CODELKUP@DL_LN_WMS A
               where A.LISTNAME = 'SCHEMA') wmsCODE
-        ON wmsCODE.UDF1 = :Armazem
-
+        ON wmsCODE.LONG_VALUE = TCEMM300.T$LCTN
+ 
  LEFT JOIN ( select MAX(B.ORDERKEY) ORDERKEY,
                     B.REFERENCEDOCUMENT
-               from + Parameters!Armazem.Value + .ORDERS@DL_LN_WMS B
-           group by B.REFERENCEDOCUMENT ) OWMS
+               from WMWHSE5.ORDERS@DL_LN_WMS B
+           group by B.REFERENCEDOCUMENT ) OWMS 
         ON OWMS.REFERENCEDOCUMENT = TDSLS400.T$ORNO
-
- LEFT JOIN BAANDB.TCISLI245601 CISLI245
-        ON CISLI245.T$SLCP = 601
+  
+ LEFT JOIN BAANDB.TCISLI245301 CISLI245
+        ON CISLI245.T$SLCP = 301
        AND CISLI245.T$ORTP = 1
        AND CISLI245.T$KOOR = 3
        AND CISLI245.T$SLSO = TDSLS401.T$ORNO
        AND CISLI245.T$PONO = TDSLS401.T$PONO
        AND CISLI245.T$SQNB = TDSLS401.T$SQNB
-       AND CISLI245.T$FIRE$L != ' '
-
-WHERE (   TDSLS420.T$HREA IN ('AES', 'PRD', 'TNA', 'AEW')
-       OR CASE WHEN (CISLI245.T$FIRE$L IS NULL AND OWMS.ORDERKEY IS NULL)
-                 THEN 'NEN'
-               ELSE   NULL
-           END = 'NEN' )
-  AND TDSLS400.T$HDST NOT IN (30, 35)
-  AND TDSLS094.T$RETO = 2
-  AND TDSLS401.T$CLYN = 2
+       AND CISLI245.T$FIRE$L != ' '    
+  
+ WHERE (   TDSLS420.T$HREA IN ('AES', 'PRD', 'TNA', 'AEW') 
+        OR CASE WHEN (CISLI245.T$FIRE$L IS NULL AND OWMS.ORDERKEY IS NULL) 
+                  THEN 'NEN' 
+                ELSE   NULL 
+            END = 'NEN' )
+   AND TDSLS400.T$HDST NOT IN (30, 35)
+   AND TDSLS094.T$RETO = 2
+   AND TDSLS401.T$CLYN = 2
+   AND ((:Armazem = 'AAA') OR (wmsCODE.UDF1 = :Armazem))
+   
+------------------------------------------------------------------------------------------------------------------
+--  ORDENS DE ARMAZEM
+------------------------------------------------------------------------------------------------------------------
 
 UNION
 
-SELECT
+SELECT 
   DISTINCT
     wmsCODE.UDF1                             FILIAL,
     wmsCODE.UDF2                             DSC_PLANTA,
@@ -117,24 +129,29 @@ SELECT
     NULL                                     DESCR_UNEG,
     'NEN'                                    ID_ULT_PONTO,
     'NÃO ENVIADO PARA WMS E NF NÃO EMITIDA'  DESCR_ULT_PONTO,
-    TRIM(WHINH220.T$ITEM)                    ITEM,
+    TRIM(WHINH220.T$ITEM)                    ITEM, 
     SYSDATE -
     WHINH200.T$ODAT                          TEMPO_STAUS
+  
+FROM       BAANDB.TWHINH200301 WHINH200
 
-FROM  BAANDB.TWHINH200601 WHINH200
-
-INNER JOIN BAANDB.TWHINH220601 WHINH220
+INNER JOIN BAANDB.TWHINH220301 WHINH220 
         ON WHINH220.T$OORG = WHINH200.T$OORG
        AND WHINH220.T$ORNO = WHINH200.T$ORNO
 
+INNER JOIN BAANDB.TTCEMM300301 TCEMM300
+        ON TCEMM300.T$COMP = 301
+       AND TCEMM300.T$TYPE = 20
+       AND TRIM(TCEMM300.T$CODE) = TRIM(WHINH220.T$CWAR)
+ 
 INNER JOIN ( select A.LONG_VALUE,
                     UPPER(A.UDF1) UDF1,
                     A.UDF2
                from ENTERPRISE.CODELKUP@DL_LN_WMS A
               where A.LISTNAME = 'SCHEMA') wmsCODE
-        ON wmsCODE.UDF1 = :Armazem
+        ON wmsCODE.LONG_VALUE = TCEMM300.T$LCTN
 
-
+           
  LEFT JOIN(       Select  3 koor,  1 oorg From DUAL
             Union Select  7 koor,  2 oorg From DUAL
             Union Select 34 koor,  3 oorg From DUAL
@@ -160,16 +177,17 @@ INNER JOIN ( select A.LONG_VALUE,
             Union Select 60 koor, 90 oorg From DUAL
             Union Select 21 koor, 61 oorg From DUAL ) KOOR2OORG
         ON KOOR2OORG.OORG = WHINH200.T$OORG
-
- LEFT JOIN BAANDB.TCISLI245601 CISLI245
-        ON CISLI245.T$SLCP = 601
+                      
+ LEFT JOIN BAANDB.TCISLI245301 CISLI245 
+        ON CISLI245.T$SLCP = 301
        AND CISLI245.T$ORTP = 2
        AND CISLI245.T$KOOR = KOOR2OORG.KOOR
        AND CISLI245.T$SLSO = WHINH220.T$ORNO
        AND CISLI245.T$OSET = WHINH200.T$OSET
        AND CISLI245.T$PONO = WHINH220.T$PONO
 
- WHERE WHINH220.T$WMSS = 10
-   AND CISLI245.T$FIRE$L  IS NULL
-   AND WHINH200.T$OORG != 1
-   AND WHINH220.T$CNCL != 1  
+WHERE  WHINH220.T$WMSS = 10
+  AND CISLI245.T$FIRE$L  IS NULL
+  AND WHINH200.T$OORG != 1
+  AND WHINH220.T$CNCL != 1
+  AND ((:Armazem = 'AAA') OR (wmsCODE.UDF1 = :Armazem))
