a6d3a26859bcd9e8ee06a5c54ca013d2f87f2928
rarruda1988 <rarruda1988@gmail.com>
2016-07-27 13:56:44 -0300

    Update LN_Gestão Financeira de Transporte - DBO_Etiquetas_Correios.sql
    De acordo com o SDP: 1166074 o relatório esta trazendo alguns pedidos sem etiquetas, sendo que no LN esta correto.
    Foi comentado o Join e a condição aonde o estava limitando os pedidos enviados para o correio.

--- Relatórios LN/LN_Gestão Financeira de Transporte - DBO_Etiquetas_Correios.sql
+++ Relatórios LN/LN_Gestão Financeira de Transporte - DBO_Etiquetas_Correios.sql
@@ -4,10 +4,17 @@ SELECT
 
 FROM       BAANDB.tznfmd630301 znfmd630
   
-INNER JOIN BAANDB.tznmcs086301 znmcs086
-        ON znfmd630.t$cfrw$c = znmcs086.t$cfrw$c
+/*INNER JOIN BAANDB.tznmcs086301 znmcs086
+        ON znfmd630.t$cfrw$c = znmcs086.t$cfrw$c  --comentando a condição abaixo o join não é mais necessário
        AND znfmd630.t$fili$c = znmcs086.t$bchc$c
   
-WHERE znmcs086.t$padr$c = 2
-AND ( (:EtiquetaTodas = 0) or (znfmd630.t$etiq$c IN (:Etiqueta) and (:EtiquetaTodas = 1)) )
-OR  ( (:EntregaTodas = 0) or (znfmd630.t$pecl$c IN (:Entrega) and (:EntregaTodas = 1)) )
+WHERE znmcs086.t$padr$c = 2  -- limita os pedidos que foram enviados para os correios
+*/
+WHERE
+     ( (:EtiquetaTodas = '-1') or (znfmd630.t$etiq$c IN (:Etiqueta) and (:EtiquetaTodas = '1')) )
+AND  ( (:EntregaTodas = '-1') or (znfmd630.t$pecl$c IN (:Entrega) and (:EntregaTodas = '1')) )
+AND  trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd630.T$DATE$C,
+              'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                AT time zone 'America/Sao_Paulo') AS DATE)) 
+between :DataNotaDe
+and :DataNotaAte
