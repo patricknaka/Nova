acb9235c73c95fa8e0d295cedf420c19bd6459c7
greicecavalcante <greice.cavalcante@cnova.com>
2016-10-17 15:58:21 -0200

    SDP_1180047
    Inclusão dos campos: "Número Endereço Cliente" e "Valor Total Nota Fiscal"

--- Relatórios LN/LN_Relatório_Expedição_Regional.sql
+++ Relatórios LN/LN_Relatório_Expedição_Regional.sql
@@ -1,55 +1,56 @@
-  select distinct
-  	      znfmd630.t$pecl$c		      ENTREGA,
-  	      cisli940.t$docn$l			  NOTA,
-  	      cisli940.t$seri$l			  SERIE,
-  	      znfmd630.t$ncar$c		      CARGA,
-  	      znfmd630.t$fili$c			  FILIAL,
-  	      (select CAST((from_tz(to_timestamp(to_char(znfmd640_ETR.t$date$c,
-  	      		    'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-  	      		    AT time zone 'America/Sao_Paulo') AS DATE)
-            from BAANDB.tznfmd640301 	znfmd640_ETR
-  	       where znfmd640_ETR.t$fili$c = znfmd630.t$fili$c
+   select distinct
+          znfmd630.t$pecl$c           ENTREGA,
+          cisli940.t$docn$l           NOTA,
+          cisli940.t$seri$l           SERIE,
+          znfmd630.t$ncar$c           CARGA,
+          znfmd630.t$fili$c           FILIAL,
+          (select CAST((from_tz(to_timestamp(to_char(znfmd640_ETR.t$date$c,
+                 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                 AT time zone 'America/Sao_Paulo') AS DATE)
+            from BAANDB.tznfmd640301 znfmd640_ETR
+           where znfmd640_ETR.t$fili$c = znfmd630.t$fili$c
              and znfmd640_ETR.t$etiq$c = znfmd630.t$etiq$c
              and znfmd640_ETR.t$coct$c = 'ETR' )
-  	      					          DATA_EXPEDICAO,
+                                      DATA_EXPEDICAO,
                                 
-  	      case when znfmd630.t$dtpe$c <= '01/01/70'
-  	      then 	
-  	      	null	
-  	      else   	
-  	      	cast((from_tz(to_timestamp(to_char(znfmd630.t$dtpe$c, 'dd-mon-yyyy hh24:mi:ss'),
-  	      		'dd-mon-yyyy hh24:mi:ss'), 'gmt') at time zone 'america/sao_paulo') as date) 
-  	      end			               DATA_PROMETIDA,
+          case when znfmd630.t$dtpe$c <= '01/01/70'
+          then 
+             null
+          else   
+          cast((from_tz(to_timestamp(to_char(znfmd630.t$dtpe$c, 'dd-mon-yyyy hh24:mi:ss'),
+                'dd-mon-yyyy hh24:mi:ss'), 'gmt') at time zone 'america/sao_paulo') as date) 
+          end                         DATA_PROMETIDA,
             
-  	      case when znfmd630.t$dtco$c <= '01/01/70'
-  	      then 
-  	      	null
-                 else 
-  	      	cast((from_tz(to_timestamp(to_char(znfmd630.t$dtco$c, 'dd-mon-yyyy hh24:mi:ss'),
+          case when znfmd630.t$dtco$c <= '01/01/70'
+          then 
+             null
+          else 
+          cast((from_tz(to_timestamp(to_char(znfmd630.t$dtco$c, 'dd-mon-yyyy hh24:mi:ss'),
                         'dd-mon-yyyy hh24:mi:ss'), 'gmt') at time zone 'america/sao_paulo') as date)
-  	      end                  	      DATA_AJUSTADA,
-  	      tcmcs080.t$dsca			  TRANSPORTADORA,
-  	      tccom130.t$fovn$l  		  CNPJ,
-  	      trim(cisli941.t$item$l)     ITEM,
-  	      tcibd001.t$dsca			  ITEM_DSCA,
-  	      cisli941.T$PRIC$L           VALOR_ITEM,
-  	      --znfmd630.t$vlmr$c    	    VALOR_ITEM,
-  	      cisli941.t$dqua$l			  QTDE_ITEM,
-  	      whwmd400.t$hght 		      ALTURA,
-  	      whwmd400.t$wdth 		      LARGURA,
-  	      whwmd400.t$dpth 		      COMPRIMENTO,
-  	      tcibd001.t$wght			  PESO,
-  	      znfmd630.t$cnfe$c		      CHAVE_DANFE,
-  	      tccom139.t$dsca			  MUNICIPIO,
-  	      tccom130_cli.t$cste		  UF,
-  	      tccom130_cli.t$pstc		  CEP,
+          end                         DATA_AJUSTADA,
+          tcmcs080.t$dsca             TRANSPORTADORA,
+          tccom130.t$fovn$l           CNPJ,
+          trim(cisli941.t$item$l)     ITEM,
+          tcibd001.t$dsca             ITEM_DSCA,
+          cisli941.t$dqua$l           QTDE_ITEM,  
+          cisli941.T$PRIC$L           VALOR_ITEM,
+          --znfmd630.t$vlmr$c           VALOR_ITEM,
+          cisli940.t$amnt$l           VALOR_NOTA,
+          whwmd400.t$hght             ALTURA,
+          whwmd400.t$wdth             LARGURA,
+          whwmd400.t$dpth             COMPRIMENTO,
+          tcibd001.t$wght             PESO,
+          znfmd630.t$cnfe$c           CHAVE_DANFE,
+          tccom139.t$dsca             MUNICIPIO,
+          tccom130_cli.t$cste         UF,
+          tccom130_cli.t$pstc         CEP,
           znsls400.t$logf$c           ENDERECO_CLIENTE,
           znsls400.t$numf$c           NR_ENDERECO_CLIENTE,
-		  znsls400.t$baif$c           BAIRRO_CLIENTE,
+          znsls400.t$baif$c           BAIRRO_CLIENTE,
           znsls400.t$telf$c           TELEFONE1_CLIENTE,
-          znsls400.t$te1f$c           TELEFONE2_CLIENTE,
-		  cisli940.t$amnt$l           VALOR_NOTA
-	  
+          znsls400.t$te1f$c           TELEFONE2_CLIENTE
+
+  
      from baandb.tznfmd630301 znfmd630
 
 left join baandb.tcisli940301 cisli940
@@ -78,7 +79,7 @@ left join baandb.ttccom139301 tccom139
       and tccom139.t$cste = tccom130_cli.t$cste
       and tccom139.t$city = tccom130_cli.t$ccit
 
-left join baandb.ttdrec955301 tdrec955			
+left join baandb.ttdrec955301 tdrec955	
        on tdrec955.t$fire$l = cisli941.t$fire$l
       and tdrec955.t$line$l = cisli941.t$line$l
 
