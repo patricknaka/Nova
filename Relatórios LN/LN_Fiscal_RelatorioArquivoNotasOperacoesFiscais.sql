c2ebed4e56070beb438fa5d5f7deeb6cf460498f
julianphilippe <julian.philippe@outlook.com>
2016-10-26 10:12:15 -0200

    Update LN_Fiscal_RelatorioArquivoNotasOperacoesFiscais.sql

--- Relatórios LN/LN_Fiscal_RelatorioArquivoNotasOperacoesFiscais.sql
+++ Relatórios LN/LN_Fiscal_RelatorioArquivoNotasOperacoesFiscais.sql
@@ -1,8 +1,8 @@
 SELECT 
   DISTINCT 
-    301                CODE_CIA, 
-    tcemm122.T$grid    UNID_EMPRESARIAL, 
-    tccom130.t$fovn$L  NUME_FILIAL,
+  301                CODE_CIA, 
+  tcemm122.T$grid    UNID_EMPRESARIAL, 
+  tccom130.t$fovn$L  NUME_FILIAL,
                        
     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdrec940.t$date$l,  
       'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT') 
@@ -21,6 +21,10 @@ SELECT
     whinh300.t$recd$c  RECDOC_WMS, 
     tdrec940.t$lipl$l  PLAC_VEIC, 
     tdrec940.t$logn$l  CODE_USUA, 
+    tdrec940.t$logn$l                             USER_REC,
+    Log_Nfe_P_WMS.t$logn$c                        PRONTO_PARA_ENVIAR_WMS,
+    Log_Nfe_A_WMS.t$logn$c                        AGUARDANDO_WMS,
+    Log_Nfe_AP.t$logn$c                           APROVADO,
     tdrec940.t$cnfe$l  CHAV_ACESS, 
     tdrec940.t$fdtc$l  COD_TIPO_DOCFIS, 
     tcmcs966.t$dsca$l  DESC_TIPO_DOCFIS 
@@ -35,9 +39,15 @@ INNER JOIN baandb.ttdrec941301  tdrec941
         
 INNER JOIN baandb.ttccom130301 tccom130
         ON tccom130.t$cadr = tdrec940.t$sfra$l
+	
+-- MMF.sn
+INNER JOIN baandb.ttccom100301 tccom100
+        ON tccom100.t$cadr = tccom130.t$cadr
+-- MMF.en
 
 INNER JOIN baandb.ttcemm122301  tcemm122
-        ON tcemm122.t$bupa = tdrec940.t$sfra$l
+        ON tcemm122.t$bupa = tccom100.t$bpid
+--        ON tcemm122.t$bupa = tdrec940.t$sfra$l
             
  LEFT JOIN ( SELECT d.t$cnst DESC_DOMAIN_STAT,  
                     l.t$desc DESC_STAT  
@@ -69,7 +79,68 @@ INNER JOIN baandb.ttcemm122301  tcemm122
 	   
  LEFT JOIN baandb.ttcmcs966301 tcmcs966
         ON tcmcs966.t$fdtc$l = tdrec940.t$fdtc$l
-		
+        
+
+     
+    
+ LEFT JOIN ( select a.t$fire$c,
+                    a.t$stfa$c,
+                    a.t$nfes$c,
+                    a.t$logn$c
+               from baandb.tznnfe011301 a ) Log_Nfd
+        ON Log_Nfd.t$fire$c = tdrec940.t$fire$l
+       AND Log_Nfd.t$stfa$c = 5   --Impressa
+       AND Log_Nfd.t$nfes$c = 2   --Transmitida
+       
+ LEFT JOIN ( select ttaad200.t$user,
+                    ttaad200.t$name
+               from baandb.tttaad200000 ttaad200 ) Nfd_User
+        ON Nfd_User.t$user = Log_Nfd.t$logn$c
+        
+ LEFT JOIN ( select a.t$fire$c,
+                    a.t$stre$c,
+                    a.t$logn$c,
+                    max(a.t$data$c)
+               from baandb.tznnfe011301 a
+           group by a.t$fire$c,
+                    a.t$stre$c,
+                    a.t$logn$c ) Log_Nfe_P_WMS
+        ON Log_Nfe_P_WMS.t$fire$c = tdrec940.t$fire$l
+       AND Log_Nfe_P_WMS.t$stre$c = 201   --Pronto para enviar para WMS
+       
+ LEFT JOIN ( select ttaad200.t$user,
+                    ttaad200.t$name
+               from baandb.tttaad200000 ttaad200 ) Nfe_User_P_WMS
+        ON Nfe_User_P_WMS.t$user = Log_Nfe_P_WMS.t$logn$c
+        
+LEFT JOIN ( select a.t$fire$c,
+                   a.t$stre$c,
+                   a.t$logn$c,
+                   max(a.t$data$c)
+              from baandb.tznnfe011301 a
+          group by a.t$fire$c,
+                   a.t$stre$c,
+                   a.t$logn$c ) Log_Nfe_A_WMS
+        ON Log_Nfe_A_WMS.t$fire$c = tdrec940.t$fire$l
+       AND Log_Nfe_A_WMS.t$stre$c = 200   --Aguardando WMS
+       
+ LEFT JOIN ( select ttaad200.t$user,
+                    ttaad200.t$name
+               from baandb.tttaad200000 ttaad200 ) Nfe_User_A_WMS
+        ON Nfe_User_A_WMS.t$user = Log_Nfe_A_WMS.t$logn$c
+        
+
+ LEFT JOIN ( select a.t$fire$c,
+                   a.t$stre$c,
+                   a.t$logn$c,
+                   max(a.t$data$c)
+              from baandb.tznnfe011301 a
+          group by a.t$fire$c,
+                   a.t$stre$c,
+                   a.t$logn$c ) Log_Nfe_AP
+        ON Log_Nfe_AP.t$fire$c = tdrec940.t$fire$l
+       AND Log_Nfe_AP.t$stre$c = 4   --Aprovado    
+				
 WHERE Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdrec940.t$date$l,  
        'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT') 
          AT time zone 'America/Sao_Paulo') AS DATE)) 
