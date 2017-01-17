3702f4437d1d873cb50d9d3f5d03374cebf4fa27
Anderson M. de Aveiro <melloaveiro@gmail.com>
2017-01-17 16:21:42 -0300

    Update LN_Transportes_Gestao_Indicadores_Performance_Corrigida_Prometida.sql

--- Relatórios LN/LN_Transportes_Gestao_Indicadores_Performance_Corrigida_Prometida.sql
+++ Relatórios LN/LN_Transportes_Gestao_Indicadores_Performance_Corrigida_Prometida.sql
@@ -1,11 +1,18 @@
 select  
-        znfmd630.t$fili$c                                  FILIAL,
+        ( select znfmd001.t$fili$c
+          from   baandb.tznfmd001301 znfmd001,
+                 baandb.ttcmcs065301 tcmcs065,
+                 baandb.ttccom130301 tccom130_filial
+          where tcmcs065.t$cwoc = tdsls400.t$cofc
+            and tccom130_filial.t$cadr = tcmcs065.t$cadr
+            and znfmd001.t$fovn$c = tccom130_filial.t$fovn$l )
+                                                           FILIAL,
         tcmcs031.t$dsca                                    MARCA,
         cast((from_tz(to_timestamp(to_char(znfmd640_ETR.DATA_OCORRENCIA,
               'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
               AT time zone 'America/Sao_Paulo') as date)   DATA_EXPEDICAO,
         znsls401.t$pecl$c                                  PEDIDO,
-        znfmd630.t$pecl$c                                  ENTREGA,
+        znsls401.t$entr$c                                  ENTREGA,
         znsls002.t$dsca$c                                  TIPO_ENTREGA,
         tccom130t.t$dsca                                   TRANSPORTADOR,
         znsls401.t$ufen$c                                  UF,
@@ -23,9 +30,11 @@ select
                  AT time zone 'America/Sao_Paulo') as date)
           from  baandb.ttdsls401301 tdsls401
           where tdsls401.t$orno = tdsls400.t$orno )        DATA_LIM_EXPEDICAO,
-        cast((from_tz(to_timestamp(to_char(znsls401.t$dtep$c,
-                   'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-                    AT time zone 'America/Sao_Paulo') as date)  DATA_PROMETIDA,
+        ( select cast((from_tz(to_timestamp(to_char(max(tdsls401.t$prdt),
+                 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                 AT time zone 'America/Sao_Paulo') as date)
+          from  baandb.ttdsls401301 tdsls401
+          where tdsls401.t$orno = tdsls400.t$orno )        DATA_PROMETIDA,
         case when trunc(znfmd630.t$dtpe$c) = '01/01/1970'
              then null
         else cast((from_tz(to_timestamp(to_char(znfmd630.t$dtpe$c,
@@ -41,13 +50,43 @@ select
         znfmd630.t$cono$c                                  CONTRATO,
         znfmd060.t$cdes$c                                  DESCRICAO_DO_CONTRATO,
         znfmd640_FIRST.t$coci$c                            PRIMEIRA_OCORRENCIA,
-        znmcs002_FIRST.t$desc$c                            PRIMEIRA_DESC_OCORRENCIA,
-        znfmd640_FIRST.t$date$c                            PRIMEIRA_DATA_OCORRENCIA,
-        znfmd640.t$coci$c                                  ULTIMA_OCORRENCIA,
-        znmcs002.t$desc$c                                  ULTIMA_DESC_OCORRENCIA,
-        znfmd640.t$date$c                                  ULTIMA_DATA_OCORRENCIA
+        znfmd030_FIRST.t$dsci$c                            PRIMEIRA_DESC_OCORRENCIA,
+        cast((from_tz(to_timestamp(to_char(znfmd640_FIRST.t$date$c,
+                   'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                    AT time zone 'America/Sao_Paulo') as date)
+                                                           PRIMEIRA_DATA_OCORRENCIA,
+        nvl(znsls410_F.t$poco$c,znsls410.t$poco$c)         ULTIMA_OCORRENCIA,
+        nvl(znfmd030_F.t$dsci$c,nvl(znfmd030.t$dsci$c,znmcs002.t$desc$c))
+                                                           ULTIMA_DESC_OCORRENCIA,
+        nvl(cast((from_tz(to_timestamp(to_char(znsls410_F.t$dtoc$c,
+                   'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                    AT time zone 'America/Sao_Paulo') as date),
+            cast((from_tz(to_timestamp(to_char(znsls410.t$dtoc$c,
+                   'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                    AT time zone 'America/Sao_Paulo') as date))        
+                                                           ULTIMA_DATA_OCORRENCIA
+from     (  select a.t$ncia$c,
+                   a.t$uneg$c,
+                   a.t$pecl$c,
+                   a.t$sqpd$c,
+                   a.t$entr$c,
+                   min(a.t$sequ$c) t$sequ$c,
+                   a.t$orno$c,
+                   a.t$ufen$c,
+                   a.t$itpe$c,
+                   a.t$dtep$c
+            from baandb.tznsls401301 a
+            group by a.t$ncia$c,
+                     a.t$uneg$c,
+                     a.t$pecl$c,
+                     a.t$sqpd$c,
+                     a.t$entr$c,
+                     a.t$orno$c,
+                     a.t$ufen$c,
+                     a.t$itpe$c,
+                     a.t$dtep$c ) znsls401
 
-from    ( select a.t$fili$c,
+left join ( select a.t$fili$c,
                  a.t$pecl$c,
                  a.t$orno$c,
                  a.t$cfrw$c,
@@ -65,28 +104,10 @@ from    ( select a.t$fili$c,
                    a.t$dtco$c,
                    a.t$cono$c,
                    a.t$fire$c ) znfmd630
+       on znfmd630.t$pecl$c = to_char(znsls401.t$entr$c)
 
-inner join (  select a.t$ncia$c,
-                     a.t$uneg$c,
-                     a.t$pecl$c,
-                     a.t$sqpd$c,
-                     a.t$entr$c,
-                     min(a.t$sequ$c) t$sequ$c,
-                     a.t$orno$c,
-                     a.t$ufen$c,
-                     a.t$itpe$c,
-                     a.t$dtep$c
-               from baandb.tznsls401301 a
-               group by a.t$ncia$c,
-                      a.t$uneg$c,
-                     a.t$pecl$c,
-                     a.t$sqpd$c,
-                     a.t$entr$c,
-                     a.t$orno$c,
-                     a.t$ufen$c,
-                     a.t$itpe$c,
-                     a.t$dtep$c ) znsls401
-        on znsls401.t$orno$c = znfmd630.t$orno$c
+left join baandb.tcisli940301 cisli940
+       on cisli940.t$fire$l = znfmd630.t$fire$c
 
 left join ( select max(a.t$udat$c)       DATA_OCORRENCIA,
                    a.t$etiq$c, 
@@ -118,23 +139,60 @@ left join baandb.tznfmd060301 znfmd060
        on znfmd060.t$cfrw$c = znfmd630.t$cfrw$c
       and znfmd060.t$cono$c = znfmd630.t$cono$c
 
-left join ( select znfmd640.t$fili$c,
-                   znfmd640.t$etiq$c,
-                   max(znfmd640.t$coci$c) t$coci$c,
-                   max(znfmd640.t$date$c) KEEP (DENSE_RANK LAST ORDER BY t$date$c ) t$date$c
-            from baandb.tznfmd640301 znfmd640
-            group by znfmd640.t$fili$c,
-                     znfmd640.t$etiq$c ) znfmd640 
-       on znfmd640.t$fili$c = znfmd630.t$fili$c
-      and znfmd640.t$etiq$c = znfmd630.t$etiq$c
-      
+left join ( select a.t$ncia$c,
+                   a.t$uneg$c,
+                   a.t$pecl$c,
+                   a.t$sqpd$c,
+                   a.t$entr$c,
+                   max(a.t$dtoc$c) t$dtoc$c,
+                   max(a.t$poco$c) KEEP (DENSE_RANK LAST ORDER BY a.T$DTOC$C,  a.T$SEQN$C) t$poco$c
+               from baandb.tznsls410301 a,
+                    baandb.tznfmd030301 b
+            where  b.t$ocin$c = a.t$poco$c
+              and  b.t$finz$c = 1
+            group by a.t$ncia$c,
+                     a.t$uneg$c,
+                     a.t$pecl$c,
+                     a.t$sqpd$c,
+                     a.t$entr$c) znsls410_F
+       on znsls410_F.t$ncia$c = znsls401.t$ncia$c
+      and znsls410_F.t$uneg$c = znsls401.t$uneg$c
+      and znsls410_F.t$pecl$c = znsls401.t$pecl$c
+      and znsls410_F.t$sqpd$c = znsls401.t$sqpd$c
+      and znsls410_F.t$entr$c = znsls401.t$entr$c
+
+left join baandb.tznfmd030301 znfmd030_F
+       on znfmd030_F.t$ocin$c = znsls410_F.t$poco$c
+
+left join ( select a.t$ncia$c,
+                   a.t$uneg$c,
+                   a.t$pecl$c,
+                   a.t$sqpd$c,
+                   a.t$entr$c,
+                   max(a.t$dtoc$c) t$dtoc$c,
+                   max(a.t$poco$c) KEEP (DENSE_RANK LAST ORDER BY a.T$DTOC$C,  a.T$SEQN$C) t$poco$c
+            from baandb.tznsls410301 a
+            group by a.t$ncia$c,
+                     a.t$uneg$c,
+                     a.t$pecl$c,
+                     a.t$sqpd$c,
+                     a.t$entr$c) znsls410
+       on znsls410.t$ncia$c = znsls401.t$ncia$c
+      and znsls410.t$uneg$c = znsls401.t$uneg$c
+      and znsls410.t$pecl$c = znsls401.t$pecl$c
+      and znsls410.t$sqpd$c = znsls401.t$sqpd$c
+      and znsls410.t$entr$c = znsls401.t$entr$c
+
+left join baandb.tznfmd030301 znfmd030
+       on znfmd030.t$ocin$c = znsls410.t$poco$c
+
 left join baandb.tznmcs002301 znmcs002
-       on znmcs002.t$poco$c = znfmd640.t$coci$c
-       
+       on znmcs002.t$poco$c = znsls410.t$poco$c
+
 left join ( select znfmd640.t$fili$c,
                    znfmd640.t$etiq$c,
-                   min(znfmd640.t$coci$c) t$coci$c,
-                   min(znfmd640.t$date$c) KEEP (DENSE_RANK FIRST ORDER BY t$date$c) t$date$c
+                   min(znfmd640.t$date$c) t$date$c,
+                   min(znfmd640.t$coci$c) KEEP (DENSE_RANK FIRST ORDER BY t$date$c) t$coci$c
             from baandb.tznfmd640301 znfmd640
             where znfmd640.t$coci$c in ('CHU',
                                         'CME',
@@ -154,8 +212,8 @@ left join ( select znfmd640.t$fili$c,
        on znfmd640_FIRST.t$fili$c = znfmd630.t$fili$c
       and znfmd640_FIRST.t$etiq$c = znfmd630.t$etiq$c
 
-left join baandb.tznmcs002301 znmcs002_FIRST
-       on znmcs002_FIRST.t$poco$c = znfmd640_FIRST.t$coci$c
+left join baandb.tznfmd030301 znfmd030_FIRST
+       on znfmd030_FIRST.t$ocin$c = znfmd640_FIRST.t$coci$c
        
 left join ( select regexp_replace(tccom130.t$fovn$l, '[^0-9]', '') t$fovn$l,
                    tcmcs080.t$dsca,
@@ -176,3 +234,4 @@ where    NVL( (case when trunc(znfmd630.t$dtco$c) <= to_date('01/01/1970','DD/MM
                        AT time zone 'America/Sao_Paulo') as date)))             
           between :DATA_DE
               and :DATA_ATE
+  and    cisli940.t$fdty$l != 14  -- Retorno mercadoria de cliente
