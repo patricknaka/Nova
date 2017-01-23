c389e7db2e421363fb906aa273fdda017152c842
Anderson M. de Aveiro <melloaveiro@gmail.com>
2017-01-23 12:08:23 -0300

    Update LN_Logistica_Reversa_Status_Pedidos_Reversa.sql

--- Relatórios LN/LN_Logistica_Reversa_Status_Pedidos_Reversa.sql
+++ Relatórios LN/LN_Logistica_Reversa_Status_Pedidos_Reversa.sql
@@ -2,7 +2,11 @@ select
         znsls400.t$pecl$c                 PEDIDO,
         znsls401.t$entr$c                 ENTREGA_DEV,
         cisli940.t$cfrn$l                 APELIDO_TRANSP_COLETA,
-        znsls410_TRACKING.POCO            OCORRENCIA,
+        case when znsls410_TRACKING.POCO is not null then
+             znsls410_TRACKING.POCO
+        else
+             'ERRO'
+        end                               OCORRENCIA,
         znsls410_TRACKING.DTOC            DATA_OCORRENCIA,
         cisli940.t$docn$l                 NOTA_FISCAL_ENT,
         cisli940.t$seri$l                 SERIE_ENT,
@@ -58,23 +62,53 @@ left join baandb.ttccom130301 tccom130
 left join baandb.tznfmd001301 znfmd001
         on znfmd001.t$fovn$c = tccom130.t$fovn$l
 
-left  join  ( select    a.t$ncia$c,
-                        a.t$uneg$c,
-                        a.t$pecl$c,
-                        a.t$sqpd$c,
-                        max(a.t$dtoc$c) DTOC,
-                        max(a.t$poco$c) 
-                        keep (dense_rank last order by a.t$dtoc$c,  a.t$seqn$c)
-                                        POCO
+left join  ( select a.t$ncia$c,
+                    a.t$uneg$c,
+                    a.t$pecl$c,
+                    a.t$sqpd$c,
+                    a.t$entr$c,
+                    max(a.t$dtoc$c) DTOC,
+                    max(a.t$poco$c) keep (dense_rank last order by a.t$dtoc$c,  a.t$seqn$c)
+                                    POCO
               from    baandb.tznsls410301 a
+              where a.t$poco$c in ('CSF',
+                                   'RTC',
+                                   'DRC',
+                                   'CFS',
+                                   'CFR',
+                                   'NPC',
+                                   'CPO',
+                                   'CRT',
+                                   'ECO',
+                                   'RCO',
+                                   'ACO',
+                                   'COL',
+                                   'NCE',
+                                   'RCL',
+                                   'AC1',
+                                   'AC2',
+                                   'AC3',
+                                   'AGC',
+                                   'PEC',
+                                   'COR',
+                                   'CRD',
+                                   'FEC',
+                                   'CTC',
+                                   'CPM',
+                                   'DCM',
+                                   'RCT',
+                                   'RDV',
+                                   'CTR')
               group by  a.t$ncia$c,
                         a.t$uneg$c,
                         a.t$pecl$c,
-                        a.t$sqpd$c) znsls410_TRACKING
-        on  znsls410_TRACKING.t$ncia$c = znsls400.t$ncia$c
-       and  znsls410_TRACKING.t$uneg$c = znsls400.t$uneg$c
-       and  znsls410_TRACKING.t$pecl$c = znsls400.t$pecl$c
-       and  znsls410_TRACKING.t$sqpd$c = znsls400.t$sqpd$c
+                        a.t$sqpd$c,
+                        a.t$entr$c) znsls410_TRACKING
+        on  znsls410_TRACKING.t$ncia$c = znsls401.t$ncia$c
+       and  znsls410_TRACKING.t$uneg$c = znsls401.t$uneg$c
+       and  znsls410_TRACKING.t$pecl$c = znsls401.t$pecl$c
+       and  znsls410_TRACKING.t$sqpd$c = znsls401.t$sqpd$c
+       and  znsls410_TRACKING.t$entr$c = znsls401.t$entr$c
 
 where   znsls401.t$entr$c between :ENTREGA_DEV_DE and :ENTREGA_DEV_ATE
 and     znsls400.t$idpo$c = 'TD'   -- Troca ou devolução
