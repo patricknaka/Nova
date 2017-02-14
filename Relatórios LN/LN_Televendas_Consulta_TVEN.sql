f54e95efe56f132f1cdb9c83613f12a6e11ebf15
Anderson M. de Aveiro <melloaveiro@gmail.com>
2017-02-14 16:52:40 -0300

    SDP 1127424 - Correções
    SDP 1127424 – Consulta Televenda
    
    1)    Meio de Pagamento está saindo em branco
    2)    Descrição do item está saindo cortado, usar o campo “tcibd001.t$dscb$c
    3)    Verificar o campo “Valor de Pedido”, pois está trazendo o valor total do pedido. Se o relatório é por item, deveria trazer o valor do item.
    4)    O campo “Garantia” deveria ser impresso na mesma linha do Produto, e não como linha. As linhas de garantia tem o produto referenciado.

--- Relatórios LN/LN_Televendas_Consulta_TVEN.sql
+++ Relatórios LN/LN_Televendas_Consulta_TVEN.sql
@@ -12,17 +12,18 @@ select
         znsls401.t$entr$c                        ENTREGA,
         cisli940.t$docn$l                        NOTA_FISCAL,
         cisli940.t$seri$l                        SERIE,
-        znsls400.t$vlme$c                        VALOR_PEDIDO,
+        znsls401.t$vlun$c * znsls401.t$qtve$c    VALOR_PEDIDO,
         tcibd001.t$item                          SKU,
-        tcibd001.t$dsca                          DESCRICAO_ITEM,
+        tcibd001.t$dscb$c                        DESCRICAO_ITEM,
         MEIO_PAGTO.t$desc$c                      MEIO_PAGAMENTO,
         znsls400.t$cven$c                        VENDEDOR,
         znmcs002.t$desc$c                        STATUS_PEDIDO,
         znsls400.t$telf$c                        TEL_1,
         znsls400.t$te1f$c                        TEL_2,
         znsls400.t$te2f$c                        TEL_3,
-        zncom005.t$tpga$c                        GARANTIA_ESTENDIDA,
-        zncom005.t$pris$c                        VALOR_GARANTIA_ESTENDIDA
+        tcibd001_GARANTIA.t$mdfb$c               GARANTIA_ESTENDIDA,
+        znsls401_GARANTIA.t$vlun$c * znsls401_GARANTIA.t$qtve$c
+                                                 VALOR_GARANTIA_ESTENDIDA
 
 from baandb.tznsls400301 znsls400
 
@@ -34,18 +35,31 @@ inner join baandb.tznsls401301 znsls401
        and znsls401.t$uneg$c = znsls400.t$uneg$c
        and znsls401.t$pecl$c = znsls400.t$pecl$c
        and znsls401.t$sqpd$c = znsls400.t$sqpd$c
+       and znsls401.t$iitm$c = 'P'  -- Produto
+
+left join baandb.tznsls401301 znsls401_GARANTIA
+       on znsls401_GARANTIA.t$ncia$c = znsls401.t$ncia$c
+      and znsls401_GARANTIA.t$uneg$c = znsls401.t$uneg$c
+      and znsls401_GARANTIA.t$pecl$c = znsls401.t$pecl$c
+      and znsls401_GARANTIA.t$sqpd$c = znsls401.t$sqpd$c
+      and znsls401_GARANTIA.t$entr$c = znsls401.t$entr$c
+      and znsls401_GARANTIA.t$igar$c = znsls401.t$item$c
+      and znsls401_GARANTIA.t$pcga$c = znsls401.t$pecl$c
 
 left join baandb.ttcibd001301 tcibd001
        on tcibd001.t$item = znsls401.t$itml$c
+
+left join baandb.ttcibd001301 tcibd001_GARANTIA
+       on tcibd001_GARANTIA.t$item = znsls401_GARANTIA.t$itml$c
 
 left  join  ( select a.t$ncia$c,
                      a.t$uneg$c,
                      a.t$pecl$c,
                      a.t$sqpd$c,
                      b.t$desc$c
-              from baandb.tznsls402601 a,
-                   baandb.tzncmg007601 b
-              where  b.t$mpgt$c = a.t$idmp$c ) MEIO_PAGTO
+              from baandb.tznsls402301 a,
+                   baandb.tzncmg007301 b
+              where  b.t$mpgs$c = a.t$idmp$c ) MEIO_PAGTO
         on MEIO_PAGTO.t$ncia$c = znsls400.t$ncia$c
        and MEIO_PAGTO.t$uneg$c = znsls400.t$uneg$c
        and MEIO_PAGTO.t$pecl$c = znsls400.t$pecl$c
@@ -76,14 +90,6 @@ left join ( select a.t$ncia$c,
 left join baandb.tznmcs002301 znmcs002
        on znmcs002.t$poco$c = znsls410.POCO
 
-left join baandb.tzncom005301 zncom005
-       on zncom005.t$ncia$c = znsls401.t$ncia$c
-      and zncom005.t$uneg$c = znsls401.t$uneg$c
-      and zncom005.t$pecl$c = znsls401.t$pecl$c
-      and zncom005.t$sqpd$c = znsls401.t$sqpd$c
-      and zncom005.t$entr$c = znsls401.t$entr$c
-      and zncom005.t$sequ$c = znsls401.t$sequ$c
-
 left join baandb.tcisli245301 cisli245
        on cisli245.t$slcp = 301
       and cisli245.t$ortp = 1   -- Ordem/programação de venda
@@ -100,3 +106,4 @@ where   cast((from_tz(to_timestamp(to_char(tdsls400.t$odat,
         between :DATA_COMPRA_DE
             and :DATA_COMPRA_ATE
   and   znsls400.t$idca$c = 'TVE'
+  and   znsls400.t$idpo$c = 'LJ'
