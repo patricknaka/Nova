46d1d09bcf2cd90c734dc9f48df98d1c1693a0c8
rarruda1988 <rarruda1988@gmail.com>
2017-02-23 17:16:21 -0300

    SDP: 1360514
    Chamado aberto para incluir a coluna Referência de Devolução.

--- Relatórios LN/LN_Fiscal - Notas_Fiscais_Emitidas_Devolução_no_Ato.sql
+++ Relatórios LN/LN_Fiscal - Notas_Fiscais_Emitidas_Devolução_no_Ato.sql
@@ -1,7 +1,7 @@
 SELECT 
  DISTINCT 
        301                      NUM_CIA,
-       tccom130.t$fovn$l        CNPJ_FILIAL,
+       cisli940.t$cofc$l || ' - ' ||  cisli940.t$sfra$l       CNPJ_FILIAL,
        tdrec940.t$cofc$l        DEPTO_FILIAL,
        tcmcs065.t$dsca          DESR_FILIAL,
        tdrec941.t$fire$l        REF_FIS_ENTR,    
@@ -22,7 +22,15 @@ SELECT
        cisli940.t$seri$l        SERIE_NFD,
        whinh300.t$recd$c        RECDOC,
        tdrec940.t$lipl$l        PLACA,
-       cisli940.t$cnfe$l        CHAVE 
+       cisli940.t$cnfe$l        CHAVE,
+       cisli940.t$amnt$l        Valor_Total_NF,
+       NVL( ( select Trim(tttxt010.t$text)
+             from baandb.ttttxt010301 tttxt010 
+            where tttxt010.t$ctxt = cisli940.t$obse$l),' ') 
+                                MOTIVO_DEVOLUCAO,
+       znnfe011.t$logn$c || ' - '
+       || login.t$name          USUARIO,
+       znrec013.T$RFFA$c        REF_DEVOLUCAO
 
 FROM       baandb.ttcmcs065301 tcmcs065
 
@@ -31,13 +39,16 @@ INNER JOIN baandb.ttdrec940301 tdrec940
 
 INNER JOIN baandb.ttdrec941301 tdrec941
         ON tdrec941.t$fire$l = tdrec940.t$fire$l
-
+              
 INNER JOIN baandb.tcisli941301 cisli941
         ON cisli941.t$fire$l = tdrec941.t$rfdv$c
        AND cisli941.t$line$l = tdrec941.t$lfdv$c
 
 INNER JOIN baandb.tcisli940301 cisli940
         ON cisli940.t$fire$l = cisli941.t$fire$l
+
+LEFT JOIN  baandb.tznrec013301 znrec013
+on znrec013.T$RFRE$C = cisli940.t$fire$l  
 
 INNER JOIN baandb.ttccom100301 tccom100
         ON tccom100.t$bpid   = tdrec940.t$bpid$l
@@ -48,6 +59,16 @@ INNER JOIN baandb.ttccom100301 tccom100
  LEFT JOIN baandb.twhinh300301 whinh300
         ON whinh300.t$fire$c = tdrec940.t$fire$l
     
+      Left Join  baandb.tznnfe011301 znnfe011
+      on   znnfe011.t$fire$c = cisli940.t$fire$l   
+      AND znnfe011.t$nfes$c = 2
+      
+    
+LEFT JOIN (select a.t$user,
+                    a.t$name   
+               from baandb.tttaad200000 a) login
+        ON login.t$user = znnfe011.t$logn$C   
+
 WHERE cisli940.t$fdty$l = 5
   AND tdrec940.t$cofc$l IN (:Filial)
   AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdrec940.t$adat$l, 'DD-MON-YYYY HH24:MI:SS'), 
