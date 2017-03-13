7600f2a1153410502d9b5f1681c233793998bfed
Anderson M. de Aveiro <melloaveiro@gmail.com>
2017-03-13 11:06:57 -0300

    SDP 1227902 - Ajustes
    - Quantidade de Entregas divergente:
      Terei que verificar na query 3050 alguma leitura ou condição que esta faz e que não está sendo considerado na nova query para eliminar esta divergência;
      Quanto às entregas Nike, terei que replicar a nova query apontando para a companhia onde estão os dados da Nike.
    
    - Total de Etiquetas por Entrega:
       Estarei verificando este problema comparando com a query do relatório original.
    
    - Falta de Colunas:
      Foi seguido o modelo que está na descrição da solicitação (Informações de maior relevância), porém, estaremos incluindo os demais campos que estão faltando em relação ao relatório original.

--- Relatórios LN/LN_Transportes_Expedicao_Sislogica.sql
+++ Relatórios LN/LN_Transportes_Expedicao_Sislogica.sql
@@ -1,30 +1,44 @@
 select  
         znfmd630.t$fili$c                                  ID_FILIAL,
-        (select a.t$idca$c
-         from   baandb.tznsls400301 a
-         where  a.t$ncia$c = znsls401.t$ncia$c
-           and  a.t$uneg$c = znsls401.t$uneg$c
-           and  a.t$pecl$c = znsls401.t$pecl$c
-           and  a.t$sqpd$c = znsls401.t$sqpd$c)            CANAL,
+        znsls400.t$idca$c                                  CANAL,
         znsls401.t$itpe$c                                  ID_TIPO_ENTREGA,
         znsls002.t$dsca$c                                  DESCR_TIPO_ENTREGA,   
         znsls401.t$uneg$c                                  ID_UNEG,
         znint002.t$desc$c                                  DESCR_UNEG,
         znsls401.t$entr$c                                  ENTREGA,
         znsls401.t$pecl$c                                  PEDIDO,
-        znsls410.t$docn$c                                  NOTA,
-        znsls410.t$seri$c                                  SERIE,
-        nvl(OXV.NOTES1, znfmd630.t$etiq$c)                 ETIQUETA,
+        cisli940.t$docn$l                                  NOTA,
+        cisli940.t$seri$l                                  SERIE,
+        nvl(oxv.notes1, znfmd630.t$etiq$c)                 ETIQUETA,
         znfmd060.t$cdes$c                                  CONTRATO,
         replace(tccom130.t$fovn$l,'/','')                  CNPJ,
         znfmd630.t$cfrw$c || ' - ' || tcmcs080.t$dsca      TRANSPORTADORA,   
-        tcmcs080.t$SEAK  APELIDO,   
+        tcmcs080.t$seak                                    APELIDO,   
+        ( select a.t$creg$c
+            from baandb.tznfmd062301 a 
+           where a.t$cfrw$c = znfmd630.t$cfrw$c
+             and a.t$cono$c = znfmd630.t$cono$c
+             and a.t$cepd$c <= znsls400.t$cepf$c
+             and a.t$cepa$c >= znsls400.t$cepf$c
+             and rownum = 1)                               CAPITAL_INTERIOR,
+        nvl(shd.originalqty,znsls401.t$qtve$c)             QTD_VOL,
+        nvl(shd.product_cube, ( select sum(a.t$quan$l) t$quan$l
+                                  from baandb.tcisli956301 a
+                                 where a.t$fire$l = cisli940.t$fire$l ))
+                                                           VOLUME,
+        nvl(shd.product_weight,cisli940.t$nwgt$l)          PESO,
+        cisli940.t$gamt$l                                  VL_SEM_FRETE,
+        znfmd630.t$vlfc$c                                  FRETE,
+        ''                                                 FRETE_MODIF,
+        cisli940.t$fght$l                                  FRETE_SITE,
+        cisli940.t$amnt$l                                  VL_TOTAL,
         cast((from_tz(to_timestamp(to_char(znsls401.t$dtep$c - znsls401.t$pzcd$c,
               'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-               AT time zone 'America/Sao_Paulo') AS DATE)
-                                                           DT_LIMITE,
+               AT time zone 'America/Sao_Paulo') AS DATE)  DT_LIMITE,
+        znsls401.t$pzcd$c                                  PZ_CD,
+        znsls401.t$pztr$c                                  PZ_TRANSIT,
         znfmd630.t$ncar$c                                  CARGA,
-        cast((from_tz(to_timestamp(to_char(znsls410.t$dtoc$c,
+        cast((from_tz(to_timestamp(to_char(znfmd640.t$udat$c,
               'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
                AT time zone 'America/Sao_Paulo') AS DATE)
                                                            DT_ETR,    
@@ -46,33 +60,64 @@ select
                    'DD-MON-YYYY HH24:MI:SS'),'DD-MON-YYYY HH24:MI:SS'), 'GMT')
                     AT time zone 'America/Sao_Paulo') AS DATE)
         end                                                DATA_CORRIGIDA,
-        (select cast((from_tz(to_timestamp(to_char(a.t$dtem$c,
+        cast(replace(own_mis.filtro_mis(znsls400.t$nomf$c),';','')
+             as varchar(120))                              CLIENTE,
+        znsls400.t$fovn$c                                  CPF_CNPJ,
+        cast(replace(own_mis.filtro_mis(znsls400.t$logf$c),';','')
+             as varchar(180))                              ENDERECO,
+        znsls400.t$numf$c                                  NUMERO,
+        cast(replace(own_mis.filtro_mis(znsls400.t$baif$c),';','')
+             as varchar(90))                               BAIRRO,
+        cast(replace(own_mis.filtro_mis(znsls400.t$reff$c),';','')
+             as varchar(300))                              REFERENCIA,
+        znsls400.t$cepf$c                                  CEP,
+        znsls400.t$cidf$c                                  CIDADE,
+        znsls400.t$uffa$c                                  UF,
+        znsls400.t$telf$c                                  TELEFONE_1,
+        znsls400.t$te1f$c                                  TELEFONE_2,
+        znsls400.t$te2f$c                                  TELEFONE_3,
+        znsls400.t$emaf$c                                  EMAIL,
+        case when znsls401.t$idpa$c = '1' then 'Manhã'
+             when znsls401.t$idpa$c = '2' then 'Tarde'
+             when znsls401.t$idpa$c = '3' then 'Noite'
+        else ''
+        end                                                PERIODO,
+        cast((from_tz(to_timestamp(to_char(znsls400.t$dtem$c,
               'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-               AT time zone 'America/Sao_Paulo') AS DATE)
-         from   baandb.tznsls400301 a
-         where  a.t$ncia$c = znsls401.t$ncia$c
-           and  a.t$uneg$c = znsls401.t$uneg$c
-           and  a.t$pecl$c = znsls401.t$pecl$c
-           and  a.t$sqpd$c = znsls401.t$sqpd$c)            DT_COMPRA
+               AT time zone 'America/Sao_Paulo') AS DATE)  DT_COMPRA
 
-from    ( select a.t$fili$c,
-                 a.t$cfrw$c,
-                 a.t$cono$c,
-                 a.t$orno$c,
-                 a.t$ncar$c,
-                 a.t$dtpe$c,
-                 a.t$dtco$c,
-                 a.t$pecl$c,
-                 max(a.t$etiq$c) t$etiq$c
-          from   baandb.tznfmd630301 a
-          group by a.t$fili$c,
-                   a.t$cfrw$c,
-                   a.t$cono$c,
-                   a.t$orno$c,
-                   a.t$ncar$c,
-                   a.t$dtpe$c,
-                   a.t$dtco$c,
-                   a.t$pecl$c) znfmd630
+  from ( select max(a.t$udat$c) t$udat$c,
+                max(a.t$etiq$c) keep (dense_rank last order by a.t$udat$c ) t$etiq$c,
+                    b.t$pecl$c,
+                    a.t$fili$c
+           from baandb.tznfmd640301 a
+     inner join baandb.tznfmd630301 b
+             on a.t$fili$c = b.t$fili$c
+            and a.t$etiq$c = b.t$etiq$c
+          where a.t$coct$c = 'ETR'
+            and a.t$torg$c = 1
+       group by b.t$pecl$c,
+                a.t$fili$c ) znfmd640
+
+inner join baandb.tznfmd630301 znfmd630
+        on znfmd630.t$fili$c = znfmd640.t$fili$c
+       and znfmd630.t$etiq$c = znfmd640.t$etiq$c
+       and znfmd630.t$pecl$c = znfmd640.t$pecl$c
+
+inner join ( select a.t$ncia$c,
+                    a.t$uneg$c,
+                    a.t$pecl$c,
+                    a.t$sqpd$c,
+                    a.t$entr$c,
+                    a.t$orno$c
+               from baandb.tznsls004301 a
+           group by a.t$ncia$c,
+                    a.t$uneg$c,
+                    a.t$pecl$c,
+                    a.t$sqpd$c,
+                    a.t$entr$c,
+                    a.t$orno$c ) znsls004
+        on znsls004.t$orno$c = znfmd630.t$orno$c
 
 inner join baandb.tznfmd060301 znfmd060
         on znfmd060.t$cfrw$c = znfmd630.t$cfrw$c
@@ -83,6 +128,10 @@ inner join baandb.ttcmcs080301 tcmcs080
 
 inner join baandb.ttccom130301 tccom130
         on tccom130.t$cadr = tcmcs080.t$cadr$l
+
+inner join baandb.tcisli940301 cisli940
+        on cisli940.t$fire$l = znfmd630.t$fire$c
+       and cisli940.t$fdty$l != 14
 
 inner join ( select a.t$worg,
                     a.t$worn,
@@ -99,52 +148,42 @@ inner join ( select a.t$ncia$c,
                     a.t$pecl$c,
                     a.t$sqpd$c,
                     a.t$entr$c,
-                    a.t$itpe$c,
                     a.t$orno$c,
-                    a.t$dtep$c,
-                    a.t$pzcd$c
-             from   baandb.tznsls401301 a
+                    a.t$itpe$c,
+                    a.t$pzcd$c,
+                    a.t$pztr$c,
+                    min(a.t$dtep$c) t$dtep$c,
+                    min(a.t$idpa$c) t$idpa$c,
+                    sum(a.t$qtve$c) t$qtve$c
+               from baandb.tznsls401301 a
+              where a.t$iitm$c = 'P'
              group by a.t$ncia$c,
                       a.t$uneg$c,
                       a.t$pecl$c,
                       a.t$sqpd$c,
                       a.t$entr$c,
-                      a.t$itpe$c,
                       a.t$orno$c,
-                      a.t$dtep$c,
-                      a.t$pzcd$c ) znsls401
-        on znsls401.t$orno$c = whinh431.t$worn
+                      a.t$itpe$c,
+                      a.t$pzcd$c,
+                      a.t$pztr$c) znsls401
+        on znsls401.t$ncia$c = znsls004.t$ncia$c
+       and znsls401.t$uneg$c = znsls004.t$uneg$c
+       and znsls401.t$pecl$c = znsls004.t$pecl$c
+       and znsls401.t$sqpd$c = znsls004.t$sqpd$c
+       and znsls401.t$entr$c = znsls004.t$entr$c
 
-inner join baandb.tznint002301 znint002
-        on znint002.t$ncia$c = znsls401.t$ncia$c
-       and znint002.t$uneg$c = znsls401.t$uneg$c
+inner join baandb.tznsls400301 znsls400
+        on znsls400.t$ncia$c = znsls401.t$ncia$c
+       and znsls400.t$uneg$c = znsls401.t$uneg$c
+       and znsls400.t$pecl$c = znsls401.t$pecl$c
+       and znsls400.t$sqpd$c = znsls401.t$sqpd$c
 
-inner join baandb.tznsls002301 znsls002
-        on znsls002.t$tpen$c = znsls401.t$itpe$c
+left join baandb.tznint002301 znint002
+       on znint002.t$ncia$c = znsls401.t$ncia$c
+      and znint002.t$uneg$c = znsls401.t$uneg$c
 
-inner join ( select a.t$ncia$c,
-                    a.t$uneg$c,
-                    a.t$pecl$c,
-                    a.t$sqpd$c,
-                    a.t$entr$c,
-                    a.t$docn$c,
-                    a.t$seri$c,  
-                    min(a.t$dtoc$c) t$dtoc$c,
-                    min(a.t$poco$c) KEEP (DENSE_RANK FIRST ORDER BY a.T$DTOC$C,  a.T$SEQN$C) t$poco$c
-             from baandb.tznsls410301 a
-             where a.t$poco$c = 'ETR'
-             group by a.t$ncia$c,
-                      a.t$uneg$c,
-                      a.t$pecl$c,
-                      a.t$sqpd$c,
-                      a.t$entr$c,
-                      a.t$docn$c,
-                      a.t$seri$c) znsls410
-        on znsls410.t$ncia$c = znsls401.t$ncia$c
-       and znsls410.t$uneg$c = znsls401.t$uneg$c
-       and znsls410.t$pecl$c = znsls401.t$pecl$c
-       and znsls410.t$sqpd$c = znsls401.t$sqpd$c
-       and znsls410.t$entr$c = znsls401.t$entr$c
+left join baandb.tznsls002301 znsls002
+       on znsls002.t$tpen$c = znsls401.t$itpe$c
 
 left join wmwhse3.orderdetail@dl_ln_wms shd
        on substr(shd.externorderkey,5,9) = whinh431.t$shpm
@@ -153,9 +192,8 @@ left join wmwhse3.orderdetailxvas@dl_ln_wms oxv
        on oxv.orderkey = shd.orderkey
       and oxv.orderlinenumber = shd.orderlinenumber
       
-where znsls410.t$dtoc$c is not null
-  and trunc(cast((from_tz(to_timestamp(to_char(znsls410.t$dtoc$c,
+where trunc(cast((from_tz(to_timestamp(to_char(znfmd640.t$udat$c,
                   'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-                   AT time zone 'America/Sao_Paulo') AS DATE)) 
+                   AT time zone 'America/Sao_Paulo') AS DATE))
       between :DATA_INI
           and :DATA_FIM
