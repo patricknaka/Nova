f3124f0a566c4004d5d6367ebf36bb545cdc327c
julianphilippe <julian.philippe@outlook.com>
2017-03-14 10:24:25 -0300

    1355816
    Alteração de coluna RECEB_FISICO_ITEM para substituir valor para zero quando há mais de uma linha.

--- Relatórios LN/LN_Suprimentos_Recebimentos_item_eHub.sql
+++ Relatórios LN/LN_Suprimentos_Recebimentos_item_eHub.sql
@@ -1,4 +1,22 @@
-select  
+
+ select q1.ORDEM_COMPRA,  
+        q1.REF_FISCAL,  
+        q1.DATA_APROVACAO_FISCAL,  
+        q1.ITEM,  
+        q1.DESC_ITEM,  
+        case when nr = 1  
+          then q1.RECEB_FISICO_ITEM  
+        else 0 end RECEB_FISICO_ITEM,  
+        q1.TOTAL_RECEB_FISCAL,  
+        q1.NOTA_FISCAL,  
+        q1.ASN,  
+        q1.DATA_ASN,  
+        q1.DATA_WMS_COCKPIT,  
+        q1.TOTAL_RECEB_FISICO_REF_FISCAL,  
+        q1.QTDE_ARMAZENADA_ITEM,  
+        q1.DATA_ARMAZENAGEM_WMS,  
+        q1.MOD_FAB_ITEM  
+ from (select  
      REC947.OC                     ORDEM_COMPRA,  
      tdrec940.t$fire$l             REF_FISCAL,  
      CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdrec940.t$adat$l,  
@@ -9,20 +27,26 @@ select
      tcibd001.t$dscb$c             DESC_ITEM,  
      whinh301.t$rqua               RECEB_FISICO_ITEM,  
      REC941.QTDE                   TOTAL_RECEB_FISCAL,  
+     WHINH300.T$ASNR               NOTA_FISCAL,  
+     WMS_RECEIPT.RECEIPTKEY        ASN,  
      CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(whinh300.t$crdt,  
        'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  
          AT time zone 'America/Sao_Paulo') AS DATE)  
-                                   DATA_ASN,
+                                   DATA_ASN,  
      CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(WMS_RECEIPT.CLOSEDDATE,  
        'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  
          AT time zone 'America/Sao_Paulo') AS DATE)  
-                                   DATA_WMS_COCKPIT,                   
+                                   DATA_WMS_COCKPIT,  
      INH301.QTDE                   TOTAL_RECEB_FISICO_REF_FISCAL,  
-     SUM(WMS_ITRN.QTDE_ARMAZENADA) QTDE_ARMAZENADA_ITEM,  
-     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(Max(WMS_ITRN.EFFECTIVEDATE),  
+     sum(WMS_ITRN.QTDE_ARMAZENADA) QTDE_ARMAZENADA_ITEM,  
+     trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(WMS_ITRN.EFFECTIVEDATE,  
        'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  
-         AT time zone 'America/Sao_Paulo') AS DATE)  
-                                   DATA_ARMAZENAGEM_WMS  
+         AT time zone 'America/Sao_Paulo') AS DATE))  
+                                   DATA_ARMAZENAGEM_WMS,  
+	  tcibd001.t$mdfb$c             MOD_FAB_ITEM,  
+    WMS_REC_DETAIL.RECEIPTKEY,  
+    row_number()over(partition by Trim(whinh301.t$item), WHINH300.T$ASNR order by tdrec940.t$fire$l,Trim(whinh301.t$item)) nr  
+  
   
  FROM       baandb.ttdrec940601  tdrec940  
   
@@ -59,22 +83,22 @@ select
          ON REC947.t$fire$l = tdrec940.t$fire$l  
   
   LEFT JOIN ( select a.RECEIPTKEY,  
-                     a.EXTERNRECEIPTKEY,
-                     a.CLOSEDDATE,
-                     a.STATUS,
-                     a.TYPE
+                     a.EXTERNRECEIPTKEY,  
+                     a.CLOSEDDATE,  
+                     a.STATUS,  
+                     a.TYPE  
                 from WMWHSE9.RECEIPT@DL_LN_WMS  a  
             group by a.RECEIPTKEY,  
-                     a.EXTERNRECEIPTKEY,
-                     a.CLOSEDDATE,
-                     a.STATUS,
+                     a.EXTERNRECEIPTKEY,  
+                     a.CLOSEDDATE,  
+                     a.STATUS,  
                      a.TYPE ) WMS_RECEIPT  
          ON WMS_RECEIPT.EXTERNRECEIPTKEY = '0' || '_' || TRIM(whinh300.t$sfbp) || '_' || TRIM(whinh300.t$shid) || '_' || TRIM(whinh300.t$cwar)  
   
   LEFT JOIN ( select a.RECEIPTKEY,  
                      a.SKU,  
                      a.TOID,  
-                     max(a.DATERECEIVED),  
+                     max(a.DATERECEIVED) DATERECEIVED,  
                      sum(a.QTYRECEIVED) QTDE  
                 from WMWHSE9.RECEIPTDETAIL@DL_LN_WMS  a  
             group by a.RECEIPTKEY,  
@@ -100,22 +124,28 @@ select
          ON WMS_ITRN.SKU = WMS_REC_DETAIL.SKU  
         AND WMS_ITRN.FROMID = WMS_REC_DETAIL.TOID  
   
-WHERE tdrec940.t$stat$l IN (4, 5)  
+ WHERE tdrec940.t$stat$l IN (4, 5)  
    AND Trim(tdrec940.t$cnfe$l) is not null  
    AND tdrec940.t$rfdt$l = 1  
    AND whinh301.t$rqua != 0  
-   AND WMS_RECEIPT.STATUS >= 11
-   AND WMS_RECEIPT.TYPE != 8  -- PRODUCTION
+   AND WMS_RECEIPT.STATUS> = 11  
+   AND WMS_RECEIPT.TYPE != 8  
    AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(whinh300.t$crdt,  
                'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  
                   AT time zone 'America/Sao_Paulo') AS DATE))  
-       Between ' + Parameters!DataASNDe.Value + '  
-           And ' + Parameters!DataASNAte.Value + '  
+       Between '" + Parameters!DataASNDe.Value + "'  
+           And '" + Parameters!DataASNAte.Value + "'  
   
-   AND (    ( + IIF(Parameters!Item.Value Is Nothing, 1, 0) +  = 1)  
+   AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(WMS_ITRN.EFFECTIVEDATE,  
+               'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  
+                  AT time zone 'America/Sao_Paulo') AS DATE))  
+       Between nvl('" + Parameters!DataARMDe.Value + "',trunc(WMS_ITRN.EFFECTIVEDATE))  
+           And nvl('" + Parameters!DataARMAte.Value + "',trunc(WMS_ITRN.EFFECTIVEDATE))  
+  
+   AND (    (" + IIF(Parameters!Item.Value Is Nothing, "1", "0") + " = 1)  
           OR(    (Trim(whinh301.t$item) IN  
-                 (  + IIF(Trim(Parameters!Item.Value) = , '', ' + Replace(Replace(Parameters!Item.Value,  , ), ,, ',') + ')  +  )  
-             AND ( + IIF(Parameters!Item.Value Is Nothing, 1, 0) +  = 0))))  
+                 ( " + IIF(Trim(Parameters!Item.Value) = "", "''", "'" + Replace(Replace(Parameters!Item.Value, " ", ""), ",", "','") + "'")  + " )  
+             AND (" + IIF(Parameters!Item.Value Is Nothing, "1", "0") + " = 0))))  
   
  GROUP BY REC947.OC,  
           tdrec940.t$fire$l,  
@@ -126,21 +156,44 @@ WHERE tdrec940.t$stat$l IN (4, 5)
           tcibd001.t$dscb$c,  
           whinh301.t$rqua,  
           REC941.QTDE,  
+          WHINH300.T$ASNR,  
+          WMS_RECEIPT.RECEIPTKEY,  
           CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(whinh300.t$crdt,  
             'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  
               AT time zone 'America/Sao_Paulo') AS DATE),  
-     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(WMS_RECEIPT.CLOSEDDATE,  
-       'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  
-         AT time zone 'America/Sao_Paulo') AS DATE),
-          INH301.QTDE  
-
- ORDER BY REF_FISCAL, ITEM
+          CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(WMS_RECEIPT.CLOSEDDATE,  
+            'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  
+              AT time zone 'America/Sao_Paulo') AS DATE),  
+          trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(WMS_ITRN.EFFECTIVEDATE,  
+             'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  
+               AT time zone 'America/Sao_Paulo') AS DATE)),  
+          INH301.QTDE,  
+		   tcibd001.t$mdfb$c,  
+       WMS_REC_DETAIL.RECEIPTKEY  
+  
+ ORDER BY REF_FISCAL, ITEM) q1  
  
  
  
 =
-
-" select  " &
+" select q1.ORDEM_COMPRA,  " &
+"        q1.REF_FISCAL,  " &
+"        q1.DATA_APROVACAO_FISCAL,  " &
+"        q1.ITEM,  " &
+"        q1.DESC_ITEM,  " &
+"        case when nr = 1  " &
+"          then q1.RECEB_FISICO_ITEM  " &
+"        else 0 end RECEB_FISICO_ITEM,  " &
+"        q1.TOTAL_RECEB_FISCAL,  " &
+"        q1.NOTA_FISCAL,  " &
+"        q1.ASN,  " &
+"        q1.DATA_ASN,  " &
+"        q1.DATA_WMS_COCKPIT,  " &
+"        q1.TOTAL_RECEB_FISICO_REF_FISCAL,  " &
+"        q1.QTDE_ARMAZENADA_ITEM,  " &
+"        q1.DATA_ARMAZENAGEM_WMS,  " &
+"        q1.MOD_FAB_ITEM  " &
+" from (select  " &
 "     REC947.OC                     ORDEM_COMPRA,  " &
 "     tdrec940.t$fire$l             REF_FISCAL,  " &
 "     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdrec940.t$adat$l,  " &
@@ -151,20 +204,26 @@ WHERE tdrec940.t$stat$l IN (4, 5)
 "     tcibd001.t$dscb$c             DESC_ITEM,  " &
 "     whinh301.t$rqua               RECEB_FISICO_ITEM,  " &
 "     REC941.QTDE                   TOTAL_RECEB_FISCAL,  " &
+"     WHINH300.T$ASNR               NOTA_FISCAL,  " &
+"     WMS_RECEIPT.RECEIPTKEY        ASN,  " &
 "     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(whinh300.t$crdt,  " &
 "       'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  " &
 "         AT time zone 'America/Sao_Paulo') AS DATE)  " &
-"                                   DATA_ASN," &
+"                                   DATA_ASN,  " &
 "     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(WMS_RECEIPT.CLOSEDDATE,  " &
 "       'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  " &
 "         AT time zone 'America/Sao_Paulo') AS DATE)  " &
 "                                   DATA_WMS_COCKPIT,  " &
 "     INH301.QTDE                   TOTAL_RECEB_FISICO_REF_FISCAL,  " &
-"     SUM(WMS_ITRN.QTDE_ARMAZENADA) QTDE_ARMAZENADA_ITEM,  " &
-"     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(Max(WMS_ITRN.EFFECTIVEDATE),  " &
+"     sum(WMS_ITRN.QTDE_ARMAZENADA) QTDE_ARMAZENADA_ITEM,  " &
+"     trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(WMS_ITRN.EFFECTIVEDATE,  " &
 "       'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  " &
-"         AT time zone 'America/Sao_Paulo') AS DATE)  " &
-"                                   DATA_ARMAZENAGEM_WMS  " &
+"         AT time zone 'America/Sao_Paulo') AS DATE))  " &
+"                                   DATA_ARMAZENAGEM_WMS,  " &
+"	  tcibd001.t$mdfb$c             MOD_FAB_ITEM,  " &
+"    WMS_REC_DETAIL.RECEIPTKEY,  " &
+"    row_number()over(partition by Trim(whinh301.t$item), WHINH300.T$ASNR order by tdrec940.t$fire$l,Trim(whinh301.t$item)) nr  " &
+"  " &
 "  " &
 " FROM       baandb.ttdrec940" + Parameters!Compania.Value + "  tdrec940  " &
 "  " &
@@ -201,22 +260,22 @@ WHERE tdrec940.t$stat$l IN (4, 5)
 "         ON REC947.t$fire$l = tdrec940.t$fire$l  " &
 "  " &
 "  LEFT JOIN ( select a.RECEIPTKEY,  " &
-"                     a.EXTERNRECEIPTKEY," &
-"                     a.CLOSEDDATE," &
-"                     a.STATUS," &
-"                     a.TYPE" &
+"                     a.EXTERNRECEIPTKEY,  " &
+"                     a.CLOSEDDATE,  " &
+"                     a.STATUS,  " &
+"                     a.TYPE  " &
 "                from " + IIF(Parameters!Compania.Value = 601, "WMWHSE9", "WMWHSE10") + ".RECEIPT@DL_LN_WMS  a  " &
 "            group by a.RECEIPTKEY,  " &
-"                     a.EXTERNRECEIPTKEY," &
-"                     a.CLOSEDDATE," &
-"                     a.STATUS," &
+"                     a.EXTERNRECEIPTKEY,  " &
+"                     a.CLOSEDDATE,  " &
+"                     a.STATUS,  " &
 "                     a.TYPE ) WMS_RECEIPT  " &
 "         ON WMS_RECEIPT.EXTERNRECEIPTKEY = '0' || '_' || TRIM(whinh300.t$sfbp) || '_' || TRIM(whinh300.t$shid) || '_' || TRIM(whinh300.t$cwar)  " &
 "  " &
 "  LEFT JOIN ( select a.RECEIPTKEY,  " &
 "                     a.SKU,  " &
 "                     a.TOID,  " &
-"                     max(a.DATERECEIVED),  " &
+"                     max(a.DATERECEIVED) DATERECEIVED,  " &
 "                     sum(a.QTYRECEIVED) QTDE  " &
 "                from " + IIF(Parameters!Compania.Value = 601, "WMWHSE9", "WMWHSE10") + ".RECEIPTDETAIL@DL_LN_WMS  a  " &
 "            group by a.RECEIPTKEY,  " &
@@ -246,13 +305,19 @@ WHERE tdrec940.t$stat$l IN (4, 5)
 "   AND Trim(tdrec940.t$cnfe$l) is not null  " &
 "   AND tdrec940.t$rfdt$l = 1  " &
 "   AND whinh301.t$rqua != 0  " &
-"   AND WMS_RECEIPT.STATUS> = 11" &
-"   AND WMS_RECEIPT.TYPE != 8 " &
+"   AND WMS_RECEIPT.STATUS> = 11  " &
+"   AND WMS_RECEIPT.TYPE != 8  " &
 "   AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(whinh300.t$crdt,  " &
 "               'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  " &
 "                  AT time zone 'America/Sao_Paulo') AS DATE))  " &
 "       Between '" + Parameters!DataASNDe.Value + "'  " &
 "           And '" + Parameters!DataASNAte.Value + "'  " &
+"  " &
+"   AND Trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(WMS_ITRN.EFFECTIVEDATE,  " &
+"               'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  " &
+"                  AT time zone 'America/Sao_Paulo') AS DATE))  " &
+"       Between nvl('" + Parameters!DataARMDe.Value + "',trunc(WMS_ITRN.EFFECTIVEDATE))  " &
+"           And nvl('" + Parameters!DataARMAte.Value + "',trunc(WMS_ITRN.EFFECTIVEDATE))  " &
 "  " &
 "   AND (    (" + IIF(Parameters!Item.Value Is Nothing, "1", "0") + " = 1)  " &
 "          OR(    (Trim(whinh301.t$item) IN  " &
@@ -268,12 +333,19 @@ WHERE tdrec940.t$stat$l IN (4, 5)
 "          tcibd001.t$dscb$c,  " &
 "          whinh301.t$rqua,  " &
 "          REC941.QTDE,  " &
+"          WHINH300.T$ASNR,  " &
+"          WMS_RECEIPT.RECEIPTKEY,  " &
 "          CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(whinh300.t$crdt,  " &
 "            'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  " &
 "              AT time zone 'America/Sao_Paulo') AS DATE),  " &
 "          CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(WMS_RECEIPT.CLOSEDDATE,  " &
 "            'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  " &
-"              AT time zone 'America/Sao_Paulo') AS DATE)," &
-"          INH301.QTDE  " &
+"              AT time zone 'America/Sao_Paulo') AS DATE),  " &
+"          trunc(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(WMS_ITRN.EFFECTIVEDATE,  " &
+"             'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')  " &
+"               AT time zone 'America/Sao_Paulo') AS DATE)),  " &
+"          INH301.QTDE,  " &
+"		   tcibd001.t$mdfb$c,  " &
+"       WMS_REC_DETAIL.RECEIPTKEY  " &
 "  " &
-" ORDER BY REF_FISCAL, ITEM  "
+" ORDER BY REF_FISCAL, ITEM) q1  "
