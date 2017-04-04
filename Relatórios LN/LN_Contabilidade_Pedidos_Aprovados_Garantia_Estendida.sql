b63931ede784ed086ca56b945cd81439cbaf1d05
ronaldocs <ronaldo.cristiano@gmail.com>
2017-04-04 15:22:23 -0300

    Relatório de Pedidos Aprovados com Garantia Estendida - SDP 1368615
    Ajuste nos parametros do Relatório

--- Relatórios LN/LN_Contabilidade_Pedidos_Aprovados_Garantia_Estendida.sql
+++ Relatórios LN/LN_Contabilidade_Pedidos_Aprovados_Garantia_Estendida.sql
@@ -1,43 +1,41 @@
-select znsls400.t$pecl$c                                   PEDIDO,
-       znsls400.t$sqpd$c                                   SEQ_PEDIDO,
-       cast((from_tz(to_timestamp(to_char(znsls400.t$dtem$c,
-             'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-              AT time zone 'America/Sao_Paulo') as date)   DATA_EMISSAO,
-       znsls401.t$item$c                                   ITEM,
-       znsls401.t$orno$c                                   ORDEM_VENDA,
-       znsls401.t$qtve$c                                   QTDE_VENDIDA,
-       znsls401.t$vlun$c                                   VALOR_UNITARIO,
-       znsls412.t$ttyp$c                                   TIPO_TRANSACAO,
-       znsls412.t$ninv$c                                   DOCUMENTO,
-       case when znsls400.t$sige$c = 1 then
-            'SIM'
-       else
-            'NAO'
-       end                                                 PEDIDO_SIGE,
-       case when znsls400.t$migr$c = 1 then
-            'SIM'
-       else
-            'NAO'
-       end                                                 MIGRACAO
+     select znsls400.t$pecl$c                                   PEDIDO,
+            znsls400.t$sqpd$c                                   SEQ_PEDIDO,
+            cast((from_tz(to_timestamp(to_char(znsls400.t$dtem$c,
+                  'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                   AT time zone 'America/Sao_Paulo') as date)   DATA_EMISSAO,
+            znsls401.t$item$c                                   ITEM,
+            znsls401.t$orno$c                                   ORDEM_VENDA,
+            znsls401.t$qtve$c                                   QTDE_VENDIDA,
+            znsls401.t$vlun$c                                   VALOR_UNITARIO,
+            znsls412.t$ttyp$c                                   TIPO_TRANSACAO,
+            znsls412.t$ninv$c                                   DOCUMENTO,
+            case when znsls400.t$sige$c = 1 
+                   then 'SIM'
+                 else   'NAO'
+            end                                                 PEDIDO_SIGE,
+            case when znsls400.t$migr$c = 1 
+                   then 'SIM'
+                 else   'NAO'
+            end                                                 MIGRACAO
 
-  from ( select a.t$ncia$c,
-                a.t$uneg$c,
-                a.t$pecl$c,
-                a.t$sqpd$c,
-                a.t$item$c,
-                a.t$orno$c,
-                a.t$vlun$c,
-                a.t$qtve$c
-           from baandb.tznsls401301 a
-          where a.t$item$c in (5837695,5837696,5837697,5837698)
-       group by a.t$ncia$c,
-                a.t$uneg$c,
-                a.t$pecl$c,
-                a.t$sqpd$c,
-                a.t$item$c,
-                a.t$orno$c,
-                a.t$vlun$c,
-                a.t$qtve$c) znsls401
+      from ( select a.t$ncia$c,
+                    a.t$uneg$c,
+                    a.t$pecl$c,
+                    a.t$sqpd$c,
+                    a.t$item$c,
+                    a.t$orno$c,
+                    a.t$vlun$c,
+                    a.t$qtve$c
+               from baandb.tznsls401301 a
+              where a.t$item$c in (5837695,5837696,5837697,5837698)
+           group by a.t$ncia$c,
+                    a.t$uneg$c,
+                    a.t$pecl$c,
+                    a.t$sqpd$c,
+                    a.t$item$c,
+                    a.t$orno$c,
+                    a.t$vlun$c,
+                    a.t$qtve$c ) znsls401
 
 inner join baandb.tznsls400301 znsls400
         on znsls400.t$ncia$c = znsls401.t$ncia$c
@@ -45,30 +43,33 @@ inner join baandb.tznsls400301 znsls400
        and znsls400.t$pecl$c = znsls401.t$pecl$c
        and znsls400.t$sqpd$c = znsls401.t$sqpd$c
 
-left join ( select a.t$ncia$c,
-                   a.t$uneg$c,
-                   a.t$pecl$c,
-                   a.t$sqpd$c,
-                   a.t$orno$c,
-                   a.t$ttyp$c,
-                   a.t$ninv$c
-              from baandb.tznsls412301 a 
-          group by a.t$ncia$c,
-                   a.t$uneg$c,
-                   a.t$pecl$c,
-                   a.t$sqpd$c,
-                   a.t$orno$c,
-                   a.t$ttyp$c,
-                   a.t$ninv$c ) znsls412
-       on znsls412.t$ncia$c = znsls401.t$ncia$c
-      and znsls412.t$uneg$c = znsls401.t$uneg$c
-      and znsls412.t$pecl$c = znsls401.t$pecl$c
-      and znsls412.t$sqpd$c = znsls401.t$sqpd$c
+ left join ( select a.t$ncia$c,
+                    a.t$uneg$c,
+                    a.t$pecl$c,
+                    a.t$sqpd$c,
+                    a.t$orno$c,
+                    a.t$ttyp$c,
+                    a.t$ninv$c
+               from baandb.tznsls412301 a 
+           group by a.t$ncia$c,
+                    a.t$uneg$c,
+                    a.t$pecl$c,
+                    a.t$sqpd$c,
+                    a.t$orno$c,
+                    a.t$ttyp$c,
+                    a.t$ninv$c ) znsls412
+        on znsls412.t$ncia$c = znsls401.t$ncia$c
+       and znsls412.t$uneg$c = znsls401.t$uneg$c
+       and znsls412.t$pecl$c = znsls401.t$pecl$c
+       and znsls412.t$sqpd$c = znsls401.t$sqpd$c
 
-where trunc(cast((from_tz(to_timestamp(to_char(znsls400.t$dtem$c,
-                  'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-                   AT time zone 'America/Sao_Paulo') as date))
-      between :DATA_EMISSAO_DE
-          and :DATA_EMISSAO_ATE
-  and znsls400.t$pecl$c in (:PEDIDO)
-  and znsls400.t$sqpd$c in (:SEQ_PEDIDO)
+     where Trunc(cast((from_tz(to_timestamp(to_char(znsls400.t$dtem$c,
+                       'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                        AT time zone 'America/Sao_Paulo') as date))
+           Between :DataEmissaoDe
+               And :DataEmissaoAte
+			   
+       and (    (znsls400.t$pecl$c in (:Pedido) and :PedidoTodos = 1  )
+             or (:PedidoTodos = 0) )
+       and (    (znsls400.t$sqpd$c in (:Sequencial) and :SequencialTodos = 1  )
+             or (:SequencialTodos = 0) )
