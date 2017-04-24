b5ee45d51d8d0f05524f5fcfa3c60983cc4077ce
ronaldocs <ronaldo.cristiano@gmail.com>
2017-04-24 11:22:33 -0300

    Relatório Pré-Nota Recebimento Fiscal - SDP 1282240
    Ajuste nos filtros

--- Relatórios LN/LN_Transportes_Relatorio_Pre_Nota_Recebimento_Fiscal.sql
+++ Relatórios LN/LN_Transportes_Relatorio_Pre_Nota_Recebimento_Fiscal.sql
@@ -1,144 +1,140 @@
-select  
-        brnfe940.t$fire$l                        REFERENCIA_FISCAL,
-        STATUS.DESCR                             STATUS,
-        brnfe940.t$frec$l                        FISCAL_RECEIPT,
-        brnfe940.t$fovn$l                        ID_FISCAL,
-        brnfe940.t$fids$l                        RAZAO_SOCIAL,
-        brnfe940.t$docn$l                        NRO_DOCUMENTO_FISCAL,
-        brnfe940.t$seri$l                        SERIE_DOCUMENTO_FISCAL,
-        TIPO_DOC.DESCR                           TIPO_DOCUMENTO_FISCAL,
-        brnfe940.t$idat$l                        DATA_EMISSAO,
-        brnfe940.t$cnfe$l                        LOCALIZADOR_NFE,
-        case when brnfe940.t$sige$c = 1 then
-                  'Sim'
-        else
-                  'Não'
-        end                                      SIGE,
-        CTE_CTRC.DESCR                           COMPLEMENTO_CTE_CTRC,
-        ORIG_FRETE.DESCR                         ORIGEM_ORDEM_FRETE
+   select brnfe940.t$fire$l                        REFERENCIA_FISCAL,
+          STATUS.DESCR                             STATUS,
+          brnfe940.t$frec$l                        FISCAL_RECEIPT,
+          brnfe940.t$fovn$l                        ID_FISCAL,
+          brnfe940.t$fids$l                        RAZAO_SOCIAL,
+          brnfe940.t$docn$l                        NRO_DOCUMENTO_FISCAL,
+          brnfe940.t$seri$l                        SERIE_DOCUMENTO_FISCAL,
+          TIPO_DOC.DESCR                           TIPO_DOCUMENTO_FISCAL,
+          brnfe940.t$idat$l                        DATA_EMISSAO,
+          brnfe940.t$cnfe$l                        LOCALIZADOR_NFE,
+          case when brnfe940.t$sige$c = 1 
+                 then 'Sim'
+               else   'Não'
+          end                                      SIGE,
+          CTE_CTRC.DESCR                           COMPLEMENTO_CTE_CTRC,
+          ORIG_FRETE.DESCR                         ORIGEM_ORDEM_FRETE
+   
+     from baandb.tbrnfe940301 brnfe940
 
-from    baandb.tbrnfe940301 brnfe940
+left join ( select l.t$desc DESCR,
+                   d.t$cnst
+              from baandb.tttadv401000 d,
+                   baandb.tttadv140000 l
+             where d.t$cpac = 'zn'
+               and d.t$cdom = 'nfe.stpr.c'
+               and l.t$clan = 'p'
+               and l.t$cpac = 'zn'
+               and l.t$clab = d.t$za_clab
+               and rpad(d.t$vers,4) ||
+                   rpad(d.t$rele,2) ||
+                   rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
+                                                   rpad(l1.t$rele,2) ||
+                                                   rpad(l1.t$cust,4)) 
+                                          from baandb.tttadv401000 l1 
+                                         where l1.t$cpac = d.t$cpac 
+                                           and l1.t$cdom = d.t$cdom )
+               and rpad(l.t$vers,4) ||
+                   rpad(l.t$rele,2) ||
+                   rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
+                                                   rpad(l1.t$rele,2) ||
+                                                   rpad(l1.t$cust,4)) 
+                                          from baandb.tttadv140000 l1 
+                                         where l1.t$clab = l.t$clab 
+                                           and l1.t$clan = l.t$clan 
+                                           and l1.t$cpac = l.t$cpac ) ) STATUS
+       on STATUS.t$cnst = brnfe940.t$stpr$c
+     
+left join ( select l.t$desc DESCR,
+                   d.t$cnst
+              from baandb.tttadv401000 d,
+                   baandb.tttadv140000 l
+             where d.t$cpac = 'tc'
+               and d.t$cdom = 'mcs.tror.l'
+               and l.t$clan = 'p'
+               and l.t$cpac = 'tc'
+               and l.t$clab = d.t$za_clab
+               and rpad(d.t$vers,4) ||
+                   rpad(d.t$rele,2) ||
+                   rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
+                                                   rpad(l1.t$rele,2) ||
+                                                   rpad(l1.t$cust,4)) 
+                                          from baandb.tttadv401000 l1 
+                                         where l1.t$cpac = d.t$cpac 
+                                           and l1.t$cdom = d.t$cdom )
+               and rpad(l.t$vers,4) ||
+                   rpad(l.t$rele,2) ||
+                   rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
+                                                   rpad(l1.t$rele,2) ||
+                                                   rpad(l1.t$cust,4)) 
+                                          from baandb.tttadv140000 l1 
+                                         where l1.t$clab = l.t$clab 
+                                           and l1.t$clan = l.t$clan 
+                                           and l1.t$cpac = l.t$cpac ) ) TIPO_DOC
+       on TIPO_DOC.t$cnst = brnfe940.t$fdot$l
+     
+left join ( select l.t$desc DESCR,
+                   d.t$cnst
+              from baandb.tttadv401000 d,
+                   baandb.tttadv140000 l
+             where d.t$cpac = 'tc'
+               and d.t$cdom = 'yesno'
+               and l.t$clan = 'p'
+               and l.t$cpac = 'tc'
+               and l.t$clab = d.t$za_clab
+               and rpad(d.t$vers,4) ||
+                   rpad(d.t$rele,2) ||
+                   rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
+                                                   rpad(l1.t$rele,2) ||
+                                                   rpad(l1.t$cust,4)) 
+                                          from baandb.tttadv401000 l1 
+                                         where l1.t$cpac = d.t$cpac 
+                                           and l1.t$cdom = d.t$cdom )
+               and rpad(l.t$vers,4) ||
+                   rpad(l.t$rele,2) ||
+                   rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
+                                                   rpad(l1.t$rele,2) ||
+                                                   rpad(l1.t$cust,4)) 
+                                          from baandb.tttadv140000 l1 
+                                         where l1.t$clab = l.t$clab 
+                                           and l1.t$clan = l.t$clan 
+                                           and l1.t$cpac = l.t$cpac ) ) CTE_CTRC
+       on CTE_CTRC.t$cnst = brnfe940.t$coct$c
+     
+left join ( select l.t$desc DESCR,
+                   d.t$cnst
+              from baandb.tttadv401000 d,
+                   baandb.tttadv140000 l
+             where d.t$cpac = 'zn'
+               and d.t$cdom = 'mcs.trans.c'
+               and l.t$clan = 'p'
+               and l.t$cpac = 'zn'
+               and l.t$clab = d.t$za_clab
+               and rpad(d.t$vers,4) ||
+                   rpad(d.t$rele,2) ||
+                   rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
+                                                   rpad(l1.t$rele,2) ||
+                                                   rpad(l1.t$cust,4)) 
+                                          from baandb.tttadv401000 l1 
+                                         where l1.t$cpac = d.t$cpac 
+                                           and l1.t$cdom = d.t$cdom )
+               and rpad(l.t$vers,4) ||
+                   rpad(l.t$rele,2) ||
+                   rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
+                                                   rpad(l1.t$rele,2) ||
+                                                   rpad(l1.t$cust,4)) 
+                                          from baandb.tttadv140000 l1 
+                                         where l1.t$clab = l.t$clab 
+                                           and l1.t$clan = l.t$clan 
+                                           and l1.t$cpac = l.t$cpac ) )
+                                                                 ORIG_FRETE
+       on ORIG_FRETE.t$cnst = brnfe940.t$torg$c
 
-left  join ( select l.t$desc DESCR,
-                    d.t$cnst
-               from baandb.tttadv401000 d,
-                    baandb.tttadv140000 l
-              where d.t$cpac = 'zn'
-                and d.t$cdom = 'nfe.stpr.c'
-                and l.t$clan = 'p'
-                and l.t$cpac = 'zn'
-                and l.t$clab = d.t$za_clab
-                and rpad(d.t$vers,4) ||
-                    rpad(d.t$rele,2) ||
-                    rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
-                                                    rpad(l1.t$rele,2) ||
-                                                    rpad(l1.t$cust,4)) 
-                                           from baandb.tttadv401000 l1 
-                                          where l1.t$cpac = d.t$cpac 
-                                            and l1.t$cdom = d.t$cdom )
-                and rpad(l.t$vers,4) ||
-                    rpad(l.t$rele,2) ||
-                    rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
-                                                    rpad(l1.t$rele,2) ||
-                                                    rpad(l1.t$cust,4)) 
-                                           from baandb.tttadv140000 l1 
-                                          where l1.t$clab = l.t$clab 
-                                            and l1.t$clan = l.t$clan 
-                                            and l1.t$cpac = l.t$cpac ) )
-                                                                  STATUS
-        on STATUS.t$cnst = brnfe940.t$stpr$c
+    where Trunc(cast((FROM_TZ(TO_TIMESTAMP(TO_CHAR(brnfe940.t$idat$l,
+                'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                 AT time zone 'America/Sao_Paulo') as date))
+          Between '01/04/2017' --:DataEmissaoDe
+              And '24/04/2017' --:DataEmissaoAte
+      and brnfe940.t$stpr$c in (:Status)
+      and ( (Trim(:ID_FISCAL) is null) OR (regexp_replace(brnfe940.t$fovn$l, '[^0-9]', '') Like '%' || regexp_replace(Trim(:ID_FISCAL), '[^0-9]', '') || '%') )
 
-left  join ( select l.t$desc DESCR,
-                    d.t$cnst
-               from baandb.tttadv401000 d,
-                    baandb.tttadv140000 l
-              where d.t$cpac = 'tc'
-                and d.t$cdom = 'mcs.tror.l'
-                and l.t$clan = 'p'
-                and l.t$cpac = 'tc'
-                and l.t$clab = d.t$za_clab
-                and rpad(d.t$vers,4) ||
-                    rpad(d.t$rele,2) ||
-                    rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
-                                                    rpad(l1.t$rele,2) ||
-                                                    rpad(l1.t$cust,4)) 
-                                           from baandb.tttadv401000 l1 
-                                          where l1.t$cpac = d.t$cpac 
-                                            and l1.t$cdom = d.t$cdom )
-                and rpad(l.t$vers,4) ||
-                    rpad(l.t$rele,2) ||
-                    rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
-                                                    rpad(l1.t$rele,2) ||
-                                                    rpad(l1.t$cust,4)) 
-                                           from baandb.tttadv140000 l1 
-                                          where l1.t$clab = l.t$clab 
-                                            and l1.t$clan = l.t$clan 
-                                            and l1.t$cpac = l.t$cpac ) )
-                                                                  TIPO_DOC
-        on TIPO_DOC.t$cnst = brnfe940.t$fdot$l
-
-left  join ( select l.t$desc DESCR,
-                    d.t$cnst
-               from baandb.tttadv401000 d,
-                    baandb.tttadv140000 l
-              where d.t$cpac = 'tc'
-                and d.t$cdom = 'yesno'
-                and l.t$clan = 'p'
-                and l.t$cpac = 'tc'
-                and l.t$clab = d.t$za_clab
-                and rpad(d.t$vers,4) ||
-                    rpad(d.t$rele,2) ||
-                    rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
-                                                    rpad(l1.t$rele,2) ||
-                                                    rpad(l1.t$cust,4)) 
-                                           from baandb.tttadv401000 l1 
-                                          where l1.t$cpac = d.t$cpac 
-                                            and l1.t$cdom = d.t$cdom )
-                and rpad(l.t$vers,4) ||
-                    rpad(l.t$rele,2) ||
-                    rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
-                                                    rpad(l1.t$rele,2) ||
-                                                    rpad(l1.t$cust,4)) 
-                                           from baandb.tttadv140000 l1 
-                                          where l1.t$clab = l.t$clab 
-                                            and l1.t$clan = l.t$clan 
-                                            and l1.t$cpac = l.t$cpac ) )
-                                                                  CTE_CTRC
-        on CTE_CTRC.t$cnst = brnfe940.t$coct$c
-
-left  join ( select l.t$desc DESCR,
-                    d.t$cnst
-               from baandb.tttadv401000 d,
-                    baandb.tttadv140000 l
-              where d.t$cpac = 'zn'
-                and d.t$cdom = 'mcs.trans.c'
-                and l.t$clan = 'p'
-                and l.t$cpac = 'zn'
-                and l.t$clab = d.t$za_clab
-                and rpad(d.t$vers,4) ||
-                    rpad(d.t$rele,2) ||
-                    rpad(d.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
-                                                    rpad(l1.t$rele,2) ||
-                                                    rpad(l1.t$cust,4)) 
-                                           from baandb.tttadv401000 l1 
-                                          where l1.t$cpac = d.t$cpac 
-                                            and l1.t$cdom = d.t$cdom )
-                and rpad(l.t$vers,4) ||
-                    rpad(l.t$rele,2) ||
-                    rpad(l.t$cust,4) = ( select max(rpad(l1.t$vers,4) ||
-                                                    rpad(l1.t$rele,2) ||
-                                                    rpad(l1.t$cust,4)) 
-                                           from baandb.tttadv140000 l1 
-                                          where l1.t$clab = l.t$clab 
-                                            and l1.t$clan = l.t$clan 
-                                            and l1.t$cpac = l.t$cpac ) )
-                                                                  ORIG_FRETE
-        on ORIG_FRETE.t$cnst = brnfe940.t$torg$c
-
-where   trunc(cast((FROM_TZ(TO_TIMESTAMP(TO_CHAR(brnfe940.t$idat$l,
-              'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-               AT time zone 'America/Sao_Paulo') as date))
-        between :DATA_EMISSAO_DE
-            and :DATA_EMISSAO_ATE
-  and   brnfe940.t$stpr$c in (:STATUS)
-  and   brnfe940.t$fovn$l in (:ID_FISCAL)
