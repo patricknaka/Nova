bf4b7927f929a618eeda0cba90c1e084ae097809
Humberto Kasai <humberto.kasai@outlook.com>
2017-05-05 12:32:22 -0300

    SDP 1393458

--- Relatórios LN/LN_Transportes_Relatorio_TNA.sql
+++ Relatórios LN/LN_Transportes_Relatorio_TNA.sql
@@ -1,4 +1,4 @@
-select DISTINCT
+select
     znint002.t$desc$c   UNEG,
     znsls401.t$pecl$c   PEDIDO_CLIENTE,
     znsls401.t$entr$c   ENTREGA,
@@ -13,12 +13,11 @@ select DISTINCT
     tcmcs080.T$dsca     NOME_TRANSPORTADORA,
     znsls401.t$itpe$c   TIPO_DE_ENTREGA,
     znsls002.t$dsca$c   DESCRICAO_TIPO_ENTREGA,
-    znsls401.t$orno$c   ORDEM_DE_VENDA,
+    znsls401.t$orno$c   ORDEM_DE_VENDA_PEDIDO,
     tdsls400.t$sotp     TIPO_ORDEM_VENDAS,
     tdsls094.t$dsca     MOTIVO_TNA,
     TIPO_TRANSP.DESCR   TIPO_DE_TRANSPORTE,
-
-    tdsls420.t$orno     ORDEM_VENDA,
+    tdsls420.t$orno     ORDEM_VENDA_TNA,
     tdsls420.t$hrea     BLOQUEIO,
     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znsls401.t$dtep$c, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
           AT time zone 'America/Sao_Paulo') AS DATE)  
@@ -34,29 +33,60 @@ select DISTINCT
                         DATA_ENTREGA_PLANEJADA,
     CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(tdsls401.t$prdt, 'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
           AT time zone 'America/Sao_Paulo') AS DATE)  
-                        DATA_RECEBIMENTO_PLANEJADO
-          
+                        DATA_RECEBIMENTO_PLANEJADO,
+    case when tdsls400.t$fdty$l = 14 then   --OV TNA = retorno mercadoria cliente
+          case when znsls400.t$idpo$c = 'TD' then
+                znsls401.t$entr$c
+          end
+    end                 ENTREGA_DEVOLUCAO_TNA,
+    case when tdsls400.t$fdty$l = 14 then   --OV TNA = retorno mercadoria cliente
+        case when znsls400.t$sige$c = 1 then   --sim
+            'SIGE'
+        else
+            NVL(znsls401_venda.t$orno$c,znsls401.t$orno$c)
+        end
+    else
+        znsls401.t$orno$c
+    end                 OV_SAIDA_TNA,
+    case when tdsls400.t$fdty$l = 14 then   --OV TNA = retorno mercadoria cliente
+        case when znsls400.t$sige$c = 1 then
+            'SIGE'
+        else
+            NVL(tdsls400_venda.t$ictr$c,tdsls400_INS.t$ictr$c)
+        end
+    else
+        tdsls400.t$ictr$c
+    end                 CONTRATO_OV_SAIDA_TNA
+
 from baandb.ttdsls420301 tdsls420
 
 INNER join  baandb.tznsls004301 znsls004  
         on  znsls004.t$orno$c = tdsls420.t$orno
+       and  znsls004.t$pono$c = 10
 
-INNER join  baandb.ttdsls401301 tdsls401
-        on  tdsls401.t$orno = znsls004.t$orno$c
-       and  tdsls401.t$pono = znsls004.t$pono$c
-
-INNER join  baandb.ttdsls400301 tdsls400
-        on  tdsls400.t$orno = znsls004.t$orno$c
-
-INNER join baandb.ttdsls094301 tdsls094
-        on tdsls094.t$sotp = tdsls400.t$sotp
+INNER join  baandb.tznsls400301 znsls400
+        on znsls400.t$ncia$c = znsls004.t$ncia$c
+       and znsls400.t$uneg$c = znsls004.t$uneg$c
+       and znsls400.t$pecl$c = znsls004.t$pecl$c
+       and znsls400.t$sqpd$c = znsls004.t$sqpd$c
         
 INNER join  baandb.tznsls401301 znsls401
-         on znsls401.t$ncia$c = znsls004.t$ncia$c
+        on znsls401.t$ncia$c = znsls004.t$ncia$c
        and znsls401.t$uneg$c = znsls004.t$uneg$c
        and znsls401.t$pecl$c = znsls004.t$pecl$c
        and znsls401.t$sqpd$c = znsls004.t$sqpd$c
        and znsls401.t$entr$c = znsls004.t$entr$c
+       and znsls401.t$sequ$c = znsls004.t$sequ$c
+
+INNER join  baandb.ttdsls400301 tdsls400
+        on  tdsls400.t$orno = znsls004.t$orno$c
+        
+INNER join  baandb.ttdsls401301 tdsls401
+        on  tdsls401.t$orno = znsls004.t$orno$c
+       and  tdsls401.t$pono = znsls004.t$pono$c
+
+INNER join baandb.ttdsls094301 tdsls094
+        on tdsls094.t$sotp = tdsls400.t$sotp
 
 INNER join baandb.tznint002301 znint002
         on znint002.t$ncia$c = znsls004.t$ncia$c
@@ -93,7 +123,14 @@ left join ( select a.t$ncia$c,
        and znsls410.t$sqpd$c = znsls401.t$sqpd$c
        and znsls410.t$entr$c = znsls401.t$entr$c
 
-left join baandb.tznfmd630301 znfmd630
+left join ( select a.t$orno$c,
+                   a.t$cfrw$c,
+                   a.t$cono$c
+            from baandb.tznfmd630301 a
+            where a.t$ncte$c != ' '     --ha ordens de frete com mais de uma transportadora, por erro na base
+            group by  a.t$orno$c,
+                      a.t$cfrw$c,
+                      a.t$cono$c ) znfmd630
         on znfmd630.t$orno$c = znsls401.t$orno$c
         
 left join baandb.tznfmd060301 znfmd060
@@ -134,6 +171,22 @@ left join  (  SELECT  d.t$cnst,
                                            and l1.t$clan = l.t$clan 
                                            and l1.t$cpac = l.t$cpac )) TIPO_TRANSP                               
           on TIPO_TRANSP.t$cnst = znfmd060.t$ttra$c 
-        
+
+left join baandb.tznsls401301 znsls401_venda
+       on znsls401_venda.t$ncia$c = znsls401.t$ncia$c
+      and znsls401_venda.t$uneg$c = znsls401.t$uneg$c
+      and znsls401_venda.t$pecl$c = znsls401.t$pvdt$c
+      and znsls401_venda.t$sqpd$c = znsls401.t$sedt$c
+      and znsls401_venda.t$entr$c = znsls401.t$endt$c
+      and znsls401_venda.t$sequ$c = znsls401.t$sidt$c
+      
+left join baandb.ttdsls400301 tdsls400_venda
+       on tdsls400_venda.t$orno = znsls401_venda.t$orno$c
+
+left join baandb.ttdsls400301 tdsls400_INS
+       on tdsls400_INS.t$orno = znsls401.t$orno$c
+       
 where tdsls420.t$pono = 0
   and tdsls420.t$hrea = 'TNA'
+  
+  
