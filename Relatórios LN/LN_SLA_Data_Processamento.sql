fad3a46f774ea26530fa32b0026cd8eb9fd824ad
Humberto Kasai <humberto.kasai@outlook.com>
2017-05-23 18:50:29 -0300

    SDP 1401315

--- Relatórios LN/LN_SLA_Data_Processamento.sql
+++ Relatórios LN/LN_SLA_Data_Processamento.sql
@@ -1,4 +1,5 @@
-    SELECT znsls401.t$entr$c          ENTREGA,
+SELECT /*+ USE_CONCAT NO_CPU_COSTING */
+           znsls401.t$entr$c          ENTREGA,
            znsls401.t$sequ$c          SEQUENCIAL,
            znfmd630.t$docn$c          NOTA,
            znfmd630.t$seri$c          SERIE,
@@ -19,23 +20,19 @@
                              AT time zone 'America/Sao_Paulo') AS DATE)
                                     DATA_PROMETIDA,
                                    
-           CASE WHEN OCORR.DT_PROC < = to_date('01-01-1980','DD-MM-YYYY') 
+           CASE WHEN znfmd640.t$udat$c < = to_date('01-01-1980','DD-MM-YYYY') 
                   THEN NULL
-                ELSE CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(OCORR.DT_PROC, 
-                       'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-                         AT time zone 'America/Sao_Paulo') AS DATE)
+           ELSE   znfmd640.t$udat$c
            END                      DATA_PROCESSAMENTO,
-           OCORR.PONTO              OCORRENCIA,           --ALTERADO 
+           znfmd640.t$coci$c        OCORRENCIA,           --ALTERADO 
            znfmd040.t$dotr$c        DESC_OCORRENCIA,      --ALTERADO
            
-           CASE WHEN OCORR.DT < = to_date('01-01-1980','DD-MM-YYYY') 
+           CASE WHEN znfmd640.t$date$c < = to_date('01-01-1980','DD-MM-YYYY') 
                   THEN NULL
-                ELSE   CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(OCORR.DT, 
-                              'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
-                                AT time zone 'America/Sao_Paulo') AS DATE)
+                ELSE   znfmd640.t$date$c
             END                     DATA_OCORRENCIA,      --ALTERADO
                
-           CASE WHEN OCORR.LOGIN_PROC IN ('job_prod','jobptms', 'job_psup')
+           CASE WHEN znfmd640.t$ulog$c IN ('job_prod','jobptms', 'job_psup')
                   THEN 'EDI'
                 ELSE   'MANUAL' 
            END                      BAIXA_MANUAL_EDI,
@@ -50,7 +47,7 @@
            znsls401.t$itpe$c        TIPO_ENTREGA,
            znsls002.t$dsca$c        DESC_TIPO_ENTREGA,
       
-           CASE WHEN OCORR.t$finz$c = 1
+           CASE WHEN znfmd640.t$finz$c = 1
                   THEN 'F'
                 ELSE   'P' 
            END                      FINALIZADO_PENDENTE,
@@ -59,14 +56,26 @@
                AT time zone 'America/Sao_Paulo') AS DATE)  
                                     DATA_COMPRA,
                                           
-           CASE WHEN tcibd001.t$tptr$c = 1 THEN 'PESADO'
-                WHEN tcibd001.t$tptr$c = 2 THEN 'LEVE'
-                WHEN tcibd001.t$tptr$c = 3 THEN 'LIQUIDO'
+           CASE WHEN ( select a.t$tptr$c 
+                       from baandb.ttcibd001301 a
+                       where a.t$item = znsls401.t$itml$c )
+                                        = 1 THEN 'PESADO'
+                WHEN ( select a.t$tptr$c 
+                       from baandb.ttcibd001301 a
+                       where a.t$item = znsls401.t$itml$c ) 
+                                        = 2 THEN 'LEVE'
+                WHEN ( select a.t$tptr$c 
+                       from baandb.ttcibd001301 a
+                       where a.t$item = znsls401.t$itml$c ) = 
+                                        3 THEN 'LIQUIDO'
            END                      TIPO_ENTREGA_TRANSPORTES,
       
            znsls401.t$iitm$c        PRODUTO_NAO_PRODUTO,
-           Trim(tcibd001.t$item)    ITEM,
-           tcibd001.t$dsca          DSC_ITEM,
+           Trim(znsls401.t$itml$c)  ITEM,
+           ( select a.t$dscb$c 
+             from baandb.ttcibd001301 a
+             where a.t$item = znsls401.t$itml$c )
+                                    DSC_ITEM,
            
            znfmd630.t$fili$c        COD_FILIAL,
            
@@ -98,74 +107,73 @@
                          AT time zone 'America/Sao_Paulo') AS DATE)
            END                      DATA_ENTREGA            --Ocorrencia finalizadora
 
-      FROM ( select znfmd630.t$pecl$c,
-                    znfmd630.t$fili$c,
-                    min(znfmd630.t$etiq$c)t$etiq$c,
-                    znfmd630.t$cfrw$c,
-                    znfmd630.t$cono$c,
-                    znfmd630.t$stat$c,
-                    znfmd630.t$docn$c,
-                    znfmd630.t$seri$c,
-                    znfmd630.t$vlfc$c,
-                    znfmd630.t$dtco$c 
-               from baandb.tznfmd630301 znfmd630
-           group by znfmd630.t$pecl$c,
-                    znfmd630.t$fili$c,
-                    znfmd630.t$cfrw$c,
-                    znfmd630.t$cono$c,
-                    znfmd630.t$stat$c,
-                    znfmd630.t$docn$c,
-                    znfmd630.t$seri$c,
-                    znfmd630.t$vlfc$c,
-                    znfmd630.t$dtco$c ) znfmd630
+      FROM ( select  CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(max(znfmd640.t$udat$c),                      
+                    'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')         
+                     AT time zone 'America/Sao_Paulo') AS DATE)
+                                             t$udat$c,
+                    znfmd640.t$udat$c       DT_GMT,
+                    znfmd630.t$pecl$c,
+                    znfmd630.t$orno$c,
+                    max(znfmd640.t$etiq$c) KEEP (DENSE_RANK LAST ORDER BY znfmd640.t$udat$c ) t$etiq$c,
+                    znfmd640.t$coci$c,
+                    CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(max(znfmd640.t$date$c),                      
+                   'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')         
+                     AT time zone 'America/Sao_Paulo') AS DATE)
+                                             t$date$c,
+                    znfmd640.t$ulog$c,
+                    znfmd640.t$fili$c,
+                    znfmd030.t$finz$c
+               from BAANDB.tznfmd640301 znfmd640
+               inner join baandb.tznfmd630301 znfmd630
+                       on znfmd640.t$fili$c = znfmd630.t$fili$c
+                      and znfmd640.t$etiq$c = znfmd630.t$etiq$c
+               inner join baandb.tznfmd030301 znfmd030
+                       on znfmd030.t$ocin$c = znfmd640.t$coci$c 
+--               where znfmd640.t$udat$c Between  to_date('15-05-2017', 'DD-MM-YYYY')+0.125 And to_date('15-05-2017', 'DD-MM-YYYY')+1.12499
+               where znfmd640.t$udat$c Between  trunc(TO_DATE(:DataProcessamentoDe))-1 And trunc(TO_DATE(:DataProcessamentoAte))+1
+               group by znfmd630.t$pecl$c,
+                        znfmd630.t$orno$c,
+                        znfmd640.t$coci$c,
+                        znfmd640.t$udat$c,
+                        znfmd640.t$date$c,
+                        znfmd640.t$ulog$c,
+                        znfmd640.t$fili$c,
+                        znfmd030.t$finz$c ) znfmd640
 
+ INNER JOIN baandb.tznfmd630301 znfmd630
+         ON znfmd630.t$etiq$c = znfmd640.t$etiq$c
+        AND znfmd630.t$orno$c = znfmd640.t$orno$c
+       
  LEFT JOIN baandb.tznsls401301 znsls401
         ON TO_CHAR(znsls401.t$entr$c) = znfmd630.t$pecl$c
-
-INNER JOIN BAANDB.ttcibd001301 tcibd001
-        ON tcibd001.t$item = znsls401.t$itml$c
-
+         
  LEFT JOIN baandb.ttdsls400301 tdsls400
         ON tdsls400.t$orno = znsls401.t$orno$c
 
  LEFT JOIN baandb.ttdsls401301 tdsls401
         ON tdsls401.t$orno = znsls401.t$orno$c
        AND tdsls401.t$pono = znsls401.t$pono$c
-
+       AND tdsls401.t$sqnb = 0
+          
  LEFT JOIN baandb.tznsls400301 znsls400 --DTEM 
         ON znsls400.T$NCIA$C = znsls401.T$NCIA$C
        AND znsls400.T$UNEG$C = znsls401.T$UNEG$C
        AND znsls400.T$PECL$C = znsls401.T$PECL$C
        AND znsls400.T$SQPD$C = znsls401.T$SQPD$C
 
- LEFT JOIN ( select znfmd640d.t$coci$c        PONTO,
-                    znfmd640d.t$date$c        DT,
-                    znfmd640d.t$udat$c        DT_PROC,
-                    znfmd640d.t$ulog$c        LOGIN_PROC,
-                    znfmd640d.t$fili$c,
-                    znfmd640d.t$etiq$c,
-                    znfmd030.t$finz$c
-               from BAANDB.tznfmd640301 znfmd640d 
-         inner join baandb.tznfmd030301 znfmd030
-                 on znfmd030.t$ocin$c = znfmd640d.t$coci$c ) OCORR     --ALTERADO
-        ON OCORR.t$fili$c = znfmd630.t$fili$c
-       AND OCORR.t$etiq$c = znfmd630.t$etiq$c 
-
  LEFT JOIN baandb.tznfmd040301  znfmd040
         ON znfmd040.t$cfrw$c = znfmd630.t$cfrw$c
-       AND znfmd040.t$ocin$c = OCORR.PONTO
+       AND znfmd040.t$ocin$c = znfmd640.t$coci$c
 
-INNER JOIN ( select znfmd640d.t$date$c  DT,
-                    znfmd640d.t$fili$c,
-                    znfmd640d.t$etiq$c
+  INNER JOIN ( select znfmd640d.t$fili$c,
+                      znfmd640d.t$etiq$c,
+                      max(znfmd640d.t$date$c) DT
                from BAANDB.tznfmd640301 znfmd640d
-              where znfmd640d.t$date$c = (  SELECT max(znfmd640x.t$date$c) KEEP (DENSE_RANK LAST ORDER BY znfmd640x.t$date$c,  znfmd640x.t$udat$c)
-                                              FROM BAANDB.tznfmd640301 znfmd640x
-                                             WHERE znfmd640x.t$fili$c = znfmd640d.t$fili$c                                      
-                                               AND znfmd640x.t$etiq$c = znfmd640d.t$etiq$c
-                                               AND znfmd640x.t$coci$c = 'ETR' ) )   ETR_OCCUR
+               where znfmd640d.t$coci$c = 'ETR'
+               group by znfmd640d.t$fili$c,
+                        znfmd640d.t$etiq$c )  ETR_OCCUR
         ON ETR_OCCUR.t$fili$c = znfmd630.t$fili$c
-       AND ETR_OCCUR.t$etiq$c = znfmd630.t$etiq$c 
+       AND ETR_OCCUR.t$etiq$c = znfmd630.t$etiq$c
 
  LEFT JOIN baandb.ttcmcs080301  tcmcs080
         ON tcmcs080.t$cfrw = znfmd630.t$cfrw$c
@@ -183,7 +191,7 @@ INNER JOIN ( select znfmd640d.t$date$c  DT,
  LEFT JOIN ( select ttaad200.t$user,
                     ttaad200.t$name
                from baandb.tttaad200000 ttaad200 ) usuario
-        ON usuario.t$user = OCORR.LOGIN_PROC
+        ON usuario.t$user = znfmd640.t$ulog$c
 
  LEFT JOIN ( select znfmd062.t$creg$c,
                     znfmd062.t$cfrw$c,
@@ -193,8 +201,8 @@ INNER JOIN ( select znfmd640d.t$date$c  DT,
                from baandb.tznfmd062301  znfmd062 ) znfmd062
         ON znfmd062.t$cfrw$c = znfmd630.t$cfrw$c
        AND znfmd062.t$cono$c = znfmd630.t$cono$c
-       AND znfmd062.t$cepd$c <= ZNSLS401.t$cepe$c
-       AND znfmd062.t$cepa$c >= ZNSLS401.t$cepe$c
+       AND znsls401.t$cepe$c between znfmd062.t$cepd$c and znfmd062.t$cepa$c
+       AND znsls401.t$cepe$c != 0 
 
  LEFT JOIN baandb.tznfmd061301  znfmd061
         ON znfmd061.t$cfrw$c = znfmd630.t$cfrw$c
@@ -216,21 +224,16 @@ INNER JOIN ( select znfmd640d.t$date$c  DT,
         ON OCORR_FIM.t$fili$c = znfmd630.t$fili$c
        AND OCORR_FIM.t$etiq$c = znfmd630.t$etiq$c
 
-     WHERE Trunc(NVL(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(OCORR.DT_PROC,                      
-                   'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')         
-                     AT time zone 'America/Sao_Paulo') AS DATE), :DataProcessamentoDe))
-           Between :DataProcessamentoDe
-               And :DataProcessamentoAte
-       AND CASE WHEN OCORR.t$finz$c = 1
-                  THEN 'F'
-                ELSE   'P' 
-           END IN (:FinalizadoPendente)
-                      
+ WHERE znfmd640.t$finz$c IN (:FinalizadoPendente)
+
+  and znfmd640.t$udat$c between TO_DATE(:DataProcessamentoDe)  
+							and trunc(TO_DATE(:DataProcessamentoAte))+0.99999 
+ 
 ORDER BY znsls401.t$ncia$c,
          znsls401.t$uneg$c,
          znsls401.t$pecl$c,
          znsls401.t$sqpd$c,
          znsls401.t$entr$c,
          znsls401.t$sequ$c,
-         OCORR.DT,
-         OCORR.PONTO
+         znfmd640.t$date$c,
+         znfmd640.t$coci$c
