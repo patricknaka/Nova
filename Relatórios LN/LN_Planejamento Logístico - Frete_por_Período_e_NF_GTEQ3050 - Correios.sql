9b4e2fbacffa41488120484af308fd6095b6474d
Humberto Kasai <humberto.kasai@outlook.com>
2017-10-24 10:29:30 -0200

    Exclusão do own.mis.filtro

--- Relatórios LN/LN_Planejamento Logístico - Frete_por_Período_e_NF_GTEQ3050 - Correios.sql
+++ Relatórios LN/LN_Planejamento Logístico - Frete_por_Período_e_NF_GTEQ3050 - Correios.sql
@@ -17,10 +17,32 @@ SELECT /*+ use_concat no_cpu_costing */
           from baandb.ttcmcs080301 a
           where a.t$cfrw = znfmd630.t$cfrw$c)         TRANSPORTADORA,
        znfmd630.t$cono$c                              COD_CONTRATO,
-       cast(replace(replace(own_mis.filtro_mis(znfmd060.t$refe$c),';',''),'"','')   as varchar(100))  
+--      cast(replace(replace(own_mis.filtro_mis(znfmd060.t$refe$c),';',''),'"','')   as varchar(100))  
+      replace(replace(znfmd060.t$refe$c,';',''),'"','')
                                                       ID_EXT_CONTRATO,
        NVL(tcmcs031.t$dsca, 'Pedido Interno')         MARCA,
-       ORIGEM.DESCR                                   ORIGEM_ORDEM_FRETE
+       ORIGEM.DESCR                                   ORIGEM_ORDEM_FRETE,
+       ( select CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(a.t$date$c,
+                'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                AT time zone 'America/Sao_Paulo') AS DATE)
+          from BAANDB.tznfmd640301 a
+          where a.t$fili$c = znfmd630.t$fili$c
+            and a.t$etiq$c = znfmd630.t$etiq$c
+            and a.t$coci$c = 'ENT'
+            and ROWNUM = 1 )                          DATA_ENTREGA,
+        ( select CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(a.t$date$c,
+                'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                AT time zone 'America/Sao_Paulo') AS DATE)
+          from BAANDB.tznfmd640301 a
+          where a.t$fili$c = znfmd630.t$fili$c
+            and a.t$etiq$c = znfmd630.t$etiq$c
+            and a.t$coci$c = 'NFE'
+            and ROWNUM = 1 )                          DATA_RECEBIMENTO,
+        CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd630.t$date$c,
+                'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                AT time zone 'America/Sao_Paulo') AS DATE)
+                                                      DATA_FATURAMENTO,
+        znsls401.t$cepe$c                             CEP_ENTREGA
        
 FROM ( select CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(Max(znfmd640.t$udat$c),
               'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
@@ -38,7 +60,8 @@ FROM ( select CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(Max(znfmd640.t$udat$c),
                        on znfmd640.t$fili$c = znfmd630.t$fili$c
                       and znfmd640.t$etiq$c = znfmd630.t$etiq$c
        where znfmd640.t$coci$c = 'ETR'
-         and znfmd640.t$udat$c between trunc(to_date(:data_ini)) and trunc(to_date(:data_fim))+1.99999
+--         and znfmd640.t$udat$c between trunc(to_date(:data_ini)) and trunc(to_date(:data_fim))+1.99999
+         and znfmd640.t$udat$c between to_date(TRUNC(ADD_MONTHS(SYSDATE,-1),'MM'),'dd/mm/rrrr') and to_date(LAST_DAY(ADD_MONTHS(SYSDATE,-1)),'dd/mm/rrrr') +1.99999
        group by znfmd630.t$pecl$c,
                 znfmd640.t$fili$c,
                 znfmd640.t$etiq$c,
@@ -71,18 +94,20 @@ LEFT JOIN BAANDB.tznfmd060301 znfmd060
        ON znfmd060.t$cfrw$c = cisli940.t$cfrw$l      --A transportadora da ordem de frete pode nao ser a correta. SDP 1390455
       AND znfmd060.t$cono$c = znfmd630.t$cono$c
 
-LEFT JOIN (  select a.t$orno$c,
+LEFT JOIN (  select distinct
+                    a.t$orno$c,
                     a.t$ncia$c,
-                    a.t$uneg$c
-              from baandb.tznsls004301 a 
-              group by a.t$orno$c,
-                       a.t$ncia$c,
-                       a.t$uneg$c ) znsls004
-        ON znsls004.t$orno$c = znfmd630.t$orno$c
+                    a.t$uneg$c,
+                    a.t$pecl$c,
+                    a.t$sqpd$c,
+                    a.t$entr$c,
+                    a.t$cepe$c
+              from baandb.tznsls401301 a ) znsls401
+        ON znsls401.t$entr$c = TO_CHAR(znfmd630.t$pecl$c)
 
 LEFT JOIN baandb.tznint002301  znint002
-       ON znint002.t$ncia$c = znsls004.t$ncia$c
-      AND znint002.t$uneg$c = znsls004.t$uneg$c
+       ON znint002.t$ncia$c = znsls401.t$ncia$c
+      AND znint002.t$uneg$c = znsls401.t$uneg$c
 
 LEFT JOIN baandb.ttcmcs031301  tcmcs031
        ON znint002.t$cbrn$c = tcmcs031.t$cbrn
@@ -115,10 +140,11 @@ LEFT JOIN ( SELECT d.t$cnst CODE,
                                             and l1.t$cpac = l.t$cpac ) ) ORIGEM
         ON ORIGEM.CODE = znfmd630.t$torg$c
         
-WHERE znfmd640_ETR.DATA_FILTRO between :data_ini and :data_fim
+WHERE znfmd640_ETR.DATA_FILTRO   BETWEEN to_date(TRUNC(ADD_MONTHS(SYSDATE,-1),'MM'),'dd/mm/rrrr') and to_date(LAST_DAY(ADD_MONTHS(SYSDATE,-1)),'dd/mm/rrrr') 
+--      znfmd640_ETR.DATA_FILTRO BETWEEN :data_ini and :data_fim
   AND znfmd630.t$cfrw$c in ('T70','T72','T73','A46')
 
-UNION
+UNION ALL
 
 SELECT /*+ use_concat no_cpu_costing */ 
       distinct
@@ -139,10 +165,32 @@ SELECT /*+ use_concat no_cpu_costing */
           from baandb.ttcmcs080301 a
           where a.t$cfrw = znfmd630.t$cfrw$c)         TRANSPORTADORA,
        znfmd630.t$cono$c                              COD_CONTRATO,
-       cast(replace(replace(own_mis.filtro_mis(znfmd060.t$refe$c),';',''),'"','')   as varchar(100))  
+--      cast(replace(replace(own_mis.filtro_mis(znfmd060.t$refe$c),';',''),'"','')   as varchar(100))  
+        replace(replace(znfmd060.t$refe$c,';',''),'"','')
                                                       ID_EXT_CONTRATO,
        NVL(tcmcs031.t$dsca, 'Pedido Interno')         MARCA,
-       ORIGEM.DESCR                                   ORIGEM_ORDEM_FRETE
+       ORIGEM.DESCR                                   ORIGEM_ORDEM_FRETE,
+       ( select CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(a.t$date$c,
+                'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                AT time zone 'America/Sao_Paulo') AS DATE)
+          from BAANDB.tznfmd640301 a
+          where a.t$fili$c = znfmd630.t$fili$c
+            and a.t$etiq$c = znfmd630.t$etiq$c
+            and a.t$coci$c = 'ENT'
+            and ROWNUM = 1 )                          DATA_ENTREGA,
+        ( select CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(a.t$date$c,
+                'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                AT time zone 'America/Sao_Paulo') AS DATE)
+          from BAANDB.tznfmd640301 a
+          where a.t$fili$c = znfmd630.t$fili$c
+            and a.t$etiq$c = znfmd630.t$etiq$c
+            and a.t$coci$c = 'NFE'
+            and ROWNUM = 1 )                          DATA_RECEBIMENTO,
+        CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd630.t$date$c,
+                'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                AT time zone 'America/Sao_Paulo') AS DATE)
+                                                      DATA_FATURAMENTO,
+        znsls401.t$cepe$c                             CEP_ENTREGA
 
 FROM ( select CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(Max(znfmd640.t$udat$c),
               'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
@@ -160,7 +208,8 @@ FROM ( select CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(Max(znfmd640.t$udat$c),
                        on znfmd640.t$fili$c = znfmd630.t$fili$c
                       and znfmd640.t$etiq$c = znfmd630.t$etiq$c
        where znfmd640.t$coci$c = 'CTR'
-         and znfmd640.t$udat$c between trunc(to_date(:data_ini)) and trunc(to_date(:data_fim))+1.99999
+--         and znfmd640.t$udat$c between trunc(to_date(:data_ini)) and trunc(to_date(:data_fim))+1.99999
+         and znfmd640.t$udat$c between to_date(TRUNC(ADD_MONTHS(SYSDATE,-1),'MM'),'dd/mm/rrrr') and to_date(LAST_DAY(ADD_MONTHS(SYSDATE,-1)),'dd/mm/rrrr') +1.99999
        group by znfmd630.t$pecl$c,
                 znfmd640.t$fili$c,
                 znfmd640.t$etiq$c,
@@ -193,18 +242,20 @@ LEFT JOIN BAANDB.tznfmd060301 znfmd060
        ON znfmd060.t$cfrw$c = cisli940.t$cfrw$l      --A transportadora da ordem de frete pode nao ser a correta. SDP 1390455
       AND znfmd060.t$cono$c = znfmd630.t$cono$c
 
-LEFT JOIN (  select a.t$orno$c,
+LEFT JOIN (  select distinct
+                    a.t$orno$c,
                     a.t$ncia$c,
-                    a.t$uneg$c
-              from baandb.tznsls004301 a 
-              group by a.t$orno$c,
-                       a.t$ncia$c,
-                       a.t$uneg$c ) znsls004
-        ON znsls004.t$orno$c = znfmd630.t$orno$c
+                    a.t$uneg$c,
+                    a.t$pecl$c,
+                    a.t$sqpd$c,
+                    a.t$entr$c,
+                    a.t$cepe$c
+              from baandb.tznsls401301 a ) znsls401
+        ON znsls401.t$entr$c = TO_CHAR(znfmd630.t$pecl$c)
 
 LEFT JOIN baandb.tznint002301  znint002
-       ON znint002.t$ncia$c = znsls004.t$ncia$c
-      AND znint002.t$uneg$c = znsls004.t$uneg$c
+       ON znint002.t$ncia$c = znsls401.t$ncia$c
+      AND znint002.t$uneg$c = znsls401.t$uneg$c
 
 LEFT JOIN baandb.ttcmcs031301  tcmcs031
        ON znint002.t$cbrn$c = tcmcs031.t$cbrn
@@ -237,10 +288,11 @@ LEFT JOIN ( SELECT d.t$cnst CODE,
                                             and l1.t$cpac = l.t$cpac ) ) ORIGEM
         ON ORIGEM.CODE = znfmd630.t$torg$c
         
-WHERE znfmd640_CTR.DATA_FILTRO between :data_ini and :data_fim
+WHERE znfmd640_CTR.DATA_FILTRO   BETWEEN to_date(TRUNC(ADD_MONTHS(SYSDATE,-1),'MM'),'dd/mm/rrrr') and to_date(LAST_DAY(ADD_MONTHS(SYSDATE,-1)),'dd/mm/rrrr') 
+--        znfmd640_CTR.DATA_FILTRO BETWEEN :data_ini and :data_fim
   AND znfmd630.t$cfrw$c in ('T70','T72','T73','A46')
 
-UNION
+UNION ALL
 
 SELECT /*+ use_concat no_cpu_costing */ 
       distinct
@@ -261,10 +313,32 @@ SELECT /*+ use_concat no_cpu_costing */
           from baandb.ttcmcs080301 a
           where a.t$cfrw = znfmd630.t$cfrw$c)         TRANSPORTADORA,
        znfmd630.t$cono$c                              COD_CONTRATO,
-       cast(replace(replace(own_mis.filtro_mis(znfmd060.t$refe$c),';',''),'"','')   as varchar(100))  
+--       cast(replace(replace(own_mis.filtro_mis(znfmd060.t$refe$c),';',''),'"','')   as varchar(100)) 
+       replace(replace(znfmd060.t$refe$c,';',''),'"','')
                                                       ID_EXT_CONTRATO,
        NVL(tcmcs031.t$dsca, 'Pedido Interno')         MARCA,
-       ORIGEM.DESCR                                   ORIGEM_ORDEM_FRETE
+       ORIGEM.DESCR                                   ORIGEM_ORDEM_FRETE,
+        ( select CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(a.t$date$c,
+                'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                AT time zone 'America/Sao_Paulo') AS DATE)
+          from BAANDB.tznfmd640301 a
+          where a.t$fili$c = znfmd630.t$fili$c
+            and a.t$etiq$c = znfmd630.t$etiq$c
+            and a.t$coci$c = 'ENT'
+            and ROWNUM = 1 )                          DATA_ENTREGA,
+        ( select CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(a.t$date$c,
+                'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                AT time zone 'America/Sao_Paulo') AS DATE)
+          from BAANDB.tznfmd640301 a
+          where a.t$fili$c = znfmd630.t$fili$c
+            and a.t$etiq$c = znfmd630.t$etiq$c
+            and a.t$coci$c = 'NFE'
+            and ROWNUM = 1 )                          DATA_RECEBIMENTO,
+        CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(znfmd630.t$date$c,
+                'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
+                AT time zone 'America/Sao_Paulo') AS DATE)
+                                                      DATA_FATURAMENTO,
+        znsls401.t$cepe$c                             CEP_ENTREGA
 
 FROM ( select CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(Max(znfmd640.t$udat$c),
               'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
@@ -282,12 +356,13 @@ FROM ( select CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(Max(znfmd640.t$udat$c),
                        on znfmd640.t$fili$c = znfmd630.t$fili$c
                       and znfmd640.t$etiq$c = znfmd630.t$etiq$c
        where znfmd640.t$coci$c = 'SPC'
-         and znfmd640.t$udat$c between trunc(to_date(:data_ini)) and trunc(to_date(:data_fim))+1.99999
+--         and znfmd640.t$udat$c between trunc(to_date(:data_ini)) and trunc(to_date(:data_fim))+1.99999
+         and znfmd640.t$udat$c between to_date(TRUNC(ADD_MONTHS(SYSDATE,-1),'MM'),'dd/mm/rrrr') and to_date(LAST_DAY(ADD_MONTHS(SYSDATE,-1)),'dd/mm/rrrr') +1.99999         
        group by znfmd630.t$pecl$c,
                 znfmd640.t$fili$c,
                 znfmd640.t$etiq$c,
                 znfmd640.t$coci$c ) znfmd640_SPC
-
+                
 INNER JOIN baandb.tznfmd630301 znfmd630
         ON znfmd630.t$fili$c = znfmd640_SPC.t$fili$c
        AND znfmd630.t$etiq$c = znfmd640_SPC.t$etiq$c
@@ -315,18 +390,20 @@ LEFT JOIN BAANDB.tznfmd060301 znfmd060
        ON znfmd060.t$cfrw$c = cisli940.t$cfrw$l      --A transportadora da ordem de frete pode nao ser a correta. SDP 1390455
       AND znfmd060.t$cono$c = znfmd630.t$cono$c
 
-LEFT JOIN (  select a.t$orno$c,
+LEFT JOIN (  select distinct
+                    a.t$orno$c,
                     a.t$ncia$c,
-                    a.t$uneg$c
-              from baandb.tznsls004301 a 
-              group by a.t$orno$c,
-                       a.t$ncia$c,
-                       a.t$uneg$c ) znsls004
-        ON znsls004.t$orno$c = znfmd630.t$orno$c
+                    a.t$uneg$c,
+                    a.t$pecl$c,
+                    a.t$sqpd$c,
+                    a.t$entr$c,
+                    a.t$cepe$c
+              from baandb.tznsls401301 a ) znsls401
+        ON znsls401.t$entr$c = TO_CHAR(znfmd630.t$pecl$c)
 
 LEFT JOIN baandb.tznint002301  znint002
-       ON znint002.t$ncia$c = znsls004.t$ncia$c
-      AND znint002.t$uneg$c = znsls004.t$uneg$c
+       ON znint002.t$ncia$c = znsls401.t$ncia$c
+      AND znint002.t$uneg$c = znsls401.t$uneg$c
 
 LEFT JOIN baandb.ttcmcs031301  tcmcs031
        ON znint002.t$cbrn$c = tcmcs031.t$cbrn
@@ -359,7 +436,8 @@ LEFT JOIN ( SELECT d.t$cnst CODE,
                                             and l1.t$cpac = l.t$cpac ) ) ORIGEM
         ON ORIGEM.CODE = znfmd630.t$torg$c
     
-WHERE znfmd640_SPC.DATA_FILTRO between :data_ini and :data_fim
+WHERE znfmd640_SPC.DATA_FILTRO   BETWEEN to_date(TRUNC(ADD_MONTHS(SYSDATE,-1),'MM'),'dd/mm/rrrr') and to_date(LAST_DAY(ADD_MONTHS(SYSDATE,-1)),'dd/mm/rrrr') 
+--      znfmd640_SPC.DATA_FILTRO BETWEEN :data_ini and :data_fim
   AND znfmd630.t$cfrw$c in ('T70','T72','T73','A46')
 
 ORDER BY FILIAL, NUME_ENTREGA, ETIQUETA
