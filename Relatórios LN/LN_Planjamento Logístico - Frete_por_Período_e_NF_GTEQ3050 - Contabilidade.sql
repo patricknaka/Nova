24ed7a427e71477a41c57decfdfcd053cda45e37
Humberto Kasai <humberto.kasai@outlook.com>
2017-12-04 18:50:26 -0200

    SDP 1418369

--- Relatórios LN/LN_Planjamento Logístico - Frete_por_Período_e_NF_GTEQ3050 - Contabilidade.sql
+++ Relatórios LN/LN_Planjamento Logístico - Frete_por_Período_e_NF_GTEQ3050 - Contabilidade.sql
@@ -1,5 +1,5 @@
-SELECT /*+ use_concat parallel(32) no_cpu_costing */ 
---SELECT /*+ use_concat no_cpu_costing */ 
+--SELECT /*+ use_concat parallel(32) no_cpu_costing */ 
+SELECT /*+ use_concat no_cpu_costing */ 
       distinct
        cast(znfmd630.t$fili$c as numeric(10,0))                           FILIAL,
        NVL(tcmcs031.t$dsca,
@@ -141,41 +141,56 @@ SELECT /*+ use_concat parallel(32) no_cpu_costing */
       Round(PIS.t$amnt$l,4)                                               VL_PIS,
       Round(COFINS.t$amnt$l,4)                                            VL_COFINS,
       Round(cisli941.t$ldam$l * cisli941.t$dqua$l,4)                      DESCONTO,
-      GARANTIA.t$item$c                                                   ITEM_GARANTIA,
+      GARANTIA.t$itml$c                                                   ITEM_GARANTIA,
       GARANTIA.t$vlun$c                                                   VALOR_GARANTIA,
-      ROUND(znsls492.t$trbd$c*100, 4)                                     TAXA_CARTAO,
+
+      SEGURO.t$itml$c                                                     ITEM_SEGURO,
+      SEGURO.t$vlun$c                                                     VALOR_SEGURO,
       
+      ROUND(znsls492_1.t$trbd$c*100, 4)                                   TAXA_CARTAO_1,
+      ROUND(znsls492_1.t$vltx$c, 4)                                       VALOR_TAXA_1,
+      case when znsls492_2.t$sequ$c > znsls492_1.t$sequ$c then
+          ROUND(znsls492_2.t$trbd$c*100, 4)
+      else 0 end                                                          TAXA_CARTAO_2,
+      case when znsls492_2.t$sequ$c > znsls492_1.t$sequ$c then
+          ROUND(znsls492_2.t$vltx$c, 4)
+      else 0 end                                                          VALOR_TAXA_2,      
       NFTC.t$docn$l                                                       NF_FATURA,
       NFTC.t$seri$l                                                       SERIE_FATURA,
       Round(ICMS.t$amnt$l,4)                                              VL_ICMS,
       Round(ICMS.t$vpbr$l,4)                                              VL_FUNDO_COMB_POBREZA,
       Round(ICMS.t$vest$l,4)                                              VL_ESTADO_DESTINO,
       Round(ICMS.t$oest$l,4)                                              VL_ESTADO_ORIGEM,
-      Round(ICMS_ST.t$amnt$l,4)                                           VL_ICMS_ST
-
+      Round(ICMS_ST.t$amnt$l,4)                                           VL_ICMS_ST,
+      
+      cisli940.t$fire$l                                                   REF_FISCAL
+      
 FROM       ( select CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(Max(znfmd640_ETR.t$udat$c),
                      'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
                         AT time zone 'America/Sao_Paulo') AS DATE)        DATA_OCORRENCIA,
-                    TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(Max(znfmd630.t$date$c),
+                    TRUNC(CAST((FROM_TZ(TO_TIMESTAMP(TO_CHAR(Max(znfmd630.t$date$c),          --DATA EMISSAO NF
                           'DD-MON-YYYY HH24:MI:SS'), 'DD-MON-YYYY HH24:MI:SS'), 'GMT')
                           AT time zone 'America/Sao_Paulo') AS DATE))     DATA_FILTRO,
                     Max(znfmd640_ETR.t$etiq$c) KEEP (DENSE_RANK LAST ORDER BY znfmd640_ETR.t$udat$c ) t$etiq$c,
                     znfmd630.t$pecl$c,
                     znfmd640_ETR.t$fili$c
                from BAANDB.tznfmd640301 znfmd640_ETR
-         inner join baandb.tznfmd630301 znfmd630
-                 on znfmd640_ETR.t$fili$c = znfmd630.t$fili$c
-                and znfmd640_ETR.t$etiq$c = znfmd630.t$etiq$c
-              where znfmd640_ETR.t$coct$c = 'ETR'
-                and znfmd640_ETR.T$TORG$C = 1
-                and znfmd630.t$date$c between trunc(to_date(:data_ini)) and trunc(to_date(:data_fim))+1.99999
+                inner join baandb.tznfmd630301 znfmd630
+                        on znfmd640_ETR.t$fili$c = znfmd630.t$fili$c
+                       and znfmd640_ETR.t$etiq$c = znfmd630.t$etiq$c
+                       and znfmd630.t$torg$c = 1
+                where znfmd640_ETR.t$coct$c = 'ETR'
+                  and znfmd640_ETR.T$TORG$C = 1
+--                and znfmd630.t$date$c between trunc(to_date(:data_ini)) and trunc(to_date(:data_fim))+1.99999
+                  and znfmd630.t$pecl$c = '12294111303'
            group by znfmd630.t$pecl$c,
                     znfmd640_ETR.t$fili$c ) znfmd640_ETR
 
-INNER JOIN baandb.tznfmd630301 znfmd630
+LEFT JOIN baandb.tznfmd630301 znfmd630
         ON znfmd630.t$fili$c = znfmd640_ETR.t$fili$c
        AND znfmd630.t$etiq$c = znfmd640_ETR.t$etiq$c
        AND znfmd630.t$pecl$c = znfmd640_ETR.t$pecl$c
+       AND znfmd630.t$torg$c = 1
 
 INNER JOIN baandb.tznsls004301 znsls004
         ON znsls004.t$orno$c = znfmd630.t$orno$c
@@ -285,6 +300,7 @@ LEFT JOIN ( select  a.t$fire$l,
          ON znfmd061.t$creg$c = znfmd062.t$creg$c
         AND znfmd061.t$cono$c = znfmd630.t$cono$c
         AND znfmd061.t$cfrw$c = cisli940.t$cfrw$l
+        AND znfmd061.t$cuba$c > 1
 
   LEFT JOIN ( select  a.t$cfrw$c,
                       a.t$cono$c,
@@ -354,29 +370,99 @@ LEFT JOIN ( select  a.t$fire$l,
         AND COFINS.t$line$l = cisli941.t$line$l
         AND COFINS.t$brty$l = 6        
 
-  LEFT JOIN baandb.tznsls401301 GARANTIA
+  LEFT JOIN ( select  a.t$ncia$c,
+                      a.t$uneg$c,
+                      a.t$pecl$c,
+                      a.t$sqpd$c,
+                      a.t$entr$c,
+                      a.t$itml$c,
+                      a.t$igar$c,
+                      a.t$vlun$c
+              from baandb.tznsls401301 a 
+              inner join baandb.ttcibd001301 b
+                      on b.t$item = a.t$itml$c
+              inner join baandb.tznisa002301 c
+                      on c.t$npcl$c = b.t$npcl$c       
+              where c.t$nptp$c = 'G' ) GARANTIA
          ON GARANTIA.t$ncia$c = znsls401.t$ncia$c
         AND GARANTIA.t$uneg$c = znsls401.t$uneg$c
         AND GARANTIA.t$pecl$c = znsls401.t$pecl$c
         AND GARANTIA.t$sqpd$c = znsls401.t$sqpd$c
         AND GARANTIA.t$entr$c = znsls401.t$entr$c
-        AND GARANTIA.t$igar$c = znsls401.t$item$c
-        AND GARANTIA.t$sgar$c = znsls401.t$sequ$c
-        AND GARANTIA.t$pcga$c = znsls401.t$pecl$c
+        AND GARANTIA.t$igar$c = znsls401.t$itml$c
 
-  LEFT JOIN baandb.tznsls402301 znsls402
-         ON znsls402.t$ncia$c = znsls401.t$ncia$c
-        AND znsls402.t$uneg$c = znsls401.t$uneg$c
-        AND znsls402.t$pecl$c = znsls401.t$pecl$c
-        AND znsls402.t$sqpd$c = znsls401.t$sqpd$c
-       
-  LEFT JOIN BAANDB.TZNSLS492301 znsls492
-         ON znsls492.t$ncia$c = znsls402.t$ncia$c
-        AND znsls492.t$uneg$c = znsls402.t$uneg$c
-        AND znsls492.t$pecl$c = znsls402.t$pecl$c
-        AND znsls492.t$sqpd$c = znsls402.t$sqpd$c
-        AND znsls492.t$idmp$c = znsls402.t$idmp$c
-        AND znsls492.t$sequ$c = znsls402.t$sequ$c
+  LEFT JOIN ( select  a.t$ncia$c,
+                      a.t$uneg$c,
+                      a.t$pecl$c,
+                      a.t$sqpd$c,
+                      a.t$entr$c,
+                      a.t$itml$c,
+                      a.t$igar$c,
+                      a.t$vlun$c
+              from baandb.tznsls401301 a 
+              inner join baandb.ttcibd001301 b
+                      on b.t$item = a.t$itml$c
+              inner join baandb.tznisa002301 c
+                      on c.t$npcl$c = b.t$npcl$c       
+              where c.t$nptp$c in ('H','Q') ) SEGURO
+         ON SEGURO.t$ncia$c = znsls401.t$ncia$c
+        AND SEGURO.t$uneg$c = znsls401.t$uneg$c
+        AND SEGURO.t$pecl$c = znsls401.t$pecl$c
+        AND SEGURO.t$sqpd$c = znsls401.t$sqpd$c
+        AND SEGURO.t$entr$c = znsls401.t$entr$c
+        AND SEGURO.t$igar$c = znsls401.t$itml$c
+
+  LEFT JOIN ( select a.t$ncia$c,
+                     a.t$uneg$c,
+                     a.t$pecl$c,
+                     a.t$sqpd$c,
+                     a.t$idmp$c,
+                     min(a.t$sequ$c) t$sequ$c
+              from baandb.tznsls402301 a 
+              where a.t$idmp$c in (1,12,15)
+              group by a.t$ncia$c,
+                      a.t$uneg$c,
+                      a.t$pecl$c,
+                      a.t$sqpd$c,
+                      a.t$idmp$c ) znsls402_1
+         ON znsls402_1.t$ncia$c = znsls401.t$ncia$c
+        AND znsls402_1.t$uneg$c = znsls401.t$uneg$c
+        AND znsls402_1.t$pecl$c = znsls401.t$pecl$c
+        AND znsls402_1.t$sqpd$c = znsls401.t$sqpd$c
+            
+  LEFT JOIN BAANDB.TZNSLS492301 znsls492_1
+         ON znsls492_1.t$ncia$c = znsls402_1.t$ncia$c
+        AND znsls492_1.t$uneg$c = znsls402_1.t$uneg$c
+        AND znsls492_1.t$pecl$c = znsls402_1.t$pecl$c
+        AND znsls492_1.t$sqpd$c = znsls402_1.t$sqpd$c
+        AND znsls492_1.t$idmp$c = znsls402_1.t$idmp$c
+        AND znsls492_1.t$sequ$c = znsls402_1.t$sequ$c
+ 
+   LEFT JOIN ( select a.t$ncia$c,
+                     a.t$uneg$c,
+                     a.t$pecl$c,
+                     a.t$sqpd$c,
+                     a.t$idmp$c,
+                     max(a.t$sequ$c) t$sequ$c
+              from baandb.tznsls402301 a 
+              where a.t$idmp$c in (1,12,15)
+              group by a.t$ncia$c,
+                      a.t$uneg$c,
+                      a.t$pecl$c,
+                      a.t$sqpd$c,
+                      a.t$idmp$c ) znsls402_2
+         ON znsls402_2.t$ncia$c = znsls401.t$ncia$c
+        AND znsls402_2.t$uneg$c = znsls401.t$uneg$c
+        AND znsls402_2.t$pecl$c = znsls401.t$pecl$c
+        AND znsls402_2.t$sqpd$c = znsls401.t$sqpd$c
+            
+  LEFT JOIN BAANDB.TZNSLS492301 znsls492_2
+         ON znsls492_2.t$ncia$c = znsls402_2.t$ncia$c
+        AND znsls492_2.t$uneg$c = znsls402_2.t$uneg$c
+        AND znsls492_2.t$pecl$c = znsls402_2.t$pecl$c
+        AND znsls492_2.t$sqpd$c = znsls402_2.t$sqpd$c
+        AND znsls492_2.t$idmp$c = znsls402_2.t$idmp$c
+        AND znsls492_2.t$sequ$c = znsls402_2.t$sequ$c
       
   LEFT JOIN ( select  a.t$orno,
                       a.t$pono,
@@ -407,5 +493,8 @@ LEFT JOIN ( select  a.t$fire$l,
         AND ICMS_ST.t$brty$l = 2
         
 WHERE cisli940.t$fdty$l IN (1,15)     --Venda com pedido, Remessa Triangular
-  AND znfmd640_ETR.DATA_FILTRO
-      between :data_ini and :data_fim
+  and znsls401.t$iitm$c = 'P'
+--  AND znfmd640_ETR.DATA_FILTRO
+--      between :data_ini and :data_fim
+      
+--order by cisli940.t$docn$l
